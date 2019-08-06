ddf82bf7adceaa2da82c5b788992ebc7b102a37b
Rainer Koschke <koschke@axivion.com>
2019-08-07 00:14:32 +0200

    An example on how to create a hook for data that gets imported by drag and drop into the Project window.

---
+++ Assets/Editor/ImportProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ImportProcessor : AssetPostprocessor
+{
+    // This function is called just before any Asset is imported.
+    // This lets us control the import settings through code.
+    void OnPreprocessAsset()
+    {
+        // assetPath is the relative path of the imported asset
+        Debug.Log(assetPath);
+    } 
+}
