f3ea74f673f176fae5cfbbcfec9fd527678c8a6e
Rainer Koschke <koschke@uni-bremen.de>
2019-08-07 00:11:41 +0200

    Folder for C# code for SEECity objects. First object of this kind is one for houses.

---
+++ Assets/SEECity/House.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("House is spawned");       
+    }
+
+    // Update is called once per frame
+    /*
+    void Update()
+    {
+        
+    }
+    */
+}
