f503b6a2c4de3385438107e17584f4c9417c874d
Rainer Koschke <koschke@uni-bremen.de>
2019-08-08 12:38:17 +0200

    Renamed from House to Node.

--- Assets/SEECity/Node.cs
+++ Assets/SEECity/Node.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class House : MonoBehaviour
+public class Node : MonoBehaviour
 {
+    [Tooltip("Node type")]
+    public string type;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("House is spawned");       
+        //Debug.Log("House is spawned");
     }
 
     // Update is called once per frame
     /*
     void Update()
     {
-        
+
     }
     */
 }
