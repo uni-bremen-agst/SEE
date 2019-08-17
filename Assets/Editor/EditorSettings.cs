f5051a165e78f9e69d7154a4a66bea01bddd8326
Rainer Koschke <koschke@uni-bremen.de>
2019-08-17 16:13:18 +0200

    New classes to hold parameters for the visualization.

---
+++ Assets/Editor/EditorSettings.cs
@@ -0,0 +1,29 @@
+﻿
+namespace SEEEditor
+{
+    /// <summary>
+    /// Represents the parameters needed to create the visualization at design time.
+    /// </summary>
+    class EditorSettings : SEE.IGraphSettings
+    {
+        /// <summary>
+        /// The tag of all nodes.
+        /// </summary>
+        public string nodeTag = "Node";
+
+        /// <summary>
+        /// The tag of all edges.
+        /// </summary>
+        public string edgeTag = "Edge";
+
+        /// <summary>
+        /// The relative path to the building preftab.
+        /// </summary>
+        public string nodePrefabPath = "Assets/Prefabs/House.prefab";
+
+        /// <summary>
+        /// The relative path to the connection preftab.
+        /// </summary>
+        public string edgePreftabPath = "Assets/Prefabs/Line.prefab";
+    }
+}
---
+++ Assets/SEECity/DataModel/IGraphSettings.cs
@@ -0,0 +1,22 @@
+﻿
+namespace SEE
+{
+    /// <summary>
+    /// Settings of the graph data needed at runtime.
+    /// </summary>
+    public class IGraphSettings
+    {
+        /// <summary>
+        /// The path to the graph data.
+        /// </summary>
+        public string graphPath = "C:\\Users\\raine\\develop\\seecity\\data\\gxl\\minimal_clones.gxl";
+        //public string graphPath = "C:\\Users\\raine\\develop\\see\\data\\gxl\\linux-clones\\clones.gxl";
+        // The following graph will not work because it does not have the necessary metrics.
+        // public string graphPath = "C:\\Users\\raine\\Downloads\\codefacts.gxl";
+
+        /// <summary>
+        /// The name of the edge type of hierarchical edges.
+        /// </summary>
+        public string hierarchicalEdgeType = "Enclosing";
+    }
+}
