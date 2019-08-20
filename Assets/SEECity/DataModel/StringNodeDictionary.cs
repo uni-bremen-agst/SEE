7be01a49f62d8d9db431f05d5af830226e67681d
Rainer Koschke <koschke@uni-bremen.de>
2019-08-20 13:54:09 +0200

    New serializable dictionaries for nodes indexed by strings.

---
+++ Assets/SEECity/DataModel/StringNodeDictionary.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// A serializable Dictionary<string, INode>. Serializable dictionaries are 
+/// needed for values that are created in the editor and need to be preserved
+/// and available during the game. For such values, Unity must serialize
+/// the objects and restore them when the game is started.
+/// </summary>
+
+namespace SEE.DataModel
+{
+    [System.Serializable]
+    public class StringNodeDictionary : SerializableDictionary<string, INode> { }
+}
