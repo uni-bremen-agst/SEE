69f6ec91f401314937bdd441f2aa71a149eb7d84
Rainer Koschke <koschke@uni-bremen.de>
2019-08-22 16:48:33 +0200

    New class that simplies handling of lines.

---
+++ Assets/SEECity/Layout/LineFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace SEE.Layout
+{
+    /// <summary>
+    /// Sets attributes of lines.
+    /// </summary>
+    class LineFactory
+    {
+        const float defaultWidth = 0.01f;
+
+        internal static void SetDefaults(LineRenderer line)
+        {
+            line.sortingLayerName = "OnTop";
+            line.sortingOrder = 5;
+
+            // simplify rendering; no shadows
+            line.receiveShadows = false;
+            line.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+            SetColors(line);
+            SetWidth(line, defaultWidth);
+        }
+
+        internal static void SetColors(LineRenderer line)
+        {
+            SetColors(line, Color.green, Color.red);
+        }
+
+        internal static void SetColors(LineRenderer line, Color startColor, Color endColor)
+        {
+            line.startColor = startColor;
+            line.endColor = endColor;
+        }
+
+        internal static void SetColor(LineRenderer line, Color color)
+        {
+            SetColors(line, color, color);
+        }
+
+        internal static void SetWidth(LineRenderer line, float width)
+        {
+            line.startWidth = width;
+            line.endWidth = width;
+        }
+
+    }
+}
