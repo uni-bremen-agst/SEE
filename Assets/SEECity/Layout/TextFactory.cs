ce288e50dc7467e2edadcbfc6a1b04812a01e153
Rainer Koschke <koschke@uni-bremen.de>
2019-09-04 16:59:26 +0200

    A factory for text game object always rotating towards the camera if their distance to the camera is in a given range.

---
+++ Assets/SEECity/Layout/TextFactory.cs
@@ -0,0 +1,45 @@
+﻿using SEE.DataModel;
+using TMPro;
+using UnityEngine;
+
+namespace SEE.Layout
+{
+    /// <summary>
+    /// A factory for text objects that rotate towards the camera.
+    /// </summary>
+    internal class TextFactory
+    {
+        /// <summary>
+        /// Returns a game object showing the tiven text at given position. The
+        /// text rotates towards the main camera.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="position"></param>
+        /// <param name="fontSize"></param>
+        /// <returns></returns>
+        public static GameObject GetText(string text, Vector3 position, int fontSize)
+        {
+            GameObject result = new GameObject("Text " + text);
+            result.tag = Tags.Text;
+            result.transform.position = position;
+
+            TextMeshPro tm = result.AddComponent<TextMeshPro>();
+            tm.text = text;
+            tm.color = Color.black;
+            tm.alignment = TextAlignmentOptions.Center;
+            // TODO: Size based on depth
+            tm.fontSize = fontSize;
+
+            TextFacingCamera textFacing = result.AddComponent<TextFacingCamera>();
+            // TODO: Rendering distance based on depth
+            //textFacing.minimalDistance = 0.0f;
+            //textFacing.maximalDistance = int.MaxValue;
+
+            // No shading as this might be expensive and even distracts.
+            Renderer renderer = result.GetComponent<Renderer>();
+            renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+            return result;
+        }
+    }
+}
