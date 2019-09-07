faf24898af4c48ce2db0e0bb4e53f6c2a8850aff
Rainer Koschke <koschke@uni-bremen.de>
2019-09-07 12:41:00 +0200

    No longer is the font size passed, but the maximal width of the text.
    The visibility range of the text is chosen relative to its width.
    This way, larger circles (which have a greater width) are farther
    visible, but disappear earlier; which is what makes sense.

--- Assets/SEECity/Layout/TextFactory.cs
+++ Assets/SEECity/Layout/TextFactory.cs
@@ -13,27 +13,37 @@ namespace SEE.Layout
         /// Returns a game object showing the tiven text at given position. The
         /// text rotates towards the main camera.
         /// </summary>
-        /// <param name="text"></param>
-        /// <param name="position"></param>
-        /// <param name="fontSize"></param>
-        /// <returns></returns>
-        public static GameObject GetText(string text, Vector3 position, int fontSize)
+        /// <param name="text">the text to be drawn</param>
+        /// <param name="position">the center position at which to draw the text</param>
+        /// <param name="width">the width of the rectangle enclosing the text (essentially, 
+        /// the text width); the font size will be chose appropriately</param>
+        /// <returns>the game object representing the text</returns>
+        public static GameObject GetText(string text, Vector3 position, float width)
         {
-            GameObject result = new GameObject("Text " + text);
-            result.tag = Tags.Text;
+            GameObject result = new GameObject("Text " + text)
+            {
+                tag = Tags.Text
+            };
             result.transform.position = position;
 
             TextMeshPro tm = result.AddComponent<TextMeshPro>();
             tm.text = text;
             tm.color = Color.black;
             tm.alignment = TextAlignmentOptions.Center;
-            // TODO: Size based on depth
-            tm.fontSize = fontSize;
+            
+            RectTransform rect = tm.GetComponent<RectTransform>();
+            // We set only the width of the rectangle and leave the z axis to Unity
+            rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+            //rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, width);
 
+            tm.enableAutoSizing = true;
+            tm.fontSizeMin = 3;
+            tm.fontSizeMax = 72;
+          
             TextFacingCamera textFacing = result.AddComponent<TextFacingCamera>();
-            // TODO: Rendering distance based on depth
-            //textFacing.minimalDistance = 0.0f;
-            //textFacing.maximalDistance = int.MaxValue;
+            // Rendering distance is set relative to the text's width
+            textFacing.minimalDistance = width;
+            textFacing.maximalDistance = 10.0f * width;
 
             // No shading as this might be expensive and even distracts.
             Renderer renderer = result.GetComponent<Renderer>();
