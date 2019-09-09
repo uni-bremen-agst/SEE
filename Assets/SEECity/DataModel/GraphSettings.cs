2b61378936476330a0739811ddeafcec1a206ef2
Rainer Koschke <koschke@uni-bremen.de>
2019-09-09 09:56:02 +0200

    #8
    Multiple hierarchical edge types may be part of the settings.
    
    Added a new path to the Linux driver subsystem (at present too large unfortunately).

--- Assets/SEECity/DataModel/GraphSettings.cs
+++ Assets/SEECity/DataModel/GraphSettings.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Collections.Generic;
+
 namespace SEE
 {
     /// <summary>
@@ -11,10 +13,14 @@ namespace SEE
         /// </summary>
         
         /// Small clone graph with multiple roots to experiment with.
-        public string graphPath = "C:\\Users\\raine\\develop\\seecity\\data\\gxl\\minimal_clones.gxl";
+        //public string graphPath = "C:\\Users\\raine\\develop\\seecity\\data\\gxl\\minimal_clones.gxl";
 
         // Medium-size clone graph with single root (Linux directories whose path include "arch").
-        //public string graphPath = "C:\\Users\\raine\\develop\\SEECity\\Data\\GXL\\linux-clones\\arch-single-root.gxl";
+        public string graphPath = "C:\\Users\\raine\\develop\\SEECity\\Data\\GXL\\linux-clones\\arch-single-root.gxl";
+
+        // Larger clone graph with single root (Linux subsystem 'drivers').
+        // This graph is currently too large to play. The frame rate drops to 3 frames per second.
+        // public string graphPath = "C:\\Users\\raine\\develop\\SEECity\\Data\\GXL\\linux-clones\\drivers-single-root.gxl";
 
         // Large clone graph with multiple roots.
         //public string graphPath = "C:\\Users\\raine\\develop\\evostreets\\data\\gxl\\linux-clones\\clones.gxl";
@@ -29,8 +35,24 @@ namespace SEE
         // public string graphPath = "C:\\Users\\raine\\Downloads\\codefacts.gxl";
 
         /// <summary>
-        /// The name of the edge type of hierarchical edges.
+        /// The names of the edge types of hierarchical edges.
         /// </summary>
-        public string hierarchicalEdgeType = "Enclosing";
+        public static HashSet<string> HierarchicalEdges = Hierarchical_Edge_Types();
+
+        /// <summary>
+        /// The names of the edge types of hierarchical edges.
+        /// </summary>
+        public static HashSet<string> Hierarchical_Edge_Types()
+        {
+            HashSet<string> result = new HashSet<string>
+            {
+                "Enclosing",
+                "Belongs_To",
+                "Part_Of",
+                "Defined_In"
+            };
+            return result;
+        }
+
     }
 }
