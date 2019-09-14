de62c1f62347d775974cd6980be4ab07e2f94941
Rainer Koschke <koschke@axivion.com>
2019-09-14 09:19:17 +0200

    #12 Added documentation.

--- Assets/SEECity/Layout/IconFactory.cs
+++ Assets/SEECity/Layout/IconFactory.cs
@@ -1,16 +1,25 @@
 ﻿using System.IO;
 using UnityEngine;
-using Unity.VectorGraphics;
 using System;
-using System.Collections.Generic;
 
 namespace SEE.Layout
 {
     /// <summary>
-    /// A factory to generate sprites from SVG files.
+    /// A factory to generate sprites from sprite prefab files for all types of 
+    /// software erosion kinds we currently support (architecture violation, clones,
+    /// cycles, dead code, metrics, style, and universal).
     /// </summary>
     public class IconFactory
     {
+        /// The sprite prefabs are already generated in the Unity editor from the 
+        /// original SVG files. To do that, the SVG file must first be imported 
+        /// in the editor and then a sprite must be generated. The sprite can then
+        /// be added to the scene as a new game object, which creates an instance
+        /// in the scene hierarchy. From the scene hierarchy, the instance can be
+        /// moved to the Asset folder by click and drop to turn the instance into
+        /// a prefab, wich generates a prefab file. This prefab file can then be
+        /// loaded here.
+        
         /// <summary>
         /// The different types of software erosions.
         /// </summary>
@@ -46,7 +55,7 @@ namespace SEE.Layout
         }
 
         /// <summary>
-        /// The paths to the icon prefab files.
+        /// The paths to the sprite prefab files.
         /// </summary>
         private static readonly string[] paths = new string[]
             {
@@ -59,21 +68,37 @@ namespace SEE.Layout
             "Assets/Resources/Icons/universalSprite.prefab"
             };
 
+        /// <summary>
+        /// Constructor is made private to prevent instantiations outside of
+        /// this class. Loads all sprite prefabs from the Assets.
+        /// </summary>
         private IconFactory()
         {
             erosionToSprite = LoadAllSprites();
         }
 
+        // The single instance of this class.
         private static readonly IconFactory instance = new IconFactory();
 
+        /// <summary>
+        /// The single instance of this class.
+        /// </summary>
         public static IconFactory Instance
         {
             get => instance;
         }
 
         [SerializeField]
+        // A mapping of Erosions onto sprite prefabs. During start up we load the
+        // sprite prefabs from the Assets for each kind of Erosion and store them
+        // in this field. Later when game objects are to be created from these 
+        // prefabs, we look those prefabs up in this mapping.
         private readonly UnityEngine.Object[] erosionToSprite;
 
+        /// <summary>
+        /// Loads the sprite prefabs from the Assets for all kinds of Erosions.
+        /// </summary>
+        /// <returns>mapping of Erosions onto sprite prefabs</returns>
         private static UnityEngine.Object[] LoadAllSprites()
         {
             Erosion[] erosionIssues = (Erosion[])Enum.GetValues(typeof(Erosion));
@@ -86,6 +111,12 @@ namespace SEE.Layout
             return result;
         }
 
+        /// <summary>
+        /// Loads a sprite prefab from the given file. May return null if the file cannot
+        /// be loaded.
+        /// </summary>
+        /// <param name="filename">name of the file containing the sprite prefab</param>
+        /// <returns></returns>
         private static UnityEngine.Object LoadSprite(string filename)
         {
             Debug.LogFormat("Load sprite prefab from file {0}\n", filename);
@@ -112,6 +143,15 @@ namespace SEE.Layout
             return null;
         }
 
+        /// <summary>
+        /// Generates a sprite game object for the given kind of Erosion at the given
+        /// location. The name of the game object is the kind of Erosion. May return
+        /// null, if a prefab could not be loaded previously. This function may be
+        /// called in editor mode.
+        /// </summary>
+        /// <param name="position">the location for positioning the new game object</param>
+        /// <param name="erosion">the kind of Erosion for which to generate the game object</param>
+        /// <returns>a new game object for this type of erosion (may be null)</returns>
         public GameObject GetIcon(Vector3 position, Erosion erosion)
         {
             GameObject result;
