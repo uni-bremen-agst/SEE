81bd0971ae8f1c59289def58fecfb01f49f7d76e
Jan-Eric Oltmanns <jan_olt@uni-bremen.de>
2019-09-18 11:34:28 +0200

    First simple Leap Motion and HTC Vive integration.

--- Assets/SEECity/DataModel/GraphSettings.cs
+++ Assets/SEECity/DataModel/GraphSettings.cs
@@ -14,8 +14,8 @@ namespace SEE
 
         /// Small clone graph with multiple roots to experiment with (we cannot use Application.dataPath
         /// unfortunately, that is why this path is currently hard coded).
-        public string graphPath = "C:\\Users\\raine\\develop\\seecity\\data\\gxl\\minimal_clones.gxl";
-        public string CSVPath = "C:\\Users\\raine\\develop\\seecity\\data\\gxl\\minimal_erosions.csv";
+        public string graphPath = "C:\\Users\\student\\fs.gxl";
+        public string CSVPath = "C:\\Users\\student\\fs.csv";
 
         // Medium-size clone graph with single root (Linux directories whose path include "arch").
         //public string graphPath = "C:\\Users\\raine\\develop\\SEECity\\Data\\GXL\\linux-clones\\arch-single-root.gxl";
