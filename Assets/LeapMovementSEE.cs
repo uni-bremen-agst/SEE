d3f7a3cffdf0134a6d7aa891ff3903f01b2c62f1
Jan-Eric Oltmanns <jan_olt@uni-bremen.de>
2019-09-18 11:34:28 +0200

    First simple Leap Motion and HTC Vive integration.

---
+++ Assets/LeapMovementSEE.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+public class LeapMovementSEE : MonoBehaviour
+{
+    private Leap.Controller controller;
+    public GameObject rig;
+    public float speed = 1f;
+
+    void Start()
+    {
+        controller = new Controller();
+    }
+
+    void Update()
+    {
+        if (controller.IsConnected)
+        {
+            Frame frame = controller.Frame();
+            Hand left = new Hand();
+            Hand right = new Hand();
+            List<Hand> hands = frame.Hands;
+            if (hands.Count == 2)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Hand hand = frame.Hands[i];
+
+                    if (!hand.IsLeft)
+                    {
+                        right = hand;
+                    }
+                    else
+                    {
+                        left = hand;
+                    }
+                }
+
+                if (left.PinchDistance < 20f)
+                {
+                    Finger point = right.Fingers[1];
+                    Vector palmVector = right.PalmNormal;
+                    Vector direction = point.Direction;
+                    Vector3 camDir = Camera.main.transform.forward;
+                    Vector3 move = new Vector3(-direction.x, palmVector.y, -direction.z);
+                    rig.transform.Translate(camDir * speed);
+                    Debug.Log("y: " + palmVector.y + "x: " + direction.x + "z: " + direction.z);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Leap motion is not connected");
+        }
+
+    }
+}
