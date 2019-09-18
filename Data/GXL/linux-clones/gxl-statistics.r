267113eded38b94ee93bf3e946acea7ad1363a2b
Rainer Koschke <koschke@axivion.com>
2019-09-18 17:57:53 +0200

    #14 Removed GXL string.

--- Data/GXL/linux-clones/gxl-statistics.r
+++ Data/GXL/linux-clones/gxl-statistics.r
@@ -1,61 +1,6 @@
 library(xml2)
 library(purrr)
 
-gxl = '<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE gxl SYSTEM "http://www.gupro.de/GXL/gxl-1.0.dtd">
-<gxl xmlns:xlink="http://www.w3.org/1999/xlink">
-<graph id="arch" edgeids="true">
-<node id="N1">
-<type xlink:href="File"/>
-<attr name="Metric.Number_of_Tokens">
-<int>695</int>
-</attr>
-<attr name="Metric.LOC">
-<int>132</int>
-</attr>
-<attr name="Metric.Clone_Rate">
-<float>0.392806</float>
-</attr>
-<attr name="Source.Name">
-<string>bootp.c</string>
-</attr>
-<attr name="Linkage.Name">
-<string>arch/alpha/boot/bootp.c</string>
-</attr>
-<attr name="Source.File">
-<string>bootp.c</string>
-</attr>
-<attr name="Source.Path">
-<string>arch/alpha/boot/bootp.c</string>
-</attr>
-</node>
-    <node id="N5">
-      <type xlink:href="File"/>
-      <attr name="Metric.Number_of_Tokens">
-        <int>699</int>
-      </attr>
-      <attr name="Metric.LOC">
-        <int>139</int>
-      </attr>
-      <attr name="Metric.Clone_Rate">
-        <float>0.390558</float>
-      </attr>
-      <attr name="Source.Name">
-        <string>main.c</string>
-      </attr>
-      <attr name="Linkage.Name">
-        <string>arch/alpha/boot/main.c</string>
-      </attr>
-      <attr name="Source.File">
-        <string>main.c</string>
-      </attr>
-      <attr name="Source.Path">
-        <string>arch/alpha/boot/main.c</string>
-      </attr>
-    </node>
-</graph>
-</gxl>'
-
 #filename = "arch-single-root"
 #gxlfile = paste(filename, ".gxl", sep="")
 #csvfile = paste(filename, ".csv", sep="")
