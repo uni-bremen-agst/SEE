bb31edec19cbfb75f19e7f86f00394ad6ab500fe
Rainer Koschke <koschke@uni-bremen.de>
2019-09-19 21:21:48 +0200

    #5 Added new Tags for buildings and blocks (cubes) so that we can distinguish them.

--- Assets/SEECity/DataModel/Tags.cs
+++ Assets/SEECity/DataModel/Tags.cs
@@ -6,7 +6,9 @@
     public class Tags
     {
         public const string Graph = "Graph";
-        public const string Node = "Node";
+        public const string Node = "Node"; // for logical graph nodes (not their visual representation such as Blocks and Buildings)
+        public const string Block = "Block"; // for cubes representing nodes
+        public const string Building = "Building"; // for CScape buildings representing nodes
         public const string Edge = "Edge";
         public const string NodePrefab = "Node Prefab";
         public const string Text = "Text";
@@ -15,6 +17,6 @@
         /// <summary>
         /// All existing tags in one.
         /// </summary>
-        public static readonly string[] All = new string[] { Graph, Node, Edge, NodePrefab, Text, Erosion};
+        public static readonly string[] All = new string[] { Graph, Node, Block, Building, Edge, NodePrefab, Text, Erosion};
     }
 }
