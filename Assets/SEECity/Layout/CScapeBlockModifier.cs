93c55399a63b4aa39b75a1b1d87debb7de24134c
Rainer Koschke <koschke@uni-bremen.de>
2019-09-21 17:19:16 +0200

    #5 Modifiers of node representations (blocks: cubes or CScape buildings). Intended to be attached to the root game object representing graph nodes in the scene.

---
+++ Assets/SEECity/Layout/CScapeBlockModifier.cs
@@ -0,0 +1,56 @@
+﻿using CScape;
+using SEE.DataModel;
+using UnityEngine;
+
+namespace SEE.Layout
+{
+    /// <summary>
+    /// A block modifier for CScape buildings. It is intended to be attached
+    /// to the container game object containing a CScape building.
+    /// </summary>
+    public class CScapeBlockModifier : BlockModifier
+    {
+        public override Vector3 GetExtent()
+        {
+            // gameObject is the logical node with tag Tags.Node; it may have different
+            // visual representations. Currently, we have cubes, which are tagged by 
+            // Tags.Block, and CScape buildings, which are tagged by Tags.Building.
+            // Both of them are immediate children of gameObject.
+            GameObject child = GetChild(gameObject, Tags.Building);
+            if (child != null)
+            {
+                // It is a CScape building which has no renderer. We use its collider instead.
+                Collider collider = child.GetComponent<Collider>();
+                if (collider != null)
+                {
+                    return collider.bounds.extents;
+                }
+                else
+                {
+                    Debug.LogErrorFormat("CScape building {0} without collider.\n", gameObject.name);
+                    return Vector3.one;
+                }
+            }
+            else
+            {
+                return Vector3.zero;
+            }
+        }
+
+        public override void Scale(Vector3 scale)
+        {
+            // Scale by the number of floors of a building.
+            BuildingModifier bm = gameObject.GetComponent<BuildingModifier>();
+            if (bm == null)
+            {
+                Debug.LogErrorFormat("CScape building {0} has no building modifier.\n", gameObject.name);
+            }
+            else
+            {
+                bm.buildingWidth = (int)scale.x;
+                bm.floorNumber = (int)scale.y;
+                bm.buildingDepth = (int)scale.z;
+            }
+        }
+    }
+}
---
+++ Assets/SEECity/Layout/CubeModifier.cs
@@ -0,0 +1,29 @@
+﻿using SEE.DataModel;
+using UnityEngine;
+
+namespace SEE.Layout
+{
+    /// <summary>
+    /// A block modifier for cubes as buildings. It is intended to be attached
+    /// to the container game object containing a cube as block representation.
+    /// It deals with the specifics of cubes with respect to size and scaling.
+    /// </summary>
+    public class CubeModifier : BlockModifier
+    {
+        public override Vector3 GetExtent()
+        {
+            // Nodes represented by cubes have a renderer from which we can derive the
+            // extent.
+            Renderer renderer = gameObject.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                return renderer.bounds.extents;
+            }
+            else
+            {
+                Debug.LogErrorFormat("Node {0} (tag: {1}) without collider.\n", gameObject.name, gameObject.tag);
+                return Vector3.one;
+            }
+        }
+    }
+}
