f6b059b1c5577b125cba3d0af39181309390c27c
Florian <foranos@outlook.de>
2019-10-09 22:38:03 +0200

    Test graph animation data can now be viewed and traversed in Editor.

---
+++ Assets/SEECity/Utils/NumericalSort.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace SEE
+{
+    /// <summary>
+    /// A sort algorithm for a list of strings, that sorts by numbers in the string.
+    /// For example {a-1, a-11, a-2} becomes {a-1, a-2, a-11}.
+    /// </summary>
+    public static class NumericalSortExtension
+    {
+
+        public static IEnumerable<string> NumericalSort(this IEnumerable<string> list)
+        {
+            int maxLen = list.Select(s => s.Length).Max();
+
+            return list.Select(s => new
+            {
+                OrgStr = s,
+                SortStr = Regex.Replace(s, @"(\d+)|(\D+)", m => m.Value.PadLeft(maxLen, char.IsDigit(m.Value[0]) ? ' ' : '\xffff'))
+            })
+            .OrderBy(x => x.SortStr)
+            .Select(x => x.OrgStr);
+        }
+    }
+}
