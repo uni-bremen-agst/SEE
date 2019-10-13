03772bc00335e3ec8d8a9bfa3b436d629414f718
Torben Groß <TorbenGross@gmx.de>
2019-10-13 15:56:29 +0200

    Added donuts to CirclePackingLayout

--- Assets/SEECity/Layout/CirclePackingLayout.cs
+++ Assets/SEECity/Layout/CirclePackingLayout.cs
@@ -18,6 +18,10 @@ namespace SEE.Layout
         private GameObject RootNodes;
         private GameObject RootEdges;
 
+        private bool ShowErosions;
+        private bool ShowDonuts;
+        private readonly string[] InnerNodeMetrics;
+
         public CirclePackingLayout(bool showEdges,
                              string widthMetric, string heightMetric, string breadthMetric,
                              SerializableDictionary<string, IconFactory.Erosion> issueMap,
@@ -30,6 +34,10 @@ namespace SEE.Layout
         : base(showEdges, widthMetric, heightMetric, breadthMetric, issueMap, blockFactory, scaler, edgeWidth)
         {
             name = "Circle Packing";
+
+            ShowErosions = showErosions;
+            ShowDonuts = showDonuts;
+            InnerNodeMetrics = innerNodeMetrics;
         }
 
         /*
@@ -40,10 +48,10 @@ namespace SEE.Layout
         protected override void DrawNodes(Graph graph)
         {
             graph.SortHierarchyByName();
-            RootNodes = new GameObject();
-            RootNodes.name = "Nodes";
+            RootNodes = new GameObject("Nodes");
             RootNodes.tag = Tags.Node;
-            DrawNodes(RootNodes, graph.GetRoots(), out float out_radius);
+            List<Node> roots = graph.GetRoots();
+            DrawNodes(RootNodes, roots, out float out_radius);
         }
 
         private void DrawNodes(GameObject parent, List<Node> nodes, out float out_radius)
@@ -61,23 +69,27 @@ namespace SEE.Layout
                 float radius;
                 if (node.IsLeaf())
                 {
-                    Vector3 scale = GetScale(node);
                     DrawLeaf(gameObject, out float out_leaf_radius);
                     radius = out_leaf_radius;
                 }
                 else
                 {
-                    DrawNodes(gameObject, node.Children(), out float out_child_radius);
-                    radius = out_child_radius;
+                    DrawNodes(gameObject, node.Children(), out float out_nodes_radius);
+                    radius = out_nodes_radius;
                 }
 
                 float radians = ((float)i / (float)nodes.Count) * (2.0f * Mathf.PI);
-                gameObject.transform.position = new Vector3(Mathf.Cos(radians), 0.0f, Mathf.Sin(radians)) * radius;
+                gameObject.transform.localPosition = new Vector3(Mathf.Cos(radians), 0.0f, Mathf.Sin(radians)) * radius;
+                gameObject.transform.position = gameObject.transform.position + new Vector3(0.0f, 0.1f, 0.0f);
                 circles.Add(new Circle(gameObject.transform, radius));
             }
 
+            Vector3 position = parent.transform.position;
+            parent.transform.position = position;
+
             CirclePacker.Pack(circles, out float out_outer_radius);
-            DrawCircle(parent, out_outer_radius);
+            if (circles.Count > 1)
+                DrawOutline(parent, out_outer_radius);
             out_radius = out_outer_radius;
         }
 
@@ -86,19 +98,33 @@ namespace SEE.Layout
             Node node = leaf.GetComponent<NodeRef>().node;
 
             GameObject block = blockFactory.NewBlock();
-            blockFactory.ScaleBlock(block, GetScale(node));
+            block.name = leaf.name + " Block";
             block.transform.parent = leaf.transform;
+            block.transform.localPosition = Vector3.zero;
+            blockFactory.ScaleBlock(block, GetScale(node));
 
             Vector3 size = blockFactory.GetSize(block);
             out_leaf_radius = Mathf.Sqrt(size.x * size.x + size.z * size.z);
         }
 
-        private void DrawCircle(GameObject node, float radius)
+        private void DrawOutline(GameObject parent, float radius)
         {
-            GameObject circle = new GameObject();
-            circle.name = node.name + " border";
+            if (ShowDonuts)
+            {
+                DrawDonut(parent, radius);
+            }
+            else
+            {
+                DrawCircle(parent, radius);
+            }
+        }
+
+        private void DrawCircle(GameObject parent, float radius)
+        {
+            GameObject circle = new GameObject(parent.name + " Border");
             circle.tag = Tags.Node;
-            circle.transform.parent = node.transform;
+            circle.transform.parent = parent.transform;
+            circle.transform.localPosition = Vector3.zero;
 
             const int segments = 360;
             LineRenderer line = circle.AddComponent<LineRenderer>();
@@ -116,6 +142,24 @@ namespace SEE.Layout
                 points[i] = new Vector3(Mathf.Sin(rad) * radius, 0, Mathf.Cos(rad) * radius);
             }
             line.SetPositions(points);
+        }
+
+        private void DrawDonut(GameObject parent, float radius)
+        {
+            // TODO: move to ILayout (change in other layouts, too)
+
+            // FIXME: Derive real metrics from nodes.
+
+            GameObject donut = new GameObject(parent.name + " Donut");
+            donut.tag = Tags.Node;
+            donut.transform.parent = parent.transform;
+
+            float innerValue = UnityEngine.Random.Range(0.0f, 1.0f);
+            float m1 = UnityEngine.Random.Range(0.0f, 50.0f);
+            float m2 = UnityEngine.Random.Range(0.0f, 90.0f);
+            float m3 = UnityEngine.Random.Range(0.0f, 150.0f);
+            float m4 = UnityEngine.Random.Range(0.0f, 200.0f);
+            new DonutFactory(InnerNodeMetrics).DonutChart(donut, radius, innerValue, new float[] { m1, m2, m2, m3 }, 0.95f);
         }
 
         private Vector3 GetScale(Node node)
