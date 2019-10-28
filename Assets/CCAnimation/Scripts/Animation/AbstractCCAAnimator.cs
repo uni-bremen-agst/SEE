fe03394d81fc10c9ab0fada0d8affe9e1c5c9378
Florian <foranos@outlook.de>
2019-10-28 18:22:17 +0100

    Added Animators to make animation changing easier.

--- Assets/CCAnimation/Scripts/Animation/AbstractCCAAnimator.cs
+++ Assets/CCAnimation/Scripts/Animation/AbstractCCAAnimator.cs
@@ -1,18 +1,91 @@
-﻿using System.Collections;
+﻿using SEE.DataModel;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AbstractCCAAnimator : MonoBehaviour
+/// <summary>
+/// TODO flo:
+/// </summary>
+public abstract class AbstractCCAAnimator
 {
-    // Start is called before the first frame update
-    void Start()
+    private float _maxAnimationTime;
+
+    private bool _animationsDisabled = false;
+
+    /// <summary>
+    /// TODO flo:
+    /// </summary>
+    /// <param name="maxAnimationTime">The maximum time the animation is allowed to run.</param>
+    public AbstractCCAAnimator(float maxAnimationTime = 2)
     {
-        
+        this.MaxAnimationTime = maxAnimationTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Defines the maximum time, in seconds, the animation is allowed to take.
+    /// </summary>
+    public float MaxAnimationTime { get => _maxAnimationTime; set => _maxAnimationTime = value; }
+
+    /// <summary>
+    /// TODO flo:
+    /// </summary>
+    public bool AnimationsDisabled { get => _animationsDisabled; set => _animationsDisabled = value; }
+
+    /// <summary>
+    /// TODO flo:
+    /// </summary>
+    /// <param name="node"></param>
+    /// <param name="gameObject"></param>
+    /// <param name="position"></param>
+    /// <param name="scale"></param>
+    public void AnimateTo(Node node, GameObject gameObject, Vector3 position, Vector3 scale)
     {
-        
+        if (AnimationsDisabled)
+        {
+            gameObject.transform.position = position;
+            gameObject.transform.localScale = scale;
+        }
+        else
+        {
+            AnimateToInternal(node, gameObject, position, scale);
+        }
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="node"></param>
+    /// <param name="gameObject"></param>
+    /// <param name="position"></param>
+    /// <param name="scale"></param>
+    protected abstract void AnimateToInternal(Node node, GameObject gameObject, Vector3 position, Vector3 scale);
+
+    /// <summary>
+    /// TODO flo:
+    /// </summary>
+    /// <param name="node"></param>
+    /// <param name="gameObject"></param>
+    /// <param name="position"></param>
+    /// <param name="scale"></param>
+    /// <param name="callback"></param>
+    public void AnimateToAnd(Node node, GameObject gameObject, Vector3 position, Vector3 scale, Action<object> callback)
+    {
+        if (callback.Target.GetType() == typeof(GameObject))
+        {
+            Debug.LogError("Callback needs to be a function inside of a MonoBehaviour");
+            return;
+        }
+        AnimateToAndInternal(node, gameObject, position, scale, callback);
+    }
+
+    /// <summary>
+    /// TODO flo:
+    /// </summary>
+    /// <param name="node"></param>
+    /// <param name="gameObject"></param>
+    /// <param name="position"></param>
+    /// <param name="scale"></param>
+    /// <param name="callback"></param>
+    protected abstract void AnimateToAndInternal(Node node, GameObject gameObject, Vector3 position, Vector3 scale, Action<object> callback);
 }
---
+++ Assets/CCAnimation/Scripts/Animation/SimpleCCAAnimator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SEE.DataModel;
+using UnityEngine;
+
+/// <summary>
+/// TODO flo:
+/// </summary>
+public class SimpleCCAAnimator : AbstractCCAAnimator
+{
+    protected override void AnimateToInternal(Node node, GameObject gameObject, Vector3 position, Vector3 scale)
+    {
+        if (node.WasAdded())
+        {
+            gameObject.transform.position = position;
+            gameObject.transform.localScale = scale;
+
+            iTween.MoveFrom(gameObject, iTween.Hash(
+                "y", -100, // TODO flo: -Sizeofbuilding
+                "time", MaxAnimationTime
+            ));
+        }
+        else if (node.WasModified())
+        {
+            iTween.MoveTo(gameObject, iTween.Hash(
+                "position", position,
+                "time", MaxAnimationTime
+            ));
+            iTween.ScaleTo(gameObject, iTween.Hash(
+                "scale", scale,
+                "time", MaxAnimationTime
+            ));
+            iTween.ShakeRotation(gameObject, iTween.Hash(
+                "amount", new Vector3(0, 10, 0),
+                "time", 1,
+                "delay", 1
+            ));
+        }
+        else if (node.WasRelocated(out string oldLinkageName))
+        {
+            iTween.MoveTo(gameObject, iTween.Hash(
+                "position", position,
+                "time", MaxAnimationTime
+            ));
+            iTween.ScaleTo(gameObject, iTween.Hash(
+                "scale", scale,
+                "time", MaxAnimationTime
+            ));
+        }
+        else
+        {
+            iTween.MoveTo(gameObject, iTween.Hash(
+                "position", position,
+                "time", MaxAnimationTime
+            ));
+            iTween.ScaleTo(gameObject, iTween.Hash(
+                "scale", scale,
+                "time", MaxAnimationTime
+            ));
+        }
+    }
+
+    protected override void AnimateToAndInternal(Node node, GameObject gameObject, Vector3 position, Vector3 scale, Action<object> callback)
+    {
+        iTween.MoveTo(gameObject, iTween.Hash(
+            "y", -100,
+            "time", MaxAnimationTime,
+            "oncompletetarget", ((MonoBehaviour)callback.Target).gameObject,
+            "oncomplete", callback.Method.Name,
+            "oncompleteparams", gameObject
+        ));
+    }
+}
