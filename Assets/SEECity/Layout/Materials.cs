f2738765e8d53521b86378f683c33a4b1cba19c5
Rainer Koschke <koschke@axivion.com>
2019-11-02 13:13:46 +0100

    #24 New circle factory encapsulating the computation of a circle's size (fixes the problem with strange size value for circle lines).
    New Materials class to provide sharable default material for objects.

---
+++ Assets/SEECity/Layout/Materials.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace SEE.Layout
+{
+    internal class Materials
+    {
+        private const string shaderName = "Diffuse";
+        private static readonly Shader shader = Shader.Find(shaderName);
+        private static Material defaultMaterial = Init();
+        private static Material Init()
+        {
+            Material result = new Material(shader);
+            // Turn off reflection
+            result.EnableKeyword("_SPECULARHIGHLIGHTS_OFF");
+            result.EnableKeyword("_GLOSSYREFLECTIONS_OFF");
+            result.SetFloat("_SpecularHighlights", 0.0f);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the default material (always the identical material, no matter
+        /// how often this method is called). That means, if the caller modifies
+        /// this material, other objects using it will be affected, too.
+        /// </summary>
+        /// <returns>default material</returns>
+        public static Material DefaultMaterial()
+        {
+            return defaultMaterial;
+        }
+    }
+}
