7fe1a91212aef02a1c10e85be97e940ad296fd0f
Torben Groß <TorbenGross@gmx.de>
2019-11-03 22:38:27 +0100

    Added blur to backdrop

--- Assets/_Scripts/MenuBackdropGenerator.cs
+++ Assets/_Scripts/MenuBackdropGenerator.cs
@@ -9,7 +9,7 @@ namespace SEE
 
         void Start()
         {
-            renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+            renderTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.RGB111110Float);
             RenderTexture.active = renderTexture;
 
             Camera c = GetComponent<Camera>();
