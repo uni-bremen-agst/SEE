ca668a78d5daae48aeb237e8c56818ecd52f6a21
Rainer Koschke <koschke@uni-bremen.de>
2019-11-04 16:11:49 +0100

    Moved code creating sprites for erosion issues to separate class.

--- Assets/SEECity/Layout/GraphRenderer.cs
+++ Assets/SEECity/Layout/GraphRenderer.cs
@@ -180,7 +180,8 @@ namespace SEE.Layout
             // we also know their positions.
             if (settings.ShowErosions)
             {
-                AddErosionIssues(nodeMap.Values);
+                ErosionIssues issueDecorator = new ErosionIssues(settings.IssueMap(), leaveNodeFactory, scaler);
+                issueDecorator.Add(nodeMap.Values);
             }
             if (settings.EdgeLayout != GraphSettings.EdgeLayouts.None)
             {
@@ -330,95 +331,6 @@ namespace SEE.Layout
             innerGameObject.tag = Tags.Node;
             AttachNode(innerGameObject, node);
             return innerGameObject;
-        }
-
-        /// <summary>
-        /// Creates sprites for software-erosion indicators for all given game nodes.
-        /// </summary>
-        /// <param name="gameNodes">list of game nodes for which to create erosion visualizations</param>
-        protected void AddErosionIssues(ICollection<GameObject> gameNodes)
-        {
-            foreach (GameObject block in gameNodes)
-            {
-                AddErosionIssues(block);
-            }
-        }
-
-        /// <summary>
-        /// Stacks sprites for software-erosion issues atop of the roof of the given node
-        /// in ascending order in terms of the sprite width. The sprite width is proportional
-        /// to the normalized metric value for the erosion issue.
-        /// </summary>
-        /// <param name="node"></param>
-        protected void AddErosionIssues(GameObject gameNode)
-        {
-            Node node = gameNode.GetComponent<NodeRef>().node;
-
-            // The list of sprites for the erosion issues.
-            List<GameObject> sprites = new List<GameObject>();
-
-            // Create and scale the sprites and add them to the list of sprites.
-            foreach (KeyValuePair<string, IconFactory.Erosion> issue in settings.IssueMap())
-            {
-                if (node.TryGetNumeric(issue.Key, out float value))
-                {
-                    if (value > 0.0f)
-                    {
-                        GameObject sprite = IconFactory.Instance.GetIcon(Vector3.zero, issue.Value);
-                        sprite.name = sprite.name + " " + node.SourceName;
-
-                        Vector3 spriteSize = GetSizeOfSprite(sprite);
-                        // Scale the sprite to one Unity unit.
-                        float spriteScale = 1.0f / spriteSize.x;
-                        // Scale the erosion issue by normalization.
-                        float metricScale = scaler.GetNormalizedValue(node, issue.Key);
-                        // First: scale its width to unit size 1.0 maintaining the aspect ratio
-                        sprite.transform.localScale *= spriteScale * leaveNodeFactory.Unit();
-                        // Now scale it by the normalized metric.
-                        sprite.transform.localScale *= metricScale;
-                        sprite.transform.position = leaveNodeFactory.Roof(gameNode);
-                        sprites.Add(sprite);
-                    }
-                }
-            }
-
-            // Now we stack the sprites on top of the roof of the building in
-            // ascending order of their widths.
-            {
-                // The space that we put in between two subsequent erosion issue sprites.
-                Vector3 delta = Vector3.up / 100.0f;
-                Vector3 currentRoof = leaveNodeFactory.Roof(gameNode);
-                sprites.Sort(Comparer<GameObject>.Create((left, right) => GetSizeOfSprite(left).x.CompareTo(GetSizeOfSprite(right).x)));
-                foreach (GameObject sprite in sprites)
-                {
-                    Vector3 size = GetSizeOfSprite(sprite);
-                    // Note: Consider that the position of the sprite is its center.
-                    Vector3 halfHeight = (size.y / 2.0f) * Vector3.up;
-                    sprite.transform.position = currentRoof + delta + halfHeight;
-                    currentRoof = sprite.transform.position + halfHeight;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Returns the size of the sprite for given game node that was drawn for 
-        /// a software-erosion indicator above the roof of the node.
-        /// </summary>
-        /// <param name="gameNode"></param>
-        /// <returns>size of the sprite</returns>
-        protected static Vector3 GetSizeOfSprite(GameObject gameNode)
-        {
-            // The game object representing an erosion is a composite of 
-            // multiple LOD child objects to be drawn depending how close
-            // the camera is. The container object 'go' itself does not
-            // have a renderer. We need to obtain the renderer of the
-            // first child hat represents the object at LOD 0 instead.
-            Renderer renderer = gameNode.GetComponentInChildren<Renderer>();
-            // Note: renderer.sprite.bounds.size yields the original size
-            // of the sprite of the prefab. It does not consider the scaling.
-            // It depends only upon the imported graphic. That is why we
-            // need to use renderer.bounds.size.
-            return renderer.bounds.size;
         }
 
         /// <summary>
