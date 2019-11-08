a8263405ba6ba87b7977fee49fd9a4a37e2cb7ea
Florian <foranos@outlook.de>
2019-11-08 22:26:07 +0100

    Fixing errors from merge

--- Assets/CCAnimation/Scripts/Layout/LinearMultiScale.cs
+++ Assets/CCAnimation/Scripts/Layout/LinearMultiScale.cs
@@ -2,82 +2,37 @@
 using SEE.Layout;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+
+
 /// <summary>
-/// TODO flo doc
 /// Provides x, y, z lengths of a node based on a linear interpolation
 /// of the node's metrics.
 /// </summary>
-public class LinearMultiScale : IScale
+public class LinearMultiScale : LinearScale
 {
+    /// <summary>
+    /// Overrides the base variable to modify the values.
+    /// </summary>
+    private new readonly Dictionary<string, float> metricMaxima;
+
     /// <summary>
     /// Constructor for linear-interpolation based scaling of node metrics.
     /// The values are guaranteed to be in the range of [minimalLength,
     /// maximalLength].
     /// </summary>
-    /// <param name="graph">the graph whose node metrics are to be scaled</param>
+    /// <param name="graphs">the graphs whose node metrics are to be scaled</param>
     /// <param name="minimalLength">the mininmal value a node length can have</param>
     /// <param name="maximalLength">the maximal value a node length can have</param>
     /// <param name="metrics">node metrics for scaling</param>
     public LinearMultiScale(List<Graph> graphs, float minimalLength, float maximalLength, IList<string> metrics)
-        : base(metrics, minimalLength, maximalLength)
+        : base(graphs.FirstOrDefault(), minimalLength, maximalLength, metrics)
     {
         metricMaxima = DetermineMetricMaxima(graphs, metrics);
     }
-
-    /// <summary>
-    /// The maximal values of all metrics as a map metric-name -> maximal value.
-    /// </summary>
-    private readonly Dictionary<string, float> metricMaxima;
-
-    /// <summary>
-    /// Yields a linear interpolation of the normalized value of the given node metric.
-    /// The range of the linear interpolation is set by [minimalLength, maximalLength].
-    /// The normalization is done by dividing the value by the maximal value of
-    /// the metric. The assumption is that metric values are non-negative. If a node
-    /// does not have metric attribute, minimalLength will be returned.
-    /// </summary>
-    /// <param name="node">node for which to determine the normalized value</param>
-    /// <param name="metric">name of the node metric</param>
-    /// <returns>normalized value of node metric</returns>
-    public override float GetNormalizedValue(Node node, string metric)
-    {
-        return Mathf.Lerp(minimalLength, maximalLength, NormalizedMetric(metricMaxima, node, metric));
-    }
-
-    /// <summary>
-    /// Returns a value in the range [0.0, 1.0] representing the relative value of the given
-    /// metric in the metrics value range for the given node.
-    /// </summary>
-    /// <param name="node"></param>
-    /// <param name="metric"></param>
-    /// <returns></returns>
-    protected float NormalizedMetric(Dictionary<string, float> metricMaxima, Node node, string metric)
-    {
-        float max = metricMaxima[metric];
-
-        if (max <= 0.0f)
-        {
-            return 0.0f;
-        }
-        else if (node.TryGetNumeric(metric, out float width))
-        {
-            if (width <= 0.0f)
-            {
-                return 0.0f;
-            }
-            else
-            {
-                return (float)width / max;
-            }
-        }
-        else
-        {
-            return 0.0f;
-        }
-    }
-
+    
     /// <summary>
     /// Returns the maximal values of the given node metrics.
     /// </summary>
@@ -107,18 +62,7 @@ public class LinearMultiScale : IScale
                 }
             }
         }
-        
-        return result;
-    }
 
-    /// <summary>
-    /// Dumps metricMaxima for debugging.
-    /// </summary>
-    protected void DumpMetricMaxima(Dictionary<string, float> metricMaxima)
-    {
-        foreach (var item in metricMaxima)
-        {
-            Debug.Log("maximum of " + item.Key + ": " + item.Value + "\n");
-        }
+        return result;
     }
 }
