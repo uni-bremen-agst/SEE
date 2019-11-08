2e0e78fa5de06a6a74d6a4c4a15ad6c3462337aa
Jan-Eric Oltmanns <jan_olt@uni-bremen.de>
2019-11-08 15:47:25 +0100

    added rigged hands to the main scene with default menu attachet to the left hand.

---
+++ Assets/LeapMovementCV.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+public class LeapMovementCV : MonoBehaviour
+{
+    private Leap.Controller controller;
+    public GameObject rig; // assign in inspector
+    public Canvas menu; // assign in inspector
+
+    void Start()
+    {
+        Debug.Log("Starting LeapMovementCV\n");
+        controller = new Controller();
+        menu.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.IsConnected)
+        {
+            // it would be better to get the hand objects directly from the unity hierarchy
+            // get both hands
+            Frame frame = controller.Frame();
+            Hand left = new Hand();
+            Hand right = new Hand();
+            List<Hand> hands = frame.Hands;
+
+                for (int i = 0; i < hands.Count; i++)
+                {
+                    Hand hand = frame.Hands[i];
+
+                    if (!hand.IsLeft)
+                    {
+                        right = hand;
+                    }
+                    else
+                    {
+                        left = hand;
+                    }
+                }
+
+            //observing the triggers
+            menu.enabled = palmUp(right);
+            menu.transform.LookAt(Camera.main.transform);
+
+        }
+    }
+
+    bool palmUp(Hand hand)
+    {
+        if (hand.PalmNormal.z > 0.8)
+        {
+            return true;
+        }
+        return false;
+    }
+}
--- Assets/SelectBuilding.cs
+++ Assets/SelectBuilding.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class SelectBuilding : MonoBehaviour
 {
-    bool show = false;
-    GameObject house;
+    public Transform direction; // assign in inspector
+
+    private bool active = false;
+    private bool show = false;
+    private GameObject house;
+
+    private void Start()
+    {
+        transform.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        RaycastHit hitInfo = new RaycastHit();
+        bool hit = Physics.Raycast(direction.position, direction.TransformDirection(Vector3.forward), out hitInfo, Mathf.Infinity);
+
+        if(hit)
+        {
+            Debug.DrawRay(direction.position, direction.TransformDirection(Vector3.forward) * hitInfo.distance, Color.red);
+            if(hitInfo.collider.gameObject.CompareTag("Building"))
+            {
+                Debug.Log("Ray hit House");
+            }
+        }
+        else
+        {
+            Debug.DrawRay(direction.position, direction.TransformDirection(Vector3.forward) * 1000, Color.white);
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -22,5 +49,12 @@ public class SelectBuilding : MonoBehaviour
             collision.gameObject.GetComponent<MeshRenderer>().material = Resources.Load("BrickTextures/BricksTexture13/BricksTexture13", typeof(Material)) as Material;
             show = false;
         }
+    }
+
+    public void RayOnOff()
+    {
+        active = !active;
+        transform.gameObject.SetActive(active);
+        Debug.Log("Ray on/off");
     }
 }
