52111b75054d12c5b7700408eea91cee858697df
Torben Groß <TorbenGross@gmx.de>
2019-11-10 18:20:56 +0100

    Added search menu back

---
+++ Assets/_Scripts/UI/SearchInputField.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace SEE
+{
+
+    public class SearchInputField : MonoBehaviour
+    {
+        public GameObject scrollView;
+        public SearchManager searchManager;
+        public InputField searchField;
+
+        private bool isSelected;
+
+        void Update()
+        {
+            if (EventSystem.current.currentSelectedGameObject != null)
+            {
+                if (!isSelected)
+                {
+                    scrollView.SetActive(true);
+                }
+                isSelected = true;
+            }
+            else
+            {
+                if (isSelected)
+                {
+                    // TODO properly deactivate if click back into game etc
+                }
+                isSelected = false;
+            }
+        }
+
+        public void OnValueChanged()
+        {
+            searchManager.Filter(searchField.text);
+        }
+    }
+
+}// namespace SEE
