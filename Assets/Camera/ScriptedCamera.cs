ec425d2cda2775a78647d9fc59ca443cb7347b8a
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2019-11-13 13:40:00 +0100

    ScriptedCamera now allows to move along a path (spline).

--- Assets/Camera/ScriptedCamera.cs
+++ Assets/Camera/ScriptedCamera.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using System;
+using TinySpline;
 
 /// <summary>
 /// A script to move a camera programmatically along a path. This
@@ -8,82 +10,78 @@ using UnityEngine;
 public class ScriptedCamera : MonoBehaviour
 {
     /// <summary>
-    /// Where to start.
+    /// The coordinates to interpolate. W stores the time.
     /// </summary>
-    public Vector3 initialPosition = new Vector3(-3.5f, 15.0f, -112.0f);
-    /// <summary>
-    /// Where to finish.
-    /// </summary>
-    public Vector3 finalPosition = new Vector3(-3.5f, 15.0f, 15.0f);
-    /// <summary>
-    /// The unit of timing in seconds.
-    /// </summary>
-    public float tickUnit = 0.25f;
-    /// <summary>
-    /// The distance to be moved for every tick.
-    /// </summary>
-    public float distancePerTick = 0.5f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Initial position of the camera.
-        transform.position = initialPosition;
-        path = CreatePath();
-        current = 0;
-        time = 0.0f;
-        Debug.Log("ScriptedCamera started.\n");
-    }
+    public Vector4[] locations = new Vector4[2];
 
     /// <summary>
-    /// Timed path along which the camera should be moved.
+    /// The interpolated spline.
     /// </summary>
-    private List<Vector4> path;
-
-    /// <summary>
-    /// Adds timing parameter <paramref name="time"/> to position.
-    /// </summary>
-    /// <param name="position">position to be reached</param>
-    /// <param name="time">time to be added</param>
-    /// <returns>vector containing position and time (the latter in w co-ordinate)</returns>
-    private static Vector4 Add_Time(Vector3 position, float time)
-    {
-        // Vector3s can be implicitly converted to Vector4(w is set to zero in the result).
-        Vector4 result = position;
-        result.w = time;
-        return result;
-    }
-
-    /// <summary>
-    /// Creates a timed path from initialPosition to finalPosition.
-    /// </summary>
-    /// <returns>path from initialPosition to finalPosition</returns>
-    private List<Vector4> CreatePath()
-    {
-        List<Vector4> path = new List<Vector4>();
-
-        Vector3 position = initialPosition;
-        float t = 0.0f;
-        path.Add(Add_Time(position, t));
-
-        while (position.z <= finalPosition.z)
-        {
-            t += tickUnit;
-            position.z += distancePerTick;
-            path.Add(Add_Time(position, t));
-        }
-        return path;
-    }
+    private BSpline spline;
 
     /// <summary>
     /// Accumulated time since game start in seconds.
     /// </summary>
     private float time = 0.0f;
 
+    private IList<double> VectorsToList(Vector4[] vectors)
+    {
+        List<double> list = new List<double>();
+        foreach(Vector4 vec in vectors)
+        {
+            list.Add(vec.x);
+            list.Add(vec.y);
+            list.Add(vec.z);
+            list.Add(vec.w);
+        }
+        return list;
+    }
+
+    private Vector4[] ListToVectors(IList<double> list)
+    {
+        int num = list.Count / 4;
+        Vector4[] vectors = new Vector4[num];
+        for (int i = 0; i < num; i++)
+        {
+            vectors[i] = new Vector4(
+                (float)list[i * 4],
+                (float)list[i * 4 + 1],
+                (float)list[i * 4 + 2],
+                (float)list[i * 4 + 3]
+                );
+        }
+        return vectors;
+    }
+
     /// <summary>
-    /// The current position in the path.
+    /// Is called whenever a value is changed in the editor.
     /// </summary>
-    private int current = 0;
+    void OnValidate()
+    {
+        if (locations.Length < 2)
+        {
+            Debug.LogWarning("ScriptedCamera: Requiring at least two locations\n");
+            System.Array.Resize(ref locations, 2);
+        }
+    }
+
+    // Start is called before the first frame update.
+    void Start()
+    {
+        Debug.Log("Starting ScriptedCamera\n");
+        try
+        {
+            spline = TinySpline.Utils.interpolateCubic(VectorsToList(locations), 4);
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning($"ScriptedCamera: Interpolation failed with error '{e.Message}'\n");
+            Debug.LogWarning($"ScriptedCamera: Createing spline with default location\n");
+            spline = new BSpline(1, 4, 0);
+            spline.controlPoints = new List<double> { 0, 0, 0, 0 };
+        }
+        transform.position = ListToVectors(spline.controlPointAt(0))[0];
+    }
 
     /// <summary>
     /// Update is called once per frame and moves the camera along the
@@ -91,24 +89,8 @@ public class ScriptedCamera : MonoBehaviour
     /// </summary>
     void Update()
     {
-        if (current < path.Count)
-        {
-            // Time.deltaTime is the time since the last Update() in seconds.
-            time += Time.deltaTime;
-            // Are we to reach the next position in the path?
-            if (time >= path[current].w)
-            { 
-                //Debug.LogFormat("Moving ScriptedCamera {0}.\n", path[current]);
-
-                // Implicit conversion from Vector4 to Vector3 where the w co-ordinate is dropped.
-                // Thus, we keeop only the position data.
-                transform.position = path[current];
-                current++;
-            }
-        }
-        else
-        {
-            Debug.Log("ScriptedCamera stopped.\n");
-        }
+        // Time.deltaTime is the time since the last Update() in seconds.
+        time += Time.deltaTime;
+        transform.position = ListToVectors(spline.bisect(time, 0.001, false, 3).result)[0];
     }
 }
