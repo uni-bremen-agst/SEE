c35d8313663e1c22406779e88244905c09feeb5d
bohnsack <robboh@gmx.de>
2019-11-25 23:04:22 +0100

    Created first prototype of chart view for code cities in separate scene

---
+++ Assets/Charts/ChartDragHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChartDragHandler : MonoBehaviour, IDragHandler
+{
+	public void OnDrag(PointerEventData eventData)
+	{
+		transform.position = Input.mousePosition;
+	}
+}
---
+++ Assets/Charts/ChartUIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChartUIController : MonoBehaviour
+{
+	[SerializeField]
+	GameObject chartsClosed;
+	[SerializeField]
+	GameObject chartsOpen;
+
+	public void OpenCharts()
+	{
+		chartsClosed.SetActive(false);
+		chartsOpen.SetActive(true);
+	}
+
+	public void CloseCharts()
+	{
+		chartsClosed.SetActive(true);
+		chartsOpen.SetActive(false);
+	}
+}
