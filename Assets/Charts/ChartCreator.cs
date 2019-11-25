4b380ecb9040c3c0f606189ba879a4bb716f5f84
bohnsack <robboh@gmx.de>
2019-11-25 23:04:22 +0100

    Created first prototype of chart view for code cities in separate scene

---
+++ Assets/Charts/ChartCreator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChartCreator : MonoBehaviour
+{
+	GameObject[] dataObjects;
+	[SerializeField]
+	GameObject markerPrefab;
+	[SerializeField]
+	GameObject entries;
+
+	private void Start()
+	{
+		FindDataObjects();
+		Debug.Log(dataObjects.Length);
+		DrawData();
+	}
+
+	void FindDataObjects()
+	{
+		dataObjects = GameObject.FindGameObjectsWithTag("Building");
+	}
+
+	void DrawData()
+	{
+		float minX = dataObjects[0].transform.localScale.x;
+		float maxX = dataObjects[0].transform.localScale.x;
+		float minY = dataObjects[0].transform.localScale.y;
+		float maxY = dataObjects[0].transform.localScale.y;
+		foreach (GameObject data in dataObjects)
+		{
+			float tempX = data.transform.localScale.x;
+			if (tempX < minX)
+			{
+				minX = tempX;
+			}
+			if (tempX > maxX)
+			{
+				maxX = tempX;
+			}
+			float tempY = data.transform.localScale.y;
+			if (tempY > maxY)
+			{
+				maxY = tempY;
+			}
+			if (tempY < minY)
+			{
+				minY = tempY;
+			}
+		}
+		Debug.Log(maxX + " " + minX + " " + maxY + " " + minY);
+		RectTransform field = GetComponent<RectTransform>();
+		float width = field.rect.width / (maxX - minX);
+		float height = field.rect.height / (maxY - minY);
+		foreach (GameObject data in dataObjects)
+		{
+			GameObject marker = Instantiate(markerPrefab);
+			marker.GetComponent<ChartMarker>().LinkedObject = data;
+			marker.transform.SetParent(entries.transform);
+			marker.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+			marker.transform.position = new Vector3(((data.transform.localScale.x - minX) / maxX) * width + entries.transform.position.x, ((data.transform.localScale.y - minY) / maxY)
+				* height + entries.transform.position.y, field.transform.position.z);
+		}
+	}
+}
---
+++ Assets/Charts/ChartMarker.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChartMarker : MonoBehaviour, IPointerDownHandler
+{
+	private GameObject linkedObject;
+	public GameObject LinkedObject
+	{
+		set { linkedObject = value; }
+	}
+	[SerializeField, Range(0.1f, 1f)]
+	float clickDelay = 0.5f;
+	bool waiting = false;
+	bool running = false;
+
+	/// <summary>
+	/// Checks for a double or a single click when the mouse is clicked.
+	/// </summary>
+	/// <param name="eventData"></param>
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		if (running)
+		{
+			waiting = false;
+		}
+		else
+		{
+			StartCoroutine(WaitForDoubleClick());
+		}
+	}
+
+	/// <summary>
+	/// Checks if one or two clicks happen in a given interval.
+	/// </summary>
+	/// <returns></returns>
+	IEnumerator WaitForDoubleClick()
+	{
+		running = true;
+		waiting = true;
+		yield return new WaitForSeconds(clickDelay);
+		if (waiting)
+		{
+			HighlightLinkedObject();
+		}
+		else
+		{
+			ShowLinkedObject();
+		}
+		waiting = false;
+		running = false;
+	}
+
+	/// <summary>
+	/// Highlights the object linked to this one.
+	/// </summary>
+	public void HighlightLinkedObject()
+	{
+		Debug.Log("Your object is " + linkedObject.name);
+	}
+
+	/// <summary>
+	/// Moves the camera to view the linked object.
+	/// </summary>
+	void ShowLinkedObject()
+	{
+		Camera activeCamera = Camera.main;
+		Debug.Log("Moved Camera");
+	}
+}
