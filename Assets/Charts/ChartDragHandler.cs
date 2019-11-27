749c17a1986fdc5515091eb5a85d1c4165d04ef3
bohnsack <robboh@gmx.de>
2019-11-27 12:37:21 +0100

    Added axis labels, a drag button and two types of camera movement.

--- Assets/Charts/ChartDragHandler.cs
+++ Assets/Charts/ChartDragHandler.cs
@@ -5,8 +5,13 @@ using UnityEngine.EventSystems;
 
 public class ChartDragHandler : MonoBehaviour, IDragHandler
 {
+	[SerializeField]
+	GameObject chart;
 	public void OnDrag(PointerEventData eventData)
 	{
-		transform.position = Input.mousePosition;
+		//TODO: Check if this is local position
+		RectTransform pos = GetComponent<RectTransform>();
+		chart.transform.position = new Vector3(Input.mousePosition.x - (pos.anchoredPosition.x / 2) + (pos.rect.width / 2), Input.mousePosition.y
+			- (pos.anchoredPosition.y / 2) - (pos.rect.height / 2), Input.mousePosition.z);
 	}
 }
--- Assets/Charts/ChartMarker.cs
+++ Assets/Charts/ChartMarker.cs
@@ -1,19 +1,39 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// Contains the logic for the markers representing entries linked to objects in the chart.
+/// </summary>
 public class ChartMarker : MonoBehaviour, IPointerDownHandler
 {
+	/// <summary>
+	/// The object in the code city that is connected with this button.
+	/// </summary>
 	private GameObject linkedObject;
 	public GameObject LinkedObject
 	{
 		set { linkedObject = value; }
 	}
+	/// <summary>
+	/// The active camera in the scene.
+	/// </summary>
+	Camera activeCamera;
+	bool waiting = false;
+	bool runningClick = false;
+	Coroutine runningCamera = null;
+
+	[Header("Camera Controls")] //TODO: Move to different script?
+	[SerializeField]
+	float cameraDistance = 50f;
+	[SerializeField]
+	bool moveWithRotation = true;
+	[SerializeField]
+	float cameraFlightTime = 0.5f;
+
+	[Header("User Inputs")] //TODO: Move to different script?
 	[SerializeField, Range(0.1f, 1f)]
 	float clickDelay = 0.5f;
-	bool waiting = false;
-	bool running = false;
 
 	/// <summary>
 	/// Checks for a double or a single click when the mouse is clicked.
@@ -21,7 +41,7 @@ public class ChartMarker : MonoBehaviour, IPointerDownHandler
 	/// <param name="eventData"></param>
 	public void OnPointerDown(PointerEventData eventData)
 	{
-		if (running)
+		if (runningClick)
 		{
 			waiting = false;
 		}
@@ -37,7 +57,7 @@ public class ChartMarker : MonoBehaviour, IPointerDownHandler
 	/// <returns></returns>
 	IEnumerator WaitForDoubleClick()
 	{
-		running = true;
+		runningClick = true;
 		waiting = true;
 		yield return new WaitForSeconds(clickDelay);
 		if (waiting)
@@ -49,7 +69,7 @@ public class ChartMarker : MonoBehaviour, IPointerDownHandler
 			ShowLinkedObject();
 		}
 		waiting = false;
-		running = false;
+		runningClick = false;
 	}
 
 	/// <summary>
@@ -57,6 +77,7 @@ public class ChartMarker : MonoBehaviour, IPointerDownHandler
 	/// </summary>
 	public void HighlightLinkedObject()
 	{
+		//TODO
 		Debug.Log("Your object is " + linkedObject.name);
 	}
 
@@ -65,7 +86,64 @@ public class ChartMarker : MonoBehaviour, IPointerDownHandler
 	/// </summary>
 	void ShowLinkedObject()
 	{
-		Camera activeCamera = Camera.main;
-		Debug.Log("Moved Camera");
+		activeCamera = Camera.main; //TODO: Change to active camera and not just main camera.
+		if (moveWithRotation)
+		{
+			if (runningCamera != null)
+			{
+				StopCoroutine(runningCamera);
+				runningCamera = null;
+			}
+			Vector3 lookPos = linkedObject.transform.position - activeCamera.transform.position;
+			runningCamera = StartCoroutine(MoveCameraTo(Vector3.MoveTowards(activeCamera.transform.position, linkedObject.transform.position, lookPos.magnitude - cameraDistance), Quaternion.LookRotation(lookPos)));
+		}
+		else
+		{
+			if (runningCamera != null)
+			{
+				StopCoroutine(runningCamera);
+				runningCamera = null;
+			}
+			runningCamera = StartCoroutine(MoveCameraTo(new Vector3(linkedObject.transform.position.x, activeCamera.transform.position.y, linkedObject.transform.position.z - cameraDistance)));
+		}
+	}
+
+	/// <summary>
+	/// Moves the camera smoothly from one position to another and rotates it to look towards a specified position.
+	/// </summary>
+	/// <param name="newPos">The target position.</param>
+	/// <param name="lookAt">The position to look at.</param>
+	/// <returns></returns>
+	IEnumerator MoveCameraTo(Vector3 newPos, Quaternion lookAt)
+	{
+		Vector3 oldPos = activeCamera.transform.position;
+		if (newPos != linkedObject.transform.position)
+		{
+			Quaternion oldRot = activeCamera.transform.rotation;
+			for (float time = 0f; time <= cameraFlightTime; time += Time.deltaTime)
+			{
+				activeCamera.transform.position = Vector3.Lerp(oldPos, newPos, time * (1 / cameraFlightTime));
+				activeCamera.transform.rotation = Quaternion.Slerp(oldRot, lookAt, time * (1 / cameraFlightTime));
+				yield return new WaitForEndOfFrame();
+			}
+			activeCamera.transform.rotation = lookAt;
+			activeCamera.transform.position = newPos;
+		}
+	}
+
+	/// <summary>
+	/// Moves the camera smoothly from one position to another.
+	/// </summary>
+	/// <param name="newPos">The tarrget position.</param>
+	/// <returns></returns>
+	IEnumerator MoveCameraTo(Vector3 newPos)
+	{
+		Vector3 oldPos = activeCamera.transform.position;
+		for (float time = 0; time <= cameraFlightTime; time += Time.deltaTime)
+		{
+			activeCamera.transform.position = Vector3.Lerp(oldPos, newPos, time * (1 / cameraFlightTime));
+			yield return new WaitForEndOfFrame();
+		}
+		activeCamera.transform.position = newPos;
 	}
 }
