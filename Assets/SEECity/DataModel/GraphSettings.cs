dd7131928fd028513d628a0c6d4300a7b745ceb7
Rainer Koschke <koschke@uni-bremen.de>
2019-12-09 13:35:13 +0100

    Make EvoStreets the default as we are currently working on it.

--- Assets/SEECity/DataModel/GraphSettings.cs
+++ Assets/SEECity/DataModel/GraphSettings.cs
@@ -154,13 +154,13 @@ namespace SEE
         /// </summary>
         public enum NodeLayouts
         {
+            EvoStreets,
             Balloon,
             FlatRectanglePacking,
             Treemap,
             CirclePacking,
             Manhattan,
             ScoopEvoStreets,
-            EvoStreets,
         }
 
         /// <summary>
