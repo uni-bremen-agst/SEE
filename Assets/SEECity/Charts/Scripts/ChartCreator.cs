a6ff2715ec94e6d438fd788dc239ad2bf7ad717a
bohnsack <robboh@gmx.de>
2019-12-14 19:11:26 +0100

    Moved charts folder, Animated Building Highlights, simple VR setup

---
+++ Assets/SEECity/Charts/Scripts/ChartCreator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.SEECity.Charts.Scripts
+{
+	/// <summary>
+	/// Contains all the information needed to create the next chart.
+	/// </summary>
+	public class ChartCreator : MonoBehaviour
+	{
+		[SerializeField] private GameObject _chartPrefab;
+		[SerializeField] private Transform _chartsCanvas;
+
+		/// <summary>
+		/// Initializes the new chart as GameObject.
+		/// </summary>
+		public void CreateChart()
+		{
+			ChartContent content =
+				Instantiate(_chartPrefab, _chartsCanvas).GetComponent<ChartContent>();
+			gameObject.SetActive(false);
+		}
+	}
+}
