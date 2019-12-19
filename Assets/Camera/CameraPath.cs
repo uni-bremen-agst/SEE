fb09715c4ba2d4d2e2cdf9953e033d7ada04f7d4
Rainer Koschke <koschke@axivion.com>
2019-12-19 20:33:05 +0100

    #34 Moved path data into a separate class/file. The camera rotation is now also re-played.
    Upgraded to Unity 2019.16.f1.

--- Assets/Camera/CameraPath.cs
+++ Assets/Camera/CameraPath.cs
@@ -1,98 +1,149 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Collections;
 
-/// <summary>
-/// Represents a camera path with position and rotation of the camera in a series of 
-/// points in time.
-/// </summary>
-public class CameraPath
+namespace SEE
 {
     /// <summary>
-    /// The number of path data captured so far.
+    /// Represents a camera path with position and rotation of the camera in a series of 
+    /// points in time.
     /// </summary>
-    public int Count { get => data.Count; }
-
-    /// <summary>
-    /// The data captured about a particular position in a path at a given point in time.
-    /// </summary>
-    private struct PathData
+    public class CameraPath : IEnumerable
     {
-        public Vector3 position;    // position of the camera
-        public Quaternion rotation; // rotation of the camera
-        public float time;          // point in time in seconds since game start
+        /// <summary>
+        /// The number of path data captured so far.
+        /// </summary>
+        public int Count { get => data.Count; }
+
+        public object Current => throw new System.NotImplementedException();
 
         /// <summary>
-        /// Sets the data captured about a particular position in a path at a given point in time.
+        /// The path data captured.
+        /// </summary>
+        private List<PathData> data = new List<PathData>();
+
+        /// <summary>
+        /// Operator[key] yielding the key'th entry in the path. The first
+        /// entry has key = 0.
+        /// 
+        /// Precondition: 0 <= key < Count. 
+        /// </summary>
+        /// <param name="key">the index of the path entry to be returned</param>
+        /// <returns>key'th entry in the path</returns>
+        public PathData this[int key]
+        {
+            get => data[key];
+            //set => data[key] = value;
+        }
+
+        /// <summary>
+        /// Adds the data captured about a particular camera position and rotation in a path 
+        /// at a given point in time.
         /// </summary>
         /// <param name="position">position of the camera</param>
         /// <param name="rotation">rotation of the camera</param>
         /// <param name="time">point in time in seconds since game start</param>
-        public PathData(Vector3 position, Quaternion rotation, float time)
+        public void Add(Vector3 position, Quaternion rotation, float time)
         {
-            this.position = position;
-            this.rotation = rotation;
-            this.time = time;
-        }
-    }
-
-    /// <summary>
-    /// The path data captured.
-    /// </summary>
-    private List<PathData> data = new List<PathData>();
-
-    /// <summary>
-    /// Adds the data captured about a particular camera position and rotation in a path 
-    /// at a given point in time.
-    /// </summary>
-    /// <param name="position">position of the camera</param>
-    /// <param name="rotation">rotation of the camera</param>
-    /// <param name="time">point in time in seconds since game start</param>
-    public void Add(Vector3 position, Quaternion rotation, float time)
-    {
-        data.Add(new PathData(position, rotation, time));
-    }
-
-    /// <summary>
-    /// Saves the captured path data in a file with given filename. The file will
-    /// be overwritten if it exists.
-    /// </summary>
-    /// <param name="filename">name of the output file</param>
-    public void Save(string filename)
-    {
-        List<string> outputs = new List<string>();
-
-        foreach (PathData d in data)
-        {
-            string output = FloatToString(d.position.x)
-                          + delimiter + FloatToString(d.position.y)
-                          + delimiter + FloatToString(d.position.z)
-                          + delimiter + FloatToString(d.rotation.x)
-                          + delimiter + FloatToString(d.rotation.y)
-                          + delimiter + FloatToString(d.rotation.z)
-                          + delimiter + FloatToString(d.rotation.w)
-                          + delimiter + FloatToString(d.time);
-            outputs.Add(output);
+            data.Add(new PathData(position, rotation, time));
         }
 
-        // WriteAllLines creates a file, writes a collection of strings to the file,
-        // and then closes the file.  You do NOT need to call Flush() or Close().
-        System.IO.File.WriteAllLines(filename, outputs);
+        /// <summary>
+        /// Saves the captured path data in a file with given filename. The file will
+        /// be overwritten if it exists.
+        /// </summary>
+        /// <param name="filename">name of the output file</param>
+        public void Save(string filename)
+        {
+            List<string> outputs = new List<string>();
+
+            foreach (PathData d in data)
+            {
+                string output = FloatToString(d.position.x)
+                              + delimiter + FloatToString(d.position.y)
+                              + delimiter + FloatToString(d.position.z)
+                              + delimiter + FloatToString(d.rotation.x)
+                              + delimiter + FloatToString(d.rotation.y)
+                              + delimiter + FloatToString(d.rotation.z)
+                              + delimiter + FloatToString(d.rotation.w)
+                              + delimiter + FloatToString(d.time);
+                outputs.Add(output);
+            }
+
+            // WriteAllLines creates a file, writes a collection of strings to the file,
+            // and then closes the file.  You do NOT need to call Flush() or Close().
+            System.IO.File.WriteAllLines(filename, outputs);
+        }
+
+        /// <summary>
+        /// Converts a float value to a string with two digits and a period as a 
+        /// decimal separator.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>the float as a string in the requested format</returns>
+        private string FloatToString(float value)
+        {
+            return value.ToString("0.0", System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// The delimiter to separate data points within the same line in the output file.
+        /// </summary>
+        private static readonly char delimiter = ';';
+
+        /// <summary>
+        /// The minimal number of columns a CSV file containing path data must have:
+        /// 3 (position = Vector3) + 4 (rotation = Quaternion) + 1 (time = float) = 7.
+        /// </summary>
+        private static readonly int minimalColumns = 7;
+
+        /// <summary>
+        /// Loads the path data from a the file.
+        /// </summary>
+        public static CameraPath ReadPath(string path)
+        {
+            string[] data = System.IO.File.ReadAllLines(path);
+            CameraPath result = new CameraPath();
+
+            int i = 0;
+            foreach (string line in data)
+            {
+                string[] coordinates = line.Split(delimiter);
+
+                if (coordinates.Length < minimalColumns)
+                {
+                    Debug.LogErrorFormat
+                        ("Data format error at line {0} in file {1}: expected at least {2} entries separated by {3}. Got: {4} in '{5}'.\n",
+                         i + 1, path, minimalColumns, delimiter, coordinates.Length, line);
+                }
+                else
+                {
+                    Vector3 position;
+                    position.x = float.Parse(coordinates[0], CultureInfo.InvariantCulture);
+                    position.y = float.Parse(coordinates[1], CultureInfo.InvariantCulture);
+                    position.z = float.Parse(coordinates[2], CultureInfo.InvariantCulture);
+
+                    Quaternion rotation;
+                    rotation.x = float.Parse(coordinates[3], CultureInfo.InvariantCulture);
+                    rotation.y = float.Parse(coordinates[4], CultureInfo.InvariantCulture);
+                    rotation.z = float.Parse(coordinates[5], CultureInfo.InvariantCulture);
+                    rotation.w = float.Parse(coordinates[6], CultureInfo.InvariantCulture);
+
+                    float time;
+                    time = float.Parse(coordinates[7], CultureInfo.InvariantCulture);
+
+                    result.Add(position, rotation, time);
+                    // Note: We ignore all remaining columns.
+                }
+                i++;
+            }
+            return result;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)this.data.GetEnumerator();
+        }
     }
-
-    /// <summary>
-    /// Converts a float value to a string with two digits and a period as a 
-    /// decimal separator.
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns>the float as a string in the requested format</returns>
-    private string FloatToString(float value)
-    {
-        return value.ToString("0.0", System.Globalization.CultureInfo.InvariantCulture);
-    }
-
-    /// <summary>
-    /// The delimiter to separate data points within the same line in the output file.
-    /// </summary>
-    private const string delimiter = ";";
-
 }
