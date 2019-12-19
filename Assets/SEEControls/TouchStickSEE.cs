1cb2c5bf527f872baa4acc76f77e212748898948
Jan-Eric Oltmanns <jan_olt@uni-bremen.de>
2019-12-19 15:43:52 +0100

    Added prototype for touch joysticks.

---
+++ Assets/SEEControls/TouchStickSEE.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchStickSEE : MonoBehaviour
+{
+    private Collider2D collider;
+    private Vector2 oldScreenPos;
+
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+        oldScreenPos = transform.position;
+    }
+
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Vector3 touchPointInWorldSpace = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            Vector2 touchPointScreen = new Vector2(touchPointInWorldSpace.x, touchPointInWorldSpace.y);
+
+            if (collider == Physics2D.OverlapPoint(touchPointScreen))
+            {
+                Vector2 newPos = touchPointScreen - oldScreenPos;
+                transform.position = newPos;
+            }
+            else if(Input.touchCount == 0)
+            {
+                transform.localPosition = Vector2.zero;
+            }
+
+        }
+    }
+}
