b8a43fd7c1cd151313a6ae28f1a89ccf0e19a1de
bohnsack <robboh@gmx.de>
2020-01-06 13:41:05 +0100

    Selection of multiple markers at once by dragging a rectangle around them

---
+++ Assets/SEECity/Charts/Scripts/MultiSelectHandler.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.SEECity.Charts.Scripts
+{
+	/// <summary>
+	/// Handles selection of multiple markers in selection mode.
+	/// </summary>
+	public class MultiSelectHandler : MonoBehaviour, IPointerDownHandler, IDragHandler,
+		IPointerUpHandler
+	{
+		[SerializeField] private RectTransform _selectionRect;
+
+		private Vector3 _startingPos;
+
+		/// <summary>
+		/// Activates and sets starting position of <see cref="_selectionRect" />.
+		/// </summary>
+		/// <param name="eventData"></param>
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			_selectionRect.gameObject.SetActive(true);
+			_selectionRect.position = Input.mousePosition;
+			_startingPos = _selectionRect.position;
+			_selectionRect.sizeDelta = new Vector2(0f, 0f);
+		}
+
+		/// <summary>
+		/// Resizes the <see cref="_selectionRect" />.
+		/// </summary>
+		/// <param name="eventData"></param>
+		public void OnDrag(PointerEventData eventData)
+		{
+			bool negative = false;
+			if (Input.mousePosition.x - _startingPos.x < 0)
+			{
+				_selectionRect.sizeDelta = new Vector2(
+					Mathf.Abs(Input.mousePosition.x - _startingPos.x) / _selectionRect.lossyScale.x,
+					(Input.mousePosition.y - _startingPos.y) / _selectionRect.lossyScale.y);
+				_selectionRect.position = new Vector3(
+					_startingPos.x - _selectionRect.sizeDelta.x / 2 * _selectionRect.lossyScale.x,
+					_startingPos.y + _selectionRect.sizeDelta.y / 2 * _selectionRect.lossyScale.y,
+					Input.mousePosition.z);
+				negative = true;
+			}
+
+			if (Input.mousePosition.y - _startingPos.y < 0)
+			{
+				if (negative)
+				{
+					_selectionRect.sizeDelta = new Vector2(_selectionRect.sizeDelta.x,
+						Mathf.Abs(Input.mousePosition.y - _startingPos.y) /
+						_selectionRect.lossyScale.y);
+					_selectionRect.position = new Vector3(_selectionRect.position.x,
+						_startingPos.y - _selectionRect.sizeDelta.y / 2 *
+						_selectionRect.lossyScale.y, Input.mousePosition.z);
+				}
+				else
+				{
+					_selectionRect.sizeDelta = new Vector2(
+						(Input.mousePosition.x - _startingPos.x) / _selectionRect.lossyScale.x,
+						Mathf.Abs(Input.mousePosition.y - _startingPos.y) /
+						_selectionRect.lossyScale.y);
+					_selectionRect.position = new Vector3(
+						_startingPos.x + _selectionRect.sizeDelta.x / 2 *
+						_selectionRect.lossyScale.x,
+						_startingPos.y - _selectionRect.sizeDelta.y / 2 *
+						_selectionRect.lossyScale.y, Input.mousePosition.z);
+					negative = true;
+				}
+			}
+
+			if (!negative)
+			{
+				_selectionRect.sizeDelta =
+					new Vector2(
+						(Input.mousePosition.x - _startingPos.x) / _selectionRect.lossyScale.x,
+						(Input.mousePosition.y - _startingPos.y) / _selectionRect.lossyScale.y);
+				_selectionRect.position = new Vector3(
+					_startingPos.x + _selectionRect.sizeDelta.x / 2 * _selectionRect.lossyScale.x,
+					_startingPos.y + _selectionRect.sizeDelta.y / 2 * _selectionRect.lossyScale.y,
+					Input.mousePosition.z);
+			}
+		}
+
+		/// <summary>
+		/// Highlights all markers in <see cref="_selectionRect" /> and deactivates it.
+		/// </summary>
+		/// <param name="eventData"></param>
+		public void OnPointerUp(PointerEventData eventData)
+		{
+			//TODO: Activate markers
+			_selectionRect.gameObject.SetActive(false);
+		}
+	}
+}
