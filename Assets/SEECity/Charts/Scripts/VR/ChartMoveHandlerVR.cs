b37d6c464e222f095321363dc9e21551e88af582
bohnsack <robboh@gmx.de>
2020-01-16 15:39:49 +0100

    Charts rotate toward player and can be moved with the touchpad

--- Assets/SEECity/Charts/Scripts/VR/ChartMoveHandlerVR.cs
+++ Assets/SEECity/Charts/Scripts/VR/ChartMoveHandlerVR.cs
@@ -1,41 +1,63 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections;
+using Valve.VR;
 
 namespace Assets.SEECity.Charts.Scripts.VR
 {
 	public class ChartMoveHandlerVR : ChartMoveHandler
 	{
         private ChartContent _chartContent;
-        private Camera _mainCamera = Camera.main;
+        private Camera _mainCamera;
+        private SteamVR_Input_Sources _source;
+        private SteamVR_Action_Vector2 _moveInOut;
+        private float _chartSpeed;
+        private Camera _pointerCamera;
+        
         [SerializeField] private GameObject _physicalOpen = null;
 		[SerializeField] private GameObject _physicalClosed = null;
-        private float _chartOffset = -0.03f;
+        private Vector3 _chartOffset = new Vector3(0, 0, -0.03f);
 
         protected override void Awake()
         {
             base.Awake();
             _chartContent = transform.parent.GetComponent<ChartContent>();
+            _mainCamera = Camera.main;
+            _pointerCamera = GameObject.FindGameObjectWithTag("Pointer").GetComponent<Camera>();
+        }
+
+        protected override void GetSettingData()
+        {
+            base.GetSettingData();
+            _chartSpeed = _chartManager.ChartSpeed;
+            _source = _chartManager.Source;
+            _moveInOut = _chartManager.MoveInOut;
         }
 
         protected override void Update()
         {
             base.Update();
-            FacePlayer();
-        }
-
-        private void FacePlayer()
-        {
-            _chartContent.Parent.transform.LookAt(_mainCamera.transform);
+            Transform chart = _chartContent.Parent.transform;
+            chart.LookAt(chart.position - (_mainCamera.transform.position - chart.position));
+            if (_pointerDown)
+            {
+                //TODO: Specify source
+                if (_moveInOut.axis.y != 0)
+                {
+                    Debug.Log(_moveInOut.axis.y);
+                    Vector3 direction = _pointerCamera.transform.position - GetComponent<RectTransform>().position;
+                    _chartContent.Parent.transform.position -= direction * _moveInOut.axis.y * _chartSpeed * Time.deltaTime;
+                }
+            }
         }
 
         public override void OnDrag(PointerEventData eventData)
 		{
-			RectTransform pos = GetComponent<RectTransform>();
-            Vector3 xPos = eventData.pointerCurrentRaycast.worldPosition;
-            _chartContent.Parent.transform.position = xPos - new Vector3(pos.anchoredPosition.x * pos.lossyScale.x, pos.anchoredPosition.y * pos.lossyScale.y, 0) - new Vector3(0, 0, _chartOffset);
+            Vector3 diff = transform.position - (_chartContent.Parent.transform.position + _chartOffset);
+            _chartContent.Parent.transform.position = eventData.pointerCurrentRaycast.worldPosition - diff - _chartOffset;
         }
 
-		protected override void ToggleMinimize()
+        protected override void ToggleMinimize()
 		{
 			_physicalOpen.SetActive(_minimized);
 			_physicalClosed.SetActive(!_minimized);
--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -89,6 +89,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 MoveInOut
+        {
+            get
+            {
+                return SteamVR_Actions.default_MoveInOut;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
