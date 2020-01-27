a2d9fd22033defd5a9294347024c6fc791e8713d
bohnsack <robboh@gmx.de>
2020-01-27 17:45:33 +0100

    Added a lightbeam to highlighted objects and fixed the highlight animation

--- Assets/SEECity/Charts/Scripts/VR/ChartContentVr.cs
+++ Assets/SEECity/Charts/Scripts/VR/ChartContentVr.cs
@@ -6,5 +6,26 @@ namespace SEECity.Charts.Scripts.VR
 	{
 		public GameObject physicalOpen;
 		public GameObject physicalClosed;
+
+		public override void AreaSelection(Vector2 min, Vector2 max, bool direction)
+		{
+			float highlightDuration = _chartManager.highlightDuration;
+			if (direction)
+				foreach (GameObject marker in _activeMarkers)
+				{
+					Vector2 markerPos = marker.GetComponent<RectTransform>().anchoredPosition;
+					if (markerPos.x > min.x && markerPos.x < max.x && markerPos.y > min.y &&
+						markerPos.y < max.y)
+						marker.GetComponent<ChartMarker>().TriggerTimedHighlight(highlightDuration);
+				}
+			else
+				foreach (GameObject marker in _activeMarkers)
+				{
+					Vector2 markerPos = marker.GetComponent<RectTransform>().anchoredPosition;
+					if (markerPos.x > min.x && markerPos.x < max.x && markerPos.y < min.y &&
+						markerPos.y > max.y)
+						marker.GetComponent<ChartMarker>().TriggerTimedHighlight(highlightDuration);
+				}
+		}
 	}
 }
