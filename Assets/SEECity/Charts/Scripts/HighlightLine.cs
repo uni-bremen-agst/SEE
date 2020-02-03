ae9e4db507583a2679ff07a51ecf438553079b05
bohnsack <robboh@gmx.de>
2020-02-03 16:50:05 +0100

    Fixed VR, Disable charts when moving, select buildings in VR

---
+++ Assets/SEECity/Charts/Scripts/HighlightLine.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SEECity.Charts.Scripts
+{
+    public class HighlightLine : MonoBehaviour
+    {
+        private ChartManager _chartManager;
+
+        private Color _standardColor;
+
+        private Color _accentuationColor;
+
+        private bool _accentuated = false;
+
+        private LineRenderer _line;
+
+        private void Awake()
+        {
+            GetSettingData();
+        }
+
+        private void GetSettingData()
+        {
+            _chartManager = GameObject.FindGameObjectWithTag("ChartManager").GetComponent<ChartManager>();
+            _standardColor = _chartManager.standardColor;
+            _accentuationColor = _chartManager.accentuationColor;
+        }
+
+        private void Start()
+        {
+            _line = GetComponent<LineRenderer>();
+        }
+
+        public void ToggleAccentuation()
+        {
+            _line.startColor = _accentuated ? _standardColor : _accentuationColor;
+            _accentuated = !_accentuated;
+        }
+    }
+}
