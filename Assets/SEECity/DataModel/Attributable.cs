33d77c179ab30388caf52912a0b38a68d576314f
Rainer Koschke <koschke@axivion.com>
2020-02-04 16:34:14 +0100

    #36 Attribute no longer derives from UnityEngine.ScriptableObject: it did not work.

--- Assets/SEECity/DataModel/Attributable.cs
+++ Assets/SEECity/DataModel/Attributable.cs
@@ -28,7 +28,7 @@ namespace SEE.DataModel
     /// Specifies and implements attributable objects with named toggle, int, float, and string attributes.
     /// </summary>
     [System.Serializable]
-    public abstract class Attributable : UnityEngine.ScriptableObject, ICloneable
+    public abstract class Attributable : ICloneable
     {
         /// <summary>
         /// The set of toggle attributes. A toggle is set if it is contained in this
