9a0a79f5d786ebebf83266a4e4d661489c2c268e
Jan-Eric Oltmanns <jan_olt@uni-bremen.de>
2020-02-05 15:54:04 +0100

    Added script that handles controler input.

---
+++ Assets/SEEControls/ViveControlerInteraction.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+/// <summary>
+/// Event class for passing on axis value from input to given function in editor.
+/// The given function must have a float parameter in its head.
+/// </summary>
+[System.Serializable]
+public class AxisEvent : UnityEvent<float>
+{
+
+}
+
+/// <summary>
+/// This class makes it possible to map functions from other scripts in the editor to the controler events.
+/// Its the central point for processing controler input.
+/// No other script within the level should use controler inputs.
+/// </summary>
+public class ViveControlerInteraction : MonoBehaviour
+{
+    [SerializeField]
+    [FormerlySerializedAs("OnLeftTrigger")]
+    private UnityEvent _OnLeftTrigger = null;
+
+    public float LeftTriggerDepth = 0.5f;
+
+    [SerializeField]
+    [FormerlySerializedAs("OnLeftTriggerAxis")]
+    private AxisEvent _OnLeftTriggerAxis = null;
+
+    [SerializeField]
+    [FormerlySerializedAs("OnRightTrigger")]
+    private UnityEvent _OnRightTrigger = null;
+
+    public float RightTriggerDepth = 0.5f;
+
+    [SerializeField]
+    [FormerlySerializedAs("OnRightTriggerAxis")]
+    private AxisEvent _OnRightTriggerAxis = null;
+
+    public GameObject LeftControler;
+    public GameObject RightControler;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float LeftTriggerAxis = Input.GetAxis("LeftVRTrigger");
+        float RightTriggerAxis = Input.GetAxis("RightVRTriggerMovement");
+
+        if(_OnLeftTrigger != null)
+        {
+            if (LeftTriggerAxis > LeftTriggerDepth)
+                _OnLeftTrigger.Invoke();
+        }
+
+        if(_OnLeftTriggerAxis != null)
+        {
+            _OnLeftTriggerAxis.Invoke(LeftTriggerAxis);
+        }
+
+        if(_OnRightTrigger != null)
+        {
+            if (RightTriggerAxis > RightTriggerDepth)
+                _OnRightTrigger.Invoke();
+        }
+
+        if(_OnRightTriggerAxis != null)
+        {
+            _OnRightTriggerAxis.Invoke(RightTriggerAxis);
+        }
+    }
+
+    /// <summary>
+    /// Returns the GameObject which is assigned in the editor and represents the left controler within the level.
+    /// </summary>
+    /// <returns>the GameObject of the left controler</returns>
+    public GameObject GetLeftControler()
+    {
+        return LeftControler;
+    }
+
+    /// <summary>
+    /// Returns the GameObject which is assigned in the editor and represents the rigth controler within the level.
+    /// </summary>
+    /// <returns>the GameObject of the rigth controler</returns>
+    public GameObject GetRightControler()
+    {
+        return RightControler;
+    }
+}
