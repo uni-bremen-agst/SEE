f2e8337a520bd7b04c65101f6d43ecc1a8bf84c2
bohnsack <robboh@gmx.de>
2020-02-19 17:37:27 +0100

    Added dragging via the Labels Panel (because many tried to do that)

---
+++ Assets/SEECity/Charts/Scripts/VR/ChartDragHandlerVr.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SEECity.Charts.Scripts.VR
+{
+	public class ChartDragHandlerVr : ChartDragHandler
+	{
+		private Transform _parent;
+		private Vector3 _distance;
+
+		protected override void Awake()
+		{
+			base.Awake();
+			_parent = transform.parent.GetComponent<ChartContent>().parent.transform;
+		}
+
+		public override void OnDrag(PointerEventData eventData)
+		{
+			if (eventData.pointerCurrentRaycast.worldPosition != Vector3.zero)
+				_parent.position = eventData.pointerCurrentRaycast.worldPosition - _distance;
+			//TODO: test
+		}
+
+		public override void OnPointerDown(PointerEventData eventData)
+		{
+			_distance = eventData.pointerCurrentRaycast.worldPosition - _chart.position;
+		}
+	}
+}
