e3ba985516fa6a225e42f6e58fc194cd353dc077
Jan-Eric Oltmanns <jan_olt@uni-bremen.de>
2020-02-24 14:29:41 +0100

    Added basic action mapping concept to controls

---
+++ Assets/SEEControls/InteractionBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SEE.Controls
+{
+    public class InteractionBehavior : MonoBehaviour
+    {
+        private ActionMapping Mapping;
+
+        private void Start()
+        {
+            Mapping = new ViveActionMapping("Demomapping") as ViveActionMapping;
+            
+        }
+
+        //implementation of the mapping functions
+
+        private void FreeMovement(Vector3 direction)
+        {
+
+        }
+    }
+}
