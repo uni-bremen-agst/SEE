826bd80ba833ddf18477e76c58a422e8ca7d889e
koschke <koschke@uni-bremen.de>
2020-02-29 09:45:40 +0100

    #41 Removed unused imports.

--- Assets/SEECity/Animation/Utils/NodeExtension.cs
+++ Assets/SEECity/Animation/Utils/NodeExtension.cs
@@ -16,10 +16,8 @@
 //LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 //TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 //USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 using SEE.DataModel;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 /// <summary>
 /// Extension that simplifies access to information of a node.
