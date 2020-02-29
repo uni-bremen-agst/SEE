28b800dbe913f3d30cfd99028aa4e37ea2fadaf3
Torben Groß <TorbenGross@gmx.de>
2020-02-29 18:27:52 +0100

    Initial work on serialization alternative

---
+++ Assets/SEENet/Scripts/Serialization/SMesh.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace SEE.Net.Internal
+{
+
+    [Serializable]
+    public class SMesh : IInitializer<Mesh>
+    {
+        [Serializable]
+        public class SIndices
+        {
+            public int[] indices;
+        }
+
+        public IndexFormat indexFormat;
+        public Matrix4x4[] bindposes;
+        public int subMeshCount;
+        public Bounds bounds;
+        public Vector3[] vertices;
+        public Vector3[] normals;
+        public Vector4[] tangents;
+        public Vector2[] uv;
+        public Vector2[] uv2;
+        public Vector2[] uv3;
+        public Vector2[] uv4;
+        public Vector2[] uv5;
+        public Vector2[] uv6;
+        public Vector2[] uv7;
+        public Vector2[] uv8;
+        public Color[] colors;
+
+        public SIndices[] indices;
+        public MeshTopology[] topologies;
+        public byte[] bonesPerVertex;
+        public BoneWeight1[] boneWeights;
+
+        public SMesh(Mesh mesh)
+        {
+            indexFormat = mesh.indexFormat;
+            bindposes = mesh.bindposes;
+            subMeshCount = mesh.subMeshCount;
+            bounds = mesh.bounds;
+            vertices = mesh.vertices;
+            normals = mesh.normals;
+            tangents = mesh.tangents;
+            uv = mesh.uv;
+            uv2 = mesh.uv2;
+            uv3 = mesh.uv3;
+            uv4 = mesh.uv4;
+            uv5 = mesh.uv5;
+            uv6 = mesh.uv6;
+            uv7 = mesh.uv7;
+            uv8 = mesh.uv8;
+
+            indices = new SIndices[mesh.subMeshCount];
+            topologies = new MeshTopology[mesh.subMeshCount];
+            for (int i = 0; i < mesh.subMeshCount; i++)
+            {
+                indices[i] = new SIndices()
+                {
+                    indices = mesh.GetIndices(i)
+                };
+                topologies[i] = mesh.GetTopology(i);
+            }
+            bonesPerVertex = mesh.GetBonesPerVertex().ToArray();
+            boneWeights = mesh.GetAllBoneWeights().ToArray();
+        }
+
+        public void Initialize(Mesh value)
+        {
+            value.indexFormat = indexFormat;
+            value.bindposes = bindposes;
+            value.subMeshCount = subMeshCount;
+            value.bounds = bounds;
+            value.vertices = vertices;
+            value.normals = normals;
+            value.tangents = tangents;
+            value.uv = uv;
+            value.uv2 = uv2;
+            value.uv3 = uv3;
+            value.uv4 = uv4;
+            value.uv5 = uv5;
+            value.uv6 = uv6;
+            value.uv7 = uv7;
+            value.uv8 = uv8;
+
+            for (int i = 0; i < subMeshCount; i++)
+            {
+                value.SetIndices(indices[i].indices, topologies[i], i);
+            }
+            value.SetBoneWeights(
+                new NativeArray<byte>(bonesPerVertex, Allocator.Temp),
+                new NativeArray<BoneWeight1>(boneWeights, Allocator.Temp)
+            );
+        }
+    }
+
+}
---
+++ Assets/SEENet/SerializerTest.cs
@@ -0,0 +1,26 @@
+﻿using SEE.Net.Internal;
+using UnityEngine;
+
+public class SerializerTest : MonoBehaviour
+{
+    void Start()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+
+        SMeshFilter sMeshFilter0 = Serializer.ToSerializableObject(meshFilter);
+        string str = Serializer.Serialize(sMeshFilter0);
+        Debug.Log(str.Length);
+        Debug.Log(str);
+        SMeshFilter sMeshFilter1 = Serializer.Deserialize<SMeshFilter>(str);
+
+        sMeshFilter1.Initialize(meshFilter);
+
+
+
+        GameObject go = new GameObject("HI, MY NAME IS?!");
+        go.transform.position = new Vector3(1.1f, 0.0f, 0.0f);
+        MeshFilter goMF = go.AddComponent<MeshFilter>();
+        sMeshFilter1.Initialize(goMF);
+        MeshRenderer goMR = go.AddComponent<MeshRenderer>();
+    }
+}
