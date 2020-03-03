4687a7b1e44d7fe35e9b02123bc1bc39e41a2098
Rainer Koschke <koschke@uni-bremen.de>
2020-03-03 17:22:02 +0100

    #41 Better name. Better comments. Prepared for move to general Utils.

--- Assets/SEECity/Animation/Utils/CheckExtension.cs
+++ Assets/SEECity/Animation/Utils/CheckExtension.cs
@@ -17,45 +17,44 @@
 //TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 //USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-namespace SEE.Animation.Internal
+namespace SEE
 {
     /// <summary>
     /// Enables fast and simple checks for all types.
     /// If a check fails, an exception is thrown.
     /// </summary>
-    public static class CheckExtension
+    public static class Assertions
     {
-
         /// <summary>
-        /// Checks if a given object is null and throws a System.Exception
-        /// containing the paramName if so.
+        /// Checks whether <paramref name="obj"/> is null and throws a System.Exception
+        /// containing the <paramref name="paramName"/> if so.
         /// </summary>
         /// <typeparam name="T">type of the given object</typeparam>
-        /// <param name="obj">object to check</param>
+        /// <param name="obj">object to be checked</param>
         /// <param name="paramName">parameter name for exception message</param>
-        /// <returns>the given object to check</returns>
+        /// <returns>the given object to be checked</returns>
         public static T AssertNotNull<T>(this T obj, string paramName)
         {
             if (obj == null)
             {
-                throw new System.Exception(paramName + "must not be null");
+                throw new System.Exception(paramName + " must not be null");
             }
             return obj;
         }
 
         /// <summary>
-        /// Checks if a given string is null and not empty and throws a System.Exception
-        /// containing the paramName if so.
+        /// Checks whether <paramref name="obj"/> is null or empty and throws a System.Exception
+        /// containing <paramref name="paramName"/> if so.
         /// </summary>
-        /// <param name="obj">string to check</param>
+        /// <param name="obj">string to be checked</param>
         /// <param name="paramName">parameter name for exception message</param>
-        /// <returns>the given string to check</returns>
+        /// <returns>the given string to be checked</returns>
         public static string AssertNotNullOrEmpty(this string obj, string paramName)
         {
             obj.AssertNotNull(paramName);
             if (obj.Length == 0)
             {
-                throw new System.Exception(paramName + "must not be empty");
+                throw new System.Exception(paramName + " must not be empty");
             }
             return obj;
         }
