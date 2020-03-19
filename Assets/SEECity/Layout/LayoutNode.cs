0c2bae83ffd41546bb48559257679a27f184f30f
koschke <koschke@uni-bremen.de>
2020-03-19 12:38:43 +0100

    #42 Initial data structures for the separation of game objects and the information about these necessary to lay them out. Work in progress. GameNode is a wrapper to help during the migration phase. It will likely disappear when we are done. LayoutNode will be extended as needed.

---
+++ Assets/SEECity/Layout/LayoutNode.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace SEE.Layout
+{
+    /// <summary>
+    ///  Defines the methods for all nodes to be laid out.
+    /// </summary>
+    public interface LayoutNode
+    {
+        /// <summary>
+        /// Yields true if the given node is to be interpreted as a leaf by the layouter.
+        /// 
+        /// Note: Even leaves may have children. What to do with those is the decision of the
+        /// layouter. It may or may not lay them out.
+        /// </summary>
+        /// <returns>true if the given node is to be interpreted as a leaf by the layouter</returns>
+        bool IsLeaf();
+
+        /// <summary>
+        /// A unique ID for a node.
+        /// </summary>
+        /// <returns>unique ID for a node</returns>
+        string LinkName();
+
+        /// <summary>
+        /// Yields the scale of a node.
+        /// </summary>
+        /// <returns>scale of node</returns>
+        Vector3 GetSize();
+
+        //Vector3 Position();
+        //ICollection<LayoutNode> Children();
+    }
+}
