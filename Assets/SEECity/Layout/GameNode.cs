53e54acdc9163c217013ba2eac73cf4eb024ec7d
koschke <koschke@uni-bremen.de>
2020-03-19 12:38:43 +0100

    #42 Initial data structures for the separation of game objects and the information about these necessary to lay them out. Work in progress. GameNode is a wrapper to help during the migration phase. It will likely disappear when we are done. LayoutNode will be extended as needed.

---
+++ Assets/SEECity/Layout/GameNode.cs
@@ -0,0 +1,63 @@
+﻿using SEE.DataModel;
+using SEE.GO;
+using UnityEngine;
+
+namespace SEE.Layout
+{
+    /// <summary>
+    /// Implementation of LayoutNode. It is simply a wrapper to game objects
+    /// created for inner nodes or leaf nodes.
+    /// </summary>
+    public class GameNode : LayoutNode
+    {
+        private readonly GameObject gameObject;
+        private readonly LeafNodeFactory leafNodeFactory;
+        private readonly Node node;
+
+        public GameObject GetGameObject()
+        {
+            return gameObject;
+        }
+
+        public Node GetNode()
+        {
+            return node;
+        }
+
+        public GameNode(GameObject gameObject, LeafNodeFactory nodeFactory)
+        {
+            this.gameObject = gameObject;
+            this.leafNodeFactory = nodeFactory;
+            this.node = this.gameObject.GetComponent<NodeRef>().node;
+        }
+
+        public GameNode(GameObject gameObject)
+        {
+            this.gameObject = gameObject;
+            this.leafNodeFactory = null;
+            this.node = this.gameObject.GetComponent<NodeRef>().node;
+        }
+
+        public Vector3 GetSize()
+        {
+            if (node.IsLeaf())
+            {
+                return leafNodeFactory.GetSize(gameObject);
+            }
+            else
+            {
+                return gameObject.transform.localScale;
+            }
+        }
+
+        public bool IsLeaf()
+        {
+            return node.IsLeaf();
+        }
+
+        public string LinkName()
+        {
+            return gameObject.LinkName();
+        }
+    }
+}
