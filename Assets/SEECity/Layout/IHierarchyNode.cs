a4af187b6d1ef8f2183d984fe26bb098d0535088
koschke <koschke@uni-bremen.de>
2020-03-25 16:08:25 +0100

    #42 Removed obsolete experimental code.

--- Assets/SEECity/Layout/IHierarchyNode.cs
+++ Assets/SEECity/Layout/IHierarchyNode.cs
@@ -2,6 +2,10 @@
 
 namespace SEE.Layout
 {
+    /// <summary>
+    /// Defines the interface of nodes in a node hierarchy.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IHierarchyNode<T>
     {
         /// <summary>
@@ -32,79 +36,4 @@ namespace SEE.Layout
         /// <returns>children of this node</returns>
         ICollection<T> Children();
     }
-
-    interface IHierarchynode<T>
-    {
-        T Parent { get; }
-        ICollection<T> Children { get; }
-        int Level { get; }
-    }
-
-    interface IGraphnode<T>
-    {
-        ICollection<T> Successors { get; }
-    }
-
-    interface IGamenode
-    {
-        int Scale { get; set; }
-    }
-
-    interface ILayoutnode : IGamenode, IHierarchynode<ILayoutnode>, IGraphnode<ILayoutnode>
-    {
-    }
-
-    abstract class AbstractLayoutnode : ILayoutnode
-    {
-        public ILayoutnode Parent => throw new System.NotImplementedException();
-
-        public ICollection<ILayoutnode> Successors => throw new System.NotImplementedException();
-
-        public abstract ICollection<ILayoutnode> Children { get; }
-        public abstract int Level { get; }
-        public int Scale { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-    }
-
-    class Layoutnode : AbstractLayoutnode
-    {
-        public override ICollection<ILayoutnode> Children => throw new System.NotImplementedException();
-
-        public override int Level => throw new System.NotImplementedException();
-    }
-
-    class LCAfinder<T> where T : IHierarchynode<T>
-    {
-        public T foo(T t)
-        {
-            ICollection<T> kids = t.Children;
-            T p = t.Parent;
-            return p;
-
-        }
-    }
-
-    class LCAfinderClient
-    {
-        void bar()
-        {
-            LCAfinder<ILayoutnode> g = new LCAfinder<ILayoutnode>();
-            AbstractLayoutnode y = new Layoutnode();
-            ILayoutnode x = g.foo(y);
-        }
-    }
-
-    abstract class NLayout
-    {
-        public abstract Dictionary<IGamenode, NodeTransform> Layout(ICollection<IGamenode> layoutNodes);
-    }
-
-    /*
-    class HierarchyLayout : NLayout
-    {
-        public override Dictionary<IGamenode, NodeTransform> Layout(ICollection<ILayoutnode> layoutNodes)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-    */
 }
