83c318484040a5f4808c0b790e730d590c68ae62
Jan-Eric Oltmanns <jan_olt@uni-bremen.de>
2020-04-08 12:44:19 +0200

    Reworked action mapping. Still prototype. (Not set up)

---
+++ Assets/SEEControls/ActionMapping/KeyActionMapping.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SEE.Controls
+{
+    [CreateAssetMenu(fileName = "New KeyActionMapping", menuName = "Controls/KeyActionMapping", order = 1)]
+    public class KeyActionMapping : ActionMapping
+    {
+        [SerializeField]
+        public UnityEvent<bool> OnWKey;
+
+        [SerializeField]
+        public UnityEvent<bool> OnAKey;
+
+        [SerializeField]
+        public UnityEvent<bool> OnSKey;
+
+        [SerializeField]
+        public UnityEvent<bool> OnDKey;
+
+        public override void CheckInput()
+        {
+            if(Input.GetKey("W"))
+            {
+                OnWKey.Invoke(true);
+            }
+        }
+
+        public override string GetTypeAsString()
+        {
+            return "Key Mapping";
+        }
+    }
+}
