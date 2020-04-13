4eaa3fd3d955220d64d35d9706d0d7de4d70ef12
koschke <koschke@uni-bremen.de>
2020-04-13 10:32:02 +0200

    #65 Allows a user to hide the controllers (only hands will be shown then).

---
+++ Assets/SEEControls/ShowControllers.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace SEE.Controls
+{
+    /// <summary>
+    /// Allows a user to specify whether the controllers should be shown together
+    /// with the hands. This script is assumed to be attached to a SteamVR
+    /// Player rig.
+    /// </summary>
+    public class ShowControllers : MonoBehaviour
+    {
+        [Tooltip("Whether the controllers should be hidden.")]
+        public bool HideControllers = false;
+
+        /// <summary>
+        /// If and only if HideControllers is true, the controllers will not be shown
+        /// together with the hands.
+        /// </summary>
+        void Update()
+        {
+            foreach (var hand in Player.instance.hands)
+            {
+                if (HideControllers)
+                {
+                    hand.HideController();
+                    hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithoutController);
+                }
+                else
+                {
+                    hand.ShowController();
+                    hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController);
+                }
+            }
+        }
+    }
+}
