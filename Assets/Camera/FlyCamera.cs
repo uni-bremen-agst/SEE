e4f354922212d1802d784d1071945de09a3f8742
Rainer Koschke <koschke@axivion.com>
2020-04-18 18:33:39 +0200

    #65 Disambiguated Input.

--- Assets/Camera/FlyCamera.cs
+++ Assets/Camera/FlyCamera.cs
@@ -324,7 +324,7 @@ namespace SEE
             }
             else
             {
-                lastMouse = Input.mousePosition - lastMouse;
+                lastMouse = UnityEngine.Input.mousePosition - lastMouse;
                 lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
                 lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
                 transform.eulerAngles = lastMouse;
