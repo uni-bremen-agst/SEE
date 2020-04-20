3f0e32bb0cda7d9537e34f9bc3555b6c45f422cd
Rainer Koschke <koschke@uni-bremen.de>
2020-04-20 19:31:51 +0200

    #65 Separated SelectionAction into two subclasses with their specifics to 2D and 3D selection.
    All actions now retrieve their input directly from their logical
    input controls.

---
+++ Assets/SEEControls/Actions/Selection2DAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace SEE.Controls
+{
+    public class Selection2DAction : SelectionAction
+    {
+        protected override Vector3 Origin()
+        {
+            return MainCamera.transform.position;
+        }
+
+        protected override bool Detect(out RaycastHit hitInfo)
+        {
+            Ray ray = MainCamera.ScreenPointToRay(selectionDevice.Value);
+            //Debug.LogFormat("2d direction={0} at origin {1} and direction {2} camera {3}\n", 
+            //                selectionDevice.Value, ray.origin, ray.direction, MainCamera.transform.position);
+            //Debug.DrawRay(ray.origin, ray.direction * 10000, Color.yellow);
+            return Physics.Raycast(ray, out hitInfo);
+        }
+    }
+}
---
+++ Assets/SEEControls/Actions/Selection3DAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SEE.Controls
+{
+    public class Selection3DAction : SelectionAction
+    {
+        protected override Vector3 Origin()
+        {
+            return gameObject.transform.position;
+        }
+
+        protected override bool Detect(out RaycastHit hitInfo)
+        {
+            return Physics.Raycast(gameObject.transform.position, selectionDevice.Value, out hitInfo, Mathf.Infinity);
+        }
+    }
+}
