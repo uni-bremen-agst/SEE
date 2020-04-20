9374c5b31b24dee4ac46cded605ba6e99ef0dd18
Rainer Koschke <koschke@uni-bremen.de>
2020-04-20 20:05:07 +0200

    #65 Removed obsolete code.

---
+++ Assets/SEEControls/Devices/Direction/XRDirection.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+namespace SEE.Controls.Devices
+{
+    public class XRDirection : Direction
+    {
+        [Tooltip("The threshold at which the throttle is considered to be activated."), Range(0.01f, 1.0f)]
+        public float Threshold = 0.1f;
+
+        [Tooltip("The VR controller for directing")]
+        public Hand DirectingHand;
+
+        public override Vector3 Value
+        {
+            get => SteamVR_Actions.default_Pose.GetLocalRotation(DirectingHand.handType) * Vector3.forward;
+        }
+    }
+}
---
+++ Assets/SEEControls/Devices/Selection/XRSelection.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+namespace SEE.Controls.Devices
+{
+    public class XRSelection : Selection
+    {
+        [Tooltip("The threshold at which the trigger is considered to be activated."), Range(0.01f, 1.0f)]
+        public float Threshold = 0.1f;
+
+        [Tooltip("The VR controller for pointing")]
+        public Hand PointingHand;
+
+        private SteamVR_Action_Single TriggerAction = SteamVR_Input.GetSingleAction(defaultActionSet, "Trigger");
+
+        public override Vector3 Value
+        {
+            get => SteamVR_Actions.default_Pose.GetLocalRotation(PointingHand.handType) * Vector3.forward;
+        }
+
+        public override bool Activated
+        {
+            get => TriggerAction != null ? TriggerAction.axis >= Threshold : false;
+        }
+    }
+}
