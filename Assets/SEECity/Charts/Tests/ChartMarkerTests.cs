14bb3a27e0cc9bc7910789595b51be939a180662
bohnsack <robboh@gmx.de>
2020-04-21 21:38:03 +0200

    Tests and new sizes for text in the chart

---
+++ Assets/SEECity/Charts/Tests/ChartMarkerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ChartMarkerTests
+    {
+	    [SetUp]
+	    public void Setup()
+	    {
+		    SceneManager.LoadScene("ChartsTest");
+
+	    }
+
+	    [UnityTest]
+        public IEnumerator ChartMarkerTestsWithEnumeratorPasses()
+        {
+	        yield return null;
+        }
+    }
+}
