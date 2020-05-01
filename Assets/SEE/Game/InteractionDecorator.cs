5ba1f57c1ff4c8d2f64c6cc76c0d3efb34e679c9
koschke <koschke@uni-bremen.de>
2020-05-01 17:51:50 +0200

    #54 Decorates game objects for graph nodes with the necessary components to interact with them.

---
+++ Assets/SEE/Game/InteractionDecorator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace SEE.Controls
+{
+    public class InteractionDecorator
+    {
+        public static void PrepareForInteraction(GameObject go)
+        {
+            go.isStatic = false; // we want to move the object during the game
+            go.AddComponent<Interactable>(); // enable interactions
+            go.AddComponent<GrabObject>(); // our customized reactions to interactable's events
+                                           //go.AddComponent<Rigidbody>(); // so the object follows the laws of physics
+        }
+
+        public static void PrepareForInteraction(ICollection<GameObject> gameNodes)
+        {
+            foreach (GameObject go in gameNodes)
+            {
+                PrepareForInteraction(go);
+            }
+        }
+    }
+}
