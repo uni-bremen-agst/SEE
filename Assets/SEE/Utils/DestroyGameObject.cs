09c2827b7f14d01d23c4c8181ec031508ce251ce
koschke <koschke@uni-bremen.de>
2020-05-11 14:33:11 +0200

    #54 Added DestroyComponent().

--- Assets/SEE/Utils/DestroyGameObject.cs
+++ Assets/SEE/Utils/DestroyGameObject.cs
@@ -26,5 +26,25 @@ namespace SEE
                 UnityEngine.Object.DestroyImmediate(gameObject);
             }
         }
+
+        /// <summary>
+        /// Destroys given <paramref name="component"/> using UnityEngine.Object when in
+        /// game mode or UnityEngine.Object.DestroyImmediate when in editor mode.
+        /// </summary>
+        /// <param name="component"></param>
+        public static void DestroyComponent(Component component)
+        {
+            // We must use DestroyImmediate when we are in the editor mode.
+            if (Application.isPlaying)
+            {
+                // playing either in a built player or in the player of the editor
+                UnityEngine.Object.Destroy(component);
+            }
+            else
+            {
+                // game is not played; we are in the editor mode
+                UnityEngine.Object.DestroyImmediate(component);
+            }
+        }
     }
 }
