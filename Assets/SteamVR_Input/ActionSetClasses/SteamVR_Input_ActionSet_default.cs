22f2840771ee313016cb02e407cb7fdc6b407e0c
koschke <koschke@uni-bremen.de>
2020-05-22 14:25:34 +0200

    #82 Added new Home action for re-setting zooming to the top level.

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -129,6 +129,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean Home
+        {
+            get
+            {
+                return SteamVR_Actions.default_Home;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
