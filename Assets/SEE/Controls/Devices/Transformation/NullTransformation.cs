bc0056a1b66780ae6317910a342949e25fd0e833
koschke <koschke@uni-bremen.de>
2020-05-22 14:23:39 +0200

    #82 Marked as obsolete.

--- Assets/SEE/Controls/Devices/Transformation/NullTransformation.cs
+++ Assets/SEE/Controls/Devices/Transformation/NullTransformation.cs
@@ -1,5 +1,8 @@
-﻿namespace SEE.Controls.Devices
+﻿using System;
+
+namespace SEE.Controls.Devices
 {
+    [Obsolete("This functionality is covered by a selection device.")]
     public class NullTransformation : Transformation
     {
         public override float ZoomFactor => 1.0f;
