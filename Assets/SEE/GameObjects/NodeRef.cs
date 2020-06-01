a555c5361f228a13d59680ff61b6a475cdb57ba5
bohnsack <robboh@gmx.de>
2020-06-01 16:44:30 +0200

    Switch to new city layout and fix to tree view

--- Assets/SEE/GameObjects/NodeRef.cs
+++ Assets/SEE/GameObjects/NodeRef.cs
@@ -1,23 +1,32 @@
-﻿using OdinSerializer;
-using System;
-
+﻿using System;
+using OdinSerializer;
+using SEE.Charts.Scripts;
 using SEE.DataModel;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace SEE.GO
 {
-    /// <summary>
-    /// A reference to a graph node that can be attached to a game object as a component.
-    /// </summary>
-    public class NodeRef : SerializedMonoBehaviour
-    {
-        /// <summary>
-        /// The graph node this node reference is referring to. It will be set either
-        /// by a graph renderer while in editor mode or at runtime by way of an
-        /// AbstractSEECity object.
-        /// It will not be serialized to prevent duplicating and endless serialization
-        /// by both Unity and Odin.
-        /// </summary>
-        [NonSerialized]
-        public Node node;
-    }
+	/// <summary>
+	/// A reference to a graph node that can be attached to a game object as a component.
+	/// </summary>
+	public class NodeRef : SerializedMonoBehaviour
+	{
+		/// <summary>
+		/// The graph node this node reference is referring to. It will be set either
+		/// by a graph renderer while in editor mode or at runtime by way of an
+		/// AbstractSEECity object.
+		/// It will not be serialized to prevent duplicating and endless serialization
+		/// by both Unity and Odin.
+		/// </summary>
+		[NonSerialized] public Node node;
+
+		[FormerlySerializedAs("Highlights"), HideInInspector]
+		public NodeHighlights highlights;
+
+		public void Awake()
+		{
+			highlights = GetComponent<NodeHighlights>();
+		}
+	}
 }
