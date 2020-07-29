f8e0e61e464752233b065ffcfa4d41c6199cc016
Torben Groß <TorbenGross@gmx.de>
2020-07-29 13:53:34 +0200

    #89 Cursor axes

---
+++ Assets/Resources/Shaders/CursorAxisShader.shader
@@ -0,0 +1,52 @@
+﻿Shader "Unlit/CursorAxisShader"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+		Tags
+		{
+			"Queue" = "Overlay"
+			"IgnoreProjector" = "True"
+			"RenderType" = "Transparent"
+		}
+
+		ZTest Off
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = fixed4(0.0, 0.0, 0.0, 1.0);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/SEE/Controls/Actions/Cursor.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+namespace SEE.Controls
+{
+    internal class Cursor : MonoBehaviour
+    {
+        private const string OutlineShaderName = "Unlit/CursorOutlineShader";
+        private const string AxisShaderName = "Unlit/CursorAxisShader";
+
+        private Transform focus;
+        public Transform Focus { get => focus; set { if (value != null) { focus = value; } } }
+
+        private GameObject outline;
+        private Material outlineMaterial;
+
+        private float axisHalfLength;
+        private Material axisMaterial;
+
+        private Cursor()
+        {
+        }
+
+        private void Update()
+        {
+            if (Focus == null)
+            {
+                Focus = Table.Instance.transform;
+            }
+            transform.position = Focus.position;
+
+            axisHalfLength = 0.02f * (Camera.main.transform.position - transform.position).magnitude;
+        }
+
+        private void OnRenderObject()
+        {
+            if (axisHalfLength > 0.0f)
+            {
+                Vector3 c = focus.transform.position;
+                Vector3 x0 = new Vector3(c.x - axisHalfLength, c.y, c.z);
+                Vector3 x1 = new Vector3(c.x + axisHalfLength, c.y, c.z);
+                Vector3 y0 = new Vector3(c.x, c.y - axisHalfLength, c.z);
+                Vector3 y1 = new Vector3(c.x, c.y + axisHalfLength, c.z);
+                Vector3 z0 = new Vector3(c.x, c.y, c.z - axisHalfLength);
+                Vector3 z1 = new Vector3(c.x, c.y, c.z + axisHalfLength);
+
+                axisMaterial.SetPass(0);
+                GL.Begin(GL.LINES);
+                GL.Vertex(x0);
+                GL.Vertex(x1);
+                GL.Vertex(y0);
+                GL.Vertex(y1);
+                GL.Vertex(z0);
+                GL.Vertex(z1);
+                GL.End();
+            }
+        }
+
+        internal static Cursor Create(Transform focus)
+        {
+            GameObject go = new GameObject("Cursor");
+            Cursor c = go.AddComponent<Cursor>();
+            c.focus = focus ?? throw new System.ArgumentNullException("Focus of cursor must not be null!");
+            go.transform.position = focus.position;
+
+            c.outline = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            c.outline.transform.parent = go.transform;
+            c.outline.transform.localPosition = Vector3.zero;
+            c.outline.transform.localScale = Vector3.one;
+            c.outlineMaterial = new Material(Shader.Find(OutlineShaderName));
+            c.outlineMaterial.SetTexture("_MainTex", Tools.TextureGenerator.CreateCircleOutlineTextureR8(64, 60, 1.0f, 0.0f));
+            c.outline.GetComponent<MeshRenderer>().sharedMaterial = c.outlineMaterial;
+
+            c.axisMaterial = new Material(Shader.Find(AxisShaderName));
+
+            return c;
+        }
+    }
+}
