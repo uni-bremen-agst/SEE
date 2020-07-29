51ff2c6060e7b56f80c7ceb854f93e34c6599dd3
Torben Groß <TorbenGross@gmx.de>
2020-07-29 10:29:32 +0200

    #89 Line texture generation

--- Assets/SEE/Tools/TextureGenerator.cs
+++ Assets/SEE/Tools/TextureGenerator.cs
@@ -7,6 +7,166 @@ namespace SEE.Tools
     /// </summary>
     public static class TextureGenerator
     {
+        public static Texture2D CreateColoredTextureR8(int width, int height, Color color)
+        {
+            Texture2D result = new Texture2D(width, height, TextureFormat.R8, false);
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    result.SetPixel(x, y, color);
+                }
+            }
+            result.Apply();
+            return result;
+        }
+
+        /// <summary>
+        /// <see cref="https://github.com/danbar/murphy_line_draw/blob/master/murphy_line_draw.m"/>
+        /// </summary>
+        /// <param name="textureWidth"></param>
+        /// <param name="textureHeight"></param>
+        /// <param name="p0"></param>
+        /// <param name="p1"></param>
+        /// <param name="thickness"></param>
+        /// <param name="lineColor"></param>
+        /// <param name="backgroundColor"></param>
+        /// <returns></returns>
+        public static Texture2D CreateLineTexture(int textureWidth, int textureHeight, Vector2Int p0, Vector2Int p1, float thickness, Color lineColor, Color backgroundColor)
+        {
+            Texture2D result = CreateColoredTextureR8(textureWidth, textureHeight, backgroundColor);
+
+            int dx = p1.x - p0.x;
+            int dy = p1.y - p0.y;
+
+            int inc_x = (int)Mathf.Sign(Mathf.Sign((float)dx) + 0.5f);
+            int inc_y = (int)Mathf.Sign(Mathf.Sign((float)dy) + 0.5f);
+            if (dx < 0) dx = -dx;
+            if (dy < 0) dy = -dy;
+            
+            int len, sd0x, sd0y, dd0x, dd0y, sd1x, sd1y, dd1x, dd1y, ku, kv, kd, kt;
+            if (dx > dy)
+            {
+                len = dx;
+    
+                sd0x = 0;      sd0y = inc_y;
+                dd0x = -inc_x; dd0y = inc_y;
+
+                sd1x = inc_x; sd1y = 0;
+                dd1x = inc_x; dd1y = inc_y;
+
+                ku = 2 * dx;
+                kv = 2 * dy;
+                kd = kv - ku;
+
+                kt = dx - kv;
+            }
+            else
+            {
+                len = dy;
+    
+                sd0x = inc_x; sd0y = 0;
+                dd0x = inc_x; dd0y = -inc_y;
+
+                sd1x = 0;     sd1y = inc_y;
+                dd1x = inc_x; dd1y = inc_y;
+
+                ku = 2 * dy;
+                kv = 2 * dx;
+                kd = kv - ku;
+
+                kt = dy - kv;
+            }
+
+            float tk = 2.0f * thickness * Mathf.Sqrt(dx * dx + dy * dy);
+
+            int d0 = 0;
+            int d1 = 0;
+            int dd = 0;
+
+            while ((float)dd < tk)
+            {
+                BresenhamLineDraw(p0, d1);
+
+                if (d0 < kt)
+                {
+                    p0.x = p0.x + sd0x;
+                    p0.y = p0.y + sd0y;
+                }
+                else
+                {
+                    dd = dd + kv;
+                    d0 = d0 - ku;
+
+                    if (d1 < kt)
+                    {
+                        p0.x = p0.x + dd0x;
+                        p0.y = p0.y + dd0y;
+
+                        d1 = d1 - kv;
+                    }
+                    else
+                    {
+                        if (dx > dy)
+                        {
+                            p0.x = p0.x + dd0x;
+                        }
+                        else
+                        {
+                            p0.y = p0.y + dd0y;    
+                        }
+
+                        d1 = d1 - kd;
+                        if ((float)dd > tk)
+                        {
+                            return result;
+                        }
+
+                        BresenhamLineDraw(p0, d1);
+
+                        if (dx > dy)
+                        {
+                            p0.y = p0.y + dd0y;
+                        }
+                        else
+                        {
+                            p0.x = p0.x + dd0x;
+                        }
+                    }
+                }
+
+                dd = dd + ku;
+                d0 = d0 + kv;
+
+            }
+
+            void BresenhamLineDraw(Vector2Int _p0, int _d1)
+            {
+                for (int p = 0; p < len; p++)
+                {
+                    result.SetPixel(_p0.x, _p0.y, lineColor);
+
+                    if (_d1 <= kt)
+                    {
+                        _p0.x = _p0.x + sd1x;
+                        _p0.y = _p0.y + sd1y;
+
+                        _d1 = _d1 + kv;
+                    }
+                    else
+                    {
+                        _p0.x = _p0.x + dd1x;
+                        _p0.y = _p0.y + dd1y;
+
+                        _d1 = _d1 + kd;
+                    }
+                }
+            }
+
+            result.Apply();
+            return result;
+        }
+
         /// <summary>
         /// Creates a texture of a circle outline with width and height of
         /// (<paramref name="outerRadius"/> + 1).
@@ -21,14 +181,7 @@ namespace SEE.Tools
         public static Texture2D CreateCircleOutlineTexture(int outerRadius, int innerRadius, Color circleColor, Color backgroundColor)
         {
             int size = 2 * outerRadius + 1;
-            Texture2D result = new Texture2D(size, size, TextureFormat.R8, false);
-
-            Color[] colors = new Color[size * size];
-            for (int i = 0; i < colors.Length; i++)
-            {
-                colors[i] = backgroundColor;
-            }
-            result.SetPixels(colors);
+            Texture2D result = CreateColoredTextureR8(size + 1, size + 1, backgroundColor);
 
             int xc = outerRadius;
             int yc = outerRadius;
