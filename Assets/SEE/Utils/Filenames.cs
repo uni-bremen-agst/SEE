8ece4eaf0e0abced4cfe96810f70b965971dd656
Rainer Koschke <koschke@uni-bremen.de>
2020-08-07 11:40:54 +0200

    Added ToInternalRepresentation(string).

--- Assets/SEE/Utils/Filenames.cs
+++ Assets/SEE/Utils/Filenames.cs
@@ -7,6 +7,9 @@ namespace SEE.Utils
     /// </summary>
     public class Filenames
     {
+        private const char WindowsDirectorySeparator = '\\';
+        public const char UnixDirectorySeparator = '/';
+
         /// <summary>
         /// Returns path where all Unity directory separators have been replaced by
         /// the directory separator of the current operating-system platform.
@@ -15,14 +18,27 @@ namespace SEE.Utils
         /// <returns>path with replaced directory separators</returns>
         public static string OnCurrentPlatform(string path)
         {
-            if (Path.DirectorySeparatorChar == '\\')
+            if (Path.DirectorySeparatorChar == WindowsDirectorySeparator)
             {
-                return path.Replace('/', Path.DirectorySeparatorChar);
+                // We are running on Windows.
+                return path.Replace(UnixDirectorySeparator, Path.DirectorySeparatorChar);
             }
             else
             {
+                // We are running on Unix or Mac OS.
                 return path;
             }
+        }
+
+        /// <summary>
+        /// Returns path where all directory separators of the current operating 
+        /// system platform have been replaced by the Unix (or Unity) directory separator.
+        /// </summary>
+        /// <param name="path">path to be adjusted</param>
+        /// <returns>path with Unix directory separators</returns>
+        public static string ToInternalRepresentation(string path)
+        {
+            return path.Replace(WindowsDirectorySeparator, UnixDirectorySeparator);
         }
     }
 }
