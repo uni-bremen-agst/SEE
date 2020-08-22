fbf84b8a43026a5c70d8a7fc9914cc588ad7a55d
Torben Groß <TorbenGross@gmx.de>
2020-08-22 16:35:50 +0200

    #89 Networked position and rotation synchronization

---
+++ Assets/SEE/Net/Scripts/Actions/SynchronizeAction.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace SEE.Net
+{
+    public class SynchronizeAction : AbstractAction
+    {
+        public string uniqueGameObjectName;
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 localScale;
+
+        public SynchronizeAction(GameObject gameObject) : base(false)
+        {
+            Assert.IsNotNull(gameObject);
+            uniqueGameObjectName = gameObject.name;
+            position = gameObject.transform.position;
+            rotation = gameObject.transform.rotation;
+            localScale = gameObject.transform.localScale;
+        }
+
+        protected override bool ExecuteOnClient()
+        {
+            bool result = false;
+
+            if (IsRequester())
+            {
+                result = true;
+            }
+            else
+            {
+                GameObject gameObject = GameObject.Find(uniqueGameObjectName);
+                gameObject.GetComponent<Synchronizer>().NotifyJustReceivedUpdate();
+                if (gameObject)
+                {
+                    result = true;
+                    gameObject.transform.position = position;
+                    gameObject.transform.rotation = rotation;
+                    gameObject.transform.localScale = localScale;
+                }
+            }
+
+            return result;
+        }
+
+        protected override bool ExecuteOnServer()
+        {
+            return true;
+        }
+
+        protected override bool RedoOnClient()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool RedoOnServer()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool UndoOnClient()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool UndoOnServer()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/SEE/Net/Scripts/Synchronizer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace SEE.Net
+{
+    public class Synchronizer : MonoBehaviour
+    {
+        private bool sendUpdate;
+        public int updateTimeout;
+
+        void Start()
+        {
+            sendUpdate = false;
+            updateTimeout = 0;
+
+            if (Network.UseInOfflineMode)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                InvokeRepeating("Synchronize", 0.1f, 0.1f);
+            }
+        }
+
+        void Update()
+        {
+            sendUpdate |= Input.GetMouseButton(2) && transform.hasChanged;
+        }
+
+        public void NotifyJustReceivedUpdate()
+        {
+            updateTimeout = 3;
+        }
+
+        private void Synchronize()
+        {
+            if (sendUpdate)
+            {
+                if (updateTimeout > 0)
+                {
+                    updateTimeout--;
+                }
+                else
+                {
+                    sendUpdate = false;
+                    new SynchronizeAction(gameObject).Execute();
+                }
+            }
+        }
+    }
+}
