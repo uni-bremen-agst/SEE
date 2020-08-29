c75b0118f113942a6d7f600e1feda460e11bbc91
Rainer Koschke <koschke@uni-bremen.de>
2020-08-29 19:10:11 +0200

    The object representing a culling plane must be tagged by Tags.CodeCity.

--- Assets/SEE/GameObjects/Plane.cs
+++ Assets/SEE/GameObjects/Plane.cs
@@ -74,6 +74,8 @@ namespace SEE
         /// </summary>
         private static Plane instance = null;
 
+        private const string SearchTag = Tags.CodeCity;
+
         /// <summary>
         /// The instance of the plane.
         /// </summary>
@@ -86,11 +88,11 @@ namespace SEE
                     // FIXME: We are iterating over all objects. There should be a better way
                     // to obtain the Plane object. Moreover, we should allow multiple such
                     // objects anyhow.
-                    foreach (GameObject plane in GameObject.FindGameObjectsWithTag(Tags.CullingPlane))
+                    foreach (GameObject plane in GameObject.FindGameObjectsWithTag(SearchTag))
                     {
                         if (instance != null)
                         {
-                            Debug.LogErrorFormat("There is yet another game object tagged by named {0}.\n", Tags.CullingPlane, plane.name);                            
+                            Debug.LogErrorFormat("There is yet another game object tagged by named {0}.\n", SearchTag, plane.name);                            
                         }
                         else
                         {
@@ -107,7 +109,7 @@ namespace SEE
                     }
                     if (instance == null)
                     {
-                        throw new System.Exception("There is no game object tagged by " + Tags.CullingPlane
+                        throw new System.Exception("There is no game object tagged by " + SearchTag
                             + " with a component Plane");
                     }
                 }
