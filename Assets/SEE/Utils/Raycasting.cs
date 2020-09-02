93bb79b2facedb6be6cf5fa553ac7f0968bd6c7f
Rainer Koschke <koschke@uni-bremen.de>
2020-09-02 11:11:42 +0200

    #95 New utility for raycasting in case of transparent objects. Yields a sorted list of hits.

---
+++ Assets/SEE/Utils/Raycasting.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+
+namespace SEE.Utils
+{
+    /// <summary>
+    /// Utilities related to ray casting.
+    /// </summary>
+    public static class Raycasting 
+    {
+        /// <summary>
+        /// Returns a sorted list of hits of a ray starting at the main camera and directing
+        /// at towards the current mouse position. The list is sorted by the diancce to the camera;
+        /// closer objects are at the front. The list may be empty if no object was hit.
+        /// </summary>
+        /// <returns>sorted list of hits</returns>
+        public static RaycastHit[] SortedHits()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(ray);
+            Array.Sort(hits, (h0, h1) => h0.distance.CompareTo(h1.distance));
+            return hits;
+        }
+    }
+}
