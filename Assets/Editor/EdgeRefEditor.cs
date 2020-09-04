3c73f8d1d8bda94646e8e76c261a3f10e262d701
Rainer Koschke <koschke@uni-bremen.de>
2020-09-04 15:18:46 +0200

    #95 Custom editor for EdgeRefs added. Shared code was abstracted into common new superclass GraphElementRefEditor.

---
+++ Assets/Editor/EdgeRefEditor.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using SEE.GO;
+using UnityEngine;
+
+namespace SEEEditor
+{
+    /// <summary>
+    /// A custom editor for instances of EdgeRef.
+    /// </summary>
+    [CustomEditor(typeof(EdgeRef))]
+    [CanEditMultipleObjects]
+    public class EdgeRefEditor : GraphElementRefEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            EdgeRef edgeRef = target as EdgeRef;
+
+            GUILayout.Label("Edge attributes", EditorStyles.boldLabel);
+            if (edgeRef.edge != null)
+            {
+                EditorGUILayout.TextField("Source node", edgeRef.edge.Source.ID);
+                EditorGUILayout.TextField("Target node", edgeRef.edge.Target.ID);
+                ShowTypeAndAttributes(edgeRef.edge);
+            }
+            else
+            {
+                GUILayout.Label("Edge: NONE", EditorStyles.boldLabel);
+            }
+        }
+    }
+}
+
+#endif
---
+++ Assets/Editor/GraphElementRefEditor.cs
@@ -0,0 +1,49 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+using SEE.DataModel;
+
+namespace SEEEditor
+{
+    /// <summary>
+    /// Common abstract superclass for NodeRef and EdgeRef editors. Handles the
+    /// shared attributes.
+    /// </summary>
+    public abstract class GraphElementRefEditor : Editor
+    {
+        /// <summary>
+        /// Emits the ID, Type, and all attributes of the given <paramref name="graphElement"/>
+        /// in text fields of the editor.
+        /// </summary>
+        /// <param name="graphElement">graph elements whose attributes are to be shown</param>
+        protected static void ShowTypeAndAttributes(GraphElement graphElement)
+        {
+            EditorGUILayout.TextField("ID", graphElement.ID);
+            EditorGUILayout.TextField("Type", graphElement.Type);
+
+            GUILayout.Label("String attributes", EditorStyles.boldLabel);
+            foreach (var entry in graphElement.StringAttributes)
+            {
+                EditorGUILayout.TextField(entry.Key, entry.Value);
+            }
+            GUILayout.Label("Float attributes", EditorStyles.boldLabel);
+            foreach (var entry in graphElement.FloatAttributes)
+            {
+                EditorGUILayout.TextField(entry.Key, entry.Value.ToString());
+            }
+            GUILayout.Label("Integer attributes", EditorStyles.boldLabel);
+            foreach (var entry in graphElement.IntAttributes)
+            {
+                EditorGUILayout.TextField(entry.Key, entry.Value.ToString());
+            }
+            GUILayout.Label("Toggle attributes", EditorStyles.boldLabel);
+            foreach (var entry in graphElement.ToggleAttributes)
+            {
+                EditorGUILayout.LabelField(entry);
+            }
+        }
+    }
+}
+
+#endif
