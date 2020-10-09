a0fd2fb5296210d10fd2c8a29c45e2a4f8156fa8
Rainer Koschke <koschke@uni-bremen.de>
2020-10-09 12:29:41 +0200

    Improved code readability.

--- Assets/SEE/Controls/Actions/DesktopChartAction.cs
+++ Assets/SEE/Controls/Actions/DesktopChartAction.cs
@@ -31,11 +31,15 @@ namespace SEE.Controls
 		{
 			if (chartControlsDevice.Toggle) ChartManager.Instance.ToggleCharts();
 			if (chartControlsDevice.Select) ChartManager.Instance.ToggleSelectionMode();
-			if (!chartControlsDevice.Click) return;
-			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast(ray, out var hit, 100f) &&
-			    hit.transform.gameObject.TryGetComponent(out NodeRef _))
-				ChartManager.HighlightObject(hit.transform.gameObject, false);
+			if (chartControlsDevice.Click)
+			{
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				if (Physics.Raycast(ray, out RaycastHit hit, 100f)
+					&& hit.transform.gameObject.TryGetComponent(out NodeRef _))
+				{
+					ChartManager.HighlightObject(hit.transform.gameObject, false);
+				}
+			}
 		}
 
         private void Start()
