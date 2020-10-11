e81f1e4872eb356ff3d7d2e7bbd5d65057c6f4ba
Rainer Koschke <koschke@uni-bremen.de>
2020-10-11 16:36:28 +0200

    Re-introduced the option to print the elapsed time immediately.
    Removed the explicit attribute elapsedTime. Its value can be
    computed if necessary.

--- Assets/SEE/Utils/Performance.cs
+++ Assets/SEE/Utils/Performance.cs
@@ -27,8 +27,6 @@ namespace SEE.Utils
 
         private readonly string action;
 
-        private string elapsedTime = "00:00:00:00";
-
         private double totalTimeInMilliSeconds = 0.0;
 
         /// <summary>
@@ -48,20 +46,34 @@ namespace SEE.Utils
         /// Emits the elapsed time from the start of the performance time span
         /// until now. Reports it to Debug.Log along with the action name.
         /// </summary>
-        public void End()
+        /// <param name="print">if true, the elapsed time will be printed</param>
+        public void End(bool print = false)
         {
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
             totalTimeInMilliSeconds = ts.TotalMilliseconds;
-            string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-            this.elapsedTime = elapsedTime;
+            if (print)
+            {
+                Debug.LogFormat("Action {0} finished in {1} [h:m:s:ms] elapsed time).\n", action, GetElapsedTime());
+            }
         }
 
+        /// <summary>
+        /// Returns the elapsed time between the calls of Begin(string) and End(bool)
+        /// in the format h:m:s:ms.
+        /// </summary>
+        /// <returns>elapsed time</returns>
         public string GetElapsedTime()
         {
-            return elapsedTime;
+            TimeSpan ts = TimeSpan.FromMilliseconds(totalTimeInMilliSeconds);
+            return string.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
         }
 
+        /// <summary>
+        /// Returns the elapsed time in milliseconds between the calls of Begin(string)
+        /// and End(bool).
+        /// </summary>
+        /// <returns>elapsed time in milliseconds</returns>
         public double GetTimeInMilliSeconds()
         {
             return totalTimeInMilliSeconds;
