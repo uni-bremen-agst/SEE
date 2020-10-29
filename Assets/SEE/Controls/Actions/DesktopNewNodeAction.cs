f3dfd667e64152d5d22cf2223a699f2b6b4ae86c
Moritz B <mblecker@uni-bremen.de>
2020-10-29 09:59:38 +0100

    Adding new class DesktopNewNodeAction for handling new Nodes in Runtime. Moving Code From DesktopPlayerMovement to new Class

---
+++ Assets/SEE/Controls/Actions/DesktopNewNodeAction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesktopNewNodeAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Test for insert a new node
+        if (Input.GetKeyDown(KeyCode.N && newNode == null){
+            //create new Node and let him stick to the 
+        }
+        if (newNode != null)
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                //Change node type to the next in the list
+            }
+            if (Input.GetMouseButton(LeftMouseButton))
+            {
+                //Place node and set newNode to Null
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.N && newNode != null){
+            //exit node adding
+            //remove node from cursor
+            newNode = null;
+        }
+    }
+}
