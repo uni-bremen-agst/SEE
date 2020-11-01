6b871aa19d26a320be19ac6578a5dc6cd667dd0d
Rainer Koschke <koschke@uni-bremen.de>
2020-11-01 12:11:47 +0100

    Introduced an event system for events where the interactable object is being hovered over or no longer being hovered over, respectively. The code showing the label above the game object has now be moved to a separate class of its own, which uses this new event system.

--- Assets/SEE/Controls/Interactables/InteractableObject.cs
+++ Assets/SEE/Controls/Interactables/InteractableObject.cs
@@ -1,11 +1,7 @@
 ﻿using System.Collections.Generic;
-using SEE.DataModel.DG;
-using SEE.Game;
 using SEE.GO;
 using SEE.Utils;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Assertions;
 using Valve.VR.InteractionSystem;
 
 namespace SEE.Controls
@@ -88,22 +84,6 @@ namespace SEE.Controls
         private Interactable interactable;
 
         /// <summary>
-        /// The text label that's displayed above the object when the user hovers over it.
-        /// Will be <code>null</code> when the label is not currently being displayed.
-        /// </summary>
-        private GameObject nodeLabel;
-
-        /// <summary>
-        /// Settings for the visualization of the node.
-        /// </summary>
-        private AbstractSEECity city;
-
-        /// <summary>
-        /// True if this node is a leaf. This value is cached to avoid frequent retrievals.
-        /// </summary>
-        private bool isLeaf;
-
-        /// <summary>
         /// The synchronizer is attached to <code>this.gameObject</code>, iff it is
         /// grabbed.
         /// </summary>
@@ -139,6 +119,20 @@ namespace SEE.Controls
         /// </summary>
         private readonly Color RemoteGrabColor = Utils.ColorPalette.Viridis(0.0f);
 
+        /// <summary>
+        /// A delegate to be called when a hovering events has happened (hover over
+        /// or hover off the game object).
+        /// </summary>
+        public delegate void HoverAction();
+        /// <summary>
+        /// Event to be triggered when this game object is being hovered over.
+        /// </summary>
+        public event HoverAction HoverIn;
+        /// <summary>
+        /// Event to be triggered when this game object is no longer hovered over.
+        /// </summary>
+        public event HoverAction HoverOut;
+
         private void Awake()
         {
             ID = nextID++;
@@ -149,11 +143,6 @@ namespace SEE.Controls
             {
                 Debug.LogErrorFormat("Game object {0} has no component Interactable attached to it.\n", gameObject.name);
             }
-
-            GameObject cityGo = SceneQueries.GetCodeCity(gameObject.transform)?.gameObject;
-            Assert.IsTrue(cityGo != null);
-            cityGo.TryGetComponent(out city);
-            isLeaf = SceneQueries.IsLeaf(gameObject);
         }
 
         /// <summary>
@@ -199,7 +188,7 @@ namespace SEE.Controls
                     }
                 }
                 else
-                {
+                {                    
                     if (hasOutline)
                     {
                         DestroyImmediate(outline);
@@ -209,12 +198,21 @@ namespace SEE.Controls
 
             if (hover)
             {
-                CreateObjectLabel();
+                //HoverIn?.Invoke();
+                if (HoverIn != null)
+                {
+                    Debug.LogFormat("Hover on {0}\n", name);
+                    HoverIn();
+                }
+                else
+                {
+                    Debug.LogFormat("HoverIn is null on {0}\n", name);
+                }
                 HoveredObjects.Add(this);
             }
             else
             {
-                DestroyObjectLabel();
+                HoverOut?.Invoke();
                 HoveredObjects.Remove(this);
             }
 
@@ -222,71 +220,7 @@ namespace SEE.Controls
             {
                 new Net.SetHoverAction(this, hover).Execute();
             }
-        }
-
-        /// <summary>
-        /// Returns true iff labels are enabled for this node type.
-        /// </summary>
-        /// <returns>true iff labels are enabled for this node type</returns>
-        private bool LabelsEnabled()
-        {
-            return isLeaf && city.ShowLabel || !isLeaf && city.InnerNodeShowLabel;
-        }
-
-        /// <summary>
-        /// Creates a text label above the object with its node's SourceName if the label doesn't exist yet.
-        /// </summary>
-        private void CreateObjectLabel()
-        {
-            if (!LabelsEnabled())
-            {
-                return;  // If labels are disabled, we don't need to do anything
-            }
-
-            // If label already exists, nothing needs to be done
-            if (nodeLabel != null || !gameObject.TryGetComponent(out NodeRef nodeRef))
-            {
-                return;
-            }
-
-            Node node = nodeRef.node;
-            if (node == null)
-            {
-                return;
-            }
-
-            // Add text
-            Vector3 position = gameObject.transform.position;
-            position.y += isLeaf ? city.LeafLabelDistance : city.InnerNodeLabelDistance;
-            nodeLabel = TextFactory.GetTextWithSize(node.SourceName, position,
-                isLeaf ? city.LeafLabelFontSize : city.InnerNodeLabelFontSize, textColor: Color.black);
-            nodeLabel.transform.SetParent(gameObject.transform);
-
-            // Add connecting line between "roof" of object and text
-            Vector3 labelPosition = nodeLabel.transform.position;
-            Vector3 nodeTopPosition = gameObject.transform.position;
-            nodeTopPosition.y = BoundingBox.GetRoof(new List<GameObject> { gameObject });
-            labelPosition.y -= nodeLabel.GetComponent<TextMeshPro>().textBounds.extents.y;
-            LineFactory.Draw(nodeLabel, new[] { nodeTopPosition, labelPosition }, 0.01f,
-                Materials.New(Materials.ShaderType.TransparentLine, Color.black.ColorWithAlpha(0.9f)));
-
-            Portal.SetInfinitePortal(nodeLabel);
-            // Animate text movement
-            //iTween.MoveFrom(nodeLabel, nodeTopPosition, 1f);
-        }
-
-        /// <summary>
-        /// Destroys the text label above the object if it exists.
-        /// </summary>
-        /// <seealso cref="CreateObjectLabel"/>
-        private void DestroyObjectLabel()
-        {
-            // If labels are disabled, we don't need to do anything
-            if (LabelsEnabled() && nodeLabel != null)
-            {
-                Destroyer.DestroyGameObject(nodeLabel);
-            }
-        }
+        }        
 
         /// <summary>
         /// Visually emphasizes this object for selection.
