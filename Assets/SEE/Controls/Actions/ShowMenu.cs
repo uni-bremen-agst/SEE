ab9368d760ed0dda9ce1694ad243d764769495b1
Rainer Koschke <koschke@uni-bremen.de>
2020-11-06 17:04:46 +0100

    #118 Working toward nested menu entries. Work in progress.

--- Assets/SEE/Controls/Actions/ShowMenu.cs
+++ Assets/SEE/Controls/Actions/ShowMenu.cs
@@ -1,37 +1,45 @@
-﻿using SEE.Game;
-using SEE.GO;
-using SEE.Utils;
+﻿using System;
 using UnityEngine;
 
 namespace SEE.Controls.Actions
 {
+    /// <summary>
+    /// Provides an in-game menu.
+    /// 
+    /// NOTE: This class is currently under construction and not yet ready.
+    /// </summary>
     public class ShowMenu : MonoBehaviour
     {
-
         /// <summary>
         /// The game object representing the menu.
         /// </summary>
         private GameObject menu;
 
         /// <summary>
-        /// The factory to create the cube used as a canvas.
-        /// </summary>
-        private static CubeFactory cubeFactory;
-
-        /// <summary>
         /// The main camera the menu should be facting to.
         /// </summary>
-        private static Camera camera;
+        private static Camera mainCamera;
 
-        private static Vector3 menuScale = new Vector3(0.25f, 0.25f, 0.001f);
+        /// <summary>
+        /// Radius of the menu.
+        /// </summary>
+        [Tooltip("The radius of the circular menu.")]
+        [Range(0, 2)]
+        public float Radius = 0.3f;
 
-        private const float distanceBetweenObjectAndCamera = 1.0f;
+        /// <summary>
+        /// Radius of the menu.
+        /// </summary>
+        [Tooltip("The depth of the circular menu (z axis).")]
+        [Range(0, 0.1f)]
+        public float Depth = 0.01f;
 
-        private static CubeFactory CreateCubeFactory()
-        {
-            ColorRange colorRange = new ColorRange(Color.gray, Color.gray, 1);            
-            return new CubeFactory(Materials.ShaderType.Transparent, colorRange);
-        }
+        /// <summary>
+        /// The distance between the menu and the camera when the menu is spawned.
+        /// </summary>
+        [Tooltip("The distance between the menu and the camera when the menu is spawned.")]
+        [Range(0, 10)]
+        public float CameraDistance = 1.0f;
 
         /// <summary>
         /// If true (and only if), the menu is visible.
@@ -39,15 +47,12 @@ namespace SEE.Controls.Actions
         private bool menuIsOn = false;
 
         /// <summary>
-        /// Creates the <see cref="cubeFactory"/> if it does not exist yet.
+        /// Creates the <see cref="menu"/> if it does not exist yet.
+        /// Sets <see cref="mainCamera"/>.
         /// </summary>
         protected virtual void Start()
         {
-            if (cubeFactory == null)
-            {
-                cubeFactory = CreateCubeFactory();
-            }
-            if (camera == null)
+            if (mainCamera == null)
             {
                 if (Camera.allCameras.Length > 1)
                 {
@@ -58,21 +63,23 @@ namespace SEE.Controls.Actions
                         Debug.LogFormat("Camera: {0}\n", c.name);
                     }
                 }
-                camera = Camera.main;
+                mainCamera = Camera.main;
             }
             if (menu == null)
             {
-                menu = CreateMenu(menuScale);
+                menu = CreateMenu(entries, Radius, Depth);
                 Off();
             }
         }
+
+        public int entries = 2;
 
         /// <summary>
         /// Shows the menu.
         /// </summary>
         protected virtual void On()
         {
-            menu.transform.position = MenuCenterPosition(menuScale);
+            menu.transform.position = MenuCenterPosition();
             menu.GetComponent<Renderer>().enabled = true; // make it visible
         }
 
@@ -84,87 +91,79 @@ namespace SEE.Controls.Actions
             menu.GetComponent<Renderer>().enabled = false; // make it invisible
         }
 
-        private GameObject CreateMenu(Vector3 menuScale)
+        /// <summary>
+        /// Creates the circular menu.
+        /// </summary>
+        /// <param name="radius">the radius the circular menu should have</param>
+        /// <returns>a new circular menu with given <paramref name="radius"/></returns>
+        private static GameObject CreateMenu(int entries, float radius, float depth)
         {
-            // This menu may refer to either an inner node or a leaf.
-            // We want to draw the menu in front of all other nodes. Nodes will
-            // be put into the render queue according to their graph level. 
-            // Leaves will have the maximal graph level. The higher the offset
-            // in the render queue, the later the object will be drawn. Later
-            // drawn objects cover objects drawn earlier. By putting the menu
-            // after the highest possible node nesting level, we make sure the
-            // menu is drawn in front of all nodes.
-            //GameObject menu = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            GameObject menu = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            //GameObject menu = cubeFactory.NewBlock(0,100); // , node.ItsGraph.MaxDepth + 1
-            menu.name = "Mouse menu";
-            // cubeFactory will tag the object as Node; we want it be untagged
-            menu.tag = "Untagged";
-            // add the menu to the UI layer
-            menu.layer = LayerMask.NameToLayer("UI");        
-
-            if (false)
-            { // FIXME: Remove the collider for the time being.
-                if (menu.TryGetComponent<Collider>(out Collider collider))
-                {
-                    Destroy(collider);
-                }
-            }            
-           
-            menu.transform.localScale = menuScale;
+            GameObject menu = NewCircle(radius, depth);
             menu.transform.position = Vector3.zero; // the real position will be set when it becomes visible
-            //menu.transform.SetParent(gameObject.transform);
-            //Portal.SetInfinitePortal(menu);
+            menu.name = "Mouse menu";
+
+            AddInnerCircles(menu, radius, depth, entries);
             return menu;
         }
 
-        private Vector3 MenuCenterPosition(Vector3 menuScale)
+        private static void AddInnerCircles(GameObject menu, float radius, float depth, int entries)
         {
-            // Viewport space is normalized and relative to the camera. The bottom-left 
-            // of the camera is (0,0); the top-right is (1,1). 
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition.z = Mathf.Max(camera.nearClipPlane, distanceBetweenObjectAndCamera);
-            Vector3 r = camera.ScreenToWorldPoint(mousePosition);
-            Debug.LogFormat("mouse position {0}, world position {1}\n", mousePosition, r);
-            return r;
+            if (entries < 1 || entries > circles.Length)
+            {
+                throw new Exception("Unsupported number of inner circles: " + entries);
+            } 
+            else if (entries > 1)
+            {
+                InnerCircles selectedInnerCircles = circles[entries - 1];
+                float innerRadius = selectedInnerCircles.radius;
 
+                int i = 1;
+                foreach (Vector2 center in selectedInnerCircles.centers)
+                {
+                    GameObject inner = NewCircle(radius * innerRadius, depth);
+                    inner.name = "menu entry " + i;
+                    i++;
+                    inner.transform.SetParent(menu.transform);
+                    inner.transform.localPosition = new Vector3(center.x / 2.0f, center.y / 2.0f, 2.0f);           
+                }
+            }
+        }
 
-            Vector3 viewport = camera.ScreenToViewportPoint(mousePosition);
-            Debug.LogFormat("mouse position {0}, viewport position {1}\n", mousePosition, viewport);
-            viewport.x = Mathf.Clamp(viewport.x, 0, 1);
-            viewport.y = Mathf.Clamp(viewport.y, 0, 1);
-            // The z value is the distance from the camera in world units.
-            //viewport.z = distanceBetweenObjectAndCamera;
-            Vector3 result = camera.ViewportToWorldPoint(viewport);
-            Debug.LogFormat("adjusted viewport position {0}, result position {1}\n", viewport, result);
+        private static GameObject NewCircle(float radius, float depth)
+        {
+            GameObject menu = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            
+            menu.tag = "Untagged";
+            // add the circle to the UI layer so that it does not collide with other game objects
+            menu.layer = LayerMask.NameToLayer("UI");
+            if (menu.TryGetComponent<Renderer>(out Renderer renderer))
+            {
+                renderer.lightProbeUsage = UnityEngine.Rendering.LightProbeUsage.Off;
+                renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+                renderer.receiveShadows = false;
+            }
+            menu.transform.localScale = new Vector3(2 * radius, 2 * radius, depth);
+            return menu;
+        }
 
-            // The menu position is interpreted as the left lower corner. A transform.position,
-            // however, identifies the center of an object in Unity, hence, we need to shift
-            // the menu position accordingly to make it the center.
-            // FIXME
-            //result.x += menuScale.x / 2.0f;
-            //result.y += menuScale.y / 2.0f;
-            return result;
-
-            //Vector3 result = mousePosition;
-            //result.z = distanceBetweenObjectAndCamera;
-            //result = camera.ScreenToWorldPoint(result);
-
-            //Debug.LogFormat("2D mouse {0}, viewport mouse {1} => 3D mouse {2}\n", mousePosition, viewport, result);
-            //return result;
-
-            //Vector3 result;
-            //if (PlayerSettings.GetInputType() == PlayerSettings.PlayerInputType.Desktop
-            //    && Raycasting.RaycastNodes(out RaycastHit raycastHit))
-            //{
-            //    result = raycastHit.point;
-            //}
-            //else
-            //{
-            //    // FIXME: What to do in VR?
-            //    result = gameObject.transform.position;
-            //}
-            //return result;
+        /// <summary>
+        /// Returns the center position in world space where the menu 
+        /// should be located when it is spawned.
+        /// </summary>
+        /// <returns>center position of menu in world space</returns>
+        private Vector3 MenuCenterPosition()
+        {
+            if (PlayerSettings.GetInputType() == PlayerSettings.PlayerInputType.Desktop)
+            {
+                Vector3 mousePosition = Input.mousePosition;
+                mousePosition.z = Mathf.Max(mainCamera.nearClipPlane, CameraDistance);
+                return mainCamera.ScreenToWorldPoint(mousePosition);
+            }
+            else
+            {
+                // FIXME
+                throw new NotImplementedException("ShowMenu.MenuCenterPosition() implemented only for desktop environment.");
+            }
         }
 
         private void Update()
@@ -182,7 +181,8 @@ namespace SEE.Controls.Actions
                     On();
                 }
                 // Menu should be facing the camera
-                menu.transform.LookAt(camera.transform);
+                menu.transform.LookAt(mainCamera.transform);
+                int hitEntry = SelectedMenuEntry();
             }
             else
             {
@@ -193,28 +193,62 @@ namespace SEE.Controls.Actions
             }
         }
 
-        private void OnMouseEnter()
+        private int SelectedMenuEntry()
         {
-            if (PlayerSettings.GetInputType() == PlayerSettings.PlayerInputType.Desktop && !Raycasting.IsMouseOverGUI())
+            int result = -1;
+            if (Input.GetMouseButton(0))
             {
-                //Debug.LogFormat("mouse enters {0}\n", name);
+                Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    if (hit.collider.gameObject == menu)
+                    {
+                        Debug.LogFormat("hit {0}\n", hit.collider.gameObject.name);
+                    }
+                    else
+                    {
+                        Debug.LogFormat("hit other object {0}\n", hit.collider.gameObject.name);
+                    }
+                    
+                    Vector2 hitPoint = hit.point; // z axis will be ignored
+                    // normalize hitPoint to a circle with radius 1 (diameter 2)
+                    // where the circle's origin has 
+                }               
+            }
+            return result;
+        }
+
+        struct InnerCircles
+        {
+            public float radius;   
+            public Vector2[] centers;
+            public InnerCircles(float radius, Vector2[] centers)
+            {
+                this.radius = radius;
+                this.centers = centers;
             }
         }
 
-        private void OnMouseOver()
+        /// <summary>
+        /// A mapping of the number of equally sized inner circles to be enclosed
+        /// in an outer circle with radius 1 onto the radius and co-ordinates of
+        /// those inner circles.
+        /// 
+        /// The data were retrieved from http://hydra.nat.uni-magdeburg.de/packing/cci/cci.html.
+        /// </summary>
+        private static readonly InnerCircles[] circles =
         {
-            if (PlayerSettings.GetInputType() == PlayerSettings.PlayerInputType.Desktop)
-            {
-                //Debug.LogFormat("mouse over {0}\n", name);
-            }
-        }
+            /* 1 */ new InnerCircles(1.0f, new Vector2[]{ Vector2.zero}),
+            /* 2 */ new InnerCircles(0.5f, 
+                                     new Vector2[]{ 
+                                         new Vector2(-0.5f, 0), 
+                                         new Vector2(0.5f, 0)}),
+            /* 3 */ new InnerCircles(0.46410161518f, 
+                                     new Vector2[]{ 
+                                         new Vector2(-0.464101615137754587054892683011f, -0.267949192431122706472553658494f), 
+                                         new Vector2( 0.464101615137754587054892683011f, -0.267949192431122706472553658494f), 
+                                         new Vector2( 0.000000000000000000000000000000f,  0.535898384862245412945107316988f)}),
+        };
 
-        private void OnMouseExit()
-        {
-            if (PlayerSettings.GetInputType() == PlayerSettings.PlayerInputType.Desktop && !Raycasting.IsMouseOverGUI())
-            {
-                //Debug.LogFormat("mouse exits {0}\n", name);
-            }
-        }
     }
 }
