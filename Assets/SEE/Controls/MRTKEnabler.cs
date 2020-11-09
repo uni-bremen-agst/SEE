af01b6a7821f1da2c9d6c897bd163889938a0f70
Falko Galperin <falko1@uni-bremen.de>
2020-11-09 22:16:03 +0100

    Add SeeConfigurationProfile

---
+++ Assets/SEE/Controls/MRTKEnabler.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using UnityEngine;
+
+public class MRTKEnabler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO: MixedReality namespace seems to be missing
+       MixedRealityToolkit.Instance.enabled = false;
+    }
+}
