eecb0eaa0c9827136404081b37b03b50ade28f7a
Lino-3rdDimenzions <71646593+Lino-3rdDimenzions@users.noreply.github.com>
2020-11-23 12:19:27 +0100

    #138 method to analyze difference between nodetypes and debug log to inform the user in case of a change

--- Assets/SEE/DataModel/DG/IO/CityRestorer.cs
+++ Assets/SEE/DataModel/DG/IO/CityRestorer.cs
@@ -46,13 +46,19 @@ public class CityRestorer
     /// <param name="importPath"> The given json-file-path </param>
     /// <param name="city"> The city which is to be overwritten </param>
     public static void RestoreCity(string importPath, AbstractSEECity city)
-    { 
+    {
+        // We have to store the current enumeration of the nodetypes of the current version in order to compare 
+        // it afterwards with the stored one.
+        List<string> newNodeTypes = city.SelectedNodeTypes.Keys.ToList();
+
         // as the user picks the directory via a directory picker/ the GUI , no specific error handling is needed at this point.
         string jsonString = File.ReadAllText(importPath);
         if(VerifyCityType(importPath, city))
         {
             JsonUtility.FromJsonOverwrite(jsonString, city);
         }
+        Dictionary<string, bool> oldNodetypes = city.SelectedNodeTypes; 
+        differentNodeTypes(oldNodetypes, jsonString, newNodeTypes);
     }
 
     /// <summary>
@@ -80,4 +86,84 @@ public class CityRestorer
             return false;
         }
     }
+
+    /// <summary>
+    /// Analyazes if there is a difference between the stored nodetypes and the current nodetypes.
+    /// </summary>
+    /// <param name="jsonFile">the .json-file with the settings for the city</param>
+    /// <param name="oldNodeTypes>a dictionary of the stored nodeTypes</param>
+    /// <param name="newNodes> the city, which has to be overwritten</param>
+    /// <returns> nothing, except a DebugLog to inform the user in case of any changes regarding the nodetypes.
+    public static void differentNodeTypes(Dictionary<string, bool> oldNodeTypes, string jsonFile, List<string> newNodes)
+    { 
+        List<string> oldNodes = oldNodeTypes.Keys.ToList(); 
+        List<string> finalResults = new List<string>();
+        string difference = "";
+  
+            // In order to build the "real" difference between the stored parameters and the current one , we have to remove the duplicates.
+            // and concatenate the result afterwards in the list finalResults.
+            // The result will show if either there are new nodetypes in the latest version of the specific city or 
+            // if there are nodetypes stored which are not in the current version of the Evolutioncity anymore.
+        if (newNodes.Count() < oldNodes.Count())
+        {
+            for (int i = 0; i < oldNodeTypes.Count(); i++)
+            {
+                if (newNodes.Contains(oldNodes[i]))
+                {
+                    oldNodes.Remove(oldNodes[i]);
+                    newNodes.Remove(oldNodes[i]);
+                    i--;
+                }
+            }
+        }
+        else if ((newNodes.Count()) >= (oldNodes.Count()))
+        {
+            for (int j = 0; j < newNodes.Count(); j++)
+            {
+                if (oldNodes.Contains(newNodes[j]))
+                {
+                    oldNodes.Remove(newNodes[j]);
+                    newNodes.Remove(newNodes[j]);
+                    j--;
+                }
+            }
+        }
+        finalResults = newNodes.Concat(oldNodes).ToList(); 
+
+            // Depending on the actual amount of difference, three cases are possible:
+            // First case = There are no differenct objects, thus we can break instantly
+            // Second case = There is only one object, so it is possible to print out the result instantly and break.
+            // Third case = There are more then two objects which have changed and we have to enumerate them and consequently cut the string for the 
+            // printing-log to get a neat output for the user.
+
+            // First case
+            if (finalResults.Count == 0)
+            {
+                UnityEngine.Debug.Log("There are no new nodytpes added since you saved your profile");
+                return;
+                // Second case 
+            }
+            else if (finalResults.Count == 1)
+            {   if(oldNodes.Count==1)
+            {
+                UnityEngine.Debug.Log("Since you saved your profile the following Nodetype was deleted in the meantime :\n" + finalResults.First());
+                return;
+            }   if(newNodes.Count == 1 )
+            {
+                UnityEngine.Debug.Log("Since you saved your profile the following Nodetype was added in the meantime :\n" + finalResults.First());
+                return;
+            }
+                
+            } // Third case
+            else
+            {
+                foreach (string str in finalResults)
+                {
+                    difference += str + ",";
+                }
+            }
+            difference = difference.Substring(0, difference.Count());
+            UnityEngine.Debug.Log("Since you saved your profile and today the following Nodetypes have changed :\n" + difference);
+        }
+    
 }
