3d0225537c4975a571b07a14f396204b35c08634
Rainer Koschke <koschke@uni-bremen.de>
2020-12-02 16:40:10 +0100

    #122 Introduced explicit type Difference to better describe the possible types of changes. The creation and handling of the power beams has been moved to Marker.

--- Assets/SEE/Game/Evolution/Animators/MoveScaleShakeAnimator.cs
+++ Assets/SEE/Game/Evolution/Animators/MoveScaleShakeAnimator.cs
@@ -19,7 +19,6 @@
 
 using SEE.Layout;
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace SEE.Game.Evolution
@@ -31,7 +30,7 @@ namespace SEE.Game.Evolution
     public class MoveScaleShakeAnimator : AbstractAnimator
     {
         /// <summary>
-        /// Moves, scales, and then finally shakes (if <paramref name="wasModified"/>) the animated game object.
+        /// Moves, scales, and then finally shakes (if <paramref name="difference"/>) the animated game object.
         /// At the end of the animation, the method <paramref name="callbackName"/> will be called for the
         /// game object <paramref name="callBackTarget"/> with <paramref name="gameObject"/> as 
         /// parameter if <paramref name="callBackTarget"/> is not null. If <paramref name="callBackTarget"/>
@@ -39,13 +38,13 @@ namespace SEE.Game.Evolution
         /// </summary>
         /// <param name="gameObject">game object to be animated</param>
         /// <param name="layout">the node transformation to be applied</param>
-        /// <param name="wasModified">whether the node attached to <paramref name="gameObject"/> was modified w.r.t. to the previous graph</param>
+        /// <param name="difference">whether the node attached to <paramref name="gameObject"/> was modified w.r.t. to the previous graph</param>
         /// <param name="callBackTarget">an optional game object that should receive the callback</param>
         /// <param name="callbackName">the method name of the callback</param>
         protected override void AnimateToInternalWithCallback
                   (GameObject gameObject,
                    ILayoutNode layout,
-                   bool wasModified,
+                   Difference difference,
                    GameObject callBackTarget,
                    string callbackName,
                    Action<object> callback)
@@ -61,11 +60,6 @@ namespace SEE.Game.Evolution
             Vector3 localScale = gameObject.transform.parent == null ?
                                      layout.LocalScale
                                    : gameObject.transform.parent.InverseTransformVector(layout.LocalScale);
-
-            //Debug.LogFormat("animating {0} from pos={1} scale={2} to pos={3} scale={4}\n",
-            //    gameObject.name,
-            //    gameObject.transform.position, gameObject.transform.localScale,
-            //    position, localScale);
 
             if (gameObject.transform.localScale != localScale)
             {
@@ -111,11 +105,8 @@ namespace SEE.Game.Evolution
             }
 
             // Shake the object if it was modified.
-            if (wasModified)
+            if (difference == Difference.Changed)
             {
-                // Create a new power beam
-                BeamAnimator.GetInstance().CreatePowerBeam(position, AdditionalBeamDetails.changedBeamColor, AdditionalBeamDetails.powerBeamDimensions);
-
                 if (mustCallBack)
                 {
                     iTween.ShakeRotation(gameObject, iTween.Hash(
@@ -140,173 +131,6 @@ namespace SEE.Game.Evolution
             if (mustCallBack)
             {
                 callback?.Invoke(gameObject);
-            }
-        }
-
-        /// <summary>
-        /// Removes power beams
-        /// </summary>
-        public static void DeletePowerBeams()
-        {
-            BeamAnimator.GetInstance().ClearPowerBeams();
-        }
-
-        /// <summary>
-        /// Singleton that stores all newly created / deleted power beams
-        /// </summary>
-        public class BeamAnimator
-        {
-
-            /// <summary>
-            /// Adds power beam above gameObject that has been changed
-            /// <param name="position">Position of the parent gameObject</param>
-            /// <param name="beamColor">Color of the power beam to create</param>
-            /// </summary>
-            public void CreatePowerBeam(Vector3 position, Color beamColor, Vector3 NodeBeamDimensions)
-            {
-                // Generate power beam above updated objects
-                GameObject powerBeam = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                GameObject.DestroyImmediate(powerBeam.GetComponent<Collider>());
-                powerBeam.tag = DataModel.Tags.PowerBeam;
-                Renderer powerBeamRenderer = powerBeam.GetComponent<Renderer>();
-                powerBeam.transform.localScale = new Vector3(NodeBeamDimensions.x, 0, NodeBeamDimensions.z);
-                BeamAnimator.GetInstance().BeamHeight = NodeBeamDimensions.y;
-                powerBeam.transform.position = position;
-                // Change power beam material color
-                powerBeamRenderer.material.color = beamColor;
-                // Set power beam material to emissive
-                powerBeamRenderer.material.EnableKeyword("_EMISSION");
-                Color emissionColor = beamColor * 7f;
-                powerBeamRenderer.material.SetColor("_EmissionColor", emissionColor);
-                // Remove power beam shadow
-                powerBeamRenderer.receiveShadows = false;
-                powerBeamRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-                BeamAnimator.GetInstance().AddPowerBeam(powerBeam);
-                powerBeam.AddComponent<BeamAnimatorExecuter>();
-            }
-
-            /// <summary>
-            /// Singleton instance
-            /// </summary>
-            private static BeamAnimator Instance = null;
-
-            /// <summary>
-            /// Get the singleton instance
-            /// </summary>
-            public static BeamAnimator GetInstance()
-            {
-                if (Instance == null)
-                {
-                    Instance = new BeamAnimator();
-                }
-                return Instance;
-            }
-
-            /// <summary>
-            /// Power beam height getter/setter
-            /// </summary>
-            public float BeamHeight
-            {
-                get; set;
-            }
-
-            /// <summary>
-            /// Power beams, get animated while appearing
-            /// </summary>
-            private List<GameObject> powerBeams = new List<GameObject>();
-
-            /// <summary>
-            /// Power beams that have been removed, get animated while disappearing
-            /// </summary>
-            private GameObject[] removedBeams = new GameObject[0];
-
-            /// <summary>
-            /// Deleted power beams, updated whenever Update is called
-            /// </summary>
-            private List<GameObject> deletedBeams = new List<GameObject>();
-
-            /// <summary>
-            /// Beam appearing magic constant
-            /// </summary>
-            private const float appearingMagicNumber = 0.0025f;
-
-            /// <summary>
-            /// Beam disappearing magic number
-            /// </summary>
-            private const float disappearingMagicNumber = 0.0005f;
-
-            /// <summary>
-            /// Animation update function
-            /// </summary>
-            public void Update()
-            {
-                deletedBeams.Clear();
-
-                if (BeamHeight <= 0)
-                {
-                    BeamHeight = 3f;
-                }
-                // Animate new power beams
-                foreach (GameObject beam in powerBeams)
-                {
-                    if (beam.transform.localScale.y < BeamHeight)
-                    {
-                        beam.transform.localScale = new Vector3(beam.transform.localScale.x, beam.transform.localScale.y + appearingMagicNumber, beam.transform.localScale.z);
-                        beam.transform.position = new Vector3(beam.transform.position.x, beam.transform.position.y + appearingMagicNumber, beam.transform.position.z);
-                    }
-                    else
-                    {
-                        deletedBeams.Add(beam);
-                    }
-                }
-                foreach (GameObject beam in deletedBeams) {
-                    powerBeams.Remove(beam);
-                }
-                // Animate deleted power beams
-                foreach (GameObject deleted in removedBeams)
-                {
-                    if (deleted != null)
-                    {
-                        if (deleted.transform.localScale.y > 0f)
-                        {
-                            deleted.transform.localScale = new Vector3(deleted.transform.localScale.x, deleted.transform.localScale.y - disappearingMagicNumber, deleted.transform.localScale.z);
-                            deleted.transform.position = new Vector3(deleted.transform.position.x, deleted.transform.position.y - disappearingMagicNumber, deleted.transform.position.z);
-                        }
-                        else
-                        {
-                            GameObject.Destroy(deleted);
-                        }
-                    }
-                }
-            }
-
-            /// <summary>
-            /// Clear power beams
-            /// </summary>
-            public void ClearPowerBeams()
-            {
-                removedBeams = GameObject.FindGameObjectsWithTag(DataModel.Tags.PowerBeam);
-                powerBeams.Clear();
-            }
-
-            /// <summary>
-            /// Add a new power beam
-            /// <param name="beam">Power beam to add</param>
-            /// </summary>
-            public void AddPowerBeam(GameObject beam)
-            {
-                powerBeams.Add(beam);
-            }
-        }
-
-        /// <summary>
-        /// Attached to power beam GameObjects to call update from BeamAnimator class
-        /// </summary>
-        class BeamAnimatorExecuter : MonoBehaviour
-        {
-            private void Update()
-            {
-                BeamAnimator.GetInstance().Update();
             }
         }
     }
