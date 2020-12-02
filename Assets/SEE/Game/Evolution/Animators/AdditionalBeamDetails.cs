772b80aed021dda0fe4c34987a2e88928887af38
Rainer Koschke <koschke@uni-bremen.de>
2020-12-02 16:35:18 +0100

    #122 Moved these attributes to SEECityEvolution.

--- Assets/SEE/Game/Evolution/Animators/AdditionalBeamDetails.cs
+++ Assets/SEE/Game/Evolution/Animators/AdditionalBeamDetails.cs
@@ -8,73 +8,73 @@ namespace SEE.Game.Evolution
     public class AdditionalBeamDetails : MonoBehaviour
     {
 
-        /// <summary>
-        /// Color given to power beams of newly added nodes, inherits from inspector (default grey)
-        /// </summary>
-        public static Color newBeamColor;
+        ///// <summary>
+        ///// Color given to power beams of newly added nodes, inherits from inspector (default grey)
+        ///// </summary>
+        //public static Color newBeamColor;
 
-        /// <summary>
-        /// Color for power beams of newly added nodes, can be set in inspector
-        /// </summary>
-        [Tooltip("Changes the color the beam of newly created nodes")]
-        public Color InspectorNewBeamColor;
+        ///// <summary>
+        ///// Color for power beams of newly added nodes, can be set in inspector
+        ///// </summary>
+        //[Tooltip("Changes the color the beam of newly created nodes")]
+        //public Color InspectorNewBeamColor;
 
-        /// <summary>
-        /// Changed nodes beam color, inherits from inspector variable
-        /// </summary>
-        public static Color changedBeamColor;
+        ///// <summary>
+        ///// Changed nodes beam color, inherits from inspector variable
+        ///// </summary>
+        //public static Color changedBeamColor;
 
-        /// <summary>
-        /// Changed nodes beam color to be pickable in inspector
-        /// </summary>
-        [Tooltip("Changes the color of the beam of changed nodes")]
-        public Color InspectorChangedBeamColor;
+        ///// <summary>
+        ///// Changed nodes beam color to be pickable in inspector
+        ///// </summary>
+        //[Tooltip("Changes the color of the beam of changed nodes")]
+        //public Color InspectorChangedBeamColor;
 
-        /// <summary>
-        /// Deleted nodes beam color, inherits from inspector variable
-        /// </summary>
-        public static Color deletedBeamColor;
+        ///// <summary>
+        ///// Deleted nodes beam color, inherits from inspector variable
+        ///// </summary>
+        //public static Color deletedBeamColor;
 
-        /// <summary>
-        /// Deleted nodes beam color to be pickable in inspector
-        /// </summary>
-        [Tooltip("Sets the color the beam of deleted nodes will have")]
-        public Color InspectorDeletedBeamColor;
+        ///// <summary>
+        ///// Deleted nodes beam color to be pickable in inspector
+        ///// </summary>
+        //[Tooltip("Sets the color the beam of deleted nodes will have")]
+        //public Color InspectorDeletedBeamColor;
 
-        /// <summary>
-        /// Dimensions of power beams, inherits from inspector variable
-        /// </summary>
-        public static Vector3 powerBeamDimensions;
+        ///// <summary>
+        ///// Dimensions of power beams, inherits from inspector variable
+        ///// </summary>
+        //public static Vector3 powerBeamDimensions;
 
-        /// <summary>
-        /// Dimensions of power beams to be shown in inspector
-        /// </summary>
-        [Tooltip("Sets the width/height of power beams")]
-        public Vector3 InspectorPowerBeamDimensions;
+        ///// <summary>
+        ///// Dimensions of power beams to be shown in inspector
+        ///// </summary>
+        //[Tooltip("Sets the width/height of power beams")]
+        //public Vector3 InspectorPowerBeamDimensions;
 
-        /// <summary>
-        /// The minimal scale a power beam should have.
-        /// </summary>
-        private static readonly Vector3 minimalBowerBeamScale = new Vector3(0.02f, 3f, 0.02f);
+        ///// <summary>
+        ///// The minimal scale a power beam should have.
+        ///// </summary>
+        //private static readonly Vector3 minimalBowerBeamScale = new Vector3(0.02f, 3f, 0.02f);
 
-        void Awake()
-        {
-            newBeamColor = InspectorNewBeamColor;
-            changedBeamColor = InspectorChangedBeamColor;
-            deletedBeamColor = InspectorDeletedBeamColor;
-            powerBeamDimensions = InspectorPowerBeamDimensions;            
-            if (powerBeamDimensions.x <= minimalBowerBeamScale.x)
-            {
-                powerBeamDimensions.x = minimalBowerBeamScale.x;
-            }
-            if (powerBeamDimensions.y <= 0)
-            {
-                powerBeamDimensions.y = minimalBowerBeamScale.y;
-            }
-            if (powerBeamDimensions.z <= minimalBowerBeamScale.z)
-            {
-                powerBeamDimensions.z = minimalBowerBeamScale.z;
-            }
-        }
+        //void Awake()
+        //{
+        //    newBeamColor = InspectorNewBeamColor;
+        //    changedBeamColor = InspectorChangedBeamColor;
+        //    deletedBeamColor = InspectorDeletedBeamColor;
+        //    powerBeamDimensions = InspectorPowerBeamDimensions;            
+        //    if (powerBeamDimensions.x <= minimalBowerBeamScale.x)
+        //    {
+        //        powerBeamDimensions.x = minimalBowerBeamScale.x;
+        //    }
+        //    if (powerBeamDimensions.y <= 0)
+        //    {
+        //        powerBeamDimensions.y = minimalBowerBeamScale.y;
+        //    }
+        //    if (powerBeamDimensions.z <= minimalBowerBeamScale.z)
+        //    {
+        //        powerBeamDimensions.z = minimalBowerBeamScale.z;
+        //    }
+        //}
     }
 }
