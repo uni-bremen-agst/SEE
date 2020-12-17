4f43312a60c9163ebfc07445fb303ae58b9ee4d5
Rainer Koschke <koschke@uni-bremen.de>
2020-12-17 10:38:53 +0100

    #160 Random generator for strings.

---
+++ Assets/SEE/Utils/RandomStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace SEE.Utils
+{
+    /// <summary>
+    /// A random generator for strings.
+    /// </summary>
+    internal class RandomStrings
+    {
+        private static Random random = new Random();
+
+        /// <summary>
+        /// Returns a random string with given <paramref name="length"/>.
+        /// </summary>
+        /// <param name="length">requested length of the random string</param>
+        /// <returns>random string</returns>
+        public static string Get(int length = 20)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
