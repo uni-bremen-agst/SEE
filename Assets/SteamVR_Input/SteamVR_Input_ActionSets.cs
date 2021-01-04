3a97bca3da72f544bc3e4afb3a7b27b2cd44a3cc
Rainer Koschke <koschke@uni-bremen.de>
2021-01-04 13:27:08 +0100

    Newly generated SteamVR action bindings.

--- Assets/SteamVR_Input/SteamVR_Input_ActionSets.cs
+++ Assets/SteamVR_Input/SteamVR_Input_ActionSets.cs
@@ -19,6 +19,8 @@ namespace Valve.VR
         
         private static SteamVR_Input_ActionSet_default p__default;
         
+        private static SteamVR_Input_ActionSet_NewSet p_NewSet;
+        
         public static SteamVR_Input_ActionSet_default _default
         {
             get
@@ -27,11 +29,21 @@ namespace Valve.VR
             }
         }
         
+        public static SteamVR_Input_ActionSet_NewSet NewSet
+        {
+            get
+            {
+                return SteamVR_Actions.p_NewSet.GetCopy<SteamVR_Input_ActionSet_NewSet>();
+            }
+        }
+        
         private static void StartPreInitActionSets()
         {
             SteamVR_Actions.p__default = ((SteamVR_Input_ActionSet_default)(SteamVR_ActionSet.Create<SteamVR_Input_ActionSet_default>("/actions/default")));
+            SteamVR_Actions.p_NewSet = ((SteamVR_Input_ActionSet_NewSet)(SteamVR_ActionSet.Create<SteamVR_Input_ActionSet_NewSet>("/actions/NewSet")));
             Valve.VR.SteamVR_Input.actionSets = new Valve.VR.SteamVR_ActionSet[] {
-                    SteamVR_Actions._default};
+                    SteamVR_Actions._default,
+                    SteamVR_Actions.NewSet};
         }
     }
 }
