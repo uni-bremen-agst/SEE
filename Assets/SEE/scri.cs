c1b7a11f95fa66947d99ab4ba4284fc768faba3b
ThoreFrenzel <tfrenzel@uni-bremen.de>
2021-01-09 01:01:15 +0100

    #115 canvas is instantiated and removed while adding node

---
+++ Assets/SEE/scri.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scri : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject testgameObject2;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void InstantiateGameObject()
+    {
+          //  Instantiate(testgameObject2);
+            Debug.Log("testgameObject2 sollte sichtbar werden");
+        gameObject.AddComponent<scri2>();
+        
+
+
+    }
+
+    public void DestroyGameObject()
+    {
+        Debug.Log("testgameObject2 sollte unsichtbar werden");
+        scri2 scri22 = (scri2)gameObject.GetComponent("scri2");
+        scri22.DestroyAllChilds();
+        Destroy(gameObject.GetComponent("scri2"));
+
+    }
+
+    private static void SetVisible(GameObject gameObject, bool isVisible)
+    {
+        gameObject.SetActive(isVisible);
+
+    }
+}
+
+
+
---
+++ Assets/SEE/scri2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scri2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject testgameObject2;
+    void Start()
+    {
+        testgameObject2 = Instantiate(Resources.Load("TestPrefab", typeof(GameObject))) as GameObject;
+        Debug.Log("Start-Methode ist gerunnt");
+        Debug.Log(testgameObject2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InstantiateGameObject()
+    {
+        Instantiate(testgameObject2);
+        Debug.Log("testgameObject2 existiert noch nicht");
+
+
+
+    }
+
+    public void DestroyAllChilds()
+    {
+        foreach (Transform child in testgameObject2.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+    public void DestroyGameObject()
+    {
+        Destroy(transform.gameObject.GetComponentInParent<Canvas>().gameObject);
+    }
+}
