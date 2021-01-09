8a8c1da4310edcef8ed092787728290c438b9547
Rainer Koschke <koschke@uni-bremen.de>
2021-01-09 13:41:31 +0100

    #138 I/O code is moved from AbstractSEECity to AbstractSEECityIO.

--- Assets/SEE/Game/AbstractSEECity.cs
+++ Assets/SEE/Game/AbstractSEECity.cs
@@ -24,6 +24,26 @@ namespace SEE.Game
     /// </summary>
     public abstract partial class AbstractSEECity : SerializedMonoBehaviour
     {
+        /// <summary>
+        /// Saves the settings of this code city to <see cref="CityPath"/>.
+        /// </summary>
+        public void Save()
+        {
+            Save(CityPath.Path);
+        }
+
+        /// <summary>
+        /// Loads the settings of this code city from <see cref="CityPath"/>.
+        /// </summary>
+        public void Load()
+        {
+            Load(CityPath.Path);
+        }
+
+        /// <summary>
+        /// Saves the settings of this code city to <paramref name="filename"/>
+        /// </summary>
+        /// <param name="filename"></param>
         public void Save(string filename)
         {
             using (ConfigWriter writer = new ConfigWriter(filename))
@@ -33,178 +53,9 @@ namespace SEE.Game
         }
 
         /// <summary>
-        /// The attribute label for LODCulling in the stored configuration file.
+        /// Reads the settings of this city from <paramref name="filename"/>.
         /// </summary>
-        private const string LODCullingLabel = "LODCulling";
-        /// <summary>
-        /// The attribute label for LayoutPath in the stored configuration file.
-        /// </summary>
-        private const string LayoutPathLabel = "LayoutPath";
-        /// <summary>
-        /// The attribute label for CityPath in the stored configuration file.
-        /// </summary>
-        private const string CityPathLabel = "CityPath";
-
-        private const string WidthMetricLabel = "WidthMetric";
-        private const string HeightMetricLabel = "HeightMetric";
-        private const string DepthMetricLabel = "DepthMetric";
-        private const string LeafStyleMetricLabel = "LeafStyleMetric";
-
-        private const string LeafLabelSettingsLabel = "LeafLabelSettings";
-        private const string InnerNodeLabelSettingsLabel = "InnerNodeLabelSettings";
-        private const string LeafNodeColorRangeLabel = "LeafNodeColorRange";
-        private const string InnerNodeColorRangeLabel = "InnerNodeColorRange";
-        private const string HierarchicalEdgesLabel = "HierarchicalEdges";
-        private const string NodeTypesLabel = "NodeTypes";
-
-        private const string StyleIssueLabel = "StyleIssue";
-        private const string UniversalIssueLabel = "UniversalIssue";
-        private const string MetricIssueLabel = "MetricIssue";
-        private const string Dead_CodeIssueLabel = "Dead_CodeIssue";
-        private const string CycleIssueLabel = "CycleIssue";
-        private const string CloneIssueLabel = "CloneIssue";
-        private const string ArchitectureIssueLabel = "ArchitectureIssue";
-
-        private const string StyleIssue_SUMLabel = "StyleIssue_SUM";
-        private const string UniversalIssue_SUMLabel = "UniversalIssue_SUM";
-        private const string MetricIssue_SUMLabel = "MetricIssue_SUM";
-        private const string Dead_CodeIssue_SUMLabel = "Dead_CodeIssue_SUM";
-        private const string CycleIssue_SUMLabel = "CycleIssue_SUM";
-        private const string CloneIssue_SUMLabel = "CloneIssue_SUM";
-        private const string ArchitectureIssue_SUMLabel = "ArchitectureIssue_SUM";
-
-        private const string InnerDonutMetricLabel = "InnerDonutMetric";
-        private const string InnerNodeHeightMetricLabel = "InnerNodeHeightMetric";
-
-        private const string MinimalBlockLengthLabel = "MinimalBlockLength";
-        private const string MaximalBlockLengthLabel = "MaximalBlockLength";
-
-        private const string LeafObjectsLabel = "LeafObjects";
-        private const string InnerNodeObjectsLabel = "InnerNodeObjects";
-
-        private const string NodeLayoutLabel = "NodeLayout";
-        private const string EdgeLayoutLabel = "EdgeLayout";
-
-        private const string ZScoreScaleLabel = "ZScoreScale";
-        private const string EdgeWidthLabel = "EdgeWidth";
-        private const string ShowErosionsLabel = "ShowErosions";
-        private const string MaxErosionWidthLabel = "MaxErosionWidth";
-        private const string EdgesAboveBlocksLabel = "EdgesAboveBlocks";
-        private const string TensionLabel = "Tension";
-        private const string RDPLabel = "RDP";
-
-        private const string CoseGraphSettingsLabel = "CoseGraphSettings";
-
-        protected virtual void Save(ConfigWriter writer)
-        {
-            writer.Save(LODCulling, LODCullingLabel);
-            LayoutPath.Save(writer, LayoutPathLabel);
-            writer.Save(HierarchicalEdges.ToList<string>(), HierarchicalEdgesLabel);
-            writer.Save(nodeTypes, NodeTypesLabel);
-            CityPath.Save(writer, CityPathLabel);
-            LeafNodeColorRange.Save(writer, LeafNodeColorRangeLabel);
-            InnerNodeColorRange.Save(writer, InnerNodeColorRangeLabel);
-            writer.Save(WidthMetric, WidthMetricLabel);
-            writer.Save(HeightMetric, HeightMetricLabel);
-            writer.Save(DepthMetric, DepthMetricLabel);
-            writer.Save(LeafStyleMetric, LeafStyleMetricLabel);
-            LeafLabelSettings.Save(writer, LeafLabelSettingsLabel);
-            InnerNodeLabelSettings.Save(writer, InnerNodeLabelSettingsLabel);
-
-            writer.Save(StyleIssue, StyleIssueLabel);
-            writer.Save(UniversalIssue, UniversalIssueLabel);
-            writer.Save(MetricIssue, MetricIssueLabel);
-            writer.Save(Dead_CodeIssue, Dead_CodeIssueLabel);
-            writer.Save(CycleIssue, CycleIssueLabel);
-            writer.Save(CloneIssue, CloneIssueLabel);
-            writer.Save(ArchitectureIssue, ArchitectureIssueLabel);
-
-            writer.Save(StyleIssue_SUM, StyleIssue_SUMLabel);
-            writer.Save(UniversalIssue_SUM, UniversalIssue_SUMLabel);
-            writer.Save(MetricIssue_SUM, MetricIssue_SUMLabel);
-            writer.Save(Dead_CodeIssue_SUM, Dead_CodeIssue_SUMLabel);
-            writer.Save(CycleIssue_SUM, CycleIssue_SUMLabel);
-            writer.Save(CloneIssue_SUM, CloneIssue_SUMLabel);
-            writer.Save(ArchitectureIssue_SUM, ArchitectureIssue_SUMLabel);
-
-            writer.Save(InnerDonutMetric, InnerDonutMetricLabel);
-            writer.Save(InnerNodeHeightMetric, InnerNodeHeightMetricLabel);
-
-            writer.Save(MinimalBlockLength, MinimalBlockLengthLabel);
-            writer.Save(MaximalBlockLength, MaximalBlockLengthLabel);
-
-            writer.Save(LeafObjects.ToString(), LeafObjectsLabel);
-            writer.Save(InnerNodeObjects.ToString(), InnerNodeObjectsLabel);
-
-            writer.Save(NodeLayout.ToString(), NodeLayoutLabel);
-            writer.Save(EdgeLayout.ToString(), EdgeLayoutLabel);
-
-            writer.Save(ZScoreScale, ZScoreScaleLabel);
-            writer.Save(EdgeWidth, EdgeWidthLabel);
-            writer.Save(ShowErosions, ShowErosionsLabel);
-            writer.Save(MaxErosionWidth, MaxErosionWidthLabel);
-            writer.Save(EdgesAboveBlocks, EdgesAboveBlocksLabel);
-            writer.Save(Tension, TensionLabel);
-            writer.Save(RDP, RDPLabel);
-
-            CoseGraphSettings.Save(writer, CoseGraphSettingsLabel);
-        }
-
-        protected virtual void Restore(Dictionary<string, object> attributes)
-        {
-            ConfigIO.Restore<float>(attributes, LODCullingLabel, ref LODCulling);
-            LayoutPath.Restore(attributes, LayoutPathLabel);
-            ConfigIO.Restore(attributes, HierarchicalEdgesLabel, ref HierarchicalEdges);
-            ConfigIO.Restore(attributes, NodeTypesLabel, ref nodeTypes);
-            CityPath.Restore(attributes, CityPathLabel);
-            LeafNodeColorRange.Restore(attributes, LeafNodeColorRangeLabel);
-            InnerNodeColorRange.Restore(attributes, InnerNodeColorRangeLabel);
-            ConfigIO.Restore(attributes, WidthMetricLabel, ref WidthMetric);
-            ConfigIO.Restore(attributes, HeightMetricLabel, ref HeightMetric);
-            ConfigIO.Restore(attributes, DepthMetricLabel, ref DepthMetric);
-            ConfigIO.Restore(attributes, LeafStyleMetricLabel, ref LeafStyleMetric);
-            LeafLabelSettings.Restore(attributes, LeafLabelSettingsLabel);
-            InnerNodeLabelSettings.Restore(attributes, InnerNodeLabelSettingsLabel);
-
-            ConfigIO.Restore(attributes, StyleIssueLabel, ref StyleIssue);
-            ConfigIO.Restore(attributes, UniversalIssueLabel, ref UniversalIssue);
-            ConfigIO.Restore(attributes, MetricIssueLabel, ref MetricIssue);
-            ConfigIO.Restore(attributes, Dead_CodeIssueLabel, ref Dead_CodeIssue);
-            ConfigIO.Restore(attributes, CycleIssueLabel, ref CycleIssue);
-            ConfigIO.Restore(attributes, CloneIssueLabel, ref CloneIssue);
-            ConfigIO.Restore(attributes, ArchitectureIssueLabel, ref ArchitectureIssue);
-
-            ConfigIO.Restore(attributes, StyleIssue_SUMLabel, ref StyleIssue_SUM);
-            ConfigIO.Restore(attributes, UniversalIssue_SUMLabel, ref UniversalIssue_SUM);
-            ConfigIO.Restore(attributes, MetricIssue_SUMLabel, ref MetricIssue_SUM);
-            ConfigIO.Restore(attributes, Dead_CodeIssue_SUMLabel, ref Dead_CodeIssue_SUM);
-            ConfigIO.Restore(attributes, CycleIssue_SUMLabel, ref CycleIssue_SUM);
-            ConfigIO.Restore(attributes, CloneIssue_SUMLabel, ref CloneIssue_SUM);
-            ConfigIO.Restore(attributes, ArchitectureIssue_SUMLabel, ref ArchitectureIssue_SUM);
-
-            ConfigIO.Restore(attributes, InnerDonutMetricLabel, ref InnerDonutMetric);
-            ConfigIO.Restore(attributes, InnerNodeHeightMetricLabel, ref InnerNodeHeightMetric);
-
-            ConfigIO.Restore(attributes, MinimalBlockLengthLabel, ref MinimalBlockLength);
-            ConfigIO.Restore(attributes, MaximalBlockLengthLabel, ref MaximalBlockLength);
-
-            ConfigIO.RestoreEnum<LeafNodeKinds>(attributes, LeafObjectsLabel, ref LeafObjects);
-            ConfigIO.RestoreEnum<InnerNodeKinds>(attributes, InnerNodeObjectsLabel, ref InnerNodeObjects);
-
-            ConfigIO.RestoreEnum<NodeLayoutKind>(attributes, NodeLayoutLabel, ref NodeLayout);
-            ConfigIO.RestoreEnum<EdgeLayoutKind>(attributes, EdgeLayoutLabel, ref EdgeLayout);
-
-            ConfigIO.Restore(attributes, ZScoreScaleLabel, ref ZScoreScale);
-            ConfigIO.Restore(attributes, EdgeWidthLabel, ref EdgeWidth);
-            ConfigIO.Restore(attributes, ShowErosionsLabel, ref ShowErosions);
-            ConfigIO.Restore(attributes, MaxErosionWidthLabel, ref MaxErosionWidth);
-            ConfigIO.Restore(attributes, EdgesAboveBlocksLabel, ref EdgesAboveBlocks);
-            ConfigIO.Restore(attributes, TensionLabel, ref Tension);
-            ConfigIO.Restore(attributes, RDPLabel, ref RDP);
-
-            CoseGraphSettings.Restore(attributes, CoseGraphSettingsLabel);
-        }
-
+        /// <param name="filename"></param>
         public void Load(string filename)
         {
             using (ConfigReader stream = new ConfigReader(filename))
@@ -258,19 +109,7 @@ namespace SEE.Game
         /// they should be visualized or not.
         /// </summary>
         [NonSerialized, OdinSerialize]
-        protected Dictionary<string, bool> nodeTypes = new Dictionary<string, bool>();
-        /// <summary>
-        /// A mapping of all node types of the nodes in the graph onto whether
-        /// they should be visualized or not.
-        /// </summary>
-        public Dictionary<string, bool> SelectedNodeTypes
-        {
-            get => nodeTypes;
-            set
-            {
-                nodeTypes = value;
-            }
-        }
+        public Dictionary<string, bool> SelectedNodeTypes = new Dictionary<string, bool>();
 
         /// <summary>
         /// The path where the settings (the attributes of this class) are stored.
@@ -292,7 +131,7 @@ namespace SEE.Game
         /// </summary>
         public void ResetSelectedNodeTypes()
         {
-            nodeTypes.Clear();
+            SelectedNodeTypes.Clear();
         }
 
         /// <summary>
@@ -358,7 +197,7 @@ namespace SEE.Game
         {
             get
             {
-                foreach (bool relevant in nodeTypes.Values)
+                foreach (bool relevant in SelectedNodeTypes.Values)
                 {
                     if (!relevant)
                     {
@@ -397,10 +236,10 @@ namespace SEE.Game
                 Dictionary<string, bool> newNodeTypes = new Dictionary<string, bool>();
                 foreach (string type in newTypes)
                 {
-                    if (nodeTypes.ContainsKey(type))
+                    if (SelectedNodeTypes.ContainsKey(type))
                     {
                         // preserve existing node types
-                        newNodeTypes[type] = nodeTypes[type];
+                        newNodeTypes[type] = SelectedNodeTypes[type];
                     }
                     else
                     {
@@ -408,7 +247,7 @@ namespace SEE.Game
                         newNodeTypes[type] = true;
                     }
                 }
-                nodeTypes = newNodeTypes;
+                SelectedNodeTypes = newNodeTypes;
             }
         }
 
@@ -429,7 +268,7 @@ namespace SEE.Game
             }
             else
             {
-                ICollection<string> matches = nodeTypes.Where(pair => pair.Value == true)
+                ICollection<string> matches = SelectedNodeTypes.Where(pair => pair.Value == true)
                   .Select(pair => pair.Key).ToList();
                 Debug.Log("The following node types are relevant:\n");
                 foreach (string nodeType in matches)
@@ -861,43 +700,6 @@ namespace SEE.Game
 
                 CoseGraphSettings.LoadedForNodeTypes = SelectedNodeTypes.Where(type => type.Value == true).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
             }
-        }
-
-        /// <summary>
-        /// Saves the settings of this code city to <see cref="CityPath"/>.
-        /// </summary>
-        public void Save()
-        {
-            Save(this, CityPath.Path);
-        }
-
-        /// <summary>
-        /// Loads the settings of this code city from <see cref="CityPath"/>.
-        /// </summary>
-        public void Load()
-        {
-            Load(this, CityPath.Path);
-        }
-
-        /// <summary>
-        /// Saves <paramref name="city"/> in a file with given <paramref name="filename"/>.
-        /// </summary>
-        /// <param name="city">the city to be stored</param>
-        /// <param name="filename">the name of the file in which <paramref name="city"/> will be stored</param>
-        public static void Save(AbstractSEECity city, string filename)
-        {          
-            AbstractSEECityIO.Save(city, filename);
-        }
-
-        /// <summary>
-        /// Loads and overwrites the <paramref name="city"/> with the city read 
-        /// from the file with given <paramref name="filename"/>.
-        /// </summary>
-        /// <param name="city">the city to be overwritten</param>
-        /// <param name="filename">the name of the file from which to read the <paramref name="city"/></param>
-        public static void Load(AbstractSEECity city, string filename)
-        {
-            AbstractSEECityIO.Load(city, filename);
         }
     }
 }
