348c6694e01fbad0b67265d44934e0b8f78c62a6
Falko Galperin <falko1@uni-bremen.de>
2021-01-11 02:18:21 +0100

    #135 Improve ArchitectureHololensController

--- Assets/ArchitectureHololensController.cs
+++ Assets/ArchitectureHololensController.cs
@@ -1,23 +1,14 @@
 ﻿using Microsoft.MixedReality.Toolkit.UI;
 using Microsoft.MixedReality.Toolkit.UI.BoundsControl;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ArchitectureHololensController : MonoBehaviour
 {
-    GameObject AppBar;
+    private GameObject AppBar;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         AppBar = GameObject.Find("AppBar");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void OnMouseOver()
@@ -25,7 +16,7 @@ public class ArchitectureHololensController : MonoBehaviour
         BoundsControl boundsControl = gameObject.GetComponent<BoundsControl>();
         boundsControl.BoundsControlActivation = Microsoft.MixedReality.Toolkit.UI.BoundsControlTypes.BoundsControlActivationType.ActivateOnStart;
 
-        if(AppBar != null && boundsControl != null) 
+        if (AppBar != null && boundsControl != null) 
         {
             AppBar.GetComponent<AppBar>().Target = boundsControl;
         }
