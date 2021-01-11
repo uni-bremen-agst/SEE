d9507af3588e691827b652e85c3a04e7ebc6270c
ThoreFrenzel <tfrenzel@uni-bremen.de>
2021-01-11 21:37:06 +0100

    #115 you can set the name and the type ingame now

--- Assets/SEE/AddingNodeCanvasScript.cs
+++ Assets/SEE/AddingNodeCanvasScript.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AddingNodeCanvasScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public GameObject canvas;
+
     void Start()
     {
+        
         canvas = Instantiate(Resources.Load("NodeCanvas", typeof(GameObject))) as GameObject;
+        canvas.transform.SetParent(gameObject.transform);
+      
+
     }
 
     // Update is called once per frame
@@ -25,13 +31,13 @@ public class AddingNodeCanvasScript : MonoBehaviour
 
     public void DestroyAllChilds()
     {
+        int anzahl = canvas.transform.GetChildCount();
+      
         foreach (Transform child in canvas.transform)
         {
             GameObject.Destroy(child.gameObject);
         }
+        GameObject.Destroy(canvas);
     }
-    public void DestroyGameObject()
-    {
-        Destroy(transform.gameObject.GetComponentInParent<Canvas>().gameObject);
-    }
+    
 }
