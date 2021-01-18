a9e8391cadffff1216af39570e2c2622f16d1ae6
mblecker <mblecker@uni-bremen.de>
2021-01-18 16:39:08 +0100

    #115 Documentation of ScaleNodeNetAction

--- Assets/SEE/Net/Scripts/Actions/ScaleNodeNetAction.cs
+++ Assets/SEE/Net/Scripts/Actions/ScaleNodeNetAction.cs
@@ -8,24 +8,36 @@ public class ScaleNodeNetAction : AbstractAction
     public string gameObjectID;
     public Vector3 scale;
     public Vector3 position;
+    /// <summary>
+    /// Constructs a ScaleNodeNetAction
+    /// </summary>
+    /// <param name="GameObjectID">The id from the GameObject which should be scaled through the Network</param>
+    /// <param name="Scale">The new scale of the GameObject</param>
+    /// <param name="Positon">The new Postition of the GameObject</param>
     public ScaleNodeNetAction(string GameObjectID, Vector3 Scale, Vector3 Positon) : base()
     {
         gameObjectID = GameObjectID;
         scale = Scale;
         position = Positon;
     }
-
+    /// <summary>
+    /// Things to Execute on the Server (None for this Class)
+    /// </summary>
     protected override void ExecuteOnServer()
     {
 
     }
-
+    /// <summary>
+    /// Things to Execute on the Client Sets finds the GameObject on the Client and sets its scale and position
+    /// </summary>
     protected override void ExecuteOnClient()
     {
         if (!IsRequester())
         {
+            //Just Test Purpose START
            Debug.Log(gameObjectID);
           GameObject thrdCornerSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            //JUST TEST PURPOSE END (REMOVE IF IT WORKS!)
             GameObject scaleObj = GameObject.Find(gameObjectID);
             if(scaleObj != null)
             {
