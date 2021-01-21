1e2564a8ab5b1c62d751c19b98e97a8432031917
Leonard <>
2021-01-21 22:31:47 +0200

    added graph revision change detection, color customizeability for ui

---
+++ Assets/SEE/Game/Charts/ChartColors.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChartColors : MonoBehaviour
+{
+    /// <summary>
+    /// The color that the Labels in the scrollview will have for new nodes
+    /// </summary>
+    public Color newNodesScrollviewLabelColor;
+
+    /// <summary>
+    /// The color that the Labels in the scrollview will have for changed nodes
+    /// </summary>
+    public Color changedNodesScrollviewLabelColor;
+
+    /// <summary>
+    /// The color that the Labels in the scrollview will have for deleted nodes
+    /// </summary>
+    public Color removedNodesScrollviewLabelColor;
+
+    /// <summary>
+    /// The color the Labels in the scrollview will have when hovering aboce them
+    /// </summary>
+    public Color hoveringScrollviewLabelColor;
+}
