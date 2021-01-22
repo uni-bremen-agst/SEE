8c7d579e6dd64c4c84c43de53ae568f3a972513d
mblecker <mblecker@uni-bremen.de>
2021-01-22 14:58:22 +0100

    #115 Edit Node Net Action test

---
+++ Assets/SEE/Net/Scripts/Actions/EditNodeNetAction.cs
@@ -0,0 +1,51 @@
+﻿using SEE.DataModel.DG;
+using SEE.GO;
+using SEE.Net;
+using UnityEngine;
+
+
+public class EditNodeNetAction : AbstractAction
+{
+    public Node nodeToEdit = null;
+    public string gameObjectID;
+    /// <summary>
+    /// Constructs a EditNodeNetAction
+    /// </summary>
+    /// <param name="GameObjectID">The id from the GameObject which should be edited through the Network</param>
+    /// <param name="Node">The Node with the changes</param>
+    public EditNodeNetAction(Node node, string GameObjectID) : base()
+    {
+        nodeToEdit = node;
+        gameObjectID = GameObjectID;
+    }
+    /// <summary>
+    /// Things to Execute on the Server (None for this Class)
+    /// </summary>
+    protected override void ExecuteOnServer()
+    {
+
+    }
+    /// <summary>
+    /// Things to Execute on the Client Sets finds the GameObject on the Client and sets its scale and position
+    /// </summary>
+    protected override void ExecuteOnClient()
+    {
+        if (!IsRequester())
+        {
+            Node node = GameObject.Find(gameObjectID).GetNode();
+            if (node != null)
+            {
+                node.SourceName = nodeToEdit.SourceName;
+                node.Type = nodeToEdit.Type;
+            }
+            else
+            {
+                //FIXME: Controll if a Debug Log is the right thing
+                Debug.LogWarning("Kein GameObject gefunden: " + gameObjectID);
+            }
+        }
+
+    }
+
+
+}
