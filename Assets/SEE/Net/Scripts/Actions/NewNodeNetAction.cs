c362d7ec2ef1a3652a7dd4dea5cbf840505251a6
mblecker <mblecker@uni-bremen.de>
2021-01-23 12:42:50 +0100

    #115 City cannot be Serielizd umgehen

--- Assets/SEE/Net/Scripts/Actions/NewNodeNetAction.cs
+++ Assets/SEE/Net/Scripts/Actions/NewNodeNetAction.cs
@@ -8,7 +8,8 @@ using UnityEngine;
 
 public class NewNodeNetAction : AbstractAction
 {
-    public SEECity city;
+    public SEECity city = null;;
+    public string gameObjectID;
     public bool isInnerNode;
     public  string nodeMetrics1;
     public  string nodeMetrics2;
@@ -18,15 +19,16 @@ public class NewNodeNetAction : AbstractAction
     /// <summary>
     /// Creates a new NewNodeNetActio
     /// </summary>
-    /// <param name="City">the city for the new node</param>
+    /// <param name="GameObjectID">the  GameObject on that the city is attached for the new node</param>
     /// <param name="IsInnerNode">should it be a inner node</param>
     /// <param name="NodeMetrics1">metrics1 for the new node</param>
     /// <param name="NodeMetrics2">metrics2 for the new node</param>
     /// <param name="NodeMetrics3">metrics3 for the new node</param>
     /// <param name="Position">the postition for the new node</param>
-    public NewNodeNetAction(SEECity City, bool IsInnerNode, string NodeMetrics1, string NodeMetrics2, string NodeMetrics3, Vector3 Position) : base()
+    public NewNodeNetAction(string GameObjectID, bool IsInnerNode, string NodeMetrics1, string NodeMetrics2, string NodeMetrics3, Vector3 Position) : base()
     {
-        city = City;
+        //city = City;
+        gameObjectID = GameObjectID;
         isInnerNode = IsInnerNode;
         nodeMetrics1 = NodeMetrics1;
         nodeMetrics2 = NodeMetrics2;
@@ -47,13 +49,17 @@ public class NewNodeNetAction : AbstractAction
     {
         if (!IsRequester())
         {
-            GameObject dummy = new GameObject();
-            dummy.AddComponent<DesktopNewNodeAction>();
-            dummy.GetComponent<DesktopNewNodeAction>().SetCity(city);
-            dummy.GetComponent<DesktopNewNodeAction>().SetIsInnerNode(isInnerNode);
-            dummy.GetComponent<DesktopNewNodeAction>().SetNodeMetrics(nodeMetrics1, nodeMetrics2, nodeMetrics3);
-            Debug.Log(nodeMetrics1);
-            dummy.GetComponent<DesktopNewNodeAction>().NetworkNewNode(position);
+            SceneQueries.GetCodeCity(GameObject.Find(gameObjectID).transform)?.gameObject.TryGetComponent<SEECity>(out city);
+            if(city != null)
+            {
+                GameObject dummy = new GameObject();
+                dummy.AddComponent<DesktopNewNodeAction>();
+                dummy.GetComponent<DesktopNewNodeAction>().SetCity(city);
+                dummy.GetComponent<DesktopNewNodeAction>().SetIsInnerNode(isInnerNode);
+                dummy.GetComponent<DesktopNewNodeAction>().SetNodeMetrics(nodeMetrics1, nodeMetrics2, nodeMetrics3);
+                dummy.GetComponent<DesktopNewNodeAction>().NetworkNewNode(position);
+            }
+            
 
 
 
