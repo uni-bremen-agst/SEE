b64724d678fa095a5282c5fc9b2a07f1c455031d
janwoiton <janwoiton@gmail.com>
2021-01-25 23:20:13 +0100

    Final Version
    Task completed.
    Mode "DrawEdge" to connect gameobjects via left click.
    F1 as Button to reset selection.

--- Assets/SEE/Controls/AddEdge.cs
+++ Assets/SEE/Controls/AddEdge.cs
@@ -2,33 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using SEE.Game;
+using SEE.GO;
+using System;
 
 public class AddEdge : MonoBehaviour
 {
     // Update is called once per frame
     void Update()
     {
+        //Assigning the gameobjects to be connected.
+        //Checking whether the two gameobjects are not null and whether the are on the same graph.
         if (Input.GetMouseButtonDown(0) && hoveredObject != null)
         {
             if (from == null)
             {
                 from = hoveredObject;
-                Debug.Log(from);
             }
             if (!from.Equals(hoveredObject) && to == null)
             {
-                to = hoveredObject;
-                Debug.Log(to);
+                    to = hoveredObject;
+            }
+            else
+            {
+                throw new Exception($"The source and target of the edge cannot be the same gameobject!");
             }
         }
         if (from != null && to != null)
         {
-            SEECity city;
-            city = SceneQueries.GetCodeCity(from.transform).GetComponent<SEECity>();
-            Debug.Log(city);
-            city.Renderer.DrawEdge(from, to);
+                SEECity city;
+                city = SceneQueries.GetCodeCity(from.transform).GetComponent<SEECity>();
+                city.Renderer.DrawEdge(from, to);
+                from = null;
+                to = null;
+        }
+        //Adding the key "F1" in order to delete the first selected gameobject.
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
             from = null;
-            to = null;
         }
 
     }
