ae7464bb1563a3113070234865abb122af1ba945
ThoreFrenzel <tfrenzel@uni-bremen.de>
2021-01-25 17:23:01 +0100

    #115 further refactoring and code commentary

--- Assets/SEE/AddingNodeCanvasScript.cs
+++ Assets/SEE/AddingNodeCanvasScript.cs
@@ -4,35 +4,39 @@ using UnityEngine.UI;
 
 public class AddingNodeCanvasScript : NodeCanvasScript
 {
+    /// <summary>
+    /// The directory of the AddingNodeCanvas-prefab.
+    /// </summary>
     private string prefabDirectory = "Prefabs/NewNodeCanvas";
-    // Start is called before the first frame update
+
     void Start()
     {
-        // Note: Its important that the Prefab lays inside of the Resources-Folder to use the Resources.Load-Method.
+        /// Note: Its important that the Prefab lays inside of the Resources-Folder to use the Resources.Load-Method.
         InstantiatePrefab(prefabDirectory);
         canvas.transform.SetParent(gameObject.transform);
     }
 
     /// <summary>
     /// Extracts the given Nodename, the nodetype and wether it is a inner node or a leaf from the canvas.
+    /// Therefore, it extracts the string from the InputFields on the prefab.
+    /// Note: The sequences of the extracted Arrays are based on the sequence of the components in the prefab.
     /// </summary>
     public void GetNodeMetrics()
     {
         string inputNodename;
         string inputNodetype;
 
-
         //this part has to be removed by the new UI-Team
         AddingNodeCanvasScript script = gameObject.GetComponent<AddingNodeCanvasScript>();
 
-        //Gets the texts from the InputFields. The sequence in the array is given by the sequence of the components in the prefab.
         Component[] c = script.canvas.GetComponentsInChildren<InputField>();
         InputField inputname = (InputField)c[0];
         InputField inputtype = (InputField)c[1];
 
-        //Gets the selection of the toggleGroup. The sequence in the toggleComponent-Array is given by the sequence of the components in the prefab.
+
         Component toggleGroup = script.canvas.GetComponentInChildren<ToggleGroup>();
         Toggle[] toggles = toggleGroup.GetComponentsInChildren<Toggle>();
+
         if (toggles[0].isOn)
         {
             DesktopNewNodeAction.IsInnerNode = true;
--- Assets/SEE/Controls/Actions/DesktopNodeAction.cs
+++ Assets/SEE/Controls/Actions/DesktopNodeAction.cs
@@ -12,17 +12,23 @@ public class DesktopNodeAction : MonoBehaviour
     /// </summary>
     public GameObject hoveredObject = null;
 
+    /// <summary>
+    /// The current name of the gameObject which contains the Canvas-operations and components.
+    /// </summary>
     private readonly string nameOfCanvasObject = "CanvasObject";
 
     /// <summary>
-    /// Removes The Script
-    /// Places the new Node if not placed
+    /// Removes this script or rather the child-script.
     /// </summary>
     public virtual void RemoveScript()
     {
         Destroy(this);
     }
 
+    /// <summary>
+    /// Finds the GameObject, which contains the CanvasOperations and components
+    /// and saves it in the canvasObject-variable.
+    /// </summary>
     public void InitialiseCanvasObject()
     {
         canvasObject = GameObject.Find(nameOfCanvasObject);
