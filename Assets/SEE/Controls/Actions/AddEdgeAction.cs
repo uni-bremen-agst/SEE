01f464d8daf3c45461889a15c668ccae53643803
Rainer Koschke <koschke@uni-bremen.de>
2021-01-28 18:30:29 +0100

    #163 Added documentation. This class will illustrate what needs to be done when a developer wants to add a new action.

--- Assets/SEE/Controls/Actions/AddEdgeAction.cs
+++ Assets/SEE/Controls/Actions/AddEdgeAction.cs
@@ -11,6 +11,19 @@ namespace SEE.Controls.Actions
     /// </summary>
     public class AddEdgeAction : MonoBehaviour
     {
+        /// <summary>
+        /// Start() will register an anonymous delegate of type 
+        /// <see cref="ActionState.OnStateChangedFn"/> on the event
+        /// <see cref="ActionState.OnStateChanged"/> to be called upon every
+        /// change of the action state, where the newly entered state will
+        /// be passed as a parameter. The anonymous delegate will compare whether
+        /// this state equals <see cref="ThisActionState"/> and if so, execute
+        /// what needs to be done for this action here. If that parameter is
+        /// different from <see cref="ThisActionState"/>, this action will
+        /// put itself to sleep. 
+        /// Thus, this action will be executed only if the new state is 
+        /// <see cref="ThisActionState"/>.
+        /// </summary>
         const ActionState.Type ThisActionState = ActionState.Type.DrawEdge;
 
         /// <summary>
@@ -30,16 +43,22 @@ namespace SEE.Controls.Actions
 
         private void Start()
         {
-            ActionState.OnStateChanged += (ActionState.Type v) =>
+            // An anonymous delegate is registered for the event <see cref="ActionState.OnStateChanged"/>.
+            // This delegate will be called from <see cref="ActionState"/> upon every
+            // state changed where the passed parameter is the newly entered state.
+            ActionState.OnStateChanged += (ActionState.Type newState) =>
             {
-                if (v == ThisActionState)
+                // Is this our action state where we need to do something?
+                if (newState == ThisActionState)
                 {
+                    // The monobehaviour is enabled and Update() will be called by Unity.
                     enabled = true;
                     InteractableObject.LocalAnyHoverIn += LocalAnyHoverIn;
                     InteractableObject.LocalAnyHoverOut += LocalAnyHoverOut;
                 }
                 else
                 {
+                    // The monobehaviour is diabled and Update() no longer be called by Unity.
                     enabled = false;
                     InteractableObject.LocalAnyHoverIn -= LocalAnyHoverIn;
                     InteractableObject.LocalAnyHoverOut -= LocalAnyHoverOut;
