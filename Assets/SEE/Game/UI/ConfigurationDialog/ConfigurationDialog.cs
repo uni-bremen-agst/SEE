11849b9af859dd364cdd3958da256777ca779e55
Falko Galperin <falko1@uni-bremen.de>
2021-01-28 23:41:03 +0100

    #166 Began configuration dialog implementation
    This does not represent a working state and is
    still very WIP.

---
+++ Assets/SEE/Game/UI/ConfigurationDialog/ConfigurationDialog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SEE.Game.UI.ConfigurationDialog
+{
+    /// <summary>
+    /// A UI dialog consisting of several groups, each with configurable properties.
+    /// </summary>
+    public class ConfigurationDialog: PlatformDependentComponent
+    {
+        protected IList<ConfigurationGroup> Groups;
+    }
+}
