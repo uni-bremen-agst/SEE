06711a55d2f20c612a3557644b5ff0dff06f242e
ThoreFrenzel <tfrenzel@uni-bremen.de>
2021-01-31 00:28:14 +0100

    #115 fixed scale node with new structure, fixed editNode, set add edge to child of NodeAction

--- Assets/SEE/EditNodeAction.cs
+++ Assets/SEE/EditNodeAction.cs
@@ -36,11 +36,43 @@ namespace SEE.Controls.Actions
 
         public Progress EditProgress { get => editProgress; set => editProgress = value; }
 
+        private bool instantiated = false;
 
         void Start()
         {
             InitialiseCanvasObject();
-            ChangeState(ThisActionState);
+            // An anonymous delegate is registered for the event <see cref="ActionState.OnStateChanged"/>.
+            // This delegate will be called from <see cref="ActionState"/> upon every
+            // state changed where the passed parameter is the newly entered state.
+            ActionState.OnStateChanged += (ActionState.Type newState) =>
+            {
+                // Is this our action state where we need to do something?
+                if (newState == ThisActionState)
+                {
+                    // The monobehaviour is enabled and Update() will be called by Unity.
+                    enabled = true;
+                    InteractableObject.LocalAnyHoverIn += LocalAnyHoverIn;
+                    InteractableObject.LocalAnyHoverOut += LocalAnyHoverOut;
+                    if (!instantiated)
+                    {
+                        instantiated = true;
+                    }
+
+                }
+                else
+                {
+                    // The monobehaviour is diabled and Update() no longer be called by Unity.
+                    enabled = false;
+                    CanvasGenerator c = canvasObject.GetComponent<CanvasGenerator>();
+                    c.DestroyEditNodeCanvas();
+                    instantiated = false;
+                    InteractableObject.LocalAnyHoverIn -= LocalAnyHoverIn;
+                    InteractableObject.LocalAnyHoverOut -= LocalAnyHoverOut;
+                    hoveredObject = null;
+                }
+            };
+            enabled = ActionState.Is(ThisActionState);
+            //ChangeState(ThisActionState);
         }
 
         /// <summary>
