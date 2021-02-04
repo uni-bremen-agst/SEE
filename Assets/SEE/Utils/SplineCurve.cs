384f7a3ea3744c89b506627e6ab3dba122055dbc
Sören Untiedt <suntiedt@uni-bremen.de>
2021-02-04 15:22:16 +0100

    154_Added-Tubular-for-Meshes-on-EdgeFactory

---
+++ Assets/SEE/Utils/SplineCurve.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Curve;
+using System.Collections.Generic;
+using TinySpline;
+
+namespace Assets.SEE.Utils
+{
+    public class SplineCurve : Curve.Curve
+    {
+        private BSpline spline;
+
+        public SplineCurve(List<Vector3> points) : base(points)
+        {
+            IList<double> list = new List<double>();
+            foreach( var p in points)
+            {
+                list.Add(p.x);
+                list.Add(p.y);
+                list.Add(p.z);
+            }
+
+            spline = BSpline.InterpolateCatmullRom(list, 3, 0.5);
+        }
+
+
+        protected override Vector3 GetPoint(float t)
+        {
+            IList<double> list = spline.Eval(t).Result;
+
+            return new Vector3((float)list[0], (float)list[1], (float)list[2]);
+        }
+    }
+}
