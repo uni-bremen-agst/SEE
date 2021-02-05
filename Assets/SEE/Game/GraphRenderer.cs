ddcd936586dd25b7bfedf2fb6622192e0282811f
Rainer Koschke <koschke@uni-bremen.de>
2021-02-05 08:40:17 +0100

    #117 Edges must be decorated with the components necessary for interaction, too. Added the respective code to both DrawEdge() and EdgeLayout().

--- Assets/SEE/Game/GraphRenderer.cs
+++ Assets/SEE/Game/GraphRenderer.cs
@@ -244,14 +244,15 @@ namespace SEE.Game
             ICollection<LayoutEdge> layoutEdges = new List<LayoutEdge> { new LayoutEdge(fromLayoutNode, toLayoutNode, edge) };
             // Calculate the edge layout (for the single edge only).
             ICollection<GameObject> edges = EdgeLayout(layoutNodes, layoutEdges);
-            GameObject result = edges.FirstOrDefault();
+            GameObject resultingEdge = edges.FirstOrDefault();
+            InteractionDecorator.PrepareForInteraction(resultingEdge);
             // The edge becomes a child of the root node of the game-node hierarchy
             GameObject codeCity = SceneQueries.GetCodeCity(from.transform).gameObject;
             GameObject rootNode = SceneQueries.GetCityRootNode(codeCity).gameObject;                                        
-            result.transform.SetParent(rootNode.transform);
+            resultingEdge.transform.SetParent(rootNode.transform);
             // The portal of the new edge is inherited from the codeCity.
-            Portal.SetPortal(root: codeCity, gameObject: result);
-            return result;
+            Portal.SetPortal(root: codeCity, gameObject: resultingEdge);
+            return resultingEdge;
         }
 
         /// <summary>
@@ -334,6 +335,7 @@ namespace SEE.Game
             else
             {
                 result = edgeFactory.DrawEdges(gameNodes.Cast<ILayoutNode>().ToList(), layoutEdges);
+                InteractionDecorator.PrepareForInteraction(result);
                 AddLOD(result);
             }            
             
@@ -588,7 +590,6 @@ namespace SEE.Game
 
             // Add the node to the node hierarchy
             gameNode.transform.SetParent(itsParent.transform);
-
 
             // Prepare the node for interactions
             InteractionDecorator.PrepareForInteraction(gameNode);
@@ -1308,9 +1309,9 @@ namespace SEE.Game
         }
 
         /// <summary>
-        /// Applies ADDLOD to every game object in <paramref name="gameObjects"/>.
+        /// Applies AddLOD to every game object in <paramref name="gameObjects"/>.
         /// </summary>
-        /// <param name="gameObjects">the list of game objects where ADDLOD is to be applied</param>
+        /// <param name="gameObjects">the list of game objects where AddLOD is to be applied</param>
         private void AddLOD(ICollection<GameObject> gameObjects)
         {
             foreach (GameObject go in gameObjects)
