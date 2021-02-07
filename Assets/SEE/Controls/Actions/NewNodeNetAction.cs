7230b8ceaacb09b691bdd39fa5e7b76b24c2c926
Rainer Koschke <koschke@uni-bremen.de>
2021-02-07 13:23:47 +0100

    #115 Added periods at the end of comments. Capitalized public fields. Added implementation note that all attributes are to be public. Reformatted long parameter list of constructor. Improved comments.

--- Assets/SEE/Controls/Actions/NewNodeNetAction.cs
+++ Assets/SEE/Controls/Actions/NewNodeNetAction.cs
@@ -6,142 +6,161 @@ using UnityEngine;
 namespace SEE.Controls.Actions
 {
     /// <summary>
-    /// This class is responsible for the new-node-process via network from one client to all others and to the server. 
+    /// This class is responsible for adding a node via network from one client to all others and 
+    /// to the server. 
     /// </summary>
     public class NewNodeNetAction : AbstractAction
     {
-        /// <summary>
-        /// The city where the new node has to be placed
-        /// </summary>
-        public SEECity city = null;
+        // Note: All attributes are made public so that they will be serialized
+        // for the network transfer.
 
         /// <summary>
-        /// The ID of the creators Game-Node-Object needed to find the city to place the new node in
-        /// Just one random object inside of the code city from the new node
+        /// The city where the new node has to be placed.
         /// </summary>
-        public string gameObjectID;
+        public SEECity City = null;
 
         /// <summary>
-        /// The ID of the parent-gameObject of the new GameObject
+        /// The ID of the creators Game-Node object needed to find the city to place the new node in.
+        /// Just one random object inside the code city from the new node.
         /// </summary>
-        public string parentID;
+        public string GameObjectID;
 
         /// <summary>
-        /// true, if the new Node is an inner node, false if its a leaf.
+        /// The ID of the parent gameObject of the new GameObject.
         /// </summary>
-        public bool isInnerNode;
+        public string ParentID;
+
+        /// <summary>
+        /// true, if the new node is an inner node, false if it is a leaf.
+        /// </summary>
+        public bool IsInnerNode;
 
         /// <summary>
         /// The id of the new node.
         /// </summary>
-        public string newNodeID;
+        public string NewNodeID;
 
         /// <summary>
         /// The position of the new node.
         /// </summary>
-        public Vector3 position;
+        public Vector3 Position;
 
         /// <summary>
         /// The scale of the new node.
         /// </summary>
-        public Vector3 scale;
+        public Vector3 Scale;
 
         /// <summary>
-        /// Should the node be placed or moved
+        /// /Whether the the node should be placed or moved.
+        /// FIXME: What does true/false mean here (place or move)?
         /// </summary>
-        public bool place;
+        public bool Place;
 
         /// <summary>
-        /// Should a new node be created
+        /// Whether a new node should be created.
         /// </summary>
-        public bool create;
+        public bool Create;
 
         /// <summary>
-        /// If a node was placed where it should not be placed it needs to be destroyed in the network
+        /// True if a node was placed where it should not be placed. 
+        /// It then needs to be destroyed in the network.
         /// </summary>
-        public bool illegalPlace;
+        public bool IllegalPlace;
 
-        public string dummyName = "tmpNetNewNode";
+        /// <summary>
+        /// FIXME: Documentation required. 
+        /// </summary>
+        private static readonly string dummyName = "tmpNetNewNode";
 
         /// <summary>
         /// Creates a new NewNodeNetAction
         /// </summary>
-        /// <param name="gameObjectID">the  GameObject on that the city is attached for the new node</param>
-        /// <param name="isInnerNode">should it be a inner node</param>
+        /// <param name="gameObjectID">the GameObject the city is attached to for the new node</param>
+        /// <param name="isInnerNode">whether the new node should be an inner node</param>
         /// <param name="newNodeID">id for the new node</param>
         /// <param name="position">the position for the new node</param>
-        /// <param name="place">is the new node in moving or in placing state</param>
-        /// <param name="create">in the first method call a new node needs to be created, but only once</param>
-        /// <param name="illegalPlace">Was the placement illegal</param>
-        public NewNodeNetAction(string gameObjectID, bool isInnerNode, string newNodeID,Vector3 position, Vector3 scale, string parentID, bool place, bool create, bool illegalPlace) : base()
+        /// <param name="place">whether the new node is in moving or in placing state</param>
+        /// <param name="create">in the first method call a new node needs to be created, but only once (FIXME: meaning is unclear)</param>
+        /// <param name="illegalPlace">whether the placement is illegal</param>
+        public NewNodeNetAction
+            (string gameObjectID,
+             bool isInnerNode,
+             string newNodeID,
+             Vector3 position,
+             Vector3 scale,
+             string parentID,
+             bool place,
+             bool create,
+             bool illegalPlace)
+            : base()
         {
-            this.gameObjectID = gameObjectID;
-            this.parentID = parentID;
-            this.isInnerNode = isInnerNode;
-            this.newNodeID = newNodeID;
-            this.position = position;
-            this.scale = scale;
-            this.place = place;
-            this.create = create;
-            this.illegalPlace = illegalPlace;
+            this.GameObjectID = gameObjectID;
+            this.ParentID = parentID;
+            this.IsInnerNode = isInnerNode;
+            this.NewNodeID = newNodeID;
+            this.Position = position;
+            this.Scale = scale;
+            this.Place = place;
+            this.Create = create;
+            this.IllegalPlace = illegalPlace;
         }
 
         /// <summary>
-        /// Things to Execute on the Server (None for this Class). Nessecary, because its abstract
+        /// Things to execute on the server (none for this class). Necessary because it is abstract
         /// in the superclass.
         /// </summary>
         protected override void ExecuteOnServer()
         {
-
+            // Intentionally left blank.
         }
 
         /// <summary>
-        /// Things to Execute on the Client Creates a new GameObject on each client
+        /// Creates a new GameObject on each client.
         /// </summary>
         protected override void ExecuteOnClient()
         {
             if (!IsRequester())
             {
-                //Manages the Creation of a new node on each client
-                if (create)
+                // Manages the creation of a new node on each client.
+                if (Create)
                 {
-                    SceneQueries.GetCodeCity(GameObject.Find(gameObjectID).transform)?.gameObject.TryGetComponent(out city);
-                    if (city != null)
+                    SceneQueries.GetCodeCity(GameObject.Find(GameObjectID).transform)?.gameObject.TryGetComponent(out City);
+                    if (City != null)
                     {
                         //Just a gameObject to attatch the newNodeScript on
                         GameObject dummy = new GameObject();
                         dummy.AddComponent<NewNodeAction>();
-                        dummy.GetComponent<NewNodeAction>().Network = true; 
-                        dummy.GetComponent<NewNodeAction>().NodeID = newNodeID;
-                        dummy.GetComponent<NewNodeAction>().City = city;
-                        dummy.GetComponent<NewNodeAction>().SetIsInnerNode(isInnerNode);
+                        dummy.GetComponent<NewNodeAction>().Network = true;
+                        dummy.GetComponent<NewNodeAction>().NodeID = NewNodeID;
+                        dummy.GetComponent<NewNodeAction>().City = City;
+                        dummy.GetComponent<NewNodeAction>().SetIsInnerNode(IsInnerNode);
                         dummy.GetComponent<NewNodeAction>().NewNode();
                         dummy.name = dummyName;
 
                     }
                 }
-                //Manages the placement of the new node on each client
-                else if(place)
+                // Manages the placement of the new node on each client.
+                else if (Place)
                 {
-                    if (illegalPlace)
+                    if (IllegalPlace)
                     {
                         Object.Destroy(GameObject.Find(dummyName).GetComponent<NewNodeAction>().GONode);
                     }
                     else
                     {
-                        GameObject.Find(dummyName).GetComponent<NewNodeAction>().NetworkPlaceNode(position, scale, parentID);
+                        GameObject.Find(dummyName).GetComponent<NewNodeAction>().NetworkPlaceNode(Position, Scale, ParentID);
                     }
                     Object.Destroy(GameObject.Find(dummyName).GetComponent<NewNodeAction>());
                     Object.Destroy(GameObject.Find(dummyName));
                 }
-                //Let the new node move with the cursor of the master
+                // Let the new node move with the cursor of the master.
                 else
                 {
-                    GameObject.Find(dummyName).GetComponent<NewNodeAction>().GONode.transform.position = position;
-                    GameObject.Find(dummyName).GetComponent<NewNodeAction>().GONode.SetScale(scale);
+                    GameObject dummyGameNode = GameObject.Find(dummyName).GetComponent<NewNodeAction>().GONode;
+                    dummyGameNode.transform.position = Position;
+                    dummyGameNode.SetScale(Scale);
                 }
             }
         }
-
     }
 }
