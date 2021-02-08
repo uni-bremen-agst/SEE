8f978c3ed53649e11d6c3a7bd049c0f0b9b26ec0
mblecker <mblecker@uni-bremen.de>
2021-02-08 17:34:43 +0100

    #187 new Edge met action

--- Assets/SEE/Controls/Actions/AddEdgeAction.cs
+++ Assets/SEE/Controls/Actions/AddEdgeAction.cs
@@ -97,6 +97,7 @@ namespace SEE.Controls.Actions
                         try
                         {
                             city.Renderer.DrawEdge(from, to);
+                            new AddEdgeNetAction(from.name, to.name).Execute(null);
                         }
                         catch (Exception e)
                         {
---
+++ Assets/SEE/Net/Scripts/Actions/AddEdgeNetAction.cs
@@ -0,0 +1,62 @@
+﻿using SEE.Game;
+using System;
+using UnityEngine;
+/// <summary>
+/// Creates a new edge throw the network on each client
+/// </summary>
+public class AddEdgeNetAction : SEE.Net.AbstractAction
+{
+    /// <summary>
+    /// The id of the gameObject from which the edge should be drawn
+    /// </summary>
+    public string fromId;
+
+    /// <summary>
+    /// The id of the gameObject to which the edge should be drawn
+    /// </summary>
+    public string toId;
+
+
+    /// <summary>
+    /// Constructs a AddEdgeNetAction
+    /// </summary>
+    /// <param name="fromId">The id of the gameObject from which the edge should be drawn</param>
+    /// <param name="toId">The id of the gameObject to which the edge should be drawn</param>
+    public AddEdgeNetAction(string fromId, string toId)
+    {
+        this.fromId = fromId;
+        this.toId = toId;
+    }
+
+    /// <summary>
+    /// Stuff to execute on the Server
+    /// </summary>
+    protected override void ExecuteOnServer()
+    {
+    }
+    /// <summary>
+    /// Creates a new Edge on each client
+    /// </summary>
+    protected override void ExecuteOnClient()
+    {
+        if (!IsRequester())
+        {
+            GameObject fromGO = GameObject.Find(fromId);
+            SEECity city = null;
+            SceneQueries.GetCodeCity(fromGO.transform)?.gameObject.TryGetComponent(out city);
+            if(city != null)
+            {
+                try
+                {
+                    city.GetComponent<GraphRenderer>().DrawEdge(fromGO, GameObject.Find(toId));
+                }
+                
+                catch (Exception e)
+                {
+                    Debug.LogError($"The new edge from {fromGO.name} to {GameObject.Find(toId).name} could not be created: {e.Message}.\n");
+                }
+            }
+        }
+    }
+
+}
