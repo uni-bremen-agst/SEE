8106e4cba6ae34022ae45ce01862d814d9ea3b80
Falko Galperin <falko1@uni-bremen.de>
2021-02-08 23:29:48 +0100

    #166 Make new menus functional on Desktop
    Menus can now be instantiated and used on the
    Desktop, including the action selection menu
    (formerly circular/player menu).
    The following issues have to be addressed before
    merging into master:
    1. SelectionMenu has to be implemented
    2. Icons for entries currently don't work
    3. FG color is not ideal (different BG colors should
       be used as well)

--- Assets/SEE/Game/UI/Menu/Desktop/DesktopSelectionMenu.cs
+++ Assets/SEE/Game/UI/Menu/Desktop/DesktopSelectionMenu.cs
@@ -1,6 +1,4 @@
-﻿using UnityEngine;
-
-namespace SEE.Game.UI
+﻿namespace SEE.Game.UI
 {
     /// <summary>
     /// Implements Desktop UI for selection menus.
@@ -9,17 +7,15 @@ namespace SEE.Game.UI
     {
         protected override void StartDesktop()
         {
-            // TODO: Setup for Desktop UI here (or keep method empty)
+            base.StartDesktop();
+            //TODO: Use horizontal selector
         }
 
         protected override void UpdateDesktop()
         {
-            // TODO: Just for testing purposes, pressing space currently cycles through available entries.
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                ToggleMenuEntry entry = GetActiveEntry() ?? Entries[0];
-                OnEntrySelected(Entries[(Entries.IndexOf(entry) + 1) % Entries.Count]);
-            }
+            base.UpdateDesktop();
+            // ToggleMenuEntry entry = GetActiveEntry() ?? Entries[0];
+            // OnEntrySelected(Entries[(Entries.IndexOf(entry) + 1) % Entries.Count]);
         }
     }
 }
--- Assets/SEE/Utils/ColorExtensions.cs
+++ Assets/SEE/Utils/ColorExtensions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace SEE.Utils
 {
@@ -12,6 +13,22 @@ namespace SEE.Utils
         public static Color Lighter(this Color color)
         {
             return Color.Lerp(color, Color.white, 0.5f); // To lighten by 50 %
+        }
+
+        /// <summary>
+        /// Calculates and returns the optimal text color for the given <paramref name="backgroundColor"/>.
+        /// </summary>
+        /// <remarks>The code was adapted from
+        /// <a href="https://www.codeproject.com/Articles/16565/Determining-Ideal-Text-Color-Based-on-Specified-Ba">
+        /// this W3C compliant formula</a>. <a href="https://archive.vn/wip/V4bka">(Archive Link)</a></remarks>
+        /// <param name="backgroundColor">The background color on which the text will be put</param>
+        /// <returns>the optimal text color for the given <paramref name="backgroundColor"/>.</returns>
+        public static Color IdealTextColor(this Color backgroundColor)
+        {
+            const int nThreshold = 105;
+            int bgDelta = Convert.ToInt32((backgroundColor.r * 0.299f) + (backgroundColor.g * 0.587) 
+                                                                       + (backgroundColor.b * 0.114));
+            return (255 - bgDelta < nThreshold) ? Color.black : Color.white;
         }
     }
 }
