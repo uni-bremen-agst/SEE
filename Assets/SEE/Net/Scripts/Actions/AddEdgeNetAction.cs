227c79e06736c0d7844011744407ebdb536f18bf
Rainer Koschke <koschke@uni-bremen.de>
2021-02-09 16:38:54 +0100

    #187 Added namespace.

--- Assets/SEE/Net/Scripts/Actions/AddEdgeNetAction.cs
+++ Assets/SEE/Net/Scripts/Actions/AddEdgeNetAction.cs
@@ -3,82 +3,85 @@ using SEE.GO;
 using System;
 using UnityEngine;
 
-/// <summary>
-/// Creates a new edge through the network on each client.
-/// </summary>
-public class AddEdgeNetAction : SEE.Net.AbstractAction
+namespace SEE.Controls.Actions
 {
     /// <summary>
-    /// The id of the gameObject from which the edge should be drawn.
+    /// Creates a new edge through the network on each client.
     /// </summary>
-    public string fromId;
-
-    /// <summary>
-    /// The id of the gameObject to which the edge should be drawn.
-    /// </summary>
-    public string toId;
-
-    /// <summary>
-    /// Constructs an AddEdgeNetAction.
-    /// </summary>
-    /// <param name="fromId">The id of the gameObject from which the edge should be drawn</param>
-    /// <param name="toId">The id of the gameObject to which the edge should be drawn</param>
-    public AddEdgeNetAction(string fromId, string toId)
+    public class AddEdgeNetAction : Net.AbstractAction
     {
-        this.fromId = fromId;
-        this.toId = toId;
-    }
+        /// <summary>
+        /// The id of the gameObject from which the edge should be drawn.
+        /// </summary>
+        public string fromId;
 
-    /// <summary>
-    /// Stuff to execute on the Server. Nothing to be done here.
-    /// </summary>
-    protected override void ExecuteOnServer()
-    {
-        // Intentionally left blank.
-    }
+        /// <summary>
+        /// The id of the gameObject to which the edge should be drawn.
+        /// </summary>
+        public string toId;
 
-    /// <summary>
-    /// Creates the new edge on each client.
-    /// </summary>
-    protected override void ExecuteOnClient()
-    {
-        if (!IsRequester())
+        /// <summary>
+        /// Constructs an AddEdgeNetAction.
+        /// </summary>
+        /// <param name="fromId">The id of the gameObject from which the edge should be drawn</param>
+        /// <param name="toId">The id of the gameObject to which the edge should be drawn</param>
+        public AddEdgeNetAction(string fromId, string toId)
         {
-            GameObject fromGO = GameObject.Find(fromId);
-            if (fromGO)
-            {
-                GameObject toGO = GameObject.Find(toId);
-                if (toGO)
-                {
-                    Transform codeCity = SceneQueries.GetCodeCity(fromGO.transform);
-                    if (codeCity)
-                    {
-                        if (codeCity.gameObject.TryGetComponentOrLog(out SEECity city))
-                        {
-                            try
-                            {
-                                city.Renderer.DrawEdge(fromGO, toGO);
-                            }
+            this.fromId = fromId;
+            this.toId = toId;
+        }
 
-                            catch (Exception e)
+        /// <summary>
+        /// Stuff to execute on the Server. Nothing to be done here.
+        /// </summary>
+        protected override void ExecuteOnServer()
+        {
+            // Intentionally left blank.
+        }
+
+        /// <summary>
+        /// Creates the new edge on each client.
+        /// </summary>
+        protected override void ExecuteOnClient()
+        {
+            if (!IsRequester())
+            {
+                GameObject fromGO = GameObject.Find(fromId);
+                if (fromGO)
+                {
+                    GameObject toGO = GameObject.Find(toId);
+                    if (toGO)
+                    {
+                        Transform codeCity = SceneQueries.GetCodeCity(fromGO.transform);
+                        if (codeCity)
+                        {
+                            if (codeCity.gameObject.TryGetComponentOrLog(out SEECity city))
                             {
-                                Debug.LogError($"The new edge from {fromGO.name} to {toGO.name} could not be created: {e.Message}.\n");
+                                try
+                                {
+                                    city.Renderer.DrawEdge(fromGO, toGO);
+                                }
+
+                                catch (Exception e)
+                                {
+                                    Debug.LogError($"The new edge from {fromGO.name} to {toGO.name} could not be created: {e.Message}.\n");
+                                }
                             }
+                        }
+                        else
+                        {
+                            Debug.LogError($"Game node named {fromId} is not contained in a code city.\n");
                         }
                     }
                     else
                     {
-                        Debug.LogError($"Game node named {fromId} is not contained in a code city.\n");
+                        Debug.LogError($"There is no game node named {toId}.\n");
                     }
                 }
                 else
                 {
-                    Debug.LogError($"There is no game node named {toId}.\n");
+                    Debug.LogError($"There is no game node named {fromId}.\n");
                 }
-            }
-            else
-            {
-                Debug.LogError($"There is no game node named {fromId}.\n");
             }
         }
     }
