8db34ea0e395a5a3e9b103070789a413785b1d3f
Falko Galperin <falko1@uni-bremen.de>
2021-02-09 22:49:16 +0100

    #166 Delete old classes, improve architecture a bit

--- Assets/SEE/GameObjects/Menu/PlayerMenu.cs
+++ Assets/SEE/GameObjects/Menu/PlayerMenu.cs
@@ -1,30 +1,44 @@
-﻿using SEE.Controls.Actions;
+﻿using System;
+using SEE.Controls.Actions;
+using SEE.Game.UI;
 using SEE.Utils;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions;
 
-namespace SEE.Game.UI
+namespace SEE.GO.Menu
 {
     /// <summary>
-    /// This class is responsible for creating various concrete menus in SEA.
+    /// Implements the behaviour of the in-game player menu, in which action states can be selected.
     /// </summary>
-    public static class MenuFactory
+    public class PlayerMenu : MonoBehaviour
     {
+
+        /// <summary>
+        /// An array of all possible action state types the player can be in.
+        /// </summary>
+        private readonly ActionState.Type[] actionStateTypes = (ActionState.Type[])Enum.GetValues(typeof(ActionState.Type));
+
+        /// <summary>
+        /// The UI object representing the menu the user chooses the action state from.
+        /// </summary>
+        private SelectionMenu ModeMenu;
+        
         /// <summary>
         /// This creates and returns the mode menu, with which you can select the active game mode.
         /// 
         /// Available modes are:
-        /// - Browsing
-        /// - Moving
-        /// - Mapping
-        /// - Drawing Edges
-        /// - Rotating
+        /// - Move
+        /// - Map
+        /// - Draw Edges
+        /// - Rotate
+        /// - Delete
         /// </summary>
         /// <param name="attachTo">The game object the menu should be attached to. If <c>null</c>, a
         /// new game object will be created.</param>
         /// <returns>the newly created mode menu game object, or if it wasn't null
         /// <paramref name="attachTo"/> with the mode menu attached.</returns>
-        public static GameObject CreateModeMenu(GameObject attachTo = null)
+        private static GameObject CreateModeMenu(GameObject attachTo = null)
         {
             ToggleMenuEntry[] entries = {
                 new ToggleMenuEntry(
@@ -84,6 +98,51 @@ namespace SEE.Game.UI
             }
 
             return modeMenuGO;
+        }
+
+        private void Start()
+        {
+            if (!CreateModeMenu(gameObject).TryGetComponentOrLog(out ModeMenu))
+            {
+                Destroyer.DestroyComponent(this);
+            }
+
+            ActionState.OnStateChanged += OnStateChanged;
+            Assert.IsTrue(actionStateTypes.Length <= 9, 
+                          "Only up to 9 (10 if zero is included) entries can be selected via the numbers on the keyboard!");
+        }
+        
+        /// <summary>
+        /// Called whenever the action state changes.
+        /// This updates the menu to indicate the selected value.
+        /// </summary>
+        /// <param name="value">The new action state</param>
+        private void OnStateChanged(ActionState.Type value)
+        {
+            ModeMenu.SelectEntry((int) value);
+        }
+
+        /// <summary>
+        /// The menu can be enabled/disabled by pressing the space bar.
+        /// Additionally, the action state can be selected via numbers.
+        /// </summary>
+        private void Update()
+        {
+            // Select action state via numbers on the keyboard
+            for (int i = 0; i < ModeMenu.Entries.Count; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                {
+                    ModeMenu.SelectEntry(i);
+                    break;
+                }
+            }
+
+            // space bar toggles menu            
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ModeMenu.ToggleMenu();
+            }
         }
     }
 }
