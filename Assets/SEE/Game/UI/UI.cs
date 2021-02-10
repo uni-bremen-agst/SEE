8dc07c38eaa15fea4673de3e9731400df62c87f1
Rainer Koschke <koschke@uni-bremen.de>
2021-02-10 17:54:39 +0100

    #166 The program's name is actually SEE.

--- Assets/SEE/Game/UI/UI.cs
+++ Assets/SEE/Game/UI/UI.cs
@@ -1,5 +1,5 @@
 ﻿/// <summary>
-/// SEE.Game.UI contains code for the user interface in SEA.
+/// SEE.Game.UI contains code for the user interface in SEE.
 /// This includes both the generalized model (e.g. the Menu class)
 /// and specific implementations (e.g. for VR).
 /// </summary>
