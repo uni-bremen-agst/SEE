6f82462c832f5aa1e1b73871c16b81294d24883a
Rainer Koschke <koschke@uni-bremen.de>
2021-02-12 12:54:39 +0100

    #115 Added namespace.

--- Assets/SEE/Controls/Actions/CanvasGenerator.cs
+++ Assets/SEE/Controls/Actions/CanvasGenerator.cs
@@ -1,51 +1,53 @@
-﻿using SEE.Controls.Actions;
-using UnityEngine;
+﻿using UnityEngine;
 
-/// <summary>
-/// This class instantiates and destroys the adding-node canvas, which contains the addingNodeCanvas prefab.
-/// This also applies to the EditNodeCanvas.
-/// </summary>
-public class CanvasGenerator : MonoBehaviour
+namespace SEE.Controls.Actions
 {
     /// <summary>
-    /// Instantiates the AddingNodeCanvasScript and adds it to the CanvasObject gameObject.
+    /// This class instantiates and destroys the adding-node canvas, which contains the addingNodeCanvas prefab.
+    /// This also applies to the EditNodeCanvas.
     /// </summary>
-    /// <returns>the added AddingNodeCanvasScript</returns>
-    public AddingNodeCanvasAction InstantiateAddingNodeCanvas()
+    public class CanvasGenerator : MonoBehaviour
     {
-        return gameObject.AddComponent<AddingNodeCanvasAction>();
-    }
-
-    /// <summary>
-    /// Instantiates the EditNodeCanvasScript and adds it to the CanvasObject gameObject.
-    /// </summary>
-    /// <returns>the added EditNodeCanvasScript</returns>
-    public EditNodeCanvasAction InstantiateEditNodeCanvas()
-    {
-        return gameObject.AddComponent<EditNodeCanvasAction>();
-    }
-
-    /// <summary>
-    /// Destroys the AddingNodeCanvasScript and each of its children.
-    /// </summary>
-    public void DestroyAddNodeCanvas()
-    {
-        if (gameObject.TryGetComponent(out AddingNodeCanvasAction action))
+        /// <summary>
+        /// Instantiates the AddingNodeCanvasScript and adds it to the CanvasObject gameObject.
+        /// </summary>
+        /// <returns>the added AddingNodeCanvasScript</returns>
+        public AddingNodeCanvasAction InstantiateAddingNodeCanvas()
         {
-            action.DestroyGOAndAllChilds();
-            Destroy(action);
+            return gameObject.AddComponent<AddingNodeCanvasAction>();
         }
-    }
 
-    /// <summary>
-    /// Destroys the EditNodeCanvasScript and each of its children.
-    /// </summary>
-    public void DestroyEditNodeCanvas()
-    {
-        if (gameObject.TryGetComponent(out EditNodeCanvasAction action))
+        /// <summary>
+        /// Instantiates the EditNodeCanvasScript and adds it to the CanvasObject gameObject.
+        /// </summary>
+        /// <returns>the added EditNodeCanvasScript</returns>
+        public EditNodeCanvasAction InstantiateEditNodeCanvas()
         {
-            action.DestroyGOAndAllChilds();
-            Destroy(action);
+            return gameObject.AddComponent<EditNodeCanvasAction>();
+        }
+
+        /// <summary>
+        /// Destroys the AddingNodeCanvasScript and each of its children.
+        /// </summary>
+        public void DestroyAddNodeCanvas()
+        {
+            if (gameObject.TryGetComponent(out AddingNodeCanvasAction action))
+            {
+                action.DestroyGOAndAllChilds();
+                Destroy(action);
+            }
+        }
+
+        /// <summary>
+        /// Destroys the EditNodeCanvasScript and each of its children.
+        /// </summary>
+        public void DestroyEditNodeCanvas()
+        {
+            if (gameObject.TryGetComponent(out EditNodeCanvasAction action))
+            {
+                action.DestroyGOAndAllChilds();
+                Destroy(action);
+            }
         }
     }
 }
