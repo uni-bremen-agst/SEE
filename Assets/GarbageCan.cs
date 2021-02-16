48a8b815a3685d3e288cc25884cb44c0a0e1fbd8
ThoreFrenzel <tfrenzel@uni-bremen.de>
2021-02-16 10:40:07 +0100

    #180 added an onclick-operation-placeholder to the garbage can

---
+++ Assets/GarbageCan.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class GarbageCan : MonoBehaviour
+{
+    /// <summary>
+    /// A Ray from the mouse-position to the hovered object
+    /// </summary>
+    Ray ray;
+
+    /// <summary>
+    /// The object which was hitten by the ray
+    /// </summary>
+    RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit) && hit.transform == gameObject.transform)
+        {
+            // Question: Is this mechanism not implemented yet?
+            // Fixme: Onclick - just the colliderName(GarbageCan) will be printed out - there has to be a interaction-possibility.
+            Debug.Log(hit.collider.name);
+        }
+    }
+}
