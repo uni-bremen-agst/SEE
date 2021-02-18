8da551a69fc54324af86e6898c3f473672dc588c
Robin <r.marco.2112@gmail.com>
2021-02-18 17:45:30 +0100

    Added multiplayer function for delete action

---
+++ Assets/SEE/Net/Scripts/Actions/DeleteNetAction.cs
@@ -0,0 +1,30 @@
+﻿using SEE.Controls.Actions;
+using SEE.Net;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteNetAction : AbstractAction
+{
+    public string gameObject;
+
+    public DeleteNetAction(string gameObject)
+    {
+        this.gameObject = gameObject;
+    }
+
+    protected override void ExecuteOnServer()
+    {
+    }
+
+    protected override void ExecuteOnClient()
+    {
+        if (!IsRequester())
+        {
+            Debug.Log(gameObject);
+            DeleteAction.DeleteSelectedObject(GameObject.Find(gameObject));
+        }
+    }
+
+    
+}
