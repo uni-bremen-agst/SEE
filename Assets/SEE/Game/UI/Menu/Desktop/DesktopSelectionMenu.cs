9678ff19aa028a6cda74876fac734cf7d01357d3
Falko Galperin <falko1@uni-bremen.de>
2021-02-19 16:21:54 +0100

    #166 Indicate selected entry in new menu style

--- Assets/SEE/Game/UI/Menu/Desktop/DesktopSelectionMenu.cs
+++ Assets/SEE/Game/UI/Menu/Desktop/DesktopSelectionMenu.cs
@@ -1,12 +1,17 @@
 ﻿using Michsky.UI.ModernUIPack;
+using TMPro;
 
 namespace SEE.Game.UI
 {
+
     /// <summary>
     /// Implements Desktop UI for selection menus.
     /// </summary>
     public partial class SelectionMenu
     {
+        
+        private ToggleMenuEntry currentSelectedEntry;
+        
         protected override void SetUpDesktopContent()
         {
             base.SetUpDesktopContent();
@@ -21,8 +26,27 @@ namespace SEE.Game.UI
 
         protected override void UpdateDesktop()
         {
-            //TODO: For the active element, add an animation/outline/gradient showcasing it
             base.UpdateDesktop();
+            // Only change when a different entry has been selected
+            if (currentSelectedEntry != GetActiveEntry())
+            {
+                currentSelectedEntry = GetActiveEntry();
+                foreach (ButtonManagerBasicWithIcon manager in ButtonManagers)
+                {
+                    // Indicate selected button by typesetting it [LIKE THIS]
+                    if (manager.buttonText.Equals(currentSelectedEntry.Title))
+                    {
+                        manager.buttonText = $"[{currentSelectedEntry.Title}]";
+                        manager.normalText.fontStyle = FontStyles.UpperCase;
+                    }
+                    else
+                    {
+                        manager.buttonText = manager.buttonText.TrimStart('[').TrimEnd(']');
+                        manager.normalText.fontStyle = 0;
+                    }
+                    manager.normalText.text = manager.buttonText;
+                }
+            }
         }
     }
 }
