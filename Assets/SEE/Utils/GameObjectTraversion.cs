fda7ce2fcf9b62c1f0882f6e24d43fed16cb10c7
ThoreFrenzel <tfrenzel@uni-bremen.de>
2021-02-25 11:55:15 +0100

    #180 moved parent-traversion to a new Utility-class

---
+++ Assets/SEE/Utils/GameObjectTraversion.cs
@@ -0,0 +1,43 @@
+﻿using SEE.DataModel;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameObjectTraversion
+{
+
+    public static List<GameObject> GetAllChildNodesAsGameObject(List<GameObject> childrenOfParent, GameObject parent)
+    {
+        List<GameObject> childrenOfThisParent = new List<GameObject>();
+        if (!childrenOfParent.Contains(parent))
+        {
+            childrenOfParent.Add(parent);
+        }
+        int gameNodeCount = childrenOfParent.Count;
+
+        foreach (Transform child in parent.transform)
+        {
+            if (child.gameObject.CompareTag(Tags.Node))
+            {
+                if (!childrenOfParent.Contains(child.gameObject))
+                {
+                    childrenOfParent.Add(child.gameObject);
+                }
+                childrenOfThisParent.Add(child.gameObject);
+            }
+        }
+
+        if (childrenOfParent.Count == gameNodeCount)
+        {
+            return childrenOfParent;
+        }
+        else
+        {
+            foreach (GameObject childs in childrenOfThisParent)
+            {
+                GetAllChildNodesAsGameObject(childrenOfParent,childs);
+            }
+            return childrenOfParent;
+        }
+    }
+
+}
