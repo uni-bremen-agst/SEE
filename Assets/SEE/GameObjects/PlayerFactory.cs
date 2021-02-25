d6bf6b4356300952fce1744a5cf5161540134a33
Rainer Koschke <koschke@uni-bremen.de>
2021-02-25 18:16:30 +0100

    #199 Players for HoloLens and Gamepads/Touchpads are now created at runtime, too.

--- Assets/SEE/GameObjects/PlayerFactory.cs
+++ Assets/SEE/GameObjects/PlayerFactory.cs
@@ -11,36 +11,83 @@ namespace SEE.GO
     internal static class PlayerFactory
     {
         /// <summary>
-        /// Creates and returns a desktop player from prefab Resources/Prefabs/Players/DesktopPlayer
+        /// Creates and returns a desktop player instantiated from prefab Resources/Prefabs/Players/DesktopPlayer
         /// with all required components attached to it.
         /// </summary>
         /// <param name="plane">the culling plane the DesktopPlayerMovement should be focusing</param>
         /// <returns>a player for the desktop environment</returns>
         public static GameObject CreateDesktopPlayer(Plane plane)
         {
-            // We are assuming that all necessary components are already attached in the prefab.
-            UnityEngine.Object desktopPrefab = Resources.Load<GameObject>("Prefabs/Players/DesktopPlayer");
-            GameObject desktopPlayer = GameObject.Instantiate(desktopPrefab) as GameObject;
-            UnityEngine.Assertions.Assert.IsNotNull(desktopPlayer);
-            desktopPlayer.name = PlayerInputType.DesktopPlayer.ToString();
-            desktopPlayer.tag = Tags.MainCamera;
-            desktopPlayer.GetComponent<DesktopPlayerMovement>().focusedObject = plane;
-            return desktopPlayer;
+            GameObject player = InstantiatePlayer("Prefabs/Players/DesktopPlayer");
+            player.name = PlayerInputType.DesktopPlayer.ToString();
+            player.tag = Tags.MainCamera;
+            player.GetComponent<DesktopPlayerMovement>().focusedObject = plane;
+            return player;
         }
 
         /// <summary>
-        /// Creates and returns a VR player from prefab Resources/Prefabs/Players/VRPlayer
+        /// Creates and returns a VR player instantiated from prefab Resources/Prefabs/Players/VRPlayer
         /// with all required components attached to it.
         /// </summary>
         /// <returns>a player for the VR environment</returns>
         public static GameObject CreateVRPlayer()
         {
+            GameObject player = InstantiatePlayer("Prefabs/Players/VRPlayer");
+            player.name = PlayerInputType.VRPlayer.ToString();
+            return player;
+        }
+
+        /// <summary>
+        /// Creates and returns a HoloLens player instantiated from prefab Resources/Prefabs/Players/HoloLensPlayer
+        /// with all required components attached to it.
+        /// </summary>
+        /// <returns>a player for the AR environment of HoloLens</returns>
+        public static GameObject CreateHololensPlayer()
+        {
+            GameObject player = InstantiatePlayer("Prefabs/Players/HoloLensPlayer");
+            player.name = PlayerInputType.HoloLensPlayer.ToString();
+            return player;
+        }
+
+        /// <summary>
+        /// Creates and returns a player for touchpads or gamepads instantiated from prefab Resources/Prefabs/Players/InControl
+        /// with all required components attached to it.
+        /// </summary>
+        /// <returns>a player for the gamepad/touchpad environment</returns>
+        public static GameObject CreateTouchGamepadPlayer()
+        {
+            GameObject player = InstantiatePlayer("Prefabs/Players/InControl");
+            player.name = PlayerInputType.TouchGamepadPlayer.ToString();
+            return player;
+        }
+
+        /// <summary>
+        /// Returns an instantiation of the given <paramref name="prefabPath"/>.
+        /// </summary>
+        /// <param name="prefabPath">path to the prefab; must be contained in a folder Resources within Assets</param>
+        /// <exception cref="System.Exception">thrown if <paramref name="prefabPath"/> does not denote a prefab
+        /// or that prefab cannot be instantiated</exception>
+        /// <returns>instantiated prefab</returns>
+        private static GameObject InstantiatePlayer(string prefabPath)
+        {
             // We are assuming that all necessary components are already attached in the prefab.
-            UnityEngine.Object steamVrPrefab = Resources.Load<GameObject>("Prefabs/Players/VRPlayer");
-            GameObject vrPlayer = GameObject.Instantiate(steamVrPrefab) as GameObject;
-            UnityEngine.Assertions.Assert.IsNotNull(vrPlayer);
-            vrPlayer.name = PlayerInputType.VRPlayer.ToString();
-            return vrPlayer;
+            Object prefab = Resources.Load<GameObject>(prefabPath);
+            if (prefab == null)
+            {
+                throw new System.Exception($"Prefab {prefabPath} does not exist.\n");
+            }
+            else
+            {
+                GameObject player = GameObject.Instantiate(prefab) as GameObject;
+                if (player == null)
+                {
+                    throw new System.Exception($"Prefab {prefabPath} exists but could not be instantiated.\n");
+                }
+                else
+                {
+                    return player;
+                }
+            }
         }
     }
 }
