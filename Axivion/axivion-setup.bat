38349546953d1ee56cef6ee059fcd12db944f751
Rainer Koschke <koschke@uni-bremen.de>
2021-03-03 13:00:47 +0100

    #202 Added path to Axivion Suite executables.

--- Axivion/axivion-setup.bat
+++ Axivion/axivion-setup.bat
@@ -1,3 +1,6 @@
+REM include Bauhaus bin in exectuable path
+set "PATH=C:\Program Files (x86)\Bauhaus\bin;%PATH%"
+
 set "BAUHAUS_CONFIG=%userprofile%\SEE\Axivion"
 set "AXIVION_DASHBOARD_CONFIG=%userprofile%\Axivion"
 set "AXIVION_DATABASES_DIR=%userprofile%\Axivion"
@@ -6,3 +9,6 @@ set "REQUESTS_CA_BUNDLE=%AXIVION_DASHBOARD_CONFIG%\cert\auto.crt"
 REM specific to this machine
 REM set "AXIVION_DASHBOARD_URL=https://localhost:9443/axivion/"
 set "AXIVION_DASHBOARD_URL=https://swt-jenkins.informatik.uni-bremen.de:9443/axivion/"
+
+REM execute command line parameters
+%*
