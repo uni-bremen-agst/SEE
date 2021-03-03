50597148c0ab9ad4af3c9a41ae7dd5a5ca3ae19d
axivion <koschke@axivion.com>
2021-03-03 14:37:05 +0100

    #202 Adjusting variables for Jenkins

--- Axivion/axivion-setup.bat
+++ Axivion/axivion-setup.bat
@@ -3,12 +3,15 @@ REM run the Axivion Suite tools and then executes the command line parameters
 REM passed to this batch file. If there are none, only the variables are
 REM set.
 
-REM include Bauhaus bin in exectuable path
+REM Python
 set "PATH=C:\Users\SWT\AppData\Local\Programs\Python\Python38;%PATH%"
+set "BAUHAUS_PYTHON=C:\Users\SWT\AppData\Local\Programs\Python\Python38\python.exe"
+
+REM include Bauhaus bin in exectuable path
 set "PATH=C:\Program Files (x86)\Bauhaus\bin;%PATH%"
 
 REM set "BAUHAUS_CONFIG=%userprofile%\SEE\Axivion"
-set "BAUHAUS_CONFIG=%WORKSPACE%\SEE\Axivion"
+set "BAUHAUS_CONFIG=%WORKSPACE%\Axivion"
 
 REM set "AXIVIONCI=%userprofile%\Axivion"
 set "AXIVIONCI=C:\Users\SWT\Axivion"
