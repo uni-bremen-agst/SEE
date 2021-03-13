b7c297e7f22a5800e7cb2e3a44177910dee7d774
mblecker <mblecker@uni-bremen.de>
2021-03-13 15:17:01 +0100

    #204 Test net working

--- Assets/SEE/Net/Scripts/Actions/DummyNetAction.cs
+++ Assets/SEE/Net/Scripts/Actions/DummyNetAction.cs
@@ -9,53 +9,54 @@ using SEE.Controls.Actions;
 /// Creates a new edge through the network on each client.
 /// </summary>
 public class DummyNetAction : AbstractAction
+{
+    string action;
+    string gameObjectID;
+    float posx;
+    float posy;
+    float posz;
+
+    public DummyNetAction(string action, string gameObjectID, float posx, float posy, float posz)
     {
-        string action;
-        string gameObjectID;
-        float posx;
-        float posy;
-        float posz;
+        this.action = action;
+        this.gameObjectID = gameObjectID;
+        this.posx = posx;
+        this.posy = posy;
+        this.posz = posz;
 
-        public DummyNetAction(string action, string gameObjectID, float posx, float posy, float posz)
+    }
+
+    /// <summary>
+    /// Stuff to execute on the Server. Nothing to be done here.
+    /// </summary>
+    protected override void ExecuteOnServer()
+    {
+        // Intentionally left blank.
+    }
+
+    /// <summary>
+    /// Creates the new edge on each client.
+    /// </summary>
+    protected override void ExecuteOnClient()
+    {
+        Debug.Log("NET FUNZT");
+        if (!IsRequester())
         {
-            this.action = action;
-            this.gameObjectID = gameObjectID;
-            this.posx = posx;
-            this.posy = posy;
-            this.posz = posz;
-
-        }
-
-        /// <summary>
-        /// Stuff to execute on the Server. Nothing to be done here.
-        /// </summary>
-        protected override void ExecuteOnServer()
-        {
-            // Intentionally left blank.
-        }
-
-        /// <summary>
-        /// Creates the new edge on each client.
-        /// </summary>
-        protected override void ExecuteOnClient()
-        {
-            if (!IsRequester())
+            if (action == "create")
             {
-                if (action == "create")
-                {
 
-                    DummyAction dummy = new DummyAction();
-                    dummy.CreateObjectAt(new Vector3(posx, posy, posz));
+                DummyAction dummy = new DummyAction();
+                dummy.CreateObjectAt(new Vector3(posx, posy, posz));
 
-                }
-                else if (action == "undo")
-                {
+            }
+            else if (action == "undo")
+            {
 
-                }
-                else if (action == "redo")
-                {
+            }
+            else if (action == "redo")
+            {
 
-                }
             }
         }
     }
+}
