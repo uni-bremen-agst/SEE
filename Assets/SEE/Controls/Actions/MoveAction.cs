911e80ccc881f140a9cba9ea9f971d3c7f955f25
Rainer Koschke <koschke@uni-bremen.de>
2021-03-15 12:37:21 +0100

    #180 New placeholder classes for actions to move and rotate nodes. This functionality is currently implemented in NavigationActions and its subclasses. We need that code from there to these new classes later.

---
+++ Assets/SEE/Controls/Actions/MoveAction.cs
@@ -0,0 +1,22 @@
+﻿using SEE.Utils;
+
+namespace SEE.Controls.Actions
+{
+    /// <summary>
+    /// An action to move nodes. 
+    /// 
+    /// Note: This is currently only a placeholder.
+    /// FIXME: This class needs to implemented. It must somehow be integrated with NavigationAction.
+    /// </summary>
+    class MoveAction : AbstractPlayerAction
+    {
+        /// <summary>
+        /// Returns a new instance of <see cref="MoveAction"/>.
+        /// </summary>
+        /// <returns>new instance of <see cref="MoveAction"/></returns>
+        internal static ReversibleAction CreateReversibleAction()
+        {
+            return new MoveAction();
+        }
+    }
+}
