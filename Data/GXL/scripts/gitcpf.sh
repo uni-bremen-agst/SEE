e0738fdc47c59ee25a200a20289511fc7df399f0
Rainer Koschke <koschke@axivion.com>
2021-03-24 12:22:54 +0100

    Clone detection is run on each revision. Results is stored in GXL.

--- Data/GXL/scripts/gitcpf.sh
+++ Data/GXL/scripts/gitcpf.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 # for_each_revision.sh first..last
 
+export PATH=$BAUHAUS/axivion/scripts/:$PATH
+
 let i=0
 # iterate on all merge commits into master in reverse chronological order
 for rev in $(git rev-list --merges --first-parent master HEAD | tac)
 do
-  #git checkout "$rev"
-  let i++
-  timestamp=`git show --no-patch --no-notes --pretty=%ct $rev`
-  echo "$i" "$rev" "$timestamp"
+    let i++
+    timestamp=`git show --no-patch --no-notes --pretty=%cd $rev`
+    echo "$i" "$rev" "$timestamp"
+    git checkout "$rev"
+    # detect clones
+    cpf -m 100 -C 2 -c clones.cpf -s clones.csv -B $PWD -i Assets/SEE/**.cs .
+    # generate RFG
+    cpfcsv2rfg clones.cpf clones.csv clones.rfg
+    # export to GXL
+    rfgexport -o Clones -f GXL clones.rfg clones-"$i".gxl
 done
 
 exit 0
