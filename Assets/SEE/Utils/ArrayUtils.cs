754508c19665ea5ff904738d47382a7390dbdf58
Torben Groß <TorbenGross@gmx.de>
2021-04-25 14:05:28 +0200

    #163 Scene configuration and improved inspector

---
+++ Assets/SEE/Utils/ArrayUtils.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace SEE.Utils
+{
+    public static class ArrayUtils
+    {
+        /// <summary>
+        /// Creates new array of length <paramref name="length"/> and each slot with the result of
+        /// <paramref name="objCreatorFunc"/>, respectively.
+        /// </summary>
+        /// <typeparam name="T">The type of the array.</typeparam>
+        /// <param name="length">The lenght of the array.</param>
+        /// <param name="objCreatorFunc">The function, that creates the object for each given index.</param>
+        /// <returns>The created array.</returns>
+        public static T[] New<T>(int length, Func<int, T> objCreatorFunc)
+        {
+            T[] result = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = objCreatorFunc(i);
+            }
+            return result;
+        }
+    }
+}
