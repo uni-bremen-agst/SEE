ce41c294462dfe3be01cf720cc9812959c318c40
Rainer Koschke <koschke@uni-bremen.de>
2021-05-13 16:07:26 +0200

    #324 Various forms of speech recognition for Windows 10.

---
+++ Assets/SEE/Controls/SpeechInput.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SEE.Controls
+{
+    /// <summary>
+    /// Super class for speech input.
+    /// </summary>
+    public abstract class SpeechInput : MonoBehaviour
+    {
+        // Useful information about this speech recognition:
+        // https://lightbuzz.com/speech-recognition-unity-update/
+    }
+}
