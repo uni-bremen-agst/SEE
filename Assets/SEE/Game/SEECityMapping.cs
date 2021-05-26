31f3516cbaa4fbadfe78593e13b92a49ae65fb44
Rainer Koschke <koschke@uni-bremen.de>
2021-05-26 17:16:50 +0200

    #163 Added comments. Added Save/Restore as reminder that these must be overridden when attributes are added to this class that need to be saved to a configuration file.

--- Assets/SEE/Game/SEECityMapping.cs
+++ Assets/SEE/Game/SEECityMapping.cs
@@ -1,10 +1,21 @@
 using SEE.DataModel.DG;
+using SEE.Utils;
+using System.Collections.Generic;
 using UnityEngine.Assertions;
 
 namespace SEE.Game
 {
+    /// <summary>
+    /// A code city that supports architectural mappings from
+    /// implementation nodes onto architecture nodes.
+    /// 
+    /// FIXME: Should this class rather derive from <see cref="SEECity"/>?
+    /// </summary>
     public class SEECityMapping : AbstractSEECity
     {
+        /// <summary>
+        /// The mapping of implementation nodes onto architecture nodes.
+        /// </summary>
         public Graph mappingGraph;
 
         public void ReDrawGraph()
@@ -19,8 +30,27 @@ namespace SEE.Game
         {
             Assert.IsNotNull(mappingGraph);
 
-            GraphRenderer graphRenderer = new GraphRenderer(this, mappingGraph);
-            graphRenderer.Draw(gameObject);
+            new GraphRenderer(this, mappingGraph).Draw(gameObject);
+        }
+
+        //--------------------------------
+        // Configuration file input/output
+        //--------------------------------
+
+        protected override void Save(ConfigWriter writer)
+        {
+            base.Save(writer);
+            // If any attribute is added to this class that should be contained in the
+            // configuration file, then do not forget to add the necessary
+            // statements here.
+        }
+
+        protected override void Restore(Dictionary<string, object> attributes)
+        {
+            base.Restore(attributes);
+            // If any attribute is added to this class that should be restored from the
+            // configuration file, then do not forget to add the necessary
+            // statements here.
         }
     }
 }
