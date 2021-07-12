199423e683401016628c3b05762de4bd1479f86b
Rainer Koschke <koschke@uni-bremen.de>
2021-07-12 14:53:09 +0200

    #62 Added license header.

--- Assets/SEE/Game/UI/ConfigMenu/VRInputModule.cs
+++ Assets/SEE/Game/UI/ConfigMenu/VRInputModule.cs
@@ -1,6 +1,29 @@
+// Copyright 2021 Ruben Smidt
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Valve.VR;
+
 namespace SEE.Game.UI.ConfigMenu
 {
     /// <summary>
@@ -11,8 +34,7 @@ namespace SEE.Game.UI.ConfigMenu
     public class VRInputModule : BaseInputModule
     {
         private SteamVR_Input_Sources _inputSource = SteamVR_Input_Sources.LeftHand;
-        private SteamVR_Action_Boolean _clickAction =
-            SteamVR_Actions._default.InteractUI;
+        private SteamVR_Action_Boolean _clickAction = SteamVR_Actions._default.InteractUI;
 
         private GameObject _currentObject;
         private PointerEventData _data;
@@ -22,8 +44,6 @@ namespace SEE.Game.UI.ConfigMenu
         protected override void Awake()
         {
             base.Awake();
-
-
             _data = new PointerEventData(eventSystem);
         }
 
