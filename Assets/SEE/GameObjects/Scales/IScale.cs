ed3997f955b1cfaf74372fb44e33d793667ef423
Falko <falko1@uni-bremen.de>
2021-07-13 00:55:54 +0200

    Implement per-level metric scaling

--- Assets/SEE/GameObjects/Scales/IScale.cs
+++ Assets/SEE/GameObjects/Scales/IScale.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using SEE.DataModel.DG;
 using UnityEngine;
 
@@ -18,14 +19,16 @@ namespace SEE.GO
         /// <param name="minimalLength">the minimal value that can be returned by this scaling</param>
         /// <param name="minimalLength">the maximal value that can be returned by this scaling</param>
         /// <param name="leavesOnly">if true, only the leaf nodes are considered</param>
-        public IScale(ICollection<Graph> graphs, IList<string> metrics, float minimalLength, float maximalLength, 
-                      bool leavesOnly)
+        protected IScale(IEnumerable<Graph> graphs, IList<string> metrics, float minimalLength, float maximalLength,
+                         bool leavesOnly)
         {
             this.metrics = metrics;
             this.minimalLength = minimalLength;
             this.maximalLength = maximalLength;
             this.leavesOnly = leavesOnly;
-            metricMaxima = DetermineMetricMaxima(graphs, metrics, leavesOnly);
+            metricMaxima = new Dictionary<string, float>();
+            metricLevelMaxima = new Dictionary<int, Dictionary<string, float>>();
+            DetermineMetricMaxima(graphs);
         }
 
         /// <summary>
@@ -39,17 +42,24 @@ namespace SEE.GO
         protected readonly Dictionary<string, float> metricMaxima;
 
         /// <summary>
+        /// The maximal values of all metrics on each given level, as a map from level -> metric-name -> maximal value.
+        /// </summary>
+        protected readonly Dictionary<int, Dictionary<string, float>> metricLevelMaxima;
+
+        /// <summary>
         /// the minimal value that can be returned by this scaling
         /// </summary>
         protected readonly float minimalLength;
+
         /// <summary>
         /// the maximal value that can be returned by this scaling
         /// </summary>
         protected readonly float maximalLength;
+
         /// <summary>
         /// If true, the normalization is done only for leaf nodes.
         /// </summary>
-        protected readonly bool leavesOnly;
+        private readonly bool leavesOnly;
 
         /// <summary>
         /// Yields a normalized value of the given node metric. The type of normalization
@@ -70,6 +80,25 @@ namespace SEE.GO
         public abstract float GetNormalizedValue(string metric, float value);
 
         /// <summary>
+        /// Yields a normalized value of the given node metric relative to the level of the node.
+        /// The type of normalization is determined by concrete subclasses.
+        /// </summary>
+        /// <param name="metric">name of the node metric</param>
+        /// <param name="node">node for which to determine the normalized value relative to its level</param>
+        /// <returns>normalized value of node metric relative to its level</returns>
+        public abstract float GetNormalizedValueForLevel(string metric, Node node);
+        
+        /// <summary>
+        /// Yields a normalized value of the given node metric relative to the given <paramref name="level"/>.
+        /// The type of normalization is determined by concrete subclasses.
+        /// </summary>
+        /// <param name="metric">metric name</param>
+        /// <param name="value">value to be normalized</param>
+        /// <param name="level">node level to which the value shall be normalized</param>
+        /// <returns>normalized value relative to given node <paramref name="level"/></returns>
+        public abstract float GetNormalizedValueForLevel(string metric, float value, int level);
+
+        /// <summary>
         /// Yields the normalized value of the maximum of the given metric.
         /// </summary>
         /// <param name="metric">metric for which to return the normalized maximum</param>
@@ -88,6 +117,29 @@ namespace SEE.GO
                 throw new Exception("A metric named " + metric + " does not exist.");
             }
         }
+        
+        /// <summary>
+        /// Yields the normalized value of the maximum of the given metric within the given <paramref name="level"/>.
+        /// </summary>
+        /// <param name="metric">metric for which to return the normalized maximum</param>
+        /// <param name="level">level from which to get the maximum</param>
+        /// <returns>normalized maximum</returns>
+        public float GetNormalizedMaximumForLevel(string metric, int level)
+        {
+            if (metricLevelMaxima.TryGetValue(level, out Dictionary<string, float> dictionary) 
+                && dictionary.TryGetValue(metric, out float value))
+            {
+                return GetNormalizedValueForLevel(metric, value, level);
+            }
+            else
+            {
+                Debug.LogError($"Attempt to retrieve the normalized maximum of metric {metric} in level {level} "
+                               + $"that is not known.\n");
+                Debug.Log("The available normalized metric maxima are as follows:\n");
+                DumpMetricMaxima(metricMaxima);
+                throw new Exception($"A metric named {metric} does not exist in level {level}.");
+            }
+        }
 
         /// <summary>
         /// Returns the normalization value of given node <paramref name="metric"/> of
@@ -98,48 +150,89 @@ namespace SEE.GO
         /// </summary>
         /// <param name="metric">name of the node metric</param>
         /// <param name="node">node whose metric is to be queried</param>
-        /// <returns></returns>
         internal float GetRelativeNormalizedValue(string metric, Node node)
+        {
+            float maximum = GetNormalizedMaximumForLevel(metric, node.Level);
+            return maximum == 0 ? 0 : GetNormalizedValueForLevel(metric, node) / maximum;
+        }
+        
+        /// <summary>
+        /// Returns the normalization value of given node <paramref name="metric"/> of
+        /// <paramref name="node"/> set in relation to the maximum value of the
+        /// normalized <paramref name="metric"/> within the node's level. Hence, the result is in [0,1].
+        /// If the maximum normalized value of <paramref name="metric"/> is 0, 0
+        /// is returned.
+        /// </summary>
+        /// <param name="metric">name of the node metric</param>
+        /// <param name="node">node whose metric is to be queried</param>
+        internal float GetRelativeNormalizedValueInLevel(string metric, Node node)
         {
             float maximum = GetNormalizedMaximum(metric);
             return maximum == 0 ? 0 : GetNormalizedValue(metric, node) / maximum;
         }
 
         /// <summary>
-        /// Returns the maximal values of the given node metrics.
+        /// Returns the maximal values of the node metrics in <see cref="metrics"/>.
         /// </summary>
         /// <param name="graphs">the set of graphs for which to determine the node metric maxima</param>
-        /// <param name="metrics">the metrics for which the maxima are to be gathered</param>
-        /// <param name="leavesOnly">if true, only the leaf nodes are considered</param>
-        /// <returns>metric maxima</returns>
-        protected static Dictionary<string, float> DetermineMetricMaxima(ICollection<Graph> graphs, IList<string> metrics, bool leavesOnly)
+        private void DetermineMetricMaxima(IEnumerable<Graph> graphs)
         {
-            Dictionary<string, float> result = new Dictionary<string, float>();
+            metricMaxima.Clear();
+            metricLevelMaxima.Clear();
+
+            // Set default maxima for metricMaxima
             foreach (string metric in metrics)
             {
-                result[metric] = 0.0f;
+                metricMaxima[metric] = 0.0f;
             }
 
             foreach (Graph graph in graphs)
             {
                 foreach (Node node in graph.Nodes())
                 {
+                    if (!metricLevelMaxima.ContainsKey(node.Level))
+                    {
+                        metricLevelMaxima[node.Level] = new Dictionary<string, float>();
+                    }
+
                     if (!leavesOnly || node.IsLeaf())
                     {
                         foreach (string metric in metrics)
                         {
                             if (node.TryGetNumeric(metric, out float value))
                             {
-                                if (value > result[metric])
+                                if (value > metricMaxima[metric])
                                 {
-                                    result[metric] = value;
+                                    metricMaxima[metric] = value;
+                                }
+
+                                if (!metricLevelMaxima[node.Level].ContainsKey(metric) ||
+                                    value > metricLevelMaxima[node.Level][metric])
+                                {
+                                    metricLevelMaxima[node.Level][metric] = value;
                                 }
                             }
                         }
                     }
                 }
             }
-            return result;
+
+            // Set default maxima for metricLevelMaxima (for each level and each metric where it no maximum has been set)
+            foreach (int level in Enumerable.Range(0, metricLevelMaxima.Keys.Max() + 1))
+            {
+                if (!metricLevelMaxima.ContainsKey(level))
+                {
+                    metricLevelMaxima[level] = new Dictionary<string, float>();
+                }
+
+                foreach (string metric in metrics)
+                {
+                    if (!metricLevelMaxima[level].ContainsKey(metric))
+                    {
+                        metricLevelMaxima[level][metric] = 0.0f;
+                    }
+                }
+            }
         }
 
         /// <summary>
--- Assets/SEE/GameObjects/Scales/LinearScale.cs
+++ Assets/SEE/GameObjects/Scales/LinearScale.cs
@@ -15,12 +15,12 @@ namespace SEE.GO
         /// The values are guaranteed to be in the range of [minimalLength, maximalLength].
         /// </summary>
         /// <param name="graphs">the set of graph whose node metrics are to be scaled</param>
-        /// <param name="minimalLength">the mininmal value a node length can have</param>
+        /// <param name="minimalLength">the minimal value a node length can have</param>
         /// <param name="maximalLength">the maximal value a node length can have</param>
         /// <param name="metrics">node metrics for scaling</param>
         /// <param name="leavesOnly">if true, only the leaf nodes are considered</param>
-        public LinearScale(ICollection<Graph> graphs, float minimalLength, float maximalLength, IList<string> metrics, bool leavesOnly)
-            : base(graphs, metrics, minimalLength, maximalLength, leavesOnly)
+        public LinearScale(IEnumerable<Graph> graphs, float minimalLength, float maximalLength, IList<string> metrics,
+                           bool leavesOnly) : base(graphs, metrics, minimalLength, maximalLength, leavesOnly)
         {
         }
 
@@ -53,13 +53,63 @@ namespace SEE.GO
         /// the metric. The assumption is that metric values are non-negative.
         /// </summary>
         /// <param name="metric">name of the node metric</param>
-        /// <param name="node">node for which to determine the normalized value</param>
+        /// <param name="value">value which shall be normalized</param>
         /// <returns>normalized value of node metric</returns>
         public override float GetNormalizedValue(string metric, float value)
         {
             float result;
 
             float max = metricMaxima[metric];
+
+            if (max <= 0.0f || value <= 0.0f)
+            {
+                result = 0.0f;
+            }
+            else
+            {
+                result = value / max;
+            }
+            return Mathf.Lerp(minimalLength, maximalLength, result);
+        }
+
+        /// <summary>
+        /// Yields a linear interpolation of the normalized value of the given node metric within the node's level.
+        /// The range of the linear interpolation is set by [minimalLength, maximalLength].
+        /// The normalization is done by dividing the value by the maximal value of
+        /// the metric. The assumption is that metric values are non-negative. If a node
+        /// does not have the metric attribute, minimalLength will be returned.
+        /// </summary>
+        /// <param name="metric">name of the node metric</param>
+        /// <param name="node">node for which to determine the normalized value</param>
+        /// <returns>normalized value of node metric</returns>
+        public override float GetNormalizedValueForLevel(string metric, Node node)
+        {
+            if (node.TryGetNumeric(metric, out float value))
+            {
+                return GetNormalizedValueForLevel(metric, value, node.Level);
+            }
+            else
+            {
+                return minimalLength;
+            }
+        }
+
+        /// <summary>
+        /// Yields a linear interpolation of the normalized value of the given node metric
+        /// within the given <paramref name="level"/>.
+        /// The range of the linear interpolation is set by [minimalLength, maximalLength].
+        /// The normalization is done by dividing the value by the maximal value of
+        /// the metric. The assumption is that metric values are non-negative.
+        /// </summary>
+        /// <param name="metric">name of the node metric</param>
+        /// <param name="value">value which shall be normalized</param>
+        /// <param name="level">node level within which the normalization shall take place</param>
+        /// <returns>normalized value of node metric</returns>
+        public override float GetNormalizedValueForLevel(string metric, float value, int level)
+        {
+            float result;
+
+            float max = metricLevelMaxima[level][metric];
 
             if (max <= 0.0f || value <= 0.0f)
             {
