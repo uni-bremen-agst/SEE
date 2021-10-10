8da0c0f07f09ef9fa7dabea9156f2d4392820ac0
Rainer Koschke <koschke@uni-bremen.de>
2021-10-10 09:41:49 +0200

    #373 Implemented suggestions from code review by falko17.

--- Assets/SEE/Controls/Actions/InteractableObjectAction.cs
+++ Assets/SEE/Controls/Actions/InteractableObjectAction.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using SEE.GO;
+using UnityEngine;
 
 namespace SEE.Controls.Actions
 {
@@ -18,9 +19,8 @@ namespace SEE.Controls.Actions
         /// </summary>
         protected virtual void Awake()
         {
-            if (!gameObject.TryGetComponent<InteractableObject>(out interactable))
+            if (!gameObject.TryGetComponentOrLog(out interactable))
             {
-                Debug.LogWarning($"Game object {name} has no {nameof(InteractableObject)} component.\n");
                 enabled = false;
             }
         }
