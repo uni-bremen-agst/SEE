7b9a9d1dd60c90a6e0dd527d7fe41b287992bc59
Falko <falko1@uni-bremen.de>
2021-10-25 18:25:59 +0200

    #95 Implement SEEReflexionCityEditor with 3 GXL files

---
+++ Assets/Editor/SEEReflexionCityEditor.cs
@@ -0,0 +1,40 @@
+﻿using SEE.Game.City;
+using SEE.Utils;
+using UnityEditor;
+using UnityEngine.Assertions;
+
+namespace SEEEditor
+{
+    /// <summary>
+    /// Custom editor for cities intended for reflexion analysis.
+    /// The reflection city is constructed by loading three separate files -- one for the implementation,
+    /// one for the architecture, and one for the mapping from implementation nodes to architecture nodes.
+    /// </summary>
+    [CustomEditor(typeof(SEECityReflexion))]
+    [CanEditMultipleObjects]
+    public class SEEReflexionCityEditor : SEECityEditor
+    {
+        /// <summary>
+        /// Displays the foldout in which users can load architecture, implementation, and mapping GXL files.
+        /// </summary>
+        protected override void Attributes()
+        {
+            SEECityReflexion city = target as SEECityReflexion;
+            Assert.IsNotNull(city);
+            showDataFiles = EditorGUILayout.Foldout(showDataFiles,
+                                                    "Data Files", true, EditorStyles.foldoutHeader);
+            if (showDataFiles)
+            {
+                city.GxlArchitecturePath = DataPathEditor.GetDataPath("GXL file (Architecture)",
+                                                                      city.GxlArchitecturePath,
+                                                                      Filenames.ExtensionWithoutPeriod(Filenames.GXLExtension));
+                city.GxlImplementationPath = DataPathEditor.GetDataPath("GXL file (Implementation)",
+                                                                        city.GxlImplementationPath,
+                                                                        Filenames.ExtensionWithoutPeriod(Filenames.GXLExtension));
+                city.GxlMappingPath = DataPathEditor.GetDataPath("GXL file (Mapping)",
+                                                                 city.GxlMappingPath,
+                                                                 Filenames.ExtensionWithoutPeriod(Filenames.GXLExtension));
+            }
+        }
+    }
+}
