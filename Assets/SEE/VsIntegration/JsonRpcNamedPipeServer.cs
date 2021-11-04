8410db2325c3f2abcfce164fd7b18681f9f00699
Jan-Philipp Schramm <schramm2@uni-bremen.de>
2021-11-04 20:21:31 +0100

    Created new script for Visual Studio integration

---
+++ Assets/SEE/VsIntegration/JsonRpcNamedPipeServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.SEE.VsIntegration
+{
+    public sealed class JsonRpcNamedPipeServer : JsonRpcServer
+    {
+        public override Task CallRemoteProcessAsync(string targetName)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task StartServerAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
