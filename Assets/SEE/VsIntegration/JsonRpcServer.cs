a344f8729148387f5875c0d931267f6704755b29
Jan-Philipp Schramm <schramm2@uni-bremen.de>
2021-11-04 20:21:31 +0100

    Created new script for Visual Studio integration

---
+++ Assets/SEE/VsIntegration/JsonRpcServer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StreamJsonRpc;
+
+namespace Assets.SEE.VsIntegration
+{
+    public abstract class JsonRpcServer : IDisposable
+    {
+        JsonRpc Rpc;
+        protected object Target;
+        protected Task Server;
+
+        public void Start(object target)
+        {
+            _ = target ?? throw new ArgumentNullException(nameof(target));
+            Target = target;
+
+            Dispose();
+
+            Server = StartServerAsync();
+        }
+
+        public abstract Task CallRemoteProcessAsync(string targetName);
+
+        public bool IsConnected()
+        {
+            return Rpc != null;
+        }
+
+        protected abstract Task StartServerAsync();
+
+        public virtual void Dispose()
+        {
+            Rpc?.Dispose();
+        }
+    }
+}
