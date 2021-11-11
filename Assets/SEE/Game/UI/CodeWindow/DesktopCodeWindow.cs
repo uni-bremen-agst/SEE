94388d642415fe44a8ab0a666237c5e08901e512
mblecker <mblecker@uni-bremen.de>
2021-11-11 18:03:10 +0100

    fix compiler

--- Assets/SEE/Game/UI/CodeWindow/DesktopCodeWindow.cs
+++ Assets/SEE/Game/UI/CodeWindow/DesktopCodeWindow.cs
@@ -255,7 +255,7 @@ namespace SEE.Game.UI.CodeWindow
                 if (((Input.GetKey(KeyCode.Backspace) && string.IsNullOrEmpty(changedText) /*timeStamp <= Time.time */) || Input.GetKeyDown(KeyCode.Backspace)) && idx > 0)
                 {
                     //timeStamp = Time.time + 0.300000f;
-                    changedText =""
+                    changedText = "";
                     if (!deleteSelectedText())
                     {
                         ICRDT.DeleteString(idx -1, idx-1, Title);
