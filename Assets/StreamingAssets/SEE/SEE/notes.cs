0306f788c5d04edc518661cfc8a74ed550720b8b
mblecker <mblecker@uni-bremen.de>
2021-11-20 20:58:20 +0100

    some debug. notes.cs filled with some comments to fix an error comming up with empty files. fix seeInput ctrl from keyDown to keyPressed to fix the issue that the ctrl + somekey not works probelay

---
+++ Assets/StreamingAssets/SEE/SEE/notes.cs
@@ -0,0 +1,4 @@
+// In diese Datei werden die Größten SEE Datein der größe nach sortiert eingetragen.
+// Ein eintrag sollte so aussehen: 
+// 1. Beispiel.cs (500 Zeilen)
+// 2. Beispiel2.cs (300 Zeilen)
