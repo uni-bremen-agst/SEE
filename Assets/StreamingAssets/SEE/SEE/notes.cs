eaac0eac96731509d47487b237f28ba48718f5e1
mblecker <mblecker@uni-bremen.de>
2021-11-27 14:19:18 +0100

    some try to auto indent the strg v inserted text and reformat code

--- Assets/StreamingAssets/SEE/SEE/notes.cs
+++ Assets/StreamingAssets/SEE/SEE/notes.cs
@@ -1,4 +1,44 @@
-// In diese Datei werden die Größten SEE Datein der größe nach sortiert eingetragen.
-// Ein eintrag sollte so aussehen: 
-// 1. Beispiel.cs (500 Zeilen)
-// 2. Beispiel2.cs (300 Zeilen)
+ï»¿// Jeder Spieler soll aus den 5 groeÃŸten Kreisen in seinem Bereich den jeweils groeÃŸten
+// Block finden ( gemeint ist zuerst die hoehe, sollte diese gleich sein, nimm den mit mehr Volumen)
+// Ist dieser gefunden erstelle ein Eintrag
+// Ein Eintrag sollte so aussehen: 
+// 1. Beispiel.cs (500 Zeilen, BeispielCircle)
+// 2. Beispiel2.cs (300 Zeilen, BeispielKreis)
+
+// Player 1 dein Bereich ist Cose.
+// Player 2 dein Bereich ist Charts
+// Player 3 Dein Bereich ist Actions
+
+// Hier folgen jetzt die goeÃŸten Dateien:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
