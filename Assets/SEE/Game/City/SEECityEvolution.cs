4b7250b58a92eb5b98cef893da3c5f120a0e045e
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2021-12-01 16:06:29 +0100

    Use EdgeMeshScheduler in EvolutionRenderer.

--- Assets/SEE/Game/City/SEECityEvolution.cs
+++ Assets/SEE/Game/City/SEECityEvolution.cs
@@ -173,8 +173,9 @@ namespace SEE.Game.City
             animationInteraction.EvolutionRenderer = evolutionRenderer;
         }
 
-        private void Start()
+        override internal void Start()
         {
+            base.Start();
             evolutionRenderer.ShowGraphEvolution();
         }
 
