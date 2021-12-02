7ae5dd8033e116547393cc61b59c25268a53aa4c
Rainer Koschke <koschke@uni-bremen.de>
2021-12-02 18:49:16 +0100

    #339 Adds the necessary movement and player components for the respective environment.

--- Assets/SEE/Game/Worlds/AvatarAdapter.cs
+++ Assets/SEE/Game/Worlds/AvatarAdapter.cs
@@ -13,33 +13,58 @@ namespace SEE.Game.Worlds
             if (IsLocalPlayer)
             {
                 // I am the avatar of the local player.
-                Debug.LogError($"{gameObject.GetFullName()} is the local player.\n");
-                GameObject player = PrefabInstantiator.InstantiatePrefab("Prefabs/Players/DesktopPlayer");
-                player.name = PlayerInputType.DesktopPlayer.ToString();
-                player.transform.SetParent(gameObject.transform);
-                player.transform.localPosition = new Vector3(0, 1.8f, 0.3f);
-                player.transform.localRotation = Quaternion.Euler(30, 0, 0);
-                EnableMovement(true);
+                Debug.Log($"{gameObject.GetFullName()} is the local player.\n");
+
+                switch (PlayerSettings.GetInputType())
+                {
+                    case PlayerInputType.DesktopPlayer:
+                    case PlayerInputType.TouchGamepadPlayer:
+                        PrepareForDesktop();
+                        break;
+                    case PlayerInputType.VRPlayer:
+                        PrepareForXR();
+                        break;
+                    default:
+                        throw new System.NotImplementedException($"Unhandled case {PlayerSettings.GetInputType()}");
+                }
             }
             else
             {
                 // I am the avatar of a remote player.
-                Debug.LogError($"{gameObject.GetFullName()} is a remote player.\n");
-                EnableMovement(false);
+                Debug.Log($"{gameObject.GetFullName()} is a remote player.\n");
             }
         }
 
-        private void EnableMovement(bool enable)
+        private void PrepareForXR()
         {
-            if (gameObject.TryGetComponent(out DesktopPlayerMovement movement))
+            GameObject vrPlayer = PrefabInstantiator.InstantiatePrefab("Prefabs/Players/VRPlayer");
+            vrPlayer.name = PlayerInputType.DesktopPlayer.ToString();
+            vrPlayer.transform.SetParent(gameObject.transform);
+            gameObject.AddComponent<XRPlayerMovement>();
+        }
+
+        private float DesktopAvatarHeight()
+        {
+            if (gameObject.TryGetComponent(out Collider collider))
             {
-                Debug.LogError($"Enabled movement for {gameObject.GetFullName()}: {enable}.\n");
-                movement.enabled = enable;
+                return collider.bounds.size.y;
             }
             else
             {
-                Debug.LogError($"{gameObject.GetFullName()} has no DesktopPlayerMovement.\n");
+                return gameObject.transform.lossyScale.y;
             }
+        }
+
+        private void PrepareForDesktop()
+        {
+            // Set up the desktop player at the top of the player just in front of it.
+            GameObject desktopPlayer = PrefabInstantiator.InstantiatePrefab("Prefabs/Players/DesktopPlayer");
+            desktopPlayer.name = PlayerInputType.DesktopPlayer.ToString();
+            desktopPlayer.transform.SetParent(gameObject.transform);
+            desktopPlayer.transform.localPosition = new Vector3(0, DesktopAvatarHeight(), 0.3f);
+            desktopPlayer.transform.localRotation = Quaternion.Euler(30, 0, 0);
+
+            gameObject.AddComponent<DesktopPlayerMovement>();
         }
     }
 }
