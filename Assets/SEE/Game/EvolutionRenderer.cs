5d26e50aaaed32c2aeb755f179180167bd18299a
Rainer Koschke <koschke@uni-bremen.de>
2021-12-04 18:25:41 +0100

    Removed obsolete imports. Added newline to debugging messages.

--- Assets/SEE/Game/EvolutionRenderer.cs
+++ Assets/SEE/Game/EvolutionRenderer.cs
@@ -20,8 +20,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Assets.SEE.Game.City;
-using Assets.SEE.GameObjects;
 using SEE.DataModel;
 using SEE.DataModel.DG;
 using SEE.Game.Charts;
