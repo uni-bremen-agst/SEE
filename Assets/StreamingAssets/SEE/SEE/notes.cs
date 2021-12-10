5a4658697ac8348f7a9205ce434442323e25b062
mblecker <mblecker@uni-bremen.de>
2021-12-10 14:25:14 +0100

    fix spell

--- Assets/StreamingAssets/SEE/SEE/notes.cs
+++ Assets/StreamingAssets/SEE/SEE/notes.cs
@@ -1,16 +1,15 @@
 ﻿// Jeder Spieler soll aus den 5 groeßten Kreisen in seinem Bereich den jeweils groeßten
-// Block finden ( gemeint ist zuerst die hoehe, sollte diese gleich sein, nimm den mit mehr Volumen)
-// Ist dieser gefunden erstelle ein Eintrag
+// Block finden. (Gemeint ist zuerst die Hoehe, sollte diese gleich sein, nimm den mit mehr Volumen.)
+// Ist dieser gefunden erstelle ein Eintrag.
 // Ein Eintrag sollte so aussehen: 
-// 1. Beispiel.cs (500 Zeilen, BeispielCircle)
-// 2. Beispiel2.cs (300 Zeilen, BeispielKreis)
+// 1. Beispiel.cs, 500 Zeilen, BeispielCircle
+// 2. Beispiel2.cs, 300 Zeilen, BeispielKreis
 
-// Player 1 dein Bereich ist Cose.
-// Player 2 dein Bereich ist Charts
-// Player 3 Dein Bereich ist Actions
+// Player 1, Dein Bereich ist Cose.
+// Player 2, Dein Bereich ist Charts
+// Player 3, Dein Bereich ist Actions
 
-// Hier folgen jetzt die goeßten Dateien:
-
+// Hier folgen jetzt die groeßten Dateien:
 
 
 
