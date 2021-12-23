cf37dbbc08fcb60c44bb279bb92dd3644af699b5
Rainer Koschke <koschke@uni-bremen.de>
2021-12-23 18:40:58 +0100

    #339 UMA Clothing Pack 1.0 imported. This packages requires a commercial license. Bought at Unity's asset store.

---
+++ Assets/Nesalis Games/ClothingPack/Scripts/ChangeRaceButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UMA.CharacterSystem;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NesalisGames
+{
+    namespace ClothingPack
+    {
+        public class ChangeRaceButton : MonoBehaviour
+        {
+            public DynamicCharacterAvatar Avatar;
+            public Text ActiveRace;
+            public List<UMA.RaceData> Races = new List<UMA.RaceData>();
+
+            private int index = 1;
+
+            public void ChangeRaceClick()
+            {
+                Avatar.ChangeRace(Races[index]);
+                ActiveRace.text = "Active race: " + Races[index].raceName;
+
+                index++;
+                if (index > Races.Count - 1) index = 0;
+            }
+        }
+    }
+}
