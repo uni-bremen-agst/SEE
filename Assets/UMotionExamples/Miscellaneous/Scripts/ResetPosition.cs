d17db2c5b63ee49bf67bf546c80274c527a50502
Rainer Koschke <koschke@uni-bremen.de>
2022-01-02 12:01:56 +0100

    Imported UMotion Pro version 1.27 (purchased licence).

---
+++ Assets/UMotionExamples/Miscellaneous/Scripts/ResetPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    public float distanceToReset = 5;
+
+    private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Vector3.Distance(startPosition, transform.position) >= distanceToReset)
+        {
+            transform.position = startPosition;
+        }
+	}
+}
