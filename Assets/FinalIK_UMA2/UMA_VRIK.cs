7bf5ebde4c32bf2fb5cc179f64cf6cbdf1d0e043
Rainer Koschke <koschke@uni-bremen.de>
2022-01-03 09:52:09 +0100

    Added UMA integration for Final IK. It was imported by clicking on the asset: Assets/Plugins/RootMotion/FinalIK/_Integration/UMA (Unity 2017.4.28f1, UMA 2.8.1).unitypackage

---
+++ Assets/FinalIK_UMA2/UMA_VRIK.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion; // Need to include the RootMotion namespace as well because of the BipedReferences
+using RootMotion.FinalIK;
+
+/// <summary>
+/// Adds VRIK to a UMA character in runtime.
+/// </summary>
+public class UMA_VRIK : MonoBehaviour
+{
+    private Animator animator;
+    public VRIK ik { get; private set; }
+
+    void Update()
+    {
+        if (ik != null) return;
+
+        // Add FBBIK as soon as an animator is present
+        animator = GetComponent<Animator>();
+        if (animator != null && animator.avatar != null)
+        {
+            AddingFBBIK(gameObject);
+        }
+    }
+
+    // Call this method whenever you need in runtime.
+    // Please note that VRIK will sample the pose of the character at initiation so at the time of calling this method,
+    // the limbs of the character should be bent in their natural directions.
+
+    void AddingFBBIK(GameObject go)
+    {
+        // setting the references
+        ik = go.AddComponent<VRIK>(); // Adding the component
+
+        ik.AutoDetectReferences();
+    }
+}
