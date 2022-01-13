f86fdfd1165f03bbd6824ddc878cdb6568c064e3
RomanGressler <gressler@uni-bremen.de>
2022-01-13 18:22:24 +0100

    almost finished menu logic

--- Assets/SEE/Game/UI/Menu/Mobile/MobileMenu.cs
+++ Assets/SEE/Game/UI/Menu/Mobile/MobileMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Michsky.UI.ModernUIPack;
 using UnityEngine.Events;
+using UnityEditor;
 
 namespace SEE.Game.UI.Menu
 {
@@ -14,7 +15,9 @@ namespace SEE.Game.UI.Menu
         /// <summary>
         /// Prefab for the buttons
         /// </summary>
-        [SerializeField] GameObject buttonPrefab;
+        [SerializeField] GameObject iconButtonPrefab;
+
+        [SerializeField] GameObject textButtonPrefab;
 
         [SerializeField] Transform menuPanelHorizontal;
 
@@ -22,39 +25,56 @@ namespace SEE.Game.UI.Menu
 
         private GameObject[][] buttons = new GameObject[5][];
 
-        private GameObject[] buttonSelect = new GameObject[2];
-
-        private GameObject[] buttonDelete = new GameObject[1];
-
-        private GameObject[] buttonDeleteMulti = new GameObject[3];
-
-        private GameObject[] buttonRotate = new GameObject[5];
-
-        private GameObject[] buttonMove = new GameObject[4];
+        private bool expanded = false;
 
 
         // Start is called before the first frame update
         void Start()
         {
-            buttonSelect[0] = (GameObject)Instantiate (buttonPrefab);
-            buttonSelect[1] = (GameObject)Instantiate (buttonPrefab);
+            #region set up buttons
+            GameObject[] buttonSelect = new GameObject[2];
+            GameObject[] buttonDelete = new GameObject[1];
+            GameObject[] buttonDeleteMulti = new GameObject[3];
+            GameObject[] buttonRotate = new GameObject[5];
+            GameObject[] buttonMove = new GameObject[4];
 
-            buttonDelete[0] = (GameObject)Instantiate (buttonPrefab);
 
-            buttonDeleteMulti[0] = (GameObject)Instantiate(buttonPrefab);
-            buttonDeleteMulti[1] = (GameObject)Instantiate(buttonPrefab);
-            buttonDeleteMulti[2] = (GameObject)Instantiate(buttonPrefab);
+            Sprite refreshSprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Modern UI Pack/Textures/Icon/Navigation/Refresh.png", typeof(Sprite));
+            Sprite checkSprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Modern UI Pack/Textures/Icon/Navigation/Check Bold.png", typeof(Sprite));
+            Sprite cancelSprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Modern UI Pack/Textures/Icon/Navigation/Cancel.png", typeof(Sprite));
+            Sprite trashSprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Modern UI Pack/Textures/Icon/Common/Trash.png", typeof(Sprite));
+            Sprite minusSprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Modern UI Pack/Textures/Icon/Navigation/Minus.png", typeof(Sprite));
+            Sprite moveSprite = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Modern UI Pack/Textures/Demo/Icons/Horizontal Selector.png", typeof(Sprite));
 
-            buttonRotate[0] = (GameObject)Instantiate(buttonPrefab);
-            buttonRotate[1] = (GameObject)Instantiate(buttonPrefab);
-            buttonRotate[2] = (GameObject)Instantiate(buttonPrefab);
-            buttonRotate[3] = (GameObject)Instantiate(buttonPrefab);
-            buttonRotate[4] = (GameObject)Instantiate(buttonPrefab);
+            buttonSelect[0] = (GameObject)Instantiate (iconButtonPrefab);
+            buttonSelect[1] = (GameObject)Instantiate (iconButtonPrefab);
+            buttonSelect[1].GetComponent<ButtonManagerBasicIcon>().buttonIcon = cancelSprite;
 
-            buttonMove[0] = (GameObject)Instantiate(buttonPrefab);
-            buttonMove[1] = (GameObject)Instantiate(buttonPrefab);
-            buttonMove[2] = (GameObject)Instantiate(buttonPrefab);
-            buttonMove[3] = (GameObject)Instantiate(buttonPrefab);
+            buttonDelete[0] = (GameObject)Instantiate (iconButtonPrefab);
+            buttonDelete[0].GetComponent<ButtonManagerBasicIcon>().buttonIcon = trashSprite;
+
+            buttonDeleteMulti[0] = (GameObject)Instantiate(iconButtonPrefab);
+            buttonDeleteMulti[0].GetComponent<ButtonManagerBasicIcon>().buttonIcon = minusSprite;
+            buttonDeleteMulti[1] = (GameObject)Instantiate(iconButtonPrefab);
+            buttonDeleteMulti[1].GetComponent<ButtonManagerBasicIcon>().buttonIcon = cancelSprite;
+            buttonDeleteMulti[2] = (GameObject)Instantiate(iconButtonPrefab);
+            buttonDeleteMulti[2].GetComponent<ButtonManagerBasicIcon>().buttonIcon = checkSprite;
+
+            buttonRotate[0] = (GameObject)Instantiate(iconButtonPrefab);
+            buttonRotate[0].GetComponent<ButtonManagerBasicIcon>().buttonIcon = refreshSprite;
+            buttonRotate[1] = (GameObject)Instantiate(textButtonPrefab);
+            buttonRotate[1].GetComponent<ButtonManagerBasic>().buttonText = "n";
+            buttonRotate[2] = (GameObject)Instantiate(textButtonPrefab);
+            buttonRotate[3] = (GameObject)Instantiate(iconButtonPrefab);
+            buttonRotate[4] = (GameObject)Instantiate(iconButtonPrefab);
+
+            buttonMove[0] = (GameObject)Instantiate(iconButtonPrefab);
+            buttonMove[0].GetComponent <ButtonManagerBasicIcon>().buttonIcon= moveSprite;
+            buttonMove[1] = (GameObject)Instantiate(textButtonPrefab);
+            buttonMove[1].GetComponent<ButtonManagerBasic>().buttonText = "n";
+            buttonMove[2] = (GameObject)Instantiate(textButtonPrefab);
+            buttonMove[3] = (GameObject)Instantiate(textButtonPrefab);
+            buttonMove[3].GetComponent<ButtonManagerBasic>().buttonText = "8";
 
             buttons[0] = buttonSelect;
             buttons[1] = buttonDelete;
@@ -69,20 +89,19 @@ namespace SEE.Game.UI.Menu
                 {
                     buttons[i][j].SetActive(false);
                 }
-
             }
 
             for (int i = 0; i < buttons.Length; i++)
             {
+                //add listener to expand menu
+                int clickedIndex = i;
+                buttons[i][0].GetComponent<ButtonManagerBasicIcon>().clickEvent.AddListener(() => selectMode(clickedIndex));
                 for (int j = 0; j < buttons[i].Length; j++)
                 {
-                    if (j == 0)
+                    if (j == 0 && i > 0)
                     {
                         buttons[i][j].transform.SetParent(menuPanelVertical);
-                        //add listener to expand menu
-                        buttons[i][j].GetComponent<ButtonManagerBasicIcon>().clickEvent.AddListener(() => CollapseMenu());
-                        int clickedIndex = i;
-                        buttons[i][j].GetComponent<ButtonManagerBasicIcon>().clickEvent.AddListener(() => SortButtons(clickedIndex));
+                        
                     }
                     else
                     {
@@ -90,49 +109,47 @@ namespace SEE.Game.UI.Menu
                     }
                 }       
             }
-            Debug.Log(buttons);
+            #endregion
         }
 
-        private void SortButtons(int ClickedIndex)
+        private void selectMode(int ClickedIndex)
         {
-            for (int i = 1; i < buttons.Length; i++)
+            if (expanded)
             {
-                if (i == ClickedIndex)
+                //set inactive first for right order 
+                for (int i = 0; i < buttons.Length; i++)
                 {
-                    GameObject[] tmpButtons = new GameObject[buttons[i].Length];
-                    tmpButtons = buttons[i];
-                    buttons[i] = buttons[0];
-                    buttons[0] = tmpButtons;
-
-                    for (int j = 1; j < buttons[i].Length; j++)
+                    for (int j = 0; j < buttons[i].Length; j++)
                     {
-                        buttons[i][j].SetActive(false);
-                    }
-                    for (int j = 1;j < buttons[0].Length; j++)
-                    {
-                        buttons[0][j].SetActive(true);
+                            if (j == 0)
+                            {
+                                buttons[i][j].SetActive(false);
+                                buttons[i][j].transform.SetParent(menuPanelVertical);
+                            }
+                            else
+                            {
+                                buttons[i][j].SetActive(false);
+                            }
                     }
                 }
-            }
-        }
-
-        private void CollapseMenu()
-        {
-            if (buttons[1][0].activeSelf == true)
-            {
-                for (int i = 1; i < buttons.Length; i++)
+                //finally set the selected button active
+                for (int k = 0; k < buttons[ClickedIndex].Length; k++)
                 {
-                    buttons[i][0].SetActive(false);
+                    //set parent to null to keep right order 
+                    buttons[ClickedIndex][k].transform.SetParent(null);
+                    buttons[ClickedIndex][k].transform.SetParent(menuPanelHorizontal);
+                    buttons[ClickedIndex][k].SetActive(true);
                 }
+            expanded = false;
             }
             else
             {
-                for (int i = 1; i < buttons.Length; i++)
+                for (int i = 0; i < buttons.Length; i++)
                 {
                     buttons[i][0].SetActive(true);
                 }
+                expanded = true;
             }
-            
         }
 
     }
