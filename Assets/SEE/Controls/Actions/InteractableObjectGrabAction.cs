39b7fcf3f5d366502845f21ec34f31d22b8ba184
Rainer Koschke <koschke@uni-bremen.de>
2022-01-24 17:18:28 +0100

    #339 Obsolete classes. Will be removed. We restructured the inheritance hierarchy. Their code was moved to their respective subclasses.

--- Assets/SEE/Controls/Actions/InteractableObjectGrabAction.cs
+++ Assets/SEE/Controls/Actions/InteractableObjectGrabAction.cs
@@ -8,50 +8,5 @@ namespace SEE.Controls.Actions
     /// </summary>
     public abstract class InteractableObjectGrabAction : InteractableObjectAction
     {
-        /// <summary>
-        /// Registers On() and Off() for the respective grabbing events.
-        /// </summary>
-        protected virtual void OnEnable()
-        {
-            if (interactable != null)
-            {
-                interactable.GrabIn += On;
-                interactable.GrabOut += Off;
-            }
-            else
-            {
-                Debug.LogError($"InteractableObjectGrabAction.OnEnable for {name} has NO interactable.\n");
-            }
-        }
-
-        /// <summary>
-        /// Unregisters On() and Off() from the respective grabbing events.
-        /// </summary>
-        protected virtual void OnDisable()
-        {
-            if (interactable != null)
-            {
-                interactable.GrabIn -= On;
-                interactable.GrabOut -= Off;
-            }
-            else
-            {
-                Debug.LogError($"InteractableObjectGrabAction.OnDisable for {name} has NO interactable.\n");
-            }
-        }
-
-        /// <summary>
-        /// Called when the object is grabbed.
-        /// </summary>
-        /// <param name="interactableObject">the object being grabbed</param>
-        /// <param name="isInitiator">true if a local user initiated this call</param>
-        protected abstract void On(InteractableObject interactableObject, bool isInitiator);
-
-        /// <summary>
-        /// Called when the object is no longer grabbed.
-        /// </summary>
-        /// <param name="interactableObject">the object being grabbed</param>
-        /// <param name="isInitiator">true if a local user initiated this call</param>
-        protected abstract void Off(InteractableObject interactableObject, bool isInitiator);
     }
 }
