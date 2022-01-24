01e32bc5bd986be3e58a0dd6d677255d87434c2f
Rainer Koschke <koschke@uni-bremen.de>
2022-01-24 17:18:28 +0100

    #339 Obsolete classes. Will be removed. We restructured the inheritance hierarchy. Their code was moved to their respective subclasses.

--- Assets/SEE/Controls/Actions/InteractableObjectSelectionAction.cs
+++ Assets/SEE/Controls/Actions/InteractableObjectSelectionAction.cs
@@ -1,6 +1,4 @@
-﻿using UnityEngine;
-
-namespace SEE.Controls.Actions
+﻿namespace SEE.Controls.Actions
 {
     /// <summary>
     /// Abstract super class of behaviours to be triggered when an interactable
@@ -8,59 +6,5 @@ namespace SEE.Controls.Actions
     /// </summary>
     public abstract class InteractableObjectSelectionAction : InteractableObjectAction
     {
-        /// <summary>
-        /// Registers On() and Off() for the respective selection events.
-        /// </summary>
-        protected virtual void OnEnable()
-        {
-            if (interactable != null)
-            {
-                interactable.SelectIn += On;
-                interactable.SelectOut += Off;
-                interactable.GrabOut += GrabOff;
-            }
-            else
-            {
-                Debug.LogError($"InteractableObjectSelectionAction.OnEnable for {name} has NO interactable.\n");
-            }
-        }
-
-        /// <summary>
-        /// Unregisters On() and Off() from the respective selection events.
-        /// </summary>
-        protected virtual void OnDisable()
-        {
-            if (interactable != null)
-            {
-                interactable.SelectIn -= On;
-                interactable.SelectOut -= Off;
-                interactable.GrabOut -= GrabOff;
-            }
-            else
-            {
-                Debug.LogError($"InteractableObjectSelectionAction.OnDisable for {name} has NO interactable.\n");
-            }
-        }
-
-        /// <summary>
-        /// Called when the object is selected.
-        /// </summary>
-        /// <param name="interactableObject">the object being selected</param>
-        /// <param name="isInitiator">true if a local user initiated this call</param>
-        protected abstract void On(InteractableObject interactableObject, bool isInitiator);
-
-        /// <summary>
-        /// Called when the object is no longer selected.
-        /// </summary>
-        /// <param name="interactableObject">the object being selected</param>
-        /// <param name="isInitiator">true if a local user initiated this call</param>
-        protected abstract void Off(InteractableObject interactableObject, bool isInitiator);
-
-        /// <summary>
-        /// Called when the object is no longer grabbed.
-        /// </summary>
-        /// <param name="interactableObject">the object being grabbed</param>
-        /// <param name="isInitiator">true if a local user initiated this call</param>
-        protected abstract void GrabOff(InteractableObject interactableObject, bool isInitiator);
     }
 }
