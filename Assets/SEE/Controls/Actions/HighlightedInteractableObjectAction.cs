58946c197fccdb2c1afe0b7ddb615f312a3e729c
Rainer Koschke <koschke@uni-bremen.de>
2022-01-25 08:29:36 +0100

    #339 Added documentation.

--- Assets/SEE/Controls/Actions/HighlightedInteractableObjectAction.cs
+++ Assets/SEE/Controls/Actions/HighlightedInteractableObjectAction.cs
@@ -55,22 +55,48 @@ namespace SEE.Controls.Actions
             }
         }
 
+        /// <summary>
+        /// Remembers the original value of <see cref="outline.OutlineColor"/> in
+        /// <see cref="initialOutlineColor"/> and sets it to either <see cref="LocalOutlineColor"/>
+        /// or <see cref="RemoteOutlineColor"/> depending upon <paramref name="isInitiator"/>.
+        /// </summary>
+        /// <param name="isInitiator">true if the caller is the initiator, that is, the one
+        /// triggering this action, rather than just a proxy propagating an action
+        /// through the network to other connected clients</param>
         protected void SetInitialAndNewOutlineColor(bool isInitiator)
         {
             initialOutlineColor = outline.OutlineColor;
             SetOutlineColor(isInitiator);
         }
 
+        /// <summary>
+        /// Sets <see cref="outline.OutlineColor"/> to either <see cref="LocalOutlineColor"/>
+        /// or <see cref="RemoteOutlineColor"/> depending upon <paramref name="isInitiator"/>.
+        /// </summary>
+        /// <param name="isInitiator">true if the caller is the initiator, that is, the one
+        /// triggering this action, rather than just a proxy propagating an action
+        /// through the network to other connected clients</param>
         protected void SetOutlineColor(bool isInitiator)
         {
             outline.OutlineColor = isInitiator ? LocalOutlineColor : RemoteOutlineColor;
         }
 
+        /// <summary>
+        /// Resets <see cref="outline.OutlineColor"/> to <see cref="initialOutlineColor"/>.
+        /// </summary>
         protected void ResetOutlineColor()
         {
             outline.OutlineColor = initialOutlineColor;
         }
 
+        /// <summary>
+        /// If <paramref name="isInitiator"/> is true, saves <see cref="enforcer.TargetAlpha"/>
+        /// in <see cref="initialAlpha"/> and sets it to 1 (no transparency). If <paramref name="isInitiator"/>
+        /// is false, nothing happens.
+        /// </summary>
+        /// <param name="isInitiator">true if the caller is the initiator, that is, the one
+        /// triggering this action, rather than just a proxy propagating an action
+        /// through the network to other connected clients</param>
         protected void SetAlpha(bool isInitiator)
         {
             if (isInitiator)
@@ -80,6 +106,13 @@ namespace SEE.Controls.Actions
             }
         }
 
+        /// <summary>
+        /// If <paramref name="isInitiator"/> is true, resets <see cref="enforcer.TargetAlpha"/>
+        /// to <see cref="initialAlpha"/>. If <paramref name="isInitiator"/> is false, nothing happens.
+        /// </summary>
+        /// <param name="isInitiator">true if the caller is the initiator, that is, the one
+        /// triggering this action, rather than just a proxy propagating an action
+        /// through the network to other connected clients</param>
         protected void ResetAlpha(bool isInitiator)
         {
             if (isInitiator)
@@ -88,12 +121,25 @@ namespace SEE.Controls.Actions
             }
         }
 
+        /// <summary>
+        /// Calls <see cref="SetInitialAndNewOutlineColor(isInitiator)"/> and
+        /// <see cref="SetAlpha(isInitiator)"/>.
+        /// </summary>
+        /// <param name="isInitiator">true if the caller is the initiator, that is, the one
+        /// triggering this action, rather than just a proxy propagating an action
+        /// through the network to other connected clients</param>
         protected void SetOutlineColorAndAlpha(bool isInitiator)
         {
             SetInitialAndNewOutlineColor(isInitiator);
             SetAlpha(isInitiator);
         }
 
+        /// <summary>
+        /// Calls <see cref="ResetOutlineColor()"/> and <see cref="ResetAlpha(isInitiator)"/>.
+        /// </summary>
+        /// <param name="isInitiator">true if the caller is the initiator, that is, the one
+        /// triggering this action, rather than just a proxy propagating an action
+        /// through the network to other connected clients</param>
         protected void ResetOutlineColorAndAlpha(bool isInitiator)
         {
             ResetOutlineColor();
