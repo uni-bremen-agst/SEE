722bab219562dd1f99d7e74b231961577c07668f
Rainer Koschke <koschke@uni-bremen.de>
2022-02-07 16:00:44 +0100

    Improved documentation. Removed obsolete tags.

--- Assets/SEE/DataModel/Tags.cs
+++ Assets/SEE/DataModel/Tags.cs
@@ -5,46 +5,81 @@
     /// </summary>
     public static class Tags
     {
-        public const string None = ""; // pseudo tag whenever a value for no tag is needed
+        /// <summary>
+        /// Pseudo tag whenever a value for no tag is needed.
+        /// </summary>
+        public const string None = "";
 
-        public const string CodeCity = "Code City"; // for objects representing a code city hosting objects tagged by Node and Edge tags.
+        /// <summary>
+        /// For objects representing a code city hosting objects tagged by Node and Edge tags.
+        /// </summary>
+        public const string CodeCity = "Code City";
 
-        // graph concepts
+        /// <summary>
+        /// For underlying Graphs (data model for global dependency graph read from GXL).
+        /// </summary>
         public const string Graph = "Graph";
-        public const string Node = "Node"; // for logical graph nodes (not their visual representation such as Blocks and Buildings)
+
+        /// <summary>
+        /// For game objects representing graph nodes visually.
+        /// </summary>
+        public const string Node = "Node";
+
+        /// <summary>
+        /// For game objects representing graph edges visually.
+        /// </summary>
         public const string Edge = "Edge";
 
-        public const string NodePrefab = "Node Prefab";
+        /// <summary>
+        /// For game objects representing texts, e.g., node labels.
+        /// </summary>
         public const string Text = "Text";
+
+        /// <summary>
+        /// For game objects representing software erosions.
+        /// </summary>
         public const string Erosion = "Erosion";
-        public const string Decoration = "Decoration"; // Planes, trees, etc.
-        public const string Path = "Path"; // for camera paths
 
-        // for visualization of dynamic execution
+        /// <summary>
+        /// For game objects representing general decorations, such as planes, furniture, etc.
+        /// </summary>
+        public const string Decoration = "Decoration";
+
+        /// <summary>
+        /// For objects represented recorded camera paths.
+        /// </summary>
+        public const string Path = "Path";
+
+        /// <summary>
+        /// For objects representing runtime information. It is used by <see cref="SEEDynCity"/>.
+        /// </summary>
         public const string Runtime = "Runtime";
+
+        /// <summary>
+        /// For objects representing dynamic function calls. It is used by for the animation of
+        /// dynamic call graphs.
+        /// </summary>
         public const string FunctionCall = "Function Call";
-        public const string JLGVisualization = "JLG Visualization";
 
-        // for culling
-        public const string CullingPlane = "CullingPlane"; // for a plane where code cities can be put on and be moved around with culling
+        /// <summary>
+        /// For a plane where code cities can be put on and be moved around with culling.
+        /// </summary>
+        public const string CullingPlane = "CullingPlane";
 
-        // metric charts
-        public const string ChartManager = "ChartManager";  // Tag of a game object having a ChartManager as a component.
-        public const string ChartContainer = "ChartContainer";
-        public const string Chart = "Chart"; // for a metric chart
-        public const string PowerBeam = "PowerBeam"; // For power beams
+        /// <summary>
+        /// For a game object having a ChartManager as a component.
+        /// </summary>
+        public const string ChartManager = "ChartManager";
 
-        // for game objects representing a UI element
-        public const string UI = "UI";
-        
-        // for the main camera in the scene
+        /// <summary>
+        /// For the main camera in the scene.
+        /// </summary>
         public const string MainCamera = "MainCamera";
 
         /// <summary>
         /// All existing tags in one.
         /// </summary>
-        public static readonly string[] All = { Graph, Node, Edge, NodePrefab, Text, Erosion, Decoration,
-              Path, Runtime, FunctionCall, CullingPlane, ChartManager, ChartContainer, Chart,
-              JLGVisualization, UI, MainCamera};
+        public static readonly string[] All = { Graph, Node, Edge, Text, Erosion, Decoration,
+              Path, Runtime, FunctionCall, CullingPlane, ChartManager, MainCamera};
     }
 }
