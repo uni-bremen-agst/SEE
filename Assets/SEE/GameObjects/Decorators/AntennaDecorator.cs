e32386a5d665e11b4bf9a653a1a330b5fd820ace
Rainer Koschke <koschke@uni-bremen.de>
2022-02-22 14:55:31 +0100

    Works now.

--- Assets/SEE/GameObjects/Decorators/AntennaDecorator.cs
+++ Assets/SEE/GameObjects/Decorators/AntennaDecorator.cs
@@ -3,79 +3,116 @@ using SEE.DataModel.DG;
 using SEE.Game;
 using SEE.Game.City;
 using SEE.GO;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace SEE.GameObjects.Decorators
 {
     /// <summary>
-    /// A decorater for game nodes generating an antenna representing various metrics
-    /// above the game node.
+    /// A decorator for game nodes generating an antenna representing various metrics
+    /// above a game node (leaf or inner alike).
     /// </summary>
     internal class AntennaDecorator
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="scaler">the scaler to be used to normalize the metric values</param>
+        /// <param name="antennaAttributes">the visual attributes to be considered for the antenna segments</param>
         public AntennaDecorator(IScale scaler, AntennaAttributes antennaAttributes)
         {
             this.scaler = scaler;
             this.antennaAttributes = antennaAttributes;
+            metricToFactory = CreateSegmentFactories(antennaAttributes);
         }
-
-        private IScale scaler;
-        private AntennaAttributes antennaAttributes;
 
         /// <summary>
-        /// Transformation method to scale numbers. Yields <paramref name="input"/> devided
-        /// by 100.
+        /// A mapping of metric names within <see cref="antennaAttributes"/> onto <see cref="CylinderFactory"/>s,
+        /// which are to be used to create the cylinders for the antenna segments.
         /// </summary>
-        /// <returns>input / 100</returns>
-        private static float Transform(float input)
-        {
-            return input;
-        }
+        private readonly Dictionary<string, CylinderFactory> metricToFactory;
 
+        /// <summary>
+        /// The scaler to be used to normalize the metric values.
+        /// </summary>
+        private readonly IScale scaler;
+
+        /// <summary>
+        /// The visual attributes to be considered for the antenna segments.
+        /// </summary>
+        private readonly AntennaAttributes antennaAttributes;
+
+        /// <summary>
+        /// Adds an antenna above <paramref name="gameNode"/>. The antenna consists of
+        /// antenna segments as specified by the <see cref="AntennaAttributes"/> passed
+        /// to the constructor. All created antenna segments will be children of
+        /// <paramref name="gameNode"/>.
+        /// </summary>
+        /// <param name="gameNode">the game node to be decorated (must have a graph
+        /// node attached)</param>
+        /// <param name="renderQueueOffset">the render queue offset of the game objects
+        /// created for the antenna segments</param>
+        /// <exception cref="Exception">thrown if <paramref name="gameNode"/> does
+        /// not have a graph node attached</exception>
         public void AddAntenna(GameObject gameNode, int renderQueueOffset = 0)
         {
             Node node = gameNode.GetNode();
 
-            // The local position of the beamMarker to be added in the current iteration
-            // relative to the parent. We will adjust only the y component of it within
-            // each iteration. At the end, the y component is the total length of the
-            // created segments.
-            Vector3 segmentPosition = Vector3.zero;
-            // The world-space scale of the beamMarker to be added in the current iteration.
-            // We will adjust only the y component of it for each iteration.
+            // The world-space position of the segment to be added in the current iteration.
+            // Initially set to the center of the roof of gameNode.
+            // We will adjust only the y component of it within each iteration.
+            Vector3 segmentPosition = gameNode.transform.position;
+            segmentPosition.y = gameNode.GetRoof();
+
+            // The world-space scale of the segment to be added in the current iteration.
+            // We will adjust only the y component of it for each iteration based on
+            // segment's metric.
             Vector3 segmentScale = new Vector3(antennaAttributes.AntennaWidth, 0, antennaAttributes.AntennaWidth);
 
-            // We will first create the segment markers as if they are to appear
-            // at the center position of parent. Only if we know their total length,
-            // we can place them below their parent.
             foreach (AntennaSection section in antennaAttributes.AntennaSections)
             {
                 if (node.TryGetNumeric(section.Metric, out float sectionMetric) && sectionMetric > 0)
                 {
-                    NodeFactory sectionFactory = GetSegmentFactory(section);
+                    NodeFactory segmentFactory = metricToFactory[section.Metric];
 
-                    // The marker should be drawn as part of the parent, hence, its render
+                    // The segment should be drawn as part of the parent, hence, its render
                     // queue offset must be equal to that of the parent.
-                    GameObject segmentObject = NewSegment(sectionFactory, renderQueueOffset);
-                    segmentObject.name = section.Metric + ": " + sectionMetric;
-                    segmentObject.tag = Tags.Decoration;
+                    GameObject segment = NewSegment(segmentFactory, renderQueueOffset);
+                    segment.name = section.Metric + ": " + sectionMetric;
+                    segment.tag = Tags.Decoration;
 
-                    float sectionHeight = Transform(scaler.GetNormalizedValue(section.Metric, sectionMetric));
-                    segmentScale.y = sectionHeight;
-                    segmentObject.transform.localScale = segmentScale;
-                    segmentObject.transform.SetParent(gameNode.transform);
-                    float sectionExtent = sectionHeight / 2;
-                    segmentPosition.y += sectionExtent;
-                    segmentObject.transform.localPosition = segmentPosition;
-                    segmentPosition.y += sectionExtent;
+                    float height = scaler.GetNormalizedValue(section.Metric, sectionMetric);
+                    segmentScale.y = height;
+                    segment.transform.localScale = segmentScale;
+
+                    // Note: The position attribute of our cylinders is actually the bottom and
+                    // not the center of the y axis.
+                    segment.transform.position = segmentPosition;
+                    segmentPosition.y += height;
+
+                    segment.transform.SetParent(gameNode.transform);
                 }
             }
+        }
 
-            // lower all segment markers so that they appear below their parent.
-            foreach (Transform child in gameNode.transform)
+        /// <summary>
+        /// Returns a mapping of metric names within <paramref name="antennaAttributes"/> onto
+        /// <see cref="CylinderFactory"/>s, which are to be used to create the cylinders for the
+        /// antenna segments. The color for these cylinders is the one provided in <paramref name="antennaAttributes"/>
+        /// for the respective metric.
+        /// </summary>
+        /// <param name="antennaAttributes">a specification of the antenna segments for which to create
+        /// the cylinder factories</param>
+        /// <returns>mapping of metrics onto factories</returns>
+        private static Dictionary<string, CylinderFactory> CreateSegmentFactories(AntennaAttributes antennaAttributes)
+        {
+            Dictionary<string, CylinderFactory> result = new Dictionary<string, CylinderFactory>(antennaAttributes.AntennaSections.Count);
+            foreach (AntennaSection section in antennaAttributes.AntennaSections)
             {
-                // FIXME
+                result[section.Metric] = new CylinderFactory(Materials.ShaderType.Opaque, new ColorRange(section.Color, section.Color, 1));
             }
+            return result;
         }
 
         /// <summary>
@@ -88,12 +125,6 @@ namespace SEE.GameObjects.Decorators
         private static GameObject NewSegment(NodeFactory factory, int renderQueueOffset)
         {
             return factory.NewBlock(0, renderQueueOffset);
-        }
-
-        private static NodeFactory GetSegmentFactory(AntennaSection section)
-        {
-            // FIXME Add a cache for these factories. They should not be created for each marker and loop iteration.
-            return new CylinderFactory(Materials.ShaderType.Opaque, new ColorRange(section.Color, section.Color, 1));
         }
     }
 }
