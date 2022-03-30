f280980433de2e7548745c124f1d90c7adcc0a4d
Rainer Koschke <koschke@uni-bremen.de>
2022-03-30 16:24:13 +0200

    #155 Added documentation. Inlined variables.

--- Assets/SEE/Layout/NodeLayouts/EvoStreets/Vector2Extension.cs
+++ Assets/SEE/Layout/NodeLayouts/EvoStreets/Vector2Extension.cs
@@ -7,12 +7,17 @@ namespace SEE.Layout.NodeLayouts.EvoStreets
     /// </summary>
     public static class Vector2Extension
     {
+        /// <summary>
+        /// Rotates the vector by given <paramref name="angle"/> in degrees.
+        /// </summary>
+        /// <param name="v">vector to be rotated</param>
+        /// <param name="angle">angle of the rotation in degree</param>
+        /// <returns></returns>
         public static Vector2 GetRotated(this Vector2 v, float angle)
         {
             float radian = angle * Mathf.Deg2Rad;
-            float _x = v.x * Mathf.Cos(radian) - v.y * Mathf.Sin(radian);
-            float _y = v.x * Mathf.Sin(radian) + v.y * Mathf.Cos(radian);
-            return new Vector2(_x, _y);
+            return new Vector2(v.x * Mathf.Cos(radian) - v.y * Mathf.Sin(radian),
+                               v.x * Mathf.Sin(radian) + v.y * Mathf.Cos(radian));
         }
     }
 }
