d9789122dddd83a14c3c742d5ab5cd057a201f64
Rainer Koschke <koschke@uni-bremen.de>
2022-04-07 15:58:19 +0200

    Added namespace. Improved code style.

--- Assets/SEE/Utils/ThreadJob.cs
+++ Assets/SEE/Utils/ThreadJob.cs
@@ -1,96 +1,109 @@
 using System.Collections;
 
-
-/// <summary>
-/// An abstract Class for a job in a new Thread.
-/// Code by Bunny83 November 2012
-/// https://answers.unity.com/questions/357033/unity3d-and-c-coroutines-vs-threading.html
-/// </summary>
-public class ThreadJob
+namespace SEE.Utils
 {
-    private bool m_IsDone = false;
-    private object m_Handle = new object();
-    private System.Threading.Thread m_Thread = null;
-
     /// <summary>
-    /// The status of the thread
+    /// A job in a new thread.
+    /// Code by Bunny83 November 2012
+    /// https://answers.unity.com/questions/357033/unity3d-and-c-coroutines-vs-threading.html
     /// </summary>
-    public bool IsDone
+    public class ThreadJob
     {
-        get
+        /// <summary>
+        /// Whether the job was done.
+        /// </summary>
+        private bool isDone = false;
+
+        /// <summary>
+        /// A handle for synchronization.
+        /// </summary>
+        private readonly object handle = new object();
+
+        /// <summary>
+        /// The thread executing the job.
+        /// </summary>
+        private System.Threading.Thread thread = null;
+
+        /// <summary>
+        /// The status of the thread.
+        /// </summary>
+        public bool IsDone
         {
-            bool tmp;
-            lock (m_Handle)
+            get
             {
-                tmp = m_IsDone;
+                bool tmp;
+                lock (handle)
+                {
+                    tmp = isDone;
+                }
+                return tmp;
             }
-            return tmp;
-        }
-        set
-        {
-            lock (m_Handle)
+            set
             {
-                m_IsDone = value;
+                lock (handle)
+                {
+                    isDone = value;
+                }
             }
         }
-    }
 
-    /// <summary>
-    /// starts a job in a new thread
-    /// </summary>
-    public virtual void Start()
-    {
-        m_Thread = new System.Threading.Thread(Run);
-        m_Thread.Start();
-    }
-
-    /// <summary>
-    /// Aborts the job
-    /// </summary>
-    public virtual void Abort()
-    {
-        m_Thread.Abort();
-    }
-
-    /// <summary>
-    /// The thread function, should be overwritten by a specific implementation
-    /// </summary>
-    protected virtual void ThreadFunction() { }
-
-    /// <summary>
-    /// That function is called on the end
-    /// </summary>
-    protected virtual void OnFinished() { }
-
-    public virtual bool Update()
-    {
-        if (IsDone)
+        /// <summary>
+        /// Starts a job in a new thread.
+        /// </summary>
+        public virtual void Start()
         {
-            OnFinished();
-            return true;
+            thread = new System.Threading.Thread(Run);
+            thread.Start();
         }
-        return false;
-    }
 
-    /// <summary>
-    /// Used to manage the thread in a coroutine 
-    ///  yield return StartCoroutine(myJob.WaitFor());
-    /// </summary>
-    /// <returns></returns>
-    public IEnumerator WaitFor()
-    {
-        while (!Update())
+        /// <summary>
+        /// Aborts the job.
+        /// </summary>
+        public virtual void Abort()
         {
-            yield return null;
+            thread.Abort();
         }
-    }
 
-    /// <summary>
-    /// Runs the Threadfunction
-    /// </summary>
-    private void Run()
-    {
-        ThreadFunction();
-        IsDone = true;
+        /// <summary>
+        /// The thread function, should be overwritten by a specific implementation.
+        /// </summary>
+        protected virtual void ThreadFunction() { }
+
+        /// <summary>
+        /// That function is called on the end.
+        /// </summary>
+        protected virtual void OnFinished() { }
+
+        public virtual bool Update()
+        {
+            if (IsDone)
+            {
+                OnFinished();
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Used to manage the thread in a coroutine, e.g.,
+        ///  yield return StartCoroutine(myJob.WaitFor());
+        /// </summary>
+        /// <returns>co-routine enumerator</returns>
+        public IEnumerator WaitFor()
+        {
+            while (!Update())
+            {
+                yield return null;
+            }
+        }
+
+        /// <summary>
+        /// Runs the <see cref="ThreadFunction"/>.
+        /// </summary>
+        private void Run()
+        {
+            ThreadFunction();
+            IsDone = true;
+        }
     }
 }
