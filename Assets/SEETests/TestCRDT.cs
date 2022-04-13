8b0a792db33ea105ae03d5ccb010916eec967025
Rainer Koschke <koschke@uni-bremen.de>
2022-04-13 16:35:23 +0200

    Tests the remote edit operations for the CRDT in play mode. These tests were originally contained in TestCRDT but there they did not work because the network was not started. Our new framework for play-mode tests handles the set up now.

--- Assets/SEETests/TestCRDT.cs
+++ Assets/SEETests/TestCRDT.cs
@@ -54,49 +54,6 @@ namespace SEETests
         }
 
         [Test]
-        public void TestRemoteDeleteChar()
-        {
-            CRDT test = new CRDT(new GUID().ToString(), "test");
-            test.AddChar('H', 0);
-            test.AddChar('A', 1);
-            test.AddChar('L', 2);
-            test.AddChar('O', 3);
-            test.AddChar('L', 2);
-            test.AddChar(' ', 5);
-            test.AddChar('!', 6);
-
-            test.RemoteDeleteChar(test.GetCRDT()[2].GetIdentifier());
-            Assert.AreEqual("HALO !", test.PrintString());
-            test.RemoteDeleteChar(test.GetCRDT()[3].GetIdentifier());
-            Assert.AreEqual("HAL !", test.PrintString());
-            try
-            {
-                test.RemoteDeleteChar(new Identifier[] { new Identifier(22, "22"), new Identifier(11, "11"), new Identifier(33, "33") });
-                Assert.Fail();
-            }
-            catch (RemoteDeleteNotPossibleException)
-            {
-            }
-        }
-
-        [Test]
-        public void RemoteAndAddCharSameTimePos0()
-        {
-            CRDT crdt1 = new CRDT("1", "test");
-            CRDT crdt2 = new CRDT("2", "test");
-
-            crdt1.AddChar('A', 0);
-            crdt2.AddChar('a', 0);
-            crdt2.AddChar('a', 0);
-
-            crdt2.RemoteAddChar('A', crdt1.GetCRDT()[0].GetIdentifier());
-            crdt1.RemoteAddChar('a', crdt2.GetCRDT()[1].GetIdentifier());
-            crdt1.RemoteAddChar('a', crdt2.GetCRDT()[1].GetIdentifier());
-            Assert.AreEqual("Aaa", crdt1.PrintString());
-            Assert.AreEqual("Aaa", crdt2.PrintString());
-        }
-
-        [Test]
         public void TestAddChar()
         {
             CRDT test = new CRDT(new GUID().ToString(), "test");
@@ -120,40 +77,6 @@ namespace SEETests
             Assert.AreEqual(1, test.ComparePosition(test.GetCRDT()[6].GetIdentifier(), test.GetCRDT()[1].GetIdentifier()));
             Assert.AreEqual(0, test.ComparePosition(test.GetCRDT()[2].GetIdentifier(), test.GetCRDT()[2].GetIdentifier()));
             Assert.AreEqual("HALLO (:!", test.PrintString());
-        }
-
-        [Test]
-        public void TestRemoteAdd()
-        {
-            CRDT crdt1 = new CRDT("1", "test");
-            CRDT crdt2 = new CRDT("2", "test");
-
-            crdt1.AddChar('H', 0);
-            crdt2.RemoteAddChar('H', crdt1.GetCRDT()[0].GetIdentifier());
-
-            crdt1.AddChar('A', 1);
-            crdt2.RemoteAddChar('A', crdt1.GetCRDT()[1].GetIdentifier());
-
-            crdt1.AddChar('L', 2);
-            crdt2.RemoteAddChar('L', crdt1.GetCRDT()[2].GetIdentifier());
-
-            crdt1.AddChar('O', 3);
-            crdt2.RemoteAddChar('O', crdt1.GetCRDT()[3].GetIdentifier());
-
-            //Simulate sync problems
-            crdt2.AddChar('l', 2);
-
-            crdt1.AddChar('!', 4);
-            crdt2.RemoteAddChar('!', crdt1.GetCRDT()[4].GetIdentifier());
-
-            crdt1.RemoteAddChar('l', crdt2.GetCRDT()[2].GetIdentifier());
-            //End sync problem
-            crdt1.AddChar('_', 0);
-            crdt2.RemoteAddChar('_', crdt1.GetCRDT()[0].GetIdentifier());
-
-
-            Print(crdt1);
-            Print(crdt2);
         }
 
         [Test]
@@ -229,12 +152,6 @@ namespace SEETests
             Assert.IsTrue(crdt.IsEmpty());
             crdt.AddChar('c', 0);
             Assert.IsFalse(crdt.IsEmpty());
-        }
-
-        private void Print(CRDT crdt)
-        {
-            Debug.LogWarning("CRDT: " + crdt.ToString());
-            Debug.LogWarning(crdt.PrintString());
         }
     }
 }
