16904e2d00522e51c35b1c5648f56f623d8f1f1b
Falko Galperin <falko1@uni-bremen.de>
2022-04-26 00:42:42 +0200

    Implement general Environment Variable Attribute #429
    This commit introduces a new attribute `EnvironmentVariable`
    which accepts the name of an environment variable as a parameter.
    If the class this attribute is used in then calls
    `SetEnvironmentVariableFields(this)`, all environment variables
    for the fields which have this attribute will be retrieved
    from the system, with their value used as the field's new value.
    If no environment variable was set on the system, nothing will
    happen.
    
    While environment variables are always given as strings, we
    will try to convert them to the type of the field, using
    Convert.ChangeType. This will work for all primitive types,
    e.g. `bool` and `int` and so on, but will probably fail
    for more complex types.
    
    For example, if you wanted to allow the field `debugLog`
    to be set by the environment variable `DEBUG_LOG`,
    you could do it like this:
    ```
    [EnvironmentVariable("DEBUG_LOG")]
    public bool debugLog;
    ```
    Then, wherever you initialize the class (i.e. the constructor
    or `Start`/`Awake`), you'll need to call
    `SetEnvironmentVariableFields(this)`:
    ```
    public void Awake()
    {
        SetEnvironmentVariableFields(this);
    }
    ```

---
+++ Assets/SEE/Utils/EnvironmentVariable.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Reflection;
+using System.Security;
+using Microsoft.SqlServer.Server;
+using UnityEngine;
+
+namespace SEE.Utils
+{
+    /// <summary>
+    /// An attribute specifying the name of an environment variable which can be used to set the value of the field
+    /// that's annotated.
+    ///
+    /// Note that just setting this attribute won't actually set the field's value.
+    /// To do this, call <see cref="EnvironmentVariableRetriever.SetEnvironmentVariableFields"/> with the instance
+    /// of the class as a parameter.
+    /// </summary>
+    /// <example>
+    /// For example, if you wanted to allow the field <c>debugLog</c>  to be set by the environment variable
+    /// <c>DEBUG_LOG</c>, you could do it like this:
+    /// <code>
+    /// [EnvironmentVariable("DEBUG_LOG")]
+    /// public bool debugLog;
+    /// </code>
+    /// Then, wherever you initialize the class (i.e. the constructor or <c>Start</c>/<c>Awake</c>),
+    /// you'll need to call <see cref="EnvironmentVariableRetriever.SetEnvironmentVariableFields"/>:
+    /// <code>
+    /// public void Awake()
+    /// {
+    ///     SetEnvironmentVariableFields(this);
+    /// }
+    /// </code>
+    /// </example>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class EnvironmentVariableAttribute : Attribute
+    {
+        /// <summary>
+        /// The name of the environment variable.
+        /// </summary>
+        public string VariableName { get; }
+
+        public EnvironmentVariableAttribute(string variableName)
+        {
+            VariableName = variableName;
+        }
+
+        public override string ToString()
+        {
+            return VariableName;
+        }
+    }
+
+    /// <summary>
+    /// Contains convenience methods for setting environment variables on a class whose fields are annotated
+    /// with <see cref="EnvironmentVariableAttribute"/>.
+    /// </summary>
+    public static class EnvironmentVariableRetriever
+    {
+        /// <summary>
+        /// Sets the value of all fields of <paramref name="target"/> which are annotated with
+        /// <see cref="EnvironmentVariableAttribute"/> to that environment variable's value, unless the
+        /// respective environment variable isn't set, in which case nothing will be done.
+        ///
+        /// If the annotated field has a type other than <c>string</c>, we will try to convert the environment
+        /// variable's value to that type. Note that this will likely only work with primitive types such as
+        /// <c>int</c>, <c>bool</c>, and so on. If the field's type is not supported, an
+        /// <see cref="InvalidCastException"/> will be thrown, since this is an error on the programmer's part.
+        /// If the field's type <em>is</em> supported but the supplied value is not in a suitable format,
+        /// a warning will be emitted and the field will be ignored, since this is an error on the user's part.
+        /// </summary>
+        /// <param name="target">Instance of the class whose fields annotated with
+        /// <see cref="EnvironmentVariableAttribute"/> shall be set to the respective
+        /// environment variable's value</param>
+        /// <typeparam name="T">Type of the given <paramref name="target"/></typeparam>
+        public static void SetEnvironmentVariableFields<T>(T target)
+        {
+            foreach (FieldInfo fieldInfo in typeof(T).GetFields())
+            {
+                Attribute attribute = Attribute.GetCustomAttribute(fieldInfo, typeof(EnvironmentVariableAttribute));
+                if (attribute is EnvironmentVariableAttribute environmentVariable)
+                {
+                    object result;
+                    try
+                    {
+                        result = Environment.GetEnvironmentVariable(environmentVariable.VariableName);
+                    }
+                    catch (SecurityException e)
+                    {
+                        // If no environment variable can be retrieved for security reasons, we'll log a warning
+                        // and stop.
+                        Debug.LogWarning($"Couldn't retrieve environment variable {environmentVariable}: {e}");
+                        break;
+                    }
+                    if (result != null)
+                    {
+                        // We may need to cast the (string) environment variable to the target type.
+                        Type fieldType = fieldInfo.FieldType;
+                        try
+                        {
+                            result = Convert.ChangeType(result, fieldType);
+                        }
+                        catch (Exception e) when (e is FormatException || e is OverflowException)
+                        {
+                            // These are the only problems that's the user's fault, hence we log it here.
+                            Debug.LogWarning($"Invalid format for environment variable {environmentVariable}: {e}");
+                            continue;
+                        }
+                        fieldInfo.SetValue(target, result);
+                    }
+                }
+            }
+        }
+    }
+}
