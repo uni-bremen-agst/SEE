039d4077462d7f1ee22aef6fca0a979115d3aacf
Falko Galperin <falko1@uni-bremen.de>
2022-05-26 17:46:34 +0200

    Skip artificial nodes during layout phase #95
    This skips all artificial nodes, i.e.,
    those without a node reference.
    For example, this can be used for artificial root nodes.

--- Assets/SEE/Game/GraphRenderer.cs
+++ Assets/SEE/Game/GraphRenderer.cs
@@ -749,6 +749,7 @@ namespace SEE.Game
         /// <summary>
         /// Transforms the given <paramref name="gameNodes"/> to a collection of LayoutNodes.
         /// Sets the node levels of all <paramref name="gameNodes"/>.
+        /// Any game objects in <paramref name="gameNodes"/> with an invalid node reference will be skipped.
         /// </summary>
         /// <param name="gameNodes">collection of game objects created to represent inner nodes or leaf nodes of a graph</param>
         /// <param name="leafNodeFactory">the leaf node factory that created the leaf nodes in <paramref name="gameNodes"/></param>
@@ -766,6 +767,11 @@ namespace SEE.Game
             foreach (GameObject gameObject in gameNodes)
             {
                 Node node = gameObject.GetComponent<NodeRef>().Value;
+                if (node == null)
+                {
+                    Debug.LogWarning($"Node {gameObject} has an invalid node reference and will be skipped!\n");
+                    continue;
+                }
                 NodeFactory factory = node.IsLeaf() ? leafNodeFactory : innerNodeFactory;
                 result.Add(new LayoutGameNode(toLayoutNode, gameObject, factory));
             }
