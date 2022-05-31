562dddfa0e8d7c54157642a93252e39794ce3f8b
Rainer Koschke <koschke@uni-bremen.de>
2022-05-31 12:41:08 +0200

    #95 Added documentation.

--- Assets/SEE/Utils/DirectoryPath.cs
+++ Assets/SEE/Utils/DirectoryPath.cs
@@ -2,13 +2,26 @@
 
 namespace SEE.Utils
 {
+    /// <summary>
+    /// A representation of paths for directories containing data that can be set absolute
+    /// in the file system or relative to one of Unity's standard folders such as
+    /// Assets, Project, etc.
+    /// </summary>
     [Serializable]
     public class DirectoryPath : DataPath
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public DirectoryPath() : base()
         {
         }
 
+        /// <summary>
+        /// Constructor where the kind of root and relative or absolute path is
+        /// derived from given <paramref name="path"/> (analogously to <see cref="Set(string)"/>).
+        /// </summary>
+        /// <param name="path">path from which to derive the kind of root and relative/absolute path</param>
         public DirectoryPath(string path) : base(path)
         {
         }
