0f6db6bb4b64085cceb7d4750f0c363954af1683
Rainer Koschke <koschke@uni-bremen.de>
2022-07-29 13:44:47 +0200

    Separated all type declarations in SEECitySettings into a file of their own.

---
+++ Assets/SEE/Game/City/AntennaAttributes.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using SEE.Utils;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace SEE.Game.City
+{
+    /// <summary>
+    /// Specifies how metrics are to be rendered as an antenna above the blocks.
+    /// </summary>
+    [Serializable]
+    [HideReferenceObjectPicker]
+    public sealed class AntennaAttributes : ConfigIO.PersistentConfigItem
+    {
+        /// <summary>
+        /// This parameter determines the sections of the antenna.
+        /// </summary>
+        [SerializeField]
+        public List<AntennaSection> AntennaSections = new List<AntennaSection>(1);
+
+        /// <summary>
+        /// This parameter determines the sections of the antenna, that is, the
+        /// order of the antenna segments and which properties they depict.
+        /// </summary>
+        //[SerializeField]
+        //[ListDrawerSettings(CustomAddFunction = "AddProperty")]
+        //public List<Property> AntennaSegments = new List<Property>();
+
+        //private Property AddProperty()
+        //{
+        //    Debug.Log("AddProperty\n");
+        //    Property result = new Property();
+        //    result.Kind = PropertyKind.Metric;
+        //    result.Name = "Metric.Mine";
+        //    return result;
+        //}
+
+        /// <summary>
+        /// The width of an antenna.
+        /// </summary>
+        public float AntennaWidth = 0.1f;
+
+        /// <summary>
+        /// Saves the settings in the configuration file.
+        ///
+        /// Implements <see cref="ConfigIO.PersistentConfigItem.Save(ConfigWriter, string)"/>.
+        /// </summary>
+        /// <param name="writer">to be used for writing the settings</param>
+        /// <param name="label">the outer label grouping the settings</param>
+        public void Save(ConfigWriter writer, string label)
+        {
+            writer.BeginGroup(label);
+            writer.Save(AntennaWidth, AntennaWidthLabel);
+            writer.Save(AntennaSections, AntennaSectionsLabel);
+            writer.EndGroup();
+        }
+
+        /// <summary>
+        /// Restores the settings from <paramref name="attributes"/> under the key <paramref name="label"/>.
+        /// The latter must be the label under which the settings were grouped, i.e., the same
+        /// value originally passed in <see cref="Save(ConfigWriter, string)"/>.
+        ///
+        /// Implements <see cref="ConfigIO.PersistentConfigItem.Save(ConfigWriter, string)"/>.
+        /// </summary>
+        /// <param name="attributes">dictionary of attributes from which to retrieve the settings</param>
+        /// <param name="label">the label for the settings (a key in <paramref name="attributes"/>)</param>
+        /// <returns>true if at least one attribute was successfully restored</returns>
+        public bool Restore(Dictionary<string, object> attributes, string label)
+        {
+            if (attributes.TryGetValue(label, out object dictionary))
+            {
+                bool result = false;
+                Dictionary<string, object> values = dictionary as Dictionary<string, object>;
+                ConfigIO.Restore(values, AntennaWidthLabel, ref AntennaWidth);
+                if (values.TryGetValue(AntennaSectionsLabel, out object antennaSections))
+                {
+                    if (!(antennaSections is IList<object> objects))
+                    {
+                        throw new InvalidCastException($"Value to be cast {antennaSections} is expected to be a list. Actual type is {antennaSections.GetType().Name}");
+                    }
+                    foreach (object anObject in objects)
+                    {
+                        if (!(anObject is Dictionary<string, object> antennaSection))
+                        {
+                            throw new InvalidCastException($"Value to be cast {anObject} is expected to be a dictionary. Actual type is {anObject.GetType().Name}");
+                        }
+                        AntennaSection section = new AntennaSection();
+                        result = section.Restore(antennaSection) || result;
+                        AddAntennaSection(section);
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                return false;
+            }
+
+            // If AntennaSections already has an antenna section for the metric in newSection,
+            // it will be removed. Then the newSection is added to it.
+            void AddAntennaSection(AntennaSection newSection)
+            {
+                AntennaSections.RemoveAll(section => section.Metric == newSection.Metric);
+                AntennaSections.Add(newSection);
+            }
+        }
+
+        /// <summary>
+        /// Label in the configuration file for the antenna width.
+        /// </summary>
+        private const string AntennaWidthLabel = "Width";
+
+        /// <summary>
+        /// Label in the configuration file for the antenna sections.
+        /// </summary>
+        private const string AntennaSectionsLabel = "AntennaSections";
+    }
+}
