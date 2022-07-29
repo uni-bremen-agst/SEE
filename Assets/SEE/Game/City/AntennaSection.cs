56c265b3fb6e663500c572515975fb1a428328b2
Rainer Koschke <koschke@uni-bremen.de>
2022-07-29 13:44:47 +0200

    Separated all type declarations in SEECitySettings into a file of their own.

---
+++ Assets/SEE/Game/City/AntennaSection.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using SEE.Utils;
+using UnityEngine;
+
+namespace SEE.Game.City
+{
+    /// <summary>
+    /// The settings of one antenna section specifying the metric that
+    /// determines the length and color of the section.
+    /// </summary>
+    [Serializable]
+    public class AntennaSection : ConfigIO.PersistentConfigItem
+    {
+        /// <summary>
+        /// The metric which should determine the length of the section.
+        /// </summary>
+        public string Metric;
+        /// <summary>
+        /// The color in which the section should be drawn.
+        /// </summary>
+        public Color Color;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="metric">the metric that should determine the length of the section</param>
+        /// <param name="color">color in the section should be drawn</param>
+        public AntennaSection(string metric, Color color)
+        {
+            Metric = metric;
+            Color = color;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public AntennaSection() : this(string.Empty, Color.white) { }
+
+        /// <summary>
+        /// Label in the configuration file for the <see cref="Metric"/>.
+        /// </summary>
+        private const string MetricLabel = "Metric";
+        /// <summary>
+        /// Label in the configuration file for the <see cref="Color"/>.
+        /// </summary>
+        private const string ColorLabel = "Color";
+
+        /// <summary>
+        /// Implements <see cref="ConfigIO.PersistentConfigItem.Save(ConfigWriter, string)"/>.
+        /// </summary>
+        public void Save(ConfigWriter writer, string label)
+        {
+            writer.BeginGroup(label);
+            writer.Save(Metric, MetricLabel);
+            writer.Save(Color, ColorLabel);
+            writer.EndGroup();
+        }
+
+        /// <summary>
+        /// Implements <see cref="ConfigIO.PersistentConfigItem.Restore(Dictionary{string, object}, string)"/>.
+        /// </summary>
+        public bool Restore(Dictionary<string, object> attributes, string label = "")
+        {
+            Dictionary<string, object> values;
+            if (string.IsNullOrEmpty(label))
+            {
+                // no label given => attributes contains already the data to be restored
+                values = attributes;
+            }
+            else if (attributes.TryGetValue(label, out object dictionary))
+            {
+                // label was given => attributes is a dictionary where we need to look up the data
+                // using the label
+                values = dictionary as Dictionary<string, object>;
+            }
+            else
+            {
+                // label was given, but attributes does not know it
+                // => no data; we cannot restore the object
+                return false;
+            }
+
+            bool metricRestored = ConfigIO.Restore(values, MetricLabel, ref Metric);
+            return ConfigIO.Restore(values, ColorLabel, ref Color) || metricRestored;
+        }
+    }
+}
---
+++ Assets/SEE/Game/City/ColorProperty.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using SEE.Utils;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace SEE.Game.City
+{
+    /// <summary>
+    /// Specifies for a node what the color is used for: either the node type
+    /// or a node metric.
+    /// </summary>
+    [Serializable]
+    public class ColorProperty : ConfigIO.PersistentConfigItem
+    {
+        /// <summary>
+        /// Whether color is used to represent the node type or a metric.
+        /// </summary>
+        [EnumToggleButtons]
+        public PropertyKind Property = PropertyKind.Type;
+
+        /// <summary>
+        /// The color used to represent the type of a node.
+        /// Used only if <see cref="Property"/> is <see cref="PropertyKind.Type"/>.
+        /// </summary>
+        [ShowIf("Property", PropertyKind.Type)]
+        public Color TypeColor = Color.white;
+
+        /// <summary>
+        /// The name of the metric determining the style (color) of a node. The
+        /// actual color is found in a <see cref="ColorMap"/> for the metrics.
+        /// Used only if <see cref="Property"/> is <see cref="PropertyKind.Metric"/>.
+        /// </summary>
+        [ShowIf("Property", PropertyKind.Metric)]
+        public string ColorMetric = string.Empty;
+
+        /// <summary>
+        /// Restores the settings from <paramref name="attributes"/> under the key <paramref name="label"/>.
+        /// The latter must be the label under which the settings were grouped, i.e., the same
+        /// value originally passed in <see cref="Save(ConfigWriter, string)"/>.
+        /// </summary>
+        /// <param name="attributes">dictionary of attributes from which to retrieve the settings</param>
+        /// <param name="label">the label for the settings (a key in <paramref name="attributes"/>)</param>
+        public bool Restore(Dictionary<string, object> attributes, string label = "")
+        {
+            if (attributes.TryGetValue(label, out object dictionary))
+            {
+                Dictionary<string, object> values = dictionary as Dictionary<string, object>;
+                ConfigIO.RestoreEnum(values, PropertyLabel, ref Property);
+                ConfigIO.Restore(values, TypeColorLabel, ref TypeColor);
+                ConfigIO.Restore(values, ColorMetricLabel, ref ColorMetric);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Saves the settings in the configuration file using <paramref name="writer"/>
+        /// under the given <paramref name="label"/>.
+        /// </summary>
+        /// <param name="writer">writer to be used to save the settings</param>
+        /// <param name="label">label under which to save the settings</param>
+        public void Save(ConfigWriter writer, string label = "")
+        {
+            writer.BeginGroup(label);
+            writer.Save(Property.ToString(), PropertyLabel);
+            writer.Save(TypeColor, TypeColorLabel);
+            writer.Save(ColorMetric, ColorMetricLabel);
+            writer.EndGroup();
+        }
+
+        /// <summary>
+        /// Label in the configuration file for a <see cref="Property"/>
+        /// </summary>
+        private const string PropertyLabel = "Property";
+        /// <summary>
+        /// Label in the configuration file for <see cref="TypeColor"/>.
+        /// </summary>
+        private const string TypeColorLabel = "TypeColor";
+        /// <summary>
+        /// Label in the configuration file for a <see cref="ColorMetric"/>
+        /// </summary>
+        private const string ColorMetricLabel = "ColorMetric";
+    }
+}
