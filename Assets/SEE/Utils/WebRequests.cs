e1e9a8f98f51f5b7b1fb492504781647ff871e34
Rainer Koschke <koschke@uni-bremen.de>
2022-09-06 16:18:57 +0200

    Improved documentation. Simplified code. Removed unused method.

--- Assets/SEE/Utils/WebRequests.cs
+++ Assets/SEE/Utils/WebRequests.cs
@@ -5,13 +5,18 @@ using UnityEngine.Networking;
 
 namespace SEE.Utils
 {
-    public class WebRequests
+    /// <summary>
+    /// Android does not allow to simply store files in a file system.
+    /// This class provides a way to read data from a JAR file
+    /// </summary>
+    public static class WebRequests
     {
         /// <summary>
         /// Returns a memory stream of the given of the file at the given jar file path.
         /// Because of the file structure in certain builds a Unity web request is needed at this point.
         /// </summary>
         /// <param name="jarPath">The jar file path to the required file</param>
+        /// <returns>stream of data contained in <paramref name="jarPath"/></returns>
         public static Stream GetStream(string jarPath)
         {
             string filename = GetFilePath(jarPath);
@@ -19,7 +24,7 @@ namespace SEE.Utils
             loadingRequest.SendWebRequest();
             while (!loadingRequest.isDone)
             {
-                if (loadingRequest.result == UnityWebRequest.Result.ConnectionError || 
+                if (loadingRequest.result == UnityWebRequest.Result.ConnectionError ||
                     loadingRequest.result == UnityWebRequest.Result.ProtocolError)
                 {
                     break;
@@ -47,7 +52,7 @@ namespace SEE.Utils
         /// </summary>
         /// <param name="jarPath">The jar file path to the required file</param>
         /// <returns>Actual file path</returns>
-        public static string GetFilePath(string jarPath)
+        private static string GetFilePath(string jarPath)
         {
             string path = "";
             if (jarPath.StartsWith("jar:file:/"))
@@ -59,9 +64,11 @@ namespace SEE.Utils
         }
 
         /// <summary>
-        /// Checks if the file to the given jar path exits
+        /// Returns true if the file to the given <paramref name="jarPath"/> exists.
         /// </summary>
+        /// <remarks>This function assumes that the requested file is contained in <see cref="Application.streamingAssetsPath"/></remarks>
         /// <param name="jarPath">The jar file path to the required file</param>
+        /// <returns>true if the file to the given <paramref name="jarPath"/> exists</returns>
         public static bool FileExists(string jarPath)
         {
             string filename = GetFilePath(jarPath);
@@ -75,24 +82,8 @@ namespace SEE.Utils
                     break;
                 }
             }
-            if (loadingRequest.result == UnityWebRequest.Result.ConnectionError ||
-                loadingRequest.result == UnityWebRequest.Result.ProtocolError)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-
-        public static DirectoryInfo SafeCreateDirectory(string path)
-        {
-            if (Directory.Exists(path))
-            {
-                return null;
-            }
-            return Directory.CreateDirectory(path);
+            return !(loadingRequest.result == UnityWebRequest.Result.ConnectionError
+                    || loadingRequest.result == UnityWebRequest.Result.ProtocolError);
         }
     }
 }
