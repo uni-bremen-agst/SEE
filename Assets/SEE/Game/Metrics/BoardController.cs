b21dc0db26ee2a38d3c78baf5018c585f4f69a24
Maximilian Wick <mwick@uni-bremen.de>
2022-09-09 16:54:38 +0200

    Major progress in prototype implementation

--- Assets/SEE/Game/Metrics/BoardController.cs
+++ Assets/SEE/Game/Metrics/BoardController.cs
@@ -1,65 +1,113 @@
 using System;
+using System.Threading.Tasks;
 using SEE.DataModel;
+using SEE.DataModel.DG;
 using SEE.GO;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
-namespace SEE.Game.MetricsVisualization
+namespace SEE.Game.Metrics
 {
     public class BoardController : MonoBehaviour
     {
-        public GameObject prefab;
-        private bool _initiated;
-    
-        // TODO: Update this automatically when a new code de city is loaded
-        private void Update()
-        {
-            if (! _initiated && Input.GetKeyDown(KeyCode.M))
-            {
-                _initiated = true;
-                GameObject[] nodes = GameObject.FindGameObjectsWithTag(Tags.Node);
-                GameObject[] edges = GameObject.FindGameObjectsWithTag(Tags.Edge);
-                int numberOfNodes = 0;
-                float sum = 0.0f;
-                foreach (GameObject node in nodes)
-                {
+        private bool _setUp;
+        
+        // All the metrics
+        private float _averageLinesOfCode;
+        
+        // References to prefabs to be bound from the unity editor.
+        public GameObject tachometerPrefab;
+        
+        // References to visualization GameObjects
+        private GameObject _linesOfCodeVisualization;
 
-                    DataModel.DG.Node graphNode = node.GetComponent<NodeRef>().Value;
-                    if (graphNode != null)
-                    {
-                        try
-                        {
-                            sum += graphNode.GetNumeric("Metric.Lines.LOC");
-                            numberOfNodes++;
-                        }
-                        catch(Exception e)
-                        {
-                            
-                        }
-                    }
-                }
-                float avg = sum / numberOfNodes;
+        private void Start()
+        {
+            // Draw all the selected visualizations on the board
+            // TODO: Dynamically decide which metrics to draw based on the user selection.
+            DrawLinesOfCodeMetric();
             
-                DrawLinesOfCodeMetric((int) avg);
-            }
+            _setUp = true;
         }
 
-        private void DrawLinesOfCodeMetric(int lineCount)
+        public async void OnGraphLoad()
         {
-            Vector2 boardSize = GetComponent<SpriteRenderer>().size;
-            Vector2 prefabSize = prefab.GetComponent<SpriteRenderer>().size;
-            float needleRotationZ;
-            if (lineCount < 350)
-                needleRotationZ = lineCount < 100 ? 130.0f : 90.0f;
-            else
-                needleRotationZ = lineCount < 400 ? -90.0f : -120.0f;
+            while (!_setUp)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+
+            // (Re-)Calculate all the metrics
+            CalculateMetrics();
+
+            // Adapt all the visualizations (e.g. change needle angle for tachometer)
+            UpdateLinesOfCodeMetric();
+        }
+
+        /// <summary>
+        /// This method calculates all the metrics for the currently loaded Graph(s). The results are saved in fields
+        /// of this class. We try to calculate as many metrics with as few iterations of loops as possible.
+        /// </summary>
+        private void CalculateMetrics()
+        {
+            GameObject[] nodes = GameObject.FindGameObjectsWithTag(Tags.Node);
+            //GameObject[] edges = GameObject.FindGameObjectsWithTag(Tags.Edge);
+            int totalNodes = 0;
+            float totalLines = 0.0f;
+
+            foreach (GameObject node in nodes)
+            {
+                Node graphNode = node.GetComponent<NodeRef>().Value;
+                if (graphNode != null && graphNode.TryGetNumeric("Metric.Lines.LOC", out var lines))
+                {
+                    totalLines += lines;
+                    totalNodes++;
+                }
+            }
+
+            if (totalNodes != 0)
+            {
+                _averageLinesOfCode = totalLines / totalNodes;    
+            }
+        }
+        
+        /// <summary>
+        /// Initially draws the lines of code metric with a default value.
+        /// This method is called once when the scene starts.
+        /// </summary>
+        private void DrawLinesOfCodeMetric()
+        {
+            Transform anchor = transform.Find("TopOne");  // TODO: Find out if getting by id is better in terms of run time
+            _linesOfCodeVisualization = Instantiate(tachometerPrefab, anchor.position, anchor.rotation, anchor);
+            TextMeshPro text = _linesOfCodeVisualization.transform.Find("TextAnchor").GetComponent<TextMeshPro>();
+            text.text = "Avg. lines of code";
+        }
+
+        /// <summary>
+        /// This method implements a mapping from a number of code lines to a number of degrees that determine how far a
+        /// tachometer needle in Unity will be rotated. The value range for the number of lines is from 0 to 1000, the
+        /// value range for the number of degrees is from 140 to -140. 0 lines of code will be mapped to 140 degrees, 
+        /// 1000 lines of code will be mapped to -140 degrees. This is a linear function and everything in between will 
+        /// be mapped accordingly. If the <paramref name="lines"/> lie outside the value range, they will be set to the
+        /// nearest value in the range.
+        /// </summary>
+        /// <param name="lines">The number of code lines</param>
+        /// <returns>The amount of degrees the needle should have</returns>
+        private static float MapLinesToDegrees(float lines)
+        {
+            if (lines > 1000)
+                lines = 1000;
+            else if (lines < 0)
+                lines = 0;
+            return (lines - 500) * -0.28f;
+        }
+        
+        private void UpdateLinesOfCodeMetric()
+        {
+            float needleRotationZ = MapLinesToDegrees(_averageLinesOfCode);
             Quaternion needleRotation = Quaternion.Euler(0.0f, 0.0f, needleRotationZ);
-            Transform anchor = transform.Find("TopOne");
-            GameObject prefabInstance = Instantiate(prefab, anchor.position, anchor.rotation, anchor);
-            prefabInstance.transform.Find("NeedleAnchor").localRotation = needleRotation;
-            TextMeshProUGUI text = prefabInstance.transform.Find("Canvas").Find("Text").GetComponent<TextMeshProUGUI>();
-            text.text = "Lines of code";
+            _linesOfCodeVisualization.transform.Find("NeedleAnchor").localRotation = needleRotation;
+            Debug.LogFormat("Set needle angle to {0}, average lines of code were {1}", needleRotationZ, _averageLinesOfCode);
         }
     }
 }
