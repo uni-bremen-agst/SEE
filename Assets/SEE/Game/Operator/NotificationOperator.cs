b334e67b80e2d0a8a83907be68d62b78cadcfb27
Falko Galperin <falko1@uni-bremen.de>
2022-10-09 18:19:26 +0200

    Display notifications in vertical list #475

---
+++ Assets/SEE/Game/Operator/NotificationOperator.cs
@@ -0,0 +1,50 @@
+using System;
+using DG.Tweening;
+using SEE.Utils;
+using UnityEngine;
+
+namespace SEE.Game.Operator
+{
+    /// <summary>
+    /// An operator offering methods for animating notification movements.
+    /// </summary>
+    public class NotificationOperator : AbstractOperator
+    {
+        /// <summary>
+        /// Operation handling position on the Y axis.
+        /// </summary>
+        private TweenOperation<float> PositionY;
+
+        public float TargetPositionY => PositionY.TargetValue;
+
+        /// <summary>
+        /// Moves the notification to the <paramref name="newY"/> position, taking <paramref name="duration"/> seconds.
+        /// </summary>
+        /// <param name="newY">the desired new target Y-coordinate</param>
+        /// <param name="duration">Time in seconds the animation should take. If set to 0, will execute directly,
+        /// that is, the value is set before control is returned to the caller.</param>
+        /// <returns>An operation callback for the requested animation</returns>
+        public IOperationCallback<Action> MoveToY(float newY, float duration)
+        {
+            Debug.Log($"Y: {newY}");
+            return PositionY.AnimateTo(newY, duration);
+        }
+
+        private void OnEnable()
+        {
+            RectTransform rectTransform = (RectTransform)transform;
+            Tween[] PositionYAction(float p, float d) => new Tween[]
+            {
+                rectTransform.DOAnchorPosY(p, d).Play()
+            };
+
+            PositionY = new TweenOperation<float>(PositionYAction, rectTransform.anchoredPosition.y);
+        }
+
+        private void OnDisable()
+        {
+            PositionY?.KillAnimator();
+            PositionY = null;
+        }
+    }
+}
