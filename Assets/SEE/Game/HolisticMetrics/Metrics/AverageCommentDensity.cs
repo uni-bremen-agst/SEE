3eb3b780c386c35584fa4abf52e3d3691d0dcdd7
Maximilian Wick <mwick@uni-bremen.de>
2022-10-11 11:04:05 +0200

    Metrics boards can handle different code cities

--- Assets/SEE/Game/HolisticMetrics/Metrics/AverageCommentDensity.cs
+++ Assets/SEE/Game/HolisticMetrics/Metrics/AverageCommentDensity.cs
@@ -1,27 +1,18 @@
-using SEE.DataModel;
 using SEE.DataModel.DG;
-using SEE.GO;
-using UnityEngine;
+using SEE.Game.City;
 
 namespace SEE.Game.HolisticMetrics.Metrics
 {
     internal class AverageCommentDensity : Metric
     {
-        internal override MetricValue Refresh()
+        internal override MetricValue Refresh(SEECity city)
         {
             float totalDensity = 0f;
             int totalNodes = 0;
 
-            foreach (GameObject graphElement in GraphElements)
+            foreach (Node node in city.LoadedGraph.Nodes())
             {
-                if (!graphElement.tag.Equals(Tags.Node))
-                {
-                    continue;
-                }
-                    
-                Node graphNode = graphElement.GetComponent<NodeRef>().Value;
-                if (graphNode != null 
-                    && graphNode.TryGetNumeric("Metric.Comment.Density", out float density))
+                if (node.TryGetNumeric("Metric.Comment.Density", out float density))
                 {
                     totalDensity += density;
                     totalNodes++;
