6acc63dd6726a930168529a41654714d77aabcbb
Maximilian Wick <mwick@uni-bremen.de>
2022-10-13 16:15:21 +0200

    Init only setter for widget controller

--- Assets/SEE/Game/HolisticMetrics/WidgetControllers/WidgetController.cs
+++ Assets/SEE/Game/HolisticMetrics/WidgetControllers/WidgetController.cs
@@ -5,7 +5,20 @@ namespace SEE.Game.HolisticMetrics.WidgetControllers
 {
     internal abstract class WidgetController : MonoBehaviour
     {
-        internal Guid ID;
+        /// <summary>
+        /// The field that saves the ID of this widget.
+        /// </summary>
+        private Guid? id;
+        
+        /// <summary>
+        /// The unique ID of this widget. It can be set once, then it is fixed.
+        /// </summary>
+        internal Guid ID
+        {
+            get => id.GetValueOrDefault();
+            set => id ??= value;  // In C# 9 this could be replaced by "init;".
+            
+        }
         
         internal abstract void Display(MetricValue metricValue);
     }
