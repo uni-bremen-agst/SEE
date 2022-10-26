db6f4d623cd0737f8e7177ca49d206976fa9858b
Rainer Koschke <koschke@uni-bremen.de>
2022-10-26 08:13:48 +0200

    #476 Parameter rdp was never used. Removed it.

--- Assets/SEE/Layout/EdgeLayouts/SplineEdgeLayout.cs
+++ Assets/SEE/Layout/EdgeLayouts/SplineEdgeLayout.cs
@@ -1,5 +1,4 @@
-﻿using SEE.Layout.Utils;
-using SEE.Utils;
+﻿using SEE.Utils;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,29 +12,14 @@ namespace SEE.Layout.EdgeLayouts
     {
         /// <summary>
         /// Constructor.
-        /// 
-        /// Parameter <paramref name="rdp"/> specifies the extent the polylines of the generated
-        /// splines are simplified. Neighboring line points whose distances fall below 
-        /// <paramref name="rdp"/> (with respect to the line drawn between their neighbors) will 
-        /// be removed. The greater the value is, the more aggressively points are removed 
-        /// (note: values greater than one are fine). A positive value close to zero results 
-        /// in a line with little to no reduction. A negative value is treated as 0. A value 
-        /// of zero has no effect.
         /// </summary>
         /// <param name="edgesAboveBlocks">if true, edges are drawn above nodes, otherwise below</param>
         /// <param name="minLevelDistance">the minimal distance between different edge levels</param>
-        /// <param name="rdp">epsilon parameter of the Ramer–Douglas–Peucker algorithm</param>
-        public SplineEdgeLayout(bool edgesAboveBlocks, float minLevelDistance, float rdp = 0.0f)
+        public SplineEdgeLayout(bool edgesAboveBlocks, float minLevelDistance)
             : base(edgesAboveBlocks, minLevelDistance)
         {
             name = "Splines";
-            this.rdp = rdp;
         }
-
-        /// <summary>
-        /// Determines to which extent the polylines of the generated splines are simplified.
-        /// </summary>
-        private readonly float rdp = 0.0f; // 0.0f means no simplification
 
         /// <summary>
         /// Adds way points to each edge in the given list of<paramref name= "edges" />
@@ -46,18 +30,18 @@ namespace SEE.Layout.EdgeLayouts
         /// do not pass through any other node and, hence, should include every node that
         /// may be in between sources and targets of any edge in <paramref name="edges"/>.
         /// </summary>
-        /// <param name="nodes">nodes whose edges are to be drawn or which are 
+        /// <param name="nodes">nodes whose edges are to be drawn or which are
         /// ancestors of any nodes whose edges are to be drawn</param>
         /// <param name="edges">edges for which to add way points</param>
         public override void Create<T>(IEnumerable<T> nodes, IEnumerable<ILayoutEdge<T>> edges)
         {
             MinMaxBlockY(nodes, out _, out _, out float maxHeight);
 
-            // The offset of the edges above or below the ground chosen relative 
+            // The offset of the edges above or below the ground chosen relative
             // to the height of the largest block.
-            // We are using a value relative to the highest node so that edges 
+            // We are using a value relative to the highest node so that edges
             // are farther away from the blocks for cities with large blocks and
-            // closer to blocks for cities with small blocks. This may help to 
+            // closer to blocks for cities with small blocks. This may help to
             // better read the edges.
             // This offset is used to draw the line somewhat below
             // or above the house (depending on the orientation).
