59055e7717ef4d6ecfd73e3cce4b0782a19549a2
Ferdinand <50866361+GniLudio@users.noreply.github.com>
2022-10-28 23:37:29 +0200

    #479 Sphere position on top of children
    The marking sphere now appears on top of all children.

--- Assets/SEE/Game/GameNodeMarker.cs
+++ Assets/SEE/Game/GameNodeMarker.cs
@@ -71,7 +71,7 @@ namespace SEE.Game
 
             // moves the sphere on top of the marked object
             Vector3 lossyScale = node.transform.lossyScale;
-            sphere.transform.position = node.transform.position + new Vector3(0, lossyScale.y/2, 0);
+            sphere.transform.position += new Vector3(0, GetHeightOffset(node), 0);
             
             // sets the sphere size to minimum of the width and depth of the node
             float diameter = Mathf.Min(lossyScale.x, lossyScale.z);
@@ -82,6 +82,22 @@ namespace SEE.Game
 
             // stores that marked node
             markedNodes[node.name] = sphere;
+        }
+        
+        /// <summary>
+        /// Calculates the height offset of the sphere, so it appears on top of the children.
+        /// </summary>
+        /// <param name="node">The node.</param>
+        /// <returns>The height offset.</returns>
+        private static float GetHeightOffset(GameObject node)
+        {
+            float nodeHeight = node.transform.position.y;
+            float height = node.transform.lossyScale.y / 2;
+            foreach (Transform transform in node.GetComponentsInChildren<Transform>())
+            {
+                height = Math.Max(height, transform.position.y - nodeHeight + transform.lossyScale.y / 2);
+            }
+            return height;
         }
     }
 }
