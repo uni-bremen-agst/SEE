a46b7f95a9986f82f1f5e937dcadcd8774880983
Maximilian Wick <mwick@uni-bremen.de>
2022-11-07 10:46:27 +0100

    Add two widgets

---
+++ Assets/SEE/Game/HolisticMetrics/WidgetControllers/BarChartController.cs
@@ -0,0 +1,100 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SEE.Game.HolisticMetrics.WidgetControllers
+{
+    /// <summary>
+    /// This class provides an interface for displaying multiple metric values on a bar chart that this script will be
+    /// attached to.
+    /// </summary>
+    internal class BarChartController : WidgetController
+    {
+        /// <summary>
+        /// The parent game object for all bars we will add to the bar chart.
+        /// </summary>
+        [SerializeField] private GameObject barsAnchor;
+        
+        /// <summary>
+        /// The labels of the diagram on the y axis.
+        /// </summary>
+        [SerializeField] private Text[] yLabels;
+
+        /// <summary>
+        /// This text will contain the name of the metric being displayed.
+        /// </summary>
+        [SerializeField] private Text titleText;
+
+        /// <summary>
+        /// The prefab of the bar that will be instantiated for each element whose metric value will be displayed.
+        /// </summary>
+        [SerializeField] private GameObject barPrefab;
+        
+        internal override void Display(MetricValue metricValue)
+        {
+            if (metricValue.GetType() == typeof(MetricValueCollection))
+            {
+                // First, remove all old bars 
+                for (int i = barsAnchor.transform.childCount - 1; i >= 0; i--)
+                {
+                    Destroy(barsAnchor.transform.GetChild(i).gameObject);
+                }
+                
+                // Cast the collection so it is usable
+                List<MetricValueRange> metricValues = ((MetricValueCollection)metricValue).MetricValues;
+                
+                // Set the name of the widget
+                titleText.text = metricValues[0].Name;
+
+                // Now set the text of the labels to the correct values
+                float minimum = metricValues[0].Lower;
+                float maximum = metricValues[0].Higher;
+                float range = maximum - minimum;
+                float stepLength = range / 5f;  // Divide by |labels| -1 because first label is at y=0, not range/6
+                if (minimum >= 0 && maximum <= 1)  // It is a percentage in [0, 1]
+                {
+                    for (int i = 0; i < yLabels.Length; i++)
+                    {
+                        float yValue = (minimum + stepLength * i) * 100;
+                        yLabels[i].text = yValue.ToString("F" + (metricValues[0].DecimalPlaces - 2)) + "%";
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < yLabels.Length; i++)
+                    {
+                        float yValue = minimum + stepLength * i;
+                        yLabels[i].text = yValue.ToString("F" + metricValues[0].DecimalPlaces);
+                    }
+                }
+                
+                // Now draw all the bars
+                float barDistance = 0f;
+                if (metricValues.Count > 1)
+                {
+                    barDistance = 870f / (metricValues.Count - 1);
+                }
+                for (int i = 0; i < metricValues.Count; i++)
+                {
+                    GameObject bar = Instantiate(barPrefab, barsAnchor.transform);
+                    Vector3 coordinates = new Vector3(
+                        barDistance * i,
+                        0f);
+                    bar.transform.localPosition = coordinates;
+                    bar.GetComponent<Image>().fillAmount = metricValues[i].Value / maximum;
+                }
+            }
+            else
+            {
+                // In case this method gets a single metric value, we put it into a new metric value collection and then
+                // recursively call this method with that collection.
+                
+                MetricValueCollection collection = new MetricValueCollection()
+                {
+                    MetricValues = new List<MetricValueRange>() { (MetricValueRange)metricValue }
+                };
+                Display(collection);
+            }
+        }
+    }
+}
