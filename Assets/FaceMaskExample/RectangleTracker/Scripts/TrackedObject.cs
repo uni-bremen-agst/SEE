d9c39d5fb9dc136d60d9e0f487bef64714c2752c
Rainer Koschke <koschke@uni-bremen.de>
2022-11-13 09:44:25 +0100

    Imported FaceMask Example version 1.0.9.

---
+++ Assets/FaceMaskExample/RectangleTracker/Scripts/TrackedObject.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e7ce6f33df637b13aa9aabfb278d9dd68d401f2f2a37b08133161f773431caaf
+size 1171
---
+++ Assets/FaceMaskExample/WebCamTextureFaceMaskAdditionalExample/WebCamTextureFaceMaskAdditionalExample.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:73fd9bc1bd2ae9d630254a007b543fdcbd615d2c39e5ad94d35e20b71c6b3245
+size 47798
