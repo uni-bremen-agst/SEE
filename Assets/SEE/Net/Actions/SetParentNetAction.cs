99ecb223bf0e12cf0d6f6a6c1bad92393b467ade
Rainer Koschke <koschke@uni-bremen.de>
2022-11-14 16:14:38 +0100

    SetParent is propagated through the network. Added documentation.

---
+++ Assets/SEE/Net/Actions/SetParentNetAction.cs
@@ -0,0 +1,73 @@
+﻿using SEE.Game;
+
+namespace SEE.Net.Actions
+{
+    /// <summary>
+    /// Propagates <see cref="ReflexionMapper.SetParent"/> or <see cref="GameNodeMover.SetParent"/>,
+    /// respectively, through the network.
+    /// </summary>
+    internal class SetParentNetAction : AbstractNetAction
+    {
+        /// <summary>
+        /// The unique name of the child gameObject that needs to be put onto a new parent.
+        /// Must be known to <see cref="GraphElementIDMap"/>.
+        /// </summary>
+        public string ChildID;
+
+        /// <summary>
+        /// The unique name of the gameObject that becomes the new parent of the child.
+        /// Must be known to <see cref="GraphElementIDMap"/>.
+        /// </summary>
+        public string NewParentID;
+
+        /// <summary>
+        /// If true, <see cref="ReflexionMapper.SetParent"/> will be called;
+        /// otherwise <see cref="GameNodeMover.SetParent"/>.
+        /// </summary>
+        public bool Reflexion;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="childID">the unique game-object name of the child to
+        /// be put and fit onto the <paramref name="newParentID"/>;
+        /// must be known to <see cref="GraphElementIDMap"/></param>
+        /// <param name="newParentID">the unique game-object name of the game object becoming the
+        /// new parent of <paramref name="childID"/>;
+        /// must be known to <see cref="GraphElementIDMap"/></param>
+        /// <param name="reflexion">if true, <see cref="ReflexionMapper.SetParent"/> will
+        /// be called; otherwise <see cref="GameNodeMover.SetParent"/></param>
+        public SetParentNetAction(string childID, string newParentID, bool reflexion)
+        {
+            ChildID = childID;
+            NewParentID = newParentID;
+            Reflexion = reflexion;
+        }
+
+        /// <summary>
+        /// Setting the new parent in all clients except the requesting client.
+        /// </summary>
+        protected override void ExecuteOnClient()
+        {
+            if (!IsRequester())
+            {
+                if (Reflexion)
+                {
+                    ReflexionMapper.SetParent(Find(ChildID), Find(NewParentID));
+                }
+                else
+                {
+                    GameNodeMover.SetParent(Find(ChildID), Find(NewParentID));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Does not do anything.
+        /// </summary>
+        protected override void ExecuteOnServer()
+        {
+            // Intentionally left blank.
+        }
+    }
+}
