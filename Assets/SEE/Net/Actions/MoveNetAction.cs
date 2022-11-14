35a7657fe2575a8e93420befec1247ed34d22862
Rainer Koschke <koschke@uni-bremen.de>
2022-11-14 14:03:00 +0100

    #476 Working towards propagation of scene manipulations to all clients.

---
+++ Assets/SEE/Net/Actions/MoveNetAction.cs
@@ -0,0 +1,68 @@
+﻿using SEE.Game;
+using UnityEngine;
+
+namespace SEE.Net.Actions
+{
+    /// <summary>
+    /// Propagates the (animated) movement of a game node through the network.
+    /// </summary>
+    internal class MoveNetAction : AbstractNetAction
+    {
+        /// <summary>
+        /// The unique name of the gameObject that needs to be moved.
+        /// Must be known to <see cref="GraphElementIDMap"/>.
+        /// </summary>
+        public string GameObjectID;
+
+        /// <summary>
+        /// Where the game object should be moved in world space.
+        /// </summary>
+        public Vector3 TargetPosition;
+
+        /// <summary>
+        /// The duration of the movement animation in seconds.
+        /// </summary>
+        public float Duration;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="gameObjectID">the unique game-object name of the game object to be moved;
+        /// must be known to <see cref="GraphElementIDMap"/></param>
+        /// <param name="targetPosition">the new position of the game object</param>
+        /// <param name="animationDuration">the duration of the movement animation in seconds</param>
+        public MoveNetAction(string gameObjectID, Vector3 targetPosition, float animationDuration)
+        {
+            GameObjectID = gameObjectID;
+            TargetPosition = targetPosition;
+            Duration = animationDuration;
+        }
+
+        /// <summary>
+        /// Movement in all clients except the requesting client.
+        /// </summary>
+        protected override void ExecuteOnClient()
+        {
+            if (!IsRequester())
+            {
+                GameObject gameObject = GraphElementIDMap.Find(GameObjectID);
+                if (gameObject != null)
+                {
+                    GameNodeMover.MoveTo(gameObject, TargetPosition, Duration);
+                }
+                else
+                {
+                    throw new System.Exception($"There is no game object with the ID {GameObjectID}.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Does not do anything.
+        /// </summary>
+        protected override void ExecuteOnServer()
+        {
+            // Intentionally left blank.
+        }
+    }
+}
