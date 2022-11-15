01805060867b619b50fac9770638949335cf2214
Rainer Koschke <koschke@uni-bremen.de>
2022-11-15 17:33:02 +0100

    Imported Odin Validator 3.1.7.

---
+++ Assets/Plugins/Sirenix/Odin Validator/EnsureCorrectOdinVersion.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:830a71d33decf0517ea6dd03a4b1e3488b0b3690abe6675b4359c62298a6ee72
+size 7465
---
+++ Assets/Plugins/Sirenix/Odin Validator/Readme.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3d57ca8f9fa3f063b6472991e907b1d02e866c9fae8842d2bcfb25326ab387f4
+size 847
