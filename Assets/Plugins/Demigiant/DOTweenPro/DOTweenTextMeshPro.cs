3c3b44f4354c36305c53e5ab70bc11669b75a183
Rainer Koschke <koschke@uni-bremen.de>
2022-11-15 16:21:52 +0100

    Update of meta files by Unity.

--- Assets/Plugins/Demigiant/DOTweenPro/DOTweenTextMeshPro.cs
+++ Assets/Plugins/Demigiant/DOTweenPro/DOTweenTextMeshPro.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b04ed4bfb966e6d8d8e1b9ee368b2bb21c86e9eff8cc47429e2119d06f239c1
-size 61851
+oid sha256:5913c47e48cf5235ddba9d60b906d75d1fe603ba31212aec242459159c5f7e2b
+size 61852
