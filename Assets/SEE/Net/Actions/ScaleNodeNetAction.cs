4e4f52fce45b39eb2a34db3aa6a46c5a869a544d
Rainer Koschke <koschke@uni-bremen.de>
2022-11-15 13:15:22 +0100

    #476 ScaleNodeNetAction only scales a node but no longer moves it.

--- Assets/SEE/Net/Actions/ScaleNodeNetAction.cs
+++ Assets/SEE/Net/Actions/ScaleNodeNetAction.cs
@@ -1,4 +1,5 @@
-﻿using SEE.GO;
+﻿using SEE.Game.Operator;
+using SEE.GO;
 using UnityEngine;
 
 namespace SEE.Net.Actions
@@ -12,29 +13,29 @@ namespace SEE.Net.Actions
         /// <summary>
         /// The id of the gameObject that has to be scaled.
         /// </summary>
-        public string UniqueGameObjectName;
+        public string GameObjectID;
 
         /// <summary>
-        /// The new scale to bring over the network.
+        /// The new local scale to transfer over the network.
         /// </summary>
-        public Vector3 Scale;
+        public Vector3 LocalScale;
 
         /// <summary>
-        /// The new position to transfer over the network.
+        /// The duration of the animation in seconds.
         /// </summary>
-        public Vector3 Position;
+        public float AnimationDuration;
 
         /// <summary>
         /// Constructs a ScaleNodeNetAction
         /// </summary>
-        /// <param name="uniqueGameObjectName">The unique name of the GameObject that should be scaled through the network</param>
-        /// <param name="scale">The new scale of the GameObject</param>
-        /// <param name="position">The new position of the GameObject</param>
-        public ScaleNodeNetAction(string uniqueGameObjectName, Vector3 scale, Vector3 position) : base()
+        /// <param name="gameObjectID">The unique name of the GameObject that should be scaled through the network</param>
+        /// <param name="localScale">The new local scale of the GameObject</param>
+        /// <param name="duration">The duration of the animation in seconds</param>
+        public ScaleNodeNetAction(string gameObjectID, Vector3 localScale, float duration = 0) : base()
         {
-            this.UniqueGameObjectName = uniqueGameObjectName;
-            this.Scale = scale;
-            Position = position;
+            GameObjectID = gameObjectID;
+            LocalScale = localScale;
+            AnimationDuration = duration;
         }
 
         /// <summary>
@@ -52,15 +53,9 @@ namespace SEE.Net.Actions
         {
             if (!IsRequester())
             {
-                GameObject scaleObj = GameObject.Find(UniqueGameObjectName);
-                if (scaleObj != null)
+                if (Find(GameObjectID).TryGetComponentOrLog(out NodeOperator nodeOperator))
                 {
-                    scaleObj.SetScale(Scale);
-                    scaleObj.transform.position = Position;
-                }
-                else
-                {
-                    Debug.LogError($"Found no game object: {UniqueGameObjectName}.\n");
+                    nodeOperator.ScaleTo(LocalScale, AnimationDuration);
                 }
             }
         }
