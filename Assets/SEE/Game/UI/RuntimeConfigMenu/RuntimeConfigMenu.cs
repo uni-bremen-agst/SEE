67fbc116df643b7b8bb0509bf3c4ee027de8f9c5
Ferdinand <50866361+GniLudio@users.noreply.github.com>
2022-11-17 21:05:40 +0100

    #503 Load City on Button Click
    First implementation of loading a city on a button click.
    (loads and draws "StreamingAssets/mini/mini.cfg")

--- Assets/SEE/Game/UI/RuntimeConfigMenu/RuntimeConfigMenu.cs
+++ Assets/SEE/Game/UI/RuntimeConfigMenu/RuntimeConfigMenu.cs
@@ -1,32 +1,41 @@
 using DynamicPanels;
 using SEE.Controls;
+using SEE.DataModel;
 using SEE.Game.City;
 using SEE.GO;
 using UnityEngine;
 using UnityEngine.UI;
+using Valve.VR;
 
 namespace SEE.Game.UI.RuntimeConfigMenu
 {
     public class RuntimeConfigMenu : MonoBehaviour
     {
+        private SteamVR_Action_Boolean openAction;
         private const string menuPrefab = "Prefabs/UI/RuntimeConfigMenu";
         private GameObject runtimeConfigMenu;
         
         void Awake()
         {
             Debug.Log("Awake Runtime Config Menu");
+            
+            openAction = SteamVR_Actions._default?.OpenSettingsMenu;
+            
             runtimeConfigMenu = Instantiate(Resources.Load<GameObject>(menuPrefab), Vector3.zero, Quaternion.identity);
             runtimeConfigMenu.SetActive(false);
             
             // adds a listener to the debug button
             GameObject debugButton = runtimeConfigMenu.transform.Find("Canvas/DebugButton").gameObject;
             Button debugButtonComponent = debugButton.GetComponent<Button>();
-            debugButtonComponent.onClick.AddListener(OnDebugClicked);
+            debugButtonComponent.onClick.AddListener(
+                () => SelectCity("StreamingAssets/mini/mini.cfg")
+                );
         }
 
         void Update()
         {
-            if (SEEInput.ToggleConfigMenu())
+            if ((PlayerSettings.GetInputType() == PlayerInputType.DesktopPlayer && SEEInput.ToggleConfigMenu()) ||
+                (PlayerSettings.GetInputType() == PlayerInputType.VRPlayer && openAction != null && openAction.GetStateDown(SteamVR_Input_Sources.Any)))
             {
                 runtimeConfigMenu.SetActive(!runtimeConfigMenu.activeSelf);
             }
@@ -35,15 +44,25 @@ namespace SEE.Game.UI.RuntimeConfigMenu
         private void OnDebugClicked()
         {
             Debug.Log("On Debug Clicked");
+
         }
 
-        private void SelectCity()
+        private void SelectCity(string cityPath)
         {
-            // TODO: Where do I get the city from?
-            // SEECity city;
-            // TODO: Relative to what?
-            //city.ConfigurationPath.RelativePath = "StreamingAssets\mini\mini.cfg";
-            // city.LoadData();
+            GameObject implementation = GameObject.FindGameObjectsWithTag(Tags.CodeCity)[0];
+            if (implementation)
+            {
+                SEECity city;
+                if (implementation.TryGetComponent(out city))
+                {
+                    city.ConfigurationPath.RelativePath = cityPath;
+                    city.LoadData();
+                    city.DrawGraph();
+                    city.LoadAndDrawGraph();
+                    
+                    runtimeConfigMenu.SetActive(false);
+                }
+            }
         } 
     }
 }
