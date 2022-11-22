a128e0d2acebd87a812b1789a04f39b16b2748a6
GniLudio <50866361+GniLudio@users.noreply.github.com>
2022-11-22 15:39:13 +0100

    #503 Reset City on Button Click
    Creates a utility class for runtime configuration.
    Adds a button for resetting the city.

--- Assets/SEE/Game/UI/RuntimeConfigMenu/RuntimeConfigMenu.cs
+++ Assets/SEE/Game/UI/RuntimeConfigMenu/RuntimeConfigMenu.cs
@@ -1,68 +1,52 @@
-using DynamicPanels;
 using SEE.Controls;
-using SEE.DataModel;
-using SEE.Game.City;
 using SEE.GO;
+using SEE.Utils;
 using UnityEngine;
-using UnityEngine.UI;
 using Valve.VR;
 
 namespace SEE.Game.UI.RuntimeConfigMenu
 {
     public class RuntimeConfigMenu : MonoBehaviour
     {
-        private SteamVR_Action_Boolean openAction;
-        private const string menuPrefab = "Prefabs/UI/RuntimeConfigMenu";
+        private SteamVR_Action_Boolean openAction = SteamVR_Actions._default?.OpenSettingsMenu;
+        private const string menuPrefabPath = "Prefabs/UI/RuntimeConfigMenu";
+        private const string switchPrefabPath = "Prefabs/UI/Input Group - Switch";
+
         private GameObject runtimeConfigMenu;
-        
+
         void Awake()
         {
             Debug.Log("Awake Runtime Config Menu");
-            
-            openAction = SteamVR_Actions._default?.OpenSettingsMenu;
-            
-            runtimeConfigMenu = Instantiate(Resources.Load<GameObject>(menuPrefab), Vector3.zero, Quaternion.identity);
+
+            runtimeConfigMenu = PrefabInstantiator.InstantiatePrefab(menuPrefabPath);
+            runtimeConfigMenu.name = "RuntimeConfigMenu";
             runtimeConfigMenu.SetActive(false);
+
+            RuntimeConfigMenuUtilities.AddActionToButton(runtimeConfigMenu, "Canvas/LoadCityButton", 
+                () => {
+                    RuntimeConfigMenuUtilities.LoadCity("mini/mini.cfg");
+                    runtimeConfigMenu.SetActive(false);
+                });
             
-            // adds a listener to the debug button
-            GameObject debugButton = runtimeConfigMenu.transform.Find("Canvas/DebugButton").gameObject;
-            Button debugButtonComponent = debugButton.GetComponent<Button>();
-            debugButtonComponent.onClick.AddListener(
-                () => SelectCity("StreamingAssets/mini/mini.cfg")
-                );
+            RuntimeConfigMenuUtilities.AddActionToButton(runtimeConfigMenu, "Canvas/ResetCityButton",
+                () =>
+                {
+                    RuntimeConfigMenuUtilities.ResetCity();
+                    runtimeConfigMenu.SetActive(false);
+                }
+            );
         }
 
         void Update()
         {
             if ((PlayerSettings.GetInputType() == PlayerInputType.DesktopPlayer && SEEInput.ToggleConfigMenu()) ||
-                (PlayerSettings.GetInputType() == PlayerInputType.VRPlayer && openAction != null && openAction.GetStateDown(SteamVR_Input_Sources.Any)))
+                (PlayerSettings.GetInputType() == PlayerInputType.VRPlayer 
+                 && openAction != null && openAction.GetStateDown(SteamVR_Input_Sources.Any)))
             {
                 runtimeConfigMenu.SetActive(!runtimeConfigMenu.activeSelf);
             }
         }
 
-        private void OnDebugClicked()
-        {
-            Debug.Log("On Debug Clicked");
 
-        }
-
-        private void SelectCity(string cityPath)
-        {
-            GameObject implementation = GameObject.FindGameObjectsWithTag(Tags.CodeCity)[0];
-            if (implementation)
-            {
-                SEECity city;
-                if (implementation.TryGetComponent(out city))
-                {
-                    city.ConfigurationPath.RelativePath = cityPath;
-                    city.LoadData();
-                    city.DrawGraph();
-                    city.LoadAndDrawGraph();
-                    
-                    runtimeConfigMenu.SetActive(false);
-                }
-            }
-        } 
     }
 }
