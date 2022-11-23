93a112344c5756e8cea2530a72d42d287f9baa47
Maximilian Wick <mwick@uni-bremen.de>
2022-11-23 14:58:11 +0100

    Added documentation

--- Assets/SEE/Game/GraphRenderer.cs
+++ Assets/SEE/Game/GraphRenderer.cs
@@ -411,6 +411,9 @@ namespace SEE.Game
                 }
             }
             
+            // This is necessary for the holistic metrics boards. They need to be informed when a code city is being
+            // drawn because then there will be a new graph loaded. In that case, the metrics boards might
+            // need to start listening for change events from that graph.
             BoardsManager.OnGraphDraw();
         }
 
