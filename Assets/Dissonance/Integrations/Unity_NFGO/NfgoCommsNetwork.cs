5f498b163eb7db8aa0f56d33d0e641ed0ecf2cc9
Rainer Koschke <koschke@uni-bremen.de>
2022-11-30 18:58:00 +0100

    Adjustments to changes in API of newer NFGO version.

--- Assets/Dissonance/Integrations/Unity_NFGO/NfgoCommsNetwork.cs
+++ Assets/Dissonance/Integrations/Unity_NFGO/NfgoCommsNetwork.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b1b0651fc5f399b223cbecd6a372cf0c7dae656c863df232da8373d2fbcc468
-size 8130
+oid sha256:20b93083628c2fca8a48b79cc349ecbbd5615f3e696827b3e52f0aec2ca5e512
+size 8120
