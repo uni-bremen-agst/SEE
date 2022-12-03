90d7d7becb8b14c2d86fbce88e7d515ed385fb5e
Rainer Koschke <koschke@uni-bremen.de>
2022-12-03 22:24:56 +0100

    Upgraded to Netcode for GameObject version 1.1.0. The sample code in Assets/Samples is based on an earlier version and, hence, deleted now. Provided an own implementation of ClientNetworkTransform taken from https://docs-multiplayer.unity3d.com/netcode/current/components/networktransform/index.html.

---
+++ Assets/SEE/Net/ClientNetworkTransform.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Netcode.Components;
+using UnityEngine;
+
+namespace SEE.Net
+{
+    /// <summary>
+    /// Used for syncing a transform with client side changes. This includes host.
+    /// Pure server as owner isn't supported by this. Please use NetworkTransform
+    /// for transforms that'll always be owned by the server.
+    /// </summary>
+    /// <remarks>This stems from https://docs-multiplayer.unity3d.com/netcode/current/components/networktransform/index.html</remarks>
+    [DisallowMultipleComponent]
+    public class ClientNetworkTransform : NetworkTransform
+    {
+        /// <summary>
+        /// Used to determine who can write to this transform. Owner client only.
+        /// This imposes state to the server. This is putting trust on your clients.
+        /// Make sure no security-sensitive features use this transform.
+        /// </summary>
+        protected override bool OnIsServerAuthoritative()
+        {
+            return false;
+        }
+    }
+}
