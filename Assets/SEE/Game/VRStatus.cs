e82ff1e5b373745c10b6411133b1f82d1a04afa9
Rainer Koschke <koschke@uni-bremen.de>
2022-12-11 10:51:47 +0100

    More information on disabling VRStatus.Enable(). Added the reasons to the FIXME.

--- Assets/SEE/Game/VRStatus.cs
+++ Assets/SEE/Game/VRStatus.cs
@@ -14,9 +14,12 @@ namespace SEE.Game
         /// Enables/disables the VR subsystem depending upon <paramref name="enable"/>.
         /// </summary>
         /// <param name="enable">if true, VR will be enabled</param>
+        [System.Obsolete]
         public static void Enable(bool enable)
         {
-            // FIXME: Temporarily disabled.
+            // FIXME: Temporarily disabled. Whether we need/want to re-enable it, depends
+            // upon the migration to Unity's new XR API. At the moment, VR will be started
+            // when a local VR player is spawned in AvatarAdapter.
             return;
 
             if (!enable)
