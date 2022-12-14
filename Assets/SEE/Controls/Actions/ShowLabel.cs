b1531b1f4822e7d41bdb6857b27e912de61f8c5d
Falko Galperin <falko1@uni-bremen.de>
2022-12-14 19:18:18 +0100

    Refactor ShowLabel into LabelOperator #499

--- Assets/SEE/Controls/Actions/ShowLabel.cs
+++ Assets/SEE/Controls/Actions/ShowLabel.cs
@@ -1,13 +1,10 @@
-﻿using DG.Tweening;
-using SEE.DataModel;
+﻿using System.Collections.Generic;
 using SEE.DataModel.DG;
 using SEE.Game;
 using SEE.Game.City;
+using SEE.Game.Operator;
 using SEE.GO;
-using SEE.Utils;
-using TMPro;
 using UnityEngine;
-using Valve.VR.InteractionSystem;
 
 namespace SEE.Controls.Actions
 {
@@ -31,6 +28,16 @@ namespace SEE.Controls.Actions
         /// True if the object is currently selected.
         /// </summary>
         private bool isSelected = false;
+
+        /// <summary>
+        /// Operator component for this object.
+        /// </summary>
+        private NodeOperator Operator;
+
+        /// <summary>
+        /// List of operators of currently displayed node labels.
+        /// </summary>
+        public static IList<NodeOperator> DisplayedLabelOperators = new List<NodeOperator>();
 
         /// <summary>
         /// Registers On() and Off() for the respective hovering and selection events.
@@ -147,300 +154,44 @@ namespace SEE.Controls.Actions
         }
 
         /// <summary>
-        /// The text label that's displayed above the object when the user hovers over it.
-        /// Will be <c>null</c> when the label is not currently being displayed.
-        /// This nodeLabel will contain a TextMeshPro component for the label text.
-        /// </summary>
-        private GameObject nodeLabel;
-
-        /// <summary>
-        /// The edge connecting a <see cref="nodeLabel"/> to its node.
-        /// Is a child of <see cref="nodeLabel"/> and contains a
-        /// LineRenderer that connects the labeled object and the label text visually.
-        /// </summary>
-        private GameObject edge;
-
-        /// <summary>
-        /// All currently active tweens, collected in a sequence.
-        /// </summary>
-        private Sequence sequence;
-
-        /// <summary>
-        /// Indicates whether we are about to destroy the label.
-        /// </summary>
-        private bool currentlyDestroying;
-
-        /// <summary>
-        /// Returns the code city holding the settings for the visualization of the node.
-        /// May be null.
-        /// </summary>
-        private AbstractSEECity City()
-        {
-            GameObject codeCityObject = SceneQueries.GetCodeCity(gameObject.transform)?.gameObject;
-            if (codeCityObject == null)
-            {
-                return null;
-            }
-            codeCityObject.TryGetComponent(out AbstractSEECity city);
-            return city;
-        }
-
-        /// <summary>
-        /// Returns the node this class is attached to.
-        /// May be null.
-        /// </summary>
-        private Node Node()
-        {
-            if (!gameObject.TryGetComponent(out NodeRef nodeRef) || nodeRef.Value == null)
-            {
-                return null;
-            }
-            return nodeRef.Value;
-        }
-
-        /// <summary>
-        /// Returns true iff labels are enabled for this kind of node.
-        /// </summary>
-        /// <param name="city">the code city holding the attributes for showing labels</param>
-        /// <param name="node">This node</param>
-        /// <returns>true iff labels are enabled for this kind of node</returns>
-        private static bool LabelsEnabled(AbstractSEECity city, Node node)
-        {
-            return city.NodeTypes.TryGetValue(node.Type, out VisualNodeAttributes visual) && visual.LabelSettings.Show;
-        }
-
-        /// <summary>
         /// Creates a text label above the object with its node's SourceName if the label doesn't exist yet.
         /// </summary>
         private void On()
         {
-            AbstractSEECity city = City();
-            if (city == null)
+            if (Operator == null)
             {
-                // The game node is currently not part of a city. This may happen, for instance,
-                // if a node is just created and not yet added to a city. In this case, we
-                // are not doing anything.
-                return;
-            }
-            Node node = Node();
-            if (node == null)
-            {
-                return;
+                Operator = gameObject.AddOrGetComponent<NodeOperator>();
             }
 
-            if (!LabelsEnabled(city, node))
+            if (Operator.Node != null)
             {
-                return; // If labels are disabled, we don't need to do anything
-            }
-
-            // If label already exists, nothing needs to be done
-            if (nodeLabel != null && !currentlyDestroying)
-            {
-                return;
-            }
-            // If sequence is being destroyed, we need to stop that and play it forwards again.
-            if (nodeLabel != null && currentlyDestroying)
-            {
-                currentlyDestroying = false;
-                // Label and its tweens already exist, so we don't need to change any of that.
-                sequence.PlayForward();
-                return;
-            }
-            currentlyDestroying = false;
-            string shownText = node.SourceName;
-
-            // The text of the label appears above the hull of the labeled game object
-            // which is including the heights of its children. A line will be drawn from the
-            // roof of the game object excluding its children to the label text.
-            // First we create the label.
-            // We define starting and ending positions for the animation.
-            Vector3 startLabelPosition = gameObject.GetTop();
-            nodeLabel = TextFactory.GetTextWithSize(
-                shownText,
-                startLabelPosition,
-                city.NodeTypes[node.Type].LabelSettings.FontSize,
-                lift: true,
-                textColor: Color.black.ColorWithAlpha(0f));
-            nodeLabel.name = $"Label {shownText}";
-            nodeLabel.transform.SetParent(gameObject.transform);
-
-            SetOutline();
-
-            // Second, add connecting line between "roof" of the game object and the text.
-            Vector3 startLinePosition = gameObject.GetRoofCenter();
-            edge = new GameObject();
-            LineFactory.Draw(edge, new[] {startLinePosition, startLinePosition}, 0.01f,
-                             Materials.New(Materials.ShaderType.TransparentLine, Color.black));
-            edge.transform.SetParent(nodeLabel.transform);
-
-            // The nodeLabel and its child edge must inherit the portal of gameObject.
-            Portal.GetPortal(gameObject, out Vector2 leftFront, out Vector2 rightBack);
-            Portal.SetPortal(nodeLabel, leftFront, rightBack);
-
-            AnimateLabel(city, node);
-        }
-
-        /// <summary>
-        /// Enables or disables an outline around TextMeshPro instances based on our platform.
-        /// </summary>
-        private void SetOutline()
-        {
-            // TODO: Previously, on the HoloLens, we made the text a bit easier to read by making it bolder.
-            //       We did this by adding a slight outline. For now, we'll keep the variable here in case
-            //       we need this feature in the future.
-            const bool enableOutline = false;
-            // However, when developing on a PC/Emulator, the background will be black, so we add a white outline.
-            Color outlineColor = Debug.isDebugBuild ? Color.white : Color.black;
-            if (nodeLabel.TryGetComponent(out TextMeshPro tm))
-            {
-                TextFactory.SetOutline(enableOutline, tm, outlineColor: outlineColor);
-            }
-            else
-            {
-                Debug.LogError("No TextMeshPro has been found on a newly created label.\n");
+                LabelAttributes settings = GetLabelSettings(Operator.Node, Operator.City);
+                if (settings.Show)
+                {
+                    DisplayedLabelOperators.Add(Operator);
+                    Operator.FadeLabel(settings.LabelAlpha, settings.AnimationDuration);
+                }
             }
         }
-
+        
         /// <summary>
-        /// Animates the given labels by fading them in and gradually changing their position.
-        /// </summary>
-        /// <param name="city">The <see cref="AbstractSEECity"/> object from which to get the settings.</param>
-        /// <param name="node">The node.</param>
-        private void AnimateLabel(AbstractSEECity city, Node node)
-        {
-            // TODO: Maybe the class in Tweens.cs should be used instead.
-            // However, I'm not sure why that class is a MonoBehaviour (shouldn't it be a static helper class?)
-            // and DOTween instead recommends using extension methods, so this is what's used here.
-            // Additionally, some specific functionality (e.g. callbacks), isn't available from Tweens.cs.
-
-            const float endAlpha = 1f;  // Alpha value the text and line will have at the end of the animation.
-            const float lineStartAlpha = endAlpha * 0.5f;  // Alpha value the start of the line should have.
-            Vector3 endLabelPosition = nodeLabel.transform.position;
-            endLabelPosition.y += city.NodeTypes[node.Type].LabelSettings.Distance;
-            // Due to the line not using world space, we need to transform its position accordingly.
-            Vector3 endLinePosition = edge.transform.InverseTransformPoint(endLabelPosition);
-            float labelTextExtent = nodeLabel.GetComponent<TextMeshPro>().textBounds.extents.y;
-            endLinePosition.y -= labelTextExtent * 1.3f; // add slight gap to make it slightly more aesthetic
-
-            float animationDuration = AnimationDuration(node, city);
-            if (animationDuration <= 0)
-            {
-                // If animation duration is set to 0, all otherwise animated attributes should be set immediately
-                SetAttributesImmediately();
-                return;
-            }
-
-            // We will play the animation backwards when going away, so we'll kill the tweens ourself.
-            sequence = DOTween.Sequence();
-            sequence.SetAutoKill(false);
-            sequence.SetLink(nodeLabel, LinkBehaviour.KillOnDestroy);
-            // By inserting all tweens in the same position, they will play simultaneously.
-            AnimateLabelText();
-            AnimateLabelLine();
-
-            #region Local Methods
-            void AnimateLabelText()
-            {
-                // Animated label to move to top and fade in
-                if (nodeLabel.TryGetComponent(out TextMeshPro text))
-                {
-                    sequence.Insert(0, nodeLabel.transform.DOMove(endLabelPosition, animationDuration));
-                    sequence.Insert(0, DOTween.ToAlpha(() => text.color, color => text.color = color, endAlpha, animationDuration));
-                    sequence.Insert(0, DOTween.ToAlpha(() => text.outlineColor, color => text.outlineColor = color, endAlpha, animationDuration));
-                }
-                else
-                {
-                    Debug.LogError("Couldn't find text component in newly created label.\n");
-                }
-            }
-
-            void AnimateLabelLine()
-            {
-                // Animated line to move to top and fade in
-                if (edge.TryGetComponent(out LineRenderer line))
-                {
-                    // Reset colors to clear first
-                    LineFactory.SetColors(line, Color.clear, Color.clear);
-
-                    // Lower start of line should be visible almost immediately due to reduced alpha (smooth transition)
-                    sequence.Insert(0, DOTween.ToAlpha(() => line.startColor, c => line.startColor = c, lineStartAlpha, animationDuration*0.1f));
-                    sequence.Insert(0, DOTween.ToAlpha(() => line.endColor, c => line.endColor = c, endAlpha, animationDuration));
-                    sequence.Insert(0, DOTween.To(() => line.GetPosition(1), p => line.SetPosition(1, p), endLinePosition, animationDuration));
-                }
-                else
-                {
-                    Debug.LogError("Couldn't find line component in newly created label.\n");
-                }
-            }
-
-            void SetAttributesImmediately()
-            {
-                // If we have an animation duration of 0, we can set the positions immediately and return.
-                if (nodeLabel.TryGetComponent(out TextMeshPro text) && edge.TryGetComponent(out LineRenderer line))
-                {
-                    nodeLabel.transform.position = endLabelPosition;
-                    text.alpha = endAlpha;
-                    line.startColor = line.startColor.ColorWithAlpha(lineStartAlpha);
-                    line.endColor = line.endColor.ColorWithAlpha(endAlpha);
-                    line.SetPosition(1, endLinePosition);
-                }
-                else
-                {
-                    Debug.LogError("Couldn't find required component in newly created label.\n");
-                }
-            }
-            #endregion
-        }
-
-        /// <summary>
-        /// Destroys the text label above the object if it exists.
+        /// Hides the text label above the object if it exists.
         ///
-        /// <seealso cref="SelectionOn"/>
         /// </summary>
+        /// <seealso cref="SelectionOn"/>
         private void Off()
         {
-            if (nodeLabel == null)
-            {
-                return;
-            }
-
-            currentlyDestroying = true;
-            if (sequence == null)
-            {
-                // If no sequence exists, animation duration is 0, so we immediately destroy the label.
-                DestroyLabel(nodeLabel);
-            }
-            else
-            {
-                // Fade out and move label down
-                sequence?.PlayBackwards();
-                sequence?.OnPause(() => DestroyLabel(nodeLabel));
-            }
+            Operator.FadeLabel(0f, 1.0f);
+            DisplayedLabelOperators.Remove(Operator);
         }
 
         /**
-         * Returns the animation duration using values defined in AbstractSEECity.
+         * Returns the label attributes for this node using values defined in AbstractSEECity.
          * <param name="node">The node.</param>
-         * <param name="city">The city object from which to retrieve the duration.
-         * If <code>null</code>, the city object will be retrieved by a call to <see cref="City"/>.</param>
          */
-        private float AnimationDuration(Node node, AbstractSEECity city = null)
+        private static LabelAttributes GetLabelSettings(Node node, AbstractSEECity city)
         {
-            city ??= City();
-            return city.NodeTypes[node.Type].LabelSettings.AnimationDuration;
-        }
-
-        /// <summary>
-        /// Destroys the node label. Should only be called after the animations have completed.
-        /// </summary>
-        private void DestroyLabel(GameObject animatedLabel) {
-            // Only destroy label if it still exists and if no animation is playing.
-            if (animatedLabel != null && currentlyDestroying)
-            {
-                Destroyer.DestroyGameObject(animatedLabel);
-            }
-
-            currentlyDestroying = false;
+            return city.NodeTypes[node.Type].LabelSettings;
         }
 
         /// <summary>
---
+++ Assets/SEE/Game/Operator/LabelOperator.cs
@@ -0,0 +1,163 @@
+using System.Linq;
+using DG.Tweening;
+using SEE.Controls.Actions;
+using SEE.GO;
+using TMPro;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace SEE.Game.Operator
+{
+    /// <summary>
+    /// Part of the <see cref="NodeOperator"/> class which is responsible for managing node labels.
+    /// </summary>
+    public partial class NodeOperator
+    {
+        /// <summary>
+        /// The game object containing the node's label.
+        /// </summary>
+        private GameObject nodeLabel;
+
+        /// <summary>
+        /// The line renderer responsible for the line which connects the node label's text to the node.
+        /// </summary>
+        private LineRenderer labelLineRenderer;
+
+        /// <summary>
+        /// The text of the node's label.
+        /// </summary>
+        private TextMeshPro labelText;
+
+        /// <summary>
+        /// Prefix for game object names of node labels.
+        /// </summary>
+        private const string LABEL_PREFIX = "Label ";
+
+        /// <summary>
+        /// Updates the position of the attached label, including its text and line.
+        /// </summary>
+        /// <param name="duration"></param>
+        private void UpdateLabelLayout(float duration)
+        {
+            // Assumption: There are only very few active labels, compared to all active and inactive labels
+            //             that may exist in the descendants of this node. Hence, we go through all active ones.
+            foreach (NodeOperator nodeOperator in ShowLabel.DisplayedLabelOperators.Where(x => x.LabelAlpha.TargetValue > 0f))
+            {
+                nodeOperator.LabelTextPosition.AnimateTo(nodeOperator.DesiredLabelTextPosition, duration);
+                nodeOperator.LabelStartLinePosition.AnimateTo(nodeOperator.DesiredLabelStartLinePosition, duration);
+                nodeOperator.LabelEndLinePosition.AnimateTo(nodeOperator.DesiredLabelEndLinePosition, duration);
+            }
+        }
+
+        /// <summary>
+        /// Creates and prepares the <see cref="nodeLabel"/> game object along with its components.
+        /// If <see cref="nodeLabel"/> already exists, nothing will happen.
+        /// </summary>
+        private void PrepareLabel()
+        {
+            string shownText = Node.SourceName;
+
+            nodeLabel = transform.Find(LABEL_PREFIX + shownText)?.gameObject;
+            if (nodeLabel == null)
+            {
+                // The text of the label appears above the hull of the labeled game object
+                // which is including the heights of its children. A line will be drawn from the
+                // roof of the game object excluding its children to the label text.
+                // First we create the label.
+                // We define starting and ending positions for the animation.
+                Vector3 startLabelPosition = gameObject.GetTop();
+                nodeLabel = TextFactory.GetTextWithSize(shownText,
+                                                        startLabelPosition,
+                                                        City.NodeTypes[Node.Type].LabelSettings.FontSize,
+                                                        lift: true,
+                                                        textColor: Color.black.ColorWithAlpha(0f));
+                nodeLabel.name = LABEL_PREFIX + shownText;
+                nodeLabel.transform.SetParent(gameObject.transform);
+
+                // Second, add connecting line between "roof" of the game object and the text.
+                Vector3 startLinePosition = gameObject.GetRoofCenter();
+                GameObject line = new GameObject
+                {
+                    name = $"{LABEL_PREFIX}{shownText} (Connecting Line)"
+                };
+                LineFactory.Draw(line, new[] { startLinePosition, startLinePosition }, 0.01f,
+                                 Materials.New(Materials.ShaderType.TransparentLine, Color.black));
+                line.transform.SetParent(nodeLabel.transform);
+
+                // The nodeLabel and its child edge must inherit the portal of gameObject.
+                Portal.GetPortal(gameObject, out Vector2 leftFront, out Vector2 rightBack);
+                Portal.SetPortal(nodeLabel, leftFront, rightBack);
+
+                // Make it invisible, initially.
+                if (nodeLabel.TryGetComponentOrLog(out labelText) && line.TryGetComponentOrLog(out labelLineRenderer))
+                {
+                    labelText.alpha = 0f;
+                    labelLineRenderer.startColor = labelLineRenderer.startColor.ColorWithAlpha(0f);
+                    labelLineRenderer.endColor = labelLineRenderer.endColor.ColorWithAlpha(0f);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Target position of the label's text.
+        /// </summary>
+        private Vector3 DesiredLabelTextPosition
+        {
+            get
+            {
+                Vector3 endLabelPosition = gameObject.GetTop(t => !t.name.StartsWith(LABEL_PREFIX));
+                if (LabelAlpha.TargetValue > 0)
+                {
+                    // Only put line and label up if the label should actually be shown.
+                    endLabelPosition.y += City.NodeTypes[Node.Type].LabelSettings.Distance;
+                }
+
+                return endLabelPosition;
+            }
+        }
+
+        /// <summary>
+        /// Desired starting position of the label's line.
+        /// </summary>
+        private Vector3 DesiredLabelStartLinePosition => gameObject.GetRoofCenter();
+
+        /// <summary>
+        /// Desired end position of the label's line.
+        /// </summary>
+        private Vector3 DesiredLabelEndLinePosition
+        {
+            get
+            {
+                // Due to the line not using world space, we need to transform its position accordingly.
+                Vector3 endLinePosition = LabelTextPosition.TargetValue;
+                float labelTextExtent = labelText.textBounds.extents.y;
+                endLinePosition.y -= labelTextExtent * 1.3f; // add slight gap to make it more aesthetic
+                return endLinePosition;
+            }
+        }
+
+        private Tween[] AnimateLabelAlphaAction(float alpha, float duration) => new Tween[]
+        {
+            // Animated label to move to top and fade in
+            DOTween.ToAlpha(() => labelText.color, color => labelText.color = color, alpha, duration).Play(),
+            // Lower start of line should be visible almost immediately due to reduced alpha (smooth transition)
+            DOTween.ToAlpha(() => labelLineRenderer.startColor, c => labelLineRenderer.startColor = c, alpha * 0.5f, duration * 0.1f).Play(),
+            DOTween.ToAlpha(() => labelLineRenderer.endColor, c => labelLineRenderer.endColor = c, alpha, duration).Play(),
+        };
+
+        private Tween[] AnimateLabelStartLinePositionAction(Vector3 startPosition, float duration) => new Tween[]
+        {
+            DOTween.To(() => labelLineRenderer.GetPosition(0), p => labelLineRenderer.SetPosition(0, p), startPosition, duration).Play()
+        };
+
+        private Tween[] AnimateLabelEndLinePositionAction(Vector3 endPosition, float duration) => new Tween[]
+        {
+            DOTween.To(() => labelLineRenderer.GetPosition(1), p => labelLineRenderer.SetPosition(1, p), endPosition, duration).Play()
+        };
+
+        private Tween[] AnimateLabelTextPositionAction(Vector3 position, float duration) => new Tween[]
+        {
+            nodeLabel.transform.DOMove(position, duration).Play()
+        };
+    }
+}
