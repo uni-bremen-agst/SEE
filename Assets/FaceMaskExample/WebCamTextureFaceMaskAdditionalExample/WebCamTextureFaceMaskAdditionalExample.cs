1bd546a658fc97af714ecddf6cac7cba18939cce
Rainer Koschke <koschke@uni-bremen.de>
2022-12-15 20:48:53 +0100

    Upgraded to FaceMask Example version 1.1.0.

--- Assets/FaceMaskExample/WebCamTextureFaceMaskAdditionalExample/WebCamTextureFaceMaskAdditionalExample.cs
+++ Assets/FaceMaskExample/WebCamTextureFaceMaskAdditionalExample/WebCamTextureFaceMaskAdditionalExample.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73fd9bc1bd2ae9d630254a007b543fdcbd615d2c39e5ad94d35e20b71c6b3245
-size 47798
+oid sha256:a3c5802f8a1395e5acdee60b42d0361fe8726490fdf26908b0ac0ddf2ae976a6
+size 47955
