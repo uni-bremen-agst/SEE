fca802d186f9da1114f2a5330079d2faca8d66e0
Rainer Koschke <koschke@uni-bremen.de>
2022-12-15 13:28:55 +0100

    Upgraded to Hightlight Plus version 9.1.1.

--- Assets/HighlightPlus/Resources/HighlightPlus/HighlightTarget.shader
+++ Assets/HighlightPlus/Resources/HighlightPlus/HighlightTarget.shader
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd93a8ad9514937b4ab6ff81354794e9c35f20bc80b84244a4856bddcbfd4f8f
-size 5188
+oid sha256:fb2693a65cff77bcc2c79c33f4d784d898fac5cdf769cdc8d3fa53941279dd0e
+size 5203
--- Assets/HighlightPlus/Scripts/HighlightEffect.cs
+++ Assets/HighlightPlus/Scripts/HighlightEffect.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28e3508e455f4c3ea2029f0c18fb06da628206ad507d0d3cab81f9b7eec09d00
-size 124459
+oid sha256:4c767c2bba1e62bc2608093a322fe7aa5c26c0d92664245b1290c5950dc8e532
+size 124539
