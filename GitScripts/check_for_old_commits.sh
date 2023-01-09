cd8f53fa7692df4ab5f18bed69956f08d4d05f6b
Falko Galperin <falko1@uni-bremen.de>
2023-01-09 19:37:46 +0100

    Modify git script behavior on CI runs

--- GitScripts/check_for_old_commits.sh
+++ GitScripts/check_for_old_commits.sh
@@ -4,9 +4,19 @@
 # This commit will only exist on old histories. If it exists, it must not be pushed,
 # otherwise all of the old history we took care to remove will be present again.
 
+set -e
+
 if git --no-replace-objects show -s d5eb0645d999623b72025d0bf19227a787af1fb1 2>/dev/null = 0; then
-    echo "Looks like you have a copy of the repository with a bad commit."
-    echo "Please save your work to a temporary location and delete this repository."
-    echo "If you create a fresh clone, that should fix this problem."
+    if [ -n "$CI" ]; then
+        echo "!!!!! DO NOT MERGE THIS BRANCH INTO MASTER !!!!!"
+        echo "You have pushed the old repository history onto the remote repository."
+        echo "Please rebase this branch to make sure only the new history (post 2023-01-01) is present."
+        echo "If you are unsure what to do, please use the ~Entwicklung channel in Mattermost."
+        echo "!!!!! DO NOT MERGE THIS BRANCH INTO MASTER !!!!!"
+    else
+        echo "Looks like you have a copy of the repository with a bad commit that should no longer be present."
+        echo "Please save your work to a temporary location and delete this repository."
+        echo "If you create a fresh clone, that should fix this problem."
+    fi
     exit 1
 fi
--- GitScripts/check_for_rendered_cities.sh
+++ GitScripts/check_for_rendered_cities.sh
@@ -4,6 +4,14 @@
 # commit will be looked at, so existing code cities will be ignored.
 # Only staged (to be committed) changes will be looked at.
 
+set -e
+
+if [ -n "$CI" ]; then
+    DIFF_COMMAND="git diff origin/master"
+else
+    DIFF_COMMAND="git diff --staged"
+fi
+
 output=""
 while IFS= read -r scene
 do
@@ -12,11 +20,20 @@ do
 Warning: Rendered CodeCities detected in scene '$scene'!"
         fi
 done << EOF
-$(git diff --staged -S"m_TagString: Node" --name-only Assets/Scenes 2> /dev/null)
+$($DIFF_COMMAND -S"m_TagString: Node" --name-only Assets/Scenes 2> /dev/null)
 EOF
 
 if [ -n "$output" ]; then
         printf '%s\nPlease delete all drawn CodeCities before committing.' "$output"
+        if [ -n "$CI" ]; then
+            echo ""
+            echo "! NOTE !"
+            echo "You have already pushed these commits to the repository."
+            echo "Before merging into master, identify the commits which have introduced"
+            echo "the rendered code cities, then rebase your branch to either remove these"
+            echo "commits or modify them so that the relevant scene remains unchanged."
+            echo "If you are unsure what to do, please use the ~Entwicklung channel in Mattermost."
+        fi
         exit 1
 fi
 exit 0
