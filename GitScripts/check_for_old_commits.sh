96d15dc2b57d7af08007e948a4ba18b0f1ee5af4
Falko Galperin <falko1@uni-bremen.de>
2023-01-09 19:15:15 +0100

    Generically run all scripts in `GitScripts` as part of hook

--- GitScripts/check_for_old_commits.sh
+++ GitScripts/check_for_old_commits.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-# This commit will only exist on old histories.
-git --no-replace-objects show -s d5eb0645d999623b72025d0bf19227a787af1fb1 2>/dev/null
-if [ $? = 0 ]; then
+# We rebased our repository on 2023-01-01.
+# This commit will only exist on old histories. If it exists, it must not be pushed,
+# otherwise all of the old history we took care to remove will be present again.
+
+if git --no-replace-objects show -s d5eb0645d999623b72025d0bf19227a787af1fb1 2>/dev/null = 0; then
     echo "Looks like you have a copy of the repository with a bad commit."
     echo "Please save your work to a temporary location and delete this repository."
     echo "If you create a fresh clone, that should fix this problem."
--- generateHook.bat
+++ generateHook.bat
@@ -2,10 +2,10 @@
 REM This script is intended for Windows environments.
 REM For POSIX environments (e.g. Linux/MacOS), please use the corresponding '.sh' script.
 REM This will install a pre-commit hook which verifies that no code cities
-REM were left in the scene. Call it by simply executing './setup_hook.bat'.
+REM were left in the scene.
 
 ECHO Installing pre-commit hook...
 
-COPY GitScripts\run_all.sh .git\hooks\pre-commit
+COPY GitScripts\run_all .git\hooks\pre-commit
 
 ECHO Pre-commit hook successfully installed!
