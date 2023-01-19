8a5c1e9a5611f1b366b74cd9b1b655f562279932
Ferdinand <50866361+GniLudio@users.noreply.github.com>
2023-01-19 16:40:04 +0100

    #503 TabMenu prefabs and comments
    Created the prefabs for the tab menu and added some comments in TabMenu.cs.

--- Assets/SEE/Game/UI/Menu/TabMenu.cs
+++ Assets/SEE/Game/UI/Menu/TabMenu.cs
@@ -8,25 +8,73 @@ namespace SEE.Game.UI.Menu
 
     public class TabMenu<T> : SelectionMenu<T> where T : ToggleMenuEntry
     {
-        // TODO: The Prefabs don't exist yet
-        protected virtual string ViewPrefab => UI_PREFAB_FOLDER + "View";
-        protected override string MenuPrefab => UI_PREFAB_FOLDER + "TabMenu";
-        protected override string EntryPrefab => UI_PREFAB_FOLDER + "TabButton";
-
-        protected virtual string ViewListPrefab => UI_PREFAB_FOLDER + "TabList";
+        protected const string TAB_PREFAB_FOLDER = UI_PREFAB_FOLDER + "TabMenu/";
         
-        protected virtual string ViewListPath => "ViewList/Content";
+        // TODO: The Prefabs don't exist yet
 
+        /// <summary>
+        /// The prefab for the tab menu.
+        /// </summary>
+        protected override string MenuPrefab => TAB_PREFAB_FOLDER + "TabMenu";
+        /// <summary>
+        /// The button list is already part of the tab menu prefab.
+        /// </summary>
+        protected override string EntryListPrefab => null;
+        /// <summary>
+        /// The button prefab for each entry.
+        /// </summary>
+        protected override string EntryPrefab => TAB_PREFAB_FOLDER + "TabButton";
+        /// <summary>
+        /// The path to the content game object.
+        /// </summary>
+        protected override string ContentPath => "MainContent";
+        /// <summary>
+        /// The path to the game object containing the buttons.
+        /// Starts at the content game object.
+        /// </summary>
+        protected override string EntryListPath => "TabList/Content";
+
+        /// <summary>
+        /// Where to find the game object containing the views.
+        /// Starts at the content game object.
+        /// </summary>
+        protected virtual string ViewListPath => "ViewList";
+        /// <summary>
+        /// The prefab for the game object containing the views.
+        /// Only necessary if no game object is found at <see cref="ViewListPath"/>.
+        /// </summary>
+        protected virtual string ViewListPrefab => null;
+        /// <summary>
+        /// The view prefab for each entry.
+        /// </summary>
+        protected virtual string ViewPrefab => TAB_PREFAB_FOLDER + "TabView";
+
+        /// <summary>
+        /// The game object containing the views.
+        /// </summary>
         protected GameObject ViewList { get; private set; }
 
-        public virtual GameObject ViewGameObject(T entry)
+        /// <summary>
+        /// Returns a view game object.
+        /// Assumes that the entry is part of the menu.
+        /// </summary>
+        /// <param name="entry">The menu entry.</param>
+        /// <returns>The view game object.</returns>
+        public virtual GameObject ViewGameObject(T entry) => ViewList.transform.Find(entry.Title).gameObject;
+
+        public TabMenu()
         {
-            return ViewList.transform.Find(entry.Title).gameObject;
+            HideAfterSelection = false;
         }
-        
+
+        /// <summary>
+        /// Initializes the menu and stores specific parts of the menu.
+        /// Creates the view list if necessary.
+        /// </summary>
         protected override void StartDesktop()
         {
             base.StartDesktop();
+            // Instantiates the view list if necessary
             if (Content.transform.Find(ViewListPath) == null)
             {
                 GameObject go = PrefabInstantiator.InstantiatePrefab(ViewListPrefab, Content.transform, false);
@@ -35,6 +83,9 @@ namespace SEE.Game.UI.Menu
             ViewList = Content.transform.Find(ViewListPath).gameObject;
         }
 
+        /// <summary>
+        /// Updates the menu and adds listeners.
+        /// </summary>
         protected override void OnStartFinished()
         {
             base.OnStartFinished();
@@ -46,6 +97,10 @@ namespace SEE.Game.UI.Menu
             OnEntryUnselected += DeactivateView;
         }
 
+        /// <summary>
+        /// Creates the view for an added entry.
+        /// </summary>
+        /// <param name="entry">The menu entry.</param>
         protected virtual void AddView(T entry)
         {
             GameObject view = PrefabInstantiator.InstantiatePrefab(ViewPrefab, ViewList.transform, false);
@@ -53,16 +108,31 @@ namespace SEE.Game.UI.Menu
             view.SetActive(false);
         }
 
+        /// <summary>
+        /// Removes the view game object for a removed menu entry.
+        /// Assumes that the entry was contained in the menu.
+        /// </summary>
+        /// <param name="entry">The menu entry.</param>
         protected virtual void RemoveView(T entry)
         {
             Destroy(ViewGameObject(entry));
         }
 
+        /// <summary>
+        /// Activates a view.
+        /// Assumes that the entry is contained in the menu.
+        /// </summary>
+        /// <param name="entry"></param>
         protected virtual void ActivateView(T entry)
         {
             ViewGameObject(entry).SetActive(true);
         }
 
+        /// <summary>
+        /// Deactivates a view.
+        /// Assumes that the entry is contained in the menu.
+        /// </summary>
+        /// <param name="entry">The menu entry.</param>
         protected virtual void DeactivateView(T entry)
         {
             ViewGameObject(entry).SetActive(false);
