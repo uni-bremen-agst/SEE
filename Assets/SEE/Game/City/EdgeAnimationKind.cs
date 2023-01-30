c8504ce79989524b3e2abb06ef63e4c8cb997647
m4xxed <m4xxed@pm.me>
2023-01-30 17:22:40 +0100

    added enum to describe kind of edge animation used #498

---
+++ Assets/SEE/Game/City/EdgeAnimationKind.cs
@@ -0,0 +1,12 @@
+namespace SEE.Game.City
+{
+    /// <summary>
+    /// The kinds of edge animations available.
+    /// </summary>
+    public enum EdgeAnimationKind : byte
+    {
+        Fading,
+        Buildup,
+        None
+    }
+}
