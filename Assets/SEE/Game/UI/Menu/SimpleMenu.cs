fc40ef3a7f28e3c22092e21333da6fe626f4c031
Ferdinand <50866361+GniLudio@users.noreply.github.com>
2023-02-04 12:22:59 +0100

    #541 Make SimpleMenu a partial class and move implementation to SimpleMenuDesktop

--- Assets/SEE/Game/UI/Menu/SimpleMenu.cs
+++ Assets/SEE/Game/UI/Menu/SimpleMenu.cs
@@ -1,27 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
-using Michsky.UI.ModernUIPack;
 using SEE.Controls;
-using SEE.Utils;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
 
 namespace SEE.Game.UI.Menu
 {
-    public class SimpleMenu : PlatformDependentComponent
+    public partial class SimpleMenu : PlatformDependentComponent
     {
-        /// <summary>
-        /// Prefab for the menu.
-        /// Requires a ModalWindowManager component.
-        /// </summary>
-        protected virtual string MenuPrefab => UI_PREFAB_FOLDER + "Menu";
-        /// <summary>
-        /// Sprite for the icon.
-        /// </summary>
-        protected virtual string IconSprite => "Materials/ModernUIPack/Settings";
-
         /// <summary>
         /// The title of this menu.
         /// </summary>
@@ -117,7 +104,6 @@ namespace SEE.Game.UI.Menu
         /// Uses <see cref="Canvas"/> by default.
         /// </summary>
         private Transform parent;
-
         /// <summary>
         /// The parent of the menu.
         /// Uses <see cref="Canvas"/> by default.
@@ -141,19 +127,6 @@ namespace SEE.Game.UI.Menu
             ShowMenu = !ShowMenu;
         }
 
-        /// <summary>
-        /// The menu game object.
-        /// </summary>
-        protected GameObject Menu { get; private set; }
-        /// <summary>
-        /// The menu manager.
-        /// </summary>
-        protected ModalWindowManager MenuManager { get; private set; }
-        /// <summary>
-        /// The menu tooltip.
-        /// </summary>
-        protected Tooltip.Tooltip MenuTooltip { get; private set; }
-    
         /// <summary>
         /// Updates the keyword listener.
         /// </summary>
@@ -179,7 +152,9 @@ namespace SEE.Game.UI.Menu
         /// <returns>titles the menu should listen to</returns>
         protected virtual IEnumerable<string> GetKeywords()
         {
-            return Enumerable.Empty<string>().Append(CloseMenuKeyword);
+            IEnumerable<string> keywords = Enumerable.Empty<string>();
+            if (CloseMenuKeyword != null) keywords = keywords.Append(CloseMenuKeyword);
+            return keywords;
         }
     
         /// <summary>
@@ -199,127 +174,9 @@ namespace SEE.Game.UI.Menu
         }
     
         /// <summary>
-        /// Updates the title.
-        /// </summary>
-        protected virtual void UpdateTitle()
-        {
-            Menu.name = Title;
-            MenuManager.titleText = Title;
-            UpdateLayout();
-        }
-
-        /// <summary>
-        /// Updates the description.
-        /// </summary>
-        protected virtual void UpdateDescription()
-        {
-            MenuManager.descriptionText = Description;
-            UpdateLayout();
-        }
-
-        /// <summary>
-        /// Updates the icon.
-        /// </summary>
-        protected virtual void UpdateIcon()
-        {
-            MenuManager.icon = Icon;
-            UpdateLayout();
-        }
-
-        /// <summary>
-        /// Updates whether the menu is shown.
-        /// </summary>
-        protected virtual void UpdateShowMenu()
-        {
-            if (ShowMenu)
-            {
-                Menu.transform.SetAsLastSibling();
-                MenuManager.OpenWindow();
-            }
-            else
-            {
-                MenuManager.CloseWindow();
-                MenuTooltip.Hide();
-            }
-        }
-
-        /// <summary>
-        /// Updates the menu layout.
-        /// </summary>
-        protected virtual void UpdateLayout()
-        {
-            MenuManager.UpdateUI();
-            LayoutRebuilder.ForceRebuildLayoutImmediate(Menu.transform as RectTransform);
-        }
-
-        /// <summary>
-        /// Initializes the menu.
-        /// </summary>
-        protected override void StartDesktop()
-        {
-            // instantiates the menu
-            Menu = PrefabInstantiator.InstantiatePrefab(MenuPrefab, Parent, false);
-            Menu.name = Title;
-            MenuManager = Menu.GetComponent<ModalWindowManager>();
-            
-            // sets the icon
-            Icon = Resources.Load<Sprite>(IconSprite);
-        
-            // creates the tooltip
-            MenuTooltip = Menu.AddComponent<Tooltip.Tooltip>();
-        }
-    
-        /// <summary>
-        /// <see cref="StartDesktop"/>
-        /// </summary>
-        protected override void StartVR() => StartDesktop();
-        
-        /// <summary>
-        /// <see cref="StartDesktop"/>
-        /// </summary>
-        protected override void StartTouchGamepad() => StartDesktop();
-
-        /// <summary>
-        /// Updates the menu and adds listeners to events.
-        /// </summary>
-        protected override void OnStartFinished()
-        {
-            base.OnStartFinished();
-            // updates the menu
-            UpdateKeywordListener();
-            UpdateTitle();
-            UpdateDescription();
-            UpdateIcon();
-            UpdateShowMenu();
-            // adds listeners for updating the menu
-            OnTitleChanged += UpdateTitle;
-            OnDescriptionChanged += UpdateDescription;
-            OnIconChanged += UpdateIcon;
-            OnShowMenuChanged += UpdateShowMenu;
-            OnShowMenuChanged += UpdateKeywordListener;
-            OnCloseMenuCommandChanged += UpdateKeywordListener; ;
-        }
-
-        /// <summary>
-        /// Updates the component for the current platform.
-        /// Destroys the component if the corresponding menu has been destroyed or was not properly initialized.
-        /// </summary>
-        protected override void Update()
-
-        {
-            // destroys the component without a menu
-            if (Menu == null)
-            {
-                Destroy(this);
-                return;
-            }
-            base.Update();
-        }
-
-        /// <summary>
         /// Destroying the component also destroys the menu.
         /// </summary>
-        private void OnDestroy()
+        protected virtual void OnDestroy()
         {
             if (Menu != null) Destroy(Menu);
         }
