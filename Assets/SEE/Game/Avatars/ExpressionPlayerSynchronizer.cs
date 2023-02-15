50afd023a0c44bea05d271a60d7e17633df3a09f
Rainer Koschke <koschke@uni-bremen.de>
2023-02-15 17:43:07 +0100

    #524 Removed redundant code of ExpressionPlayerSynchronizer and VRIKSynchronizer. A new shared abstract superclass NetSynchronizer now implements the common behaviour.

--- Assets/SEE/Game/Avatars/ExpressionPlayerSynchronizer.cs
+++ Assets/SEE/Game/Avatars/ExpressionPlayerSynchronizer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using SEE.Net.Actions;
 using UMA.PoseTools;
-using Unity.Netcode;
 using UnityEngine;
 
 namespace SEE.Game.Avatars
@@ -10,71 +9,47 @@ namespace SEE.Game.Avatars
     /// Responsible to synchronize the facial expressions
     /// of the <see cref="UMAExpressionPlayer"/> on all clients.
     ///
-    /// This component is intended to be attached to a gameobject that has a <see cref="UMAExpressionPlayer"/> as
-    /// component.
+    /// This component is intended to be attached to a gameobject that has a
+    /// <see cref="UMAExpressionPlayer"/> as component.
     /// </summary>
-    public class ExpressionPlayerSynchronizer : MonoBehaviour
+    internal class ExpressionPlayerSynchronizer : NetSynchronizer
     {
         /// <summary>
-        /// Parts of this UMAExpressionPlayer should be synchronized on all clients.
+        /// Parts of this <see cref="UMAExpressionPlayer"/> should be synchronized
+        /// on all clients.
         /// </summary>
         private UMAExpressionPlayer expressionPlayer;
 
         /// <summary>
-        /// Time in between two update cycles for the synchronization in seconds.
-        /// </summary>
-        private const float RepeatCycle = 0.05f;
-
-        /// <summary>
-        /// The network object.
-        /// </summary>
-        private NetworkObject networkObject;
-
-        /// <summary>
-        /// Timer to count elapsed time in seconds.
-        /// </summary>
-        private float timer;
-
-        /// <summary>
-        /// Waits until <see cref="UMAExpressionPlayer"/> is available and initializes <see cref="expressionPlayer"/>
-        /// and <see cref="networkObject"/>.
+        /// Waits until <see cref="UMAExpressionPlayer"/> is available and
+        /// initializes <see cref="expressionPlayer"/>.
         /// </summary>
         private IEnumerator WaitForExpressionPlayer()
         {
             // Waits for UMAExpressionPlayer to be created
             yield return new WaitUntil(() => gameObject.GetComponent<UMAExpressionPlayer>() != null);
             expressionPlayer = gameObject.GetComponent<UMAExpressionPlayer>();
-            networkObject = gameObject.GetComponent<NetworkObject>();
-            timer = 0f;
         }
 
         /// <summary>
-        /// Starts a coroutine that waits for components to be generated at runtime.
+        /// Initializes the network object and starts a coroutine that
+        /// waits for components to be generated at runtime.
         /// </summary>
-        private void Start()
+        protected override void Start()
         {
+            base.Start();
             StartCoroutine(WaitForExpressionPlayer());
-        }
-
-        /// <summary>
-        /// Executes <see cref="Synchronize"/> every <see cref="RepeatCycle"/> seconds.
-        /// </summary>
-        private void Update()
-        {
-            timer += Time.deltaTime;
-            if (timer >= RepeatCycle)
-            {
-                Synchronize();
-                timer = 0f;
-            }
         }
 
         /// <summary>
         /// This method is invoked periodically while this component is active.
         /// </summary>
-        private void Synchronize()
+        protected override void Synchronize()
         {
-            new ExpressionPlayerNetAction(expressionPlayer, networkObject.NetworkObjectId).Execute();
+            if (expressionPlayer != null)
+            {
+                new ExpressionPlayerNetAction(expressionPlayer, networkObject.NetworkObjectId).Execute();
+            }
         }
     }
 }
--- Assets/SEE/Game/Avatars/VRIKSynchronizer.cs
+++ Assets/SEE/Game/Avatars/VRIKSynchronizer.cs
@@ -1,64 +1,33 @@
 ﻿using RootMotion.FinalIK;
 using SEE.Net.Actions;
-using Unity.Netcode;
-using UnityEngine;
 
 namespace SEE.Game.Avatars
 {
     /// <summary>
     /// Responsible for synchronizing the VRIK model on all clients.
     /// </summary>
-    public class VRIKSynchronizer : MonoBehaviour
+    internal class VRIKSynchronizer : NetSynchronizer
     {
-        /// <summary>
-        /// Time in between two update cycles for the synchronization in seconds.
-        /// </summary>
-        private const float RepeatCycle = 0.05f;
-
-        /// <summary>
-        /// The network object.
-        /// </summary>
-        private NetworkObject networkObject;
-
         /// <summary>
         /// VRIK Component.
         /// </summary>
         private VRIK vrik;
 
         /// <summary>
-        /// Timer to count elapsed time.
-        /// </summary>
-        private float timer;
-
-        /// <summary>
         /// Initializes the network object and VRIK model. The periodic call of
         /// <see cref="Synchronize"/> is triggered.
         /// </summary>
-        private void Start()
+        protected override void Start()
         {
-            networkObject = gameObject.GetComponent<NetworkObject>();
+            base.Start();
             vrik = gameObject.GetComponent<VRIK>();
-            timer = 0f;
-        }
-
-        /// <summary>
-        /// Executes <see cref="Synchronize"/> every <see cref="RepeatCycle"/> seconds.
-        /// </summary>
-        private void Update()
-        {
-            timer += Time.deltaTime;
-            if (timer >= RepeatCycle)
-            {
-                Synchronize();
-                timer = 0f;
-            }
         }
 
         /// <summary>
         /// If a change should be sent, the update is sent to all clients with
         /// the <see cref="NetworkObjectId"/> and <see cref="vrik"/> as parameters.
         /// </summary>
-        private void Synchronize()
+        protected override void Synchronize()
         {
             new VRIKNetAction(networkObject.NetworkObjectId, vrik).Execute();
         }
