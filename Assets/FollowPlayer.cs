1255745fe2641734f73efbc961903785909b9c03
Li-ID <linhen@uni-bremen.de>
2023-03-02 16:10:19 +0100

    Work in Progress, kleine Anpassung

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -15,13 +15,20 @@ public class FollowPlayer : MonoBehaviour
     Vector2 nextOffset;
     Vector2 nextScale;
     private float startTime;
+    float oldX;
+    float oldY;
+    float distance;
+    float distanceOffset;
+    float distanceScale;
+    float step = 0.0001F;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.localScale = new Vector3(0.64f,0.48f,-1);
+        transform.localScale = new Vector3(0.64f,-0.48f,-1);
         rend = GetComponent<Renderer>();
+        rend.material.mainTextureScale= new(1,-1);
     }
 
     // Update is called once per frame
@@ -83,32 +90,95 @@ public class FollowPlayer : MonoBehaviour
             rend.material.mainTextureScale = Vector2.SmoothDamp(currentScale, nextScale, ref velocityScale, smoothTime);
             */
 
-            float step = 0.0001F;
 
+            /* //Jumped
             Vector2 currentOffset = rend.material.mainTextureOffset;
             Vector2 currentScale = rend.material.mainTextureScale;
-            // hat sich das ziel geändert? Falls ja dann tue was
+            Vector2 nextOffset = new Vector2(rect.x / 640, rect.y / -480);
+            Vector2 nextScale = new Vector2(rect.width / 640, (rect.height / -480));
+            float smoothTime = 0.1F;
 
 
-            if (nextOffset != new Vector2(rect.x / 640, rect.y / -480)) {
+
+            if (nextOffset != new Vector2(rect.x / 640, rect.y / -480))
+            {
                 nextOffset = new Vector2(rect.x / 640, rect.y / -480);
-                currentFlowStateOffset = step; // step = quasi wie 0 nur ein schritt weiter, damit nicht unnötig gestoppt wird, falls das ziel immer wechselt und es sofort losgeht mit der transition
+                Vector2 velocityOffset = Vector2.zero;
+                
             }
             if (nextScale != new Vector2(rect.width / 640, (rect.height / -480)))
             {
                 nextScale = new Vector2(rect.width / 640, (rect.height / -480));
-                currentFlowStateScale = step; // step = quasi wie 0 nur ein schritt weiter, damit nicht unnötig gestoppt wird, falls das ziel immer wechselt und es sofort losgeht mit der transition
+                Vector2 velocityScale = Vector2.zero;
+            }
+
+
+            // Smoothly move the camera towards that target position
+            rend.material.mainTextureOffset = Vector2.SmoothDamp(currentOffset, nextOffset, ref velocityOffset, smoothTime);
+            rend.material.mainTextureScale = Vector2.SmoothDamp(currentScale, nextScale, ref velocityScale, smoothTime);
+
+            */
+
+            
+            //FUNKT! SMooth aber nicht perfekt, manchmal zu lahm außerdem an frames gekoppelt, nicht zeit
+
+            // distance aus width/height vopm quadrat und dann wie oft die entfernung
+           // Vector2 currentCorner = new(rect.x, rect.y);
+            //Vector2 lastCorner = new(oldX, oldY); // beim ersten mal wohl null, besser programmieren
+
+            //float distance = Vector2.Distance(currentCorner,lastCorner) / rect.width ; // / width ums in relation zum quadrat zu setzen, 0.5 ist dann ein halbes quadrat verschoben
+            //oldX = rect.x;
+            //oldY = rect.y;
+           // Debug.Log("Distance: " + distance);
+
+            //if distance ist klein dann reset auf 0.0001f
+            //falls/solange distance groß ist, erhöhe den step, entsprechend der Entfernung pro update stückweise bis ein maximalwert oder keinen
+
+            Vector2 currentOffset = rend.material.mainTextureOffset;
+            Vector2 currentScale = rend.material.mainTextureScale;
+           
+
+            distanceOffset = Vector2.Distance(currentOffset, nextOffset);
+            distanceScale = Vector2.Distance(currentScale, nextScale);
+            //Debug.Log("Distance Offset: " + distanceOffset);
+            //Debug.Log("Distance Scale: " + distanceScale);
+            if (distanceOffset >= 0.05 || distanceScale >= 0.025)
+            {
+                //step = 0.001F;
+                step = step + 0.0001F;
+            }
+            else { step = 0.0001F; }
+            Debug.Log("step: " + step);
+            /*if (distanceScale >= 0.05)
+            {
+                //step = 0.001F;
+                step = step + 0.0001F;
+            }
+            else { step = 0.0001F; }*/
+
+            // hat sich das ziel geändert? Falls ja dann tue was
+            if (nextOffset != new Vector2(rect.x / 640, rect.y / -480)) {
+                nextOffset = new Vector2(rect.x / 640, rect.y / -480);
+                currentFlowStateOffset = 0; // step = quasi wie 0 nur ein schritt weiter, damit nicht unnötig gestoppt wird, falls das ziel immer wechselt und es sofort losgeht mit der transition
+            }
+            if (nextScale != new Vector2(rect.width / 640, (rect.height / -480)))
+            {
+                nextScale = new Vector2(rect.width / 640, (rect.height / -480));
+                currentFlowStateScale =  0; // step = quasi wie 0 nur ein schritt weiter, damit nicht unnötig gestoppt wird, falls das ziel immer wechselt und es sofort losgeht mit der transition
             }
             //nextOffset = new Vector2(rect.x / 640, rect.y / -480);
             //nextScale = new Vector2(rect.width / 640, (rect.height / -480));
 
-            Debug.Log("currentFlowStateOffset: " + currentFlowStateOffset);
-            Debug.Log("currentFlowStateScale: " + currentFlowStateScale);
+            //Debug.Log("currentFlowStateOffset: " + currentFlowStateOffset);
+           // Debug.Log("currentFlowStateScale: " + currentFlowStateScale);
             rend.material.mainTextureOffset = Vector2.Lerp(currentOffset, nextOffset, currentFlowStateOffset);
             rend.material.mainTextureScale = Vector2.Lerp(currentScale, nextScale, currentFlowStateScale);
+            // wie sieht das aus? geht ja nur wenn sich ziel nicht ändert
+            //step = step + step;
             currentFlowStateOffset = currentFlowStateOffset + step;
             currentFlowStateScale = currentFlowStateScale + step;
-            
+
+
 
 
 
