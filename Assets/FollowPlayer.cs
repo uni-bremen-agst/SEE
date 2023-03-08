176dea8f8dc0feeb2d6bc1e68805aa7f041f13f7
Li-ID <linhen@uni-bremen.de>
2023-03-08 19:34:15 +0100

    in arbeit: kommentare geändert

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -23,10 +23,14 @@ public class FollowPlayer : MonoBehaviour
     float step = 0.0001F;
 
 
-    // Start is called before the first frame update
+    // Start is called once,
+    // before the first frame (and the frame update cycle)
     void Start()
     {
+        // Screen der Webcam z -1 damit weg vom player y- einfachere berchenung da ...
         transform.localScale = new Vector3(0.64f,-0.48f,-1);
+
+        // gebraucht für ???? zoom oder so??? 
         rend = GetComponent<Renderer>();
         rend.material.mainTextureScale= new(1,-1);
     }
@@ -34,19 +38,22 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player == null)
+        if (player == null) //finde den spieler falls noch nicht gefunden // geht in 'start' oder zu früh?
         {
             //Debug.Log("Player searching, player: " + player);
             player = GameObject.Find("/Local Player 1/DesktopPlayer");
         }
-        else {
+        else { // falls gefunden, klemme deine Position an den Spieler..
             //Debug.Log("Player found, player:     " + player);
             transform.position = player.transform.position;
             transform.rotation = player.transform.rotation;
         }
 
         //transform.localScale += new Vector3(0.01f, 0.01f, 0);
+
+        // Keine Ahnung? ggf. das RECT was ums Gesicht gezeichnet wird
         rect = GetComponent<FrameOptimizationExample>().testRect;
+
         //Debug.Log("Rectangle: " + rect);
         //Debug.Log("Scale: " + transform.localScale);
  
@@ -65,6 +72,8 @@ public class FollowPlayer : MonoBehaviour
 
         // Quad Größe
         //von maximal 640 auf maximal 0.64 und von max 480 auf max 0.48. also von quadrat 640x420 auf QuadFläche 0.64x0.42
+       
+        // zur sicherheit, falls das Rect da ist, weil es(height) ist 0 wenn es noch nicht da ist
         if (rect.height != 0) {
             //transform.localScale = new Vector3(rect.width / 1000f, rect.height / -1000f, -1);
 
@@ -72,6 +81,7 @@ public class FollowPlayer : MonoBehaviour
             //Debug.Log("Quad Rescale");
 
             // Die breite der Anzeige ist festgelegt die Höhe nicht, eventuell immer das kleinere nehmen, aber momentan sowieso quadrat
+            // wieso sowieso quadrat
             float ratio = rect.width / rect.height;
             transform.localScale = new Vector3(0.64f, -0.64f / ratio, -1);
 
@@ -134,20 +144,24 @@ public class FollowPlayer : MonoBehaviour
             //if distance ist klein dann reset auf 0.0001f
             //falls/solange distance groß ist, erhöhe den step, entsprechend der Entfernung pro update stückweise bis ein maximalwert oder keinen
 
+            // aktuelles Tile and Ofsset vom Material was die Camera (webcam) auf dem Screen darstellt.
             Vector2 currentOffset = rend.material.mainTextureOffset;
             Vector2 currentScale = rend.material.mainTextureScale;
            
-
+            // Der Abstand zum nächsten Tile/Ofsset vom nächsten Frame(der nichtmal berechnet wurde?)
             distanceOffset = Vector2.Distance(currentOffset, nextOffset);
             distanceScale = Vector2.Distance(currentScale, nextScale);
+
+
             //Debug.Log("Distance Offset: " + distanceOffset);
             //Debug.Log("Distance Scale: " + distanceScale);
-            if (distanceOffset >= 0.05 || distanceScale >= 0.025)
+            //Falls ca. (z.B ein Quadrat entfernt) oder (z.B ca. Hälfter der Größe Weggezoomt)  
+            if (distanceOffset >= 0.05 || distanceScale >= 0.025) // Werte gerschätzt ggf. nicht sinvoll, und weiter Berechnung notwendig
             {
                 //step = 0.001F;
-                step = step + 0.0001F;
+                step = step + 0.0001F; // Geschwindigeit der Bewegung zum Gesicht wenn es woanders(weit genug weg) erkannt wird // geschätzt. // Geschwindigkeit wird pro frame erhöht
             }
-            else { step = 0.0001F; }
+            else { step = 0.0001F; } // Falls das Gesicht nur 1 Pixel oder minimal woanders (ggf. sehr oft pro sekunde) woanders erkannt wird, bewege dich dort hin, eig. ganricht, da dies nur erkennsufehler sind.. so wie irgendwo im raum.. aber sehr langsam bewegen ist okay (ist es nicht, links/rechts 1pxl bewegegeung von weneiger als 1 (0.5) oder sogar großér werden je nach implementierung ggf. nicht so gut aussehen (wie stillstand), besser ist dann sitllstand) leichte bewegung aber gut, falls nur ein kleines stück, so dass nicht extra bewegt werden muss, sieht gut aus, auch ohgne bewegung, aber falls dadurch nicht 100% oder ändlich zentriert, ist eine langsame anpassung um zu zentrieren cool und nice (optisch wünschenswert) bsp. falls jemand sein kopf 1 cm woanders hin bewegegt, und so nicht mehr mittig ist, wägre ggf. durch "keine anspassung und sltillstand"der  kopf immer minimal nichbt mittig!.. DANN könnte der Kopf ganz langsam, kaum merkbar, mittig gesetzt werden, um dies "perfekt" zu korrigieren, und keine "1cm/1mm" Mini ruckler zu erlauben oder im gegensatz "1cm/1mm ungenauigkteit", nicht zentriertheit" zu erlauben.  Müsste so insgesammt wesentlich smoother und cleaner wirken und wäre "prefekter und sinvoller" da keine Distraction gefordert wird. (direkte kommunikation ohne ablenkung in see erwünscht).L337
             Debug.Log("step: " + step);
             /*if (distanceScale >= 0.05)
             {
@@ -156,14 +170,16 @@ public class FollowPlayer : MonoBehaviour
             }
             else { step = 0.0001F; }*/
 
-            // hat sich das ziel geändert? Falls ja dann tue was
+            // hat sich das ziel geändert? Falls ja dann tue was // aktuelles quadrat wird nächsten quadrat vergleicht??
             if (nextOffset != new Vector2(rect.x / 640, rect.y / -480)) {
                 nextOffset = new Vector2(rect.x / 640, rect.y / -480);
+                //scale wird geupdatet
                 currentFlowStateOffset = 0; // step = quasi wie 0 nur ein schritt weiter, damit nicht unnötig gestoppt wird, falls das ziel immer wechselt und es sofort losgeht mit der transition
             }
             if (nextScale != new Vector2(rect.width / 640, (rect.height / -480)))
             {
                 nextScale = new Vector2(rect.width / 640, (rect.height / -480));
+                //offset wird geupdatet
                 currentFlowStateScale =  0; // step = quasi wie 0 nur ein schritt weiter, damit nicht unnötig gestoppt wird, falls das ziel immer wechselt und es sofort losgeht mit der transition
             }
             //nextOffset = new Vector2(rect.x / 640, rect.y / -480);
