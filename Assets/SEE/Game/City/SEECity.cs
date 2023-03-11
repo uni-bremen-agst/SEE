d5d54071740bb578b387c1cdb7938812c1d83806
Rainer Koschke <koschke@uni-bremen.de>
2023-03-11 18:11:13 +0100

    Added a button to the Inspector that allows to load a node layout in the game. The edges are not laid out, however.

--- Assets/SEE/Game/City/SEECity.cs
+++ Assets/SEE/Game/City/SEECity.cs
@@ -5,6 +5,8 @@ using Cysharp.Threading.Tasks;
 using SEE.DataModel.DG;
 using SEE.DataModel.DG.IO;
 using SEE.GO;
+using SEE.Layout;
+using SEE.Layout.NodeLayouts;
 using SEE.Utils;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -422,6 +424,21 @@ namespace SEE.Game.City
             {
                 Layout.IO.SLDWriter.Save(path, AllNodeDescendants(gameObject));
             }
+        }
+
+        /// <summary>
+        /// Reads the a saved layout of the city from a file named <see cref="LayoutPath"/>.
+        /// The format of the written file depends upon the file extension. If the extension
+        /// is <see cref="Filenames.GVLExtension"/> it is expected to be in the GVL format; otherwise
+        /// the file is assumed to be in the SLD format.
+        /// </summary>
+        [Button(ButtonSizes.Small)]
+        [ButtonGroup(DataButtonsGroup)]
+        [PropertyOrder(DataButtonsGroupOrderLoadLayout)]
+        public void LoadLayout()
+        {
+            ICollection<GameObject> gameNodes = AllNodeDescendants(gameObject);
+            graphRenderer.LoadLayout(gameNodes, gameObject.transform.position.y);
         }
 
         /// <summary>
