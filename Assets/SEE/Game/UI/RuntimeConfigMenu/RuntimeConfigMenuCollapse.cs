7ce15a8bd0e2f54b2ba51eb191630d06316ebb55
Mahmoud Siai <mahmoud.siai@web.de>
2023-03-15 18:38:42 +0100

    #503 Added functionality to hide and show the contents of a setting object

---
+++ Assets/SEE/Game/UI/RuntimeConfigMenu/RuntimeConfigMenuCollapse.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeConfigMenuCollapse : MonoBehaviour
+{
+    /// <summary>
+    /// Are the settings collapsed
+    /// </summary>
+    private bool settingVisibility = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// All settings of the SettingObject are shown/hidden
+    /// </summary>
+    /// <see cref="settingVisibility"/>
+    public void OnClickCollapse()
+    {
+        settingVisibility = !settingVisibility;
+        this.transform.parent.parent.Find("Content").gameObject.SetActive(settingVisibility);
+    }
+    
+    
+}
