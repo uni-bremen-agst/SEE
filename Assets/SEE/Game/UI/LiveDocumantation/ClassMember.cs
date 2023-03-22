1b4fb642f18bdc0f47ed069bfb300c50d5e43817
Hannes Kuss <hkuss@uni-bremen.de>
2023-03-22 14:12:36 +0100

    Added ClassMember prefab for displaying class members.

--- Assets/SEE/Game/UI/LiveDocumantation/ClassMember.cs
+++ Assets/SEE/Game/UI/LiveDocumantation/ClassMember.cs
@@ -1,7 +1,38 @@
+using System;
+using SEE.Utils;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace SEE.Game.UI.LiveDocumantation
 {
-    public class ClassMember
+    public class ClassMember : MonoBehaviour
     {
+        private const string PREFAB_NAME = "Prefabs/UI/LiveDocumentation/ClassMember";
+
+        public string Text { get; set; }
         
+        public void Start()
+        {
+            //       gameObject.AddComponent<CanvasRenderer>();
+
+            //       Image image = gameObject.AddComponent<Image>();
+            //      image.sprite = Sprite.Create();
+            //     var imageColor = image.color;
+            //    imageColor.a = 0.05F;
+            //   image.color = imageColor;
+
+            GameObject classMember =
+                PrefabInstantiator.InstantiatePrefab("Prefabs/UI/LiveDocumentation/ClassMember", transform, false);
+
+            TextMeshProUGUI tm = classMember.transform.Find("MemberText").gameObject.GetComponent<TextMeshProUGUI>();
+            tm.text = Text;
+
+            RectTransform rt = classMember.GetComponent<RectTransform>();
+            rt.anchorMin = new Vector2(0, 0);
+            rt.anchorMax = new Vector2(1, 1);
+            rt.pivot = new Vector2(0.5f, 0.5f);
+            
+        }
     }
 }
