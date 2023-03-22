303e0055df944e330641baf29a5026a196aed84c
Yvo <yvo@uni-bremen.de>
2023-03-22 15:23:06 +0100

    #544 Created VrMoveAction to handle Grab Events

---
+++ Assets/SEE/Controls/Actions/VrActions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Autohand;
+using SEE.GO;
+using UnityEngine;
+
+namespace SEE.Controls.Actions
+{
+    public class VrActions : MonoBehaviour
+    {
+        public void Start()
+        {
+            GrabbableExtraEvents grabbableExtraEvents = gameObject.AddOrGetComponent<GrabbableExtraEvents>();
+
+            gameObject.AddComponent<GrabbableExtraEvents>();
+            grabbableExtraEvents.OnFirstGrab = new UnityHandGrabEvent();
+            grabbableExtraEvents.OnLastRelease = new UnityHandGrabEvent();
+
+            grabbableExtraEvents.OnFirstGrab.AddListener(OnFirstGrab);
+            grabbableExtraEvents.OnLastRelease.AddListener(OnLastRelease);
+        }
+
+        private void OnFirstGrab(Hand hand, Grabbable grabbable)
+        {
+            Debug.LogWarning("first grab");
+           // VrMoveAction.CreateReversibleAction();
+        }
+
+        private void OnLastRelease(Hand hand, Grabbable grabbable)
+        {
+            Debug.LogWarning("last release");
+        }
+
+    }
+}
