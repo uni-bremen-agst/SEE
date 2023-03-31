b50dea8f568270f9dad67ca0b5e09bb839ca38f1
Rainer Koschke <koschke@uni-bremen.de>
2023-03-31 15:14:47 +0200

    Imported Runtime Transform Gizmos version 1.3.3. This plugin requires a commercial license.

---
+++ Assets/Runtime Transform Gizmos/Scripts/Gizmos/Gizmo Behaviour/IGizmoBehaviour.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa54b0e45d9aec1c3dbbf5e287d165bd52bcd29f4cde5322146a7457fce45b45
+size 1066
---
+++ Assets/Runtime Transform Gizmos/Scripts/Gizmos/Sliders/2D Plane Slider/IGizmoPlaneSlider2DController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b9b095f050b831068425a393e9a0aaa3840f16de9bcba0629824fac48041b0af
+size 275
