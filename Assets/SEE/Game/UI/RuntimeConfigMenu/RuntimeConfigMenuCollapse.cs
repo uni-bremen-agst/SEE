b6a05d0616986f30ce78ae260b042f6e6b645624
Mahmoud Siai <mahmoud.siai@web.de>
2023-04-07 15:19:39 +0200

    #503 added rotation to collapse icon and changed icon color

--- Assets/SEE/Game/UI/RuntimeConfigMenu/RuntimeConfigMenuCollapse.cs
+++ Assets/SEE/Game/UI/RuntimeConfigMenu/RuntimeConfigMenuCollapse.cs
@@ -28,6 +28,10 @@ public class RuntimeConfigMenuCollapse : MonoBehaviour
     {
         settingVisibility = !settingVisibility;
         this.transform.parent.parent.Find("Content").gameObject.SetActive(settingVisibility);
+
+        // change rotation when pressed
+        if (settingVisibility) this.transform.Find("Icon").transform.Rotate(0,0,-90);
+        else this.transform.Find("Icon").transform.Rotate(0,0,90);
     }
     
     
