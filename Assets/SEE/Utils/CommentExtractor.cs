06bfcfa553465ad95eabe5e19489c6f232c49293
Hannes Kuss <hkuss@uni-bremen.de>
2023-04-14 11:41:05 +0200

    Fix CommentExtractor TokenStream

--- Assets/SEE/Utils/CommentExtractor.cs
+++ Assets/SEE/Utils/CommentExtractor.cs
@@ -1,17 +1,23 @@
 ﻿using System.IO;
 using Antlr4.Runtime;
 using Newtonsoft.Json;
+using SEE.Game.UI.LiveDocumantation;
 
 namespace SEE.Utils
 {
     public class CommentExtractor
     {
-        public void writeInBuffer()
+        public static void writeInBuffer(LiveDocumentationBuffer buffer, string fileName)
         {
             //TODO Here you have to integrate your code
-            var input = File.ReadAllText("MyCSharpFile.cs");
+            var input = File.ReadAllText(fileName);
             var lexer = new CSharpCommentsGrammarLexer(new AntlrInputStream(input));
             var tokens = new CommonTokenStream(lexer);
+            tokens.Fill();
+            foreach (var i in tokens.GetTokens())
+            {
+                
+            }
             var parser = new CSharpCommentsGrammarParser(tokens);
         }
     }
