44687932cba45bd711ebb2ecadddc405ce6cee0e
Pichu <linhen@uni-bremen.de>
2023-05-22 22:27:41 +0200

    Spawning the FaceCam Object as an real NetworkObject. (Bugfix)

--- Assets/SEE/Game/Avatars/AvatarAdapter.cs
+++ Assets/SEE/Game/Avatars/AvatarAdapter.cs
@@ -225,8 +225,8 @@ namespace SEE.Game.Avatars
             gameObject.AddComponent<DesktopPlayerMovement>();
 
             // Set up the FaceCam Object as child of the desktopPlayer.
-            GameObject faceCam = PrefabInstantiator.InstantiatePrefab("Prefabs/FaceCam/FaceCam", desktopPlayer.transform);
-            //faceCam.GetComponent<NetworkObject>().Spawn();
+            GameObject faceCam = PrefabInstantiator.InstantiatePrefab("Prefabs/FaceCam/FaceCam");
+            faceCam.GetComponent<NetworkObject>().Spawn();
         }
 
         #region ENABLE_VR
