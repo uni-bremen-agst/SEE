7b3af5ce7f7ec27626c766f49ca882b9236b6efb
Pichu <linhen@uni-bremen.de>
2023-05-22 11:44:47 +0200

    The FaceCam prefab became an proper NetworkObject and can render a Quad.

--- Assets/SEE/Game/Avatars/AvatarAdapter.cs
+++ Assets/SEE/Game/Avatars/AvatarAdapter.cs
@@ -234,6 +234,7 @@ namespace SEE.Game.Avatars
             // Set up the FaceCam Object as child of the desktopPlayer.
             GameObject faceCam = PrefabInstantiator.InstantiatePrefab("Prefabs/FaceCam/FaceCam");
             faceCam.transform.SetParent(desktopPlayer.transform);
+            faceCam.GetComponent<NetworkObject>().Spawn();
         }
 
         #region ENABLE_VR
