ae8a859f2883b4a4d50f9aab1234d4403fb979f2
Rainer Koschke <koschke@uni-bremen.de>
2023-06-07 12:18:29 +0200

    RotateAction is now implemented as a subclass of NodeManipulationAction.

--- Assets/SEE/Controls/Actions/RotateAction.cs
+++ Assets/SEE/Controls/Actions/RotateAction.cs
@@ -1,19 +1,52 @@
-using System.Collections.Generic;
-using SEE.Game.Operator;
-using SEE.GO;
+using RTG;
 using SEE.Net.Actions;
 using SEE.Utils;
 using UnityEngine;
-using SEE.Audio;
-using RTG;
 
 namespace SEE.Controls.Actions
 {
     /// <summary>
     /// An action to rotate nodes.
     /// </summary>
-    internal class RotateAction : AbstractPlayerAction
+    internal class RotateAction : NodeManipulationAction<Quaternion>
     {
+        #region Constructors
+
+        /// <summary>
+        /// This constructor will be used when this kind of action is to be
+        /// continued with a new instance where <paramref name="gameNodeToBeContinuedWith"/>
+        /// was selected already.
+        /// </summary>
+        /// <param name="gameNodeToBeContinuedWith">the game node selected already</param>
+        private RotateAction(GameObject gameNodeToBeContinuedWith) : base()
+        {
+            Initialize();
+            StartAction(gameNodeToBeContinuedWith);
+        }
+
+        /// <summary>
+        /// This constructor will be used if no game node was selected in a previous
+        /// instance of this type of action.
+        /// </summary>
+        private RotateAction() : base()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Initializes the newly created instance by setting <see cref="currentState"/>
+        /// and <see cref="gizmo"/>.
+        /// </summary>
+        private void Initialize()
+        {
+            currentState = ReversibleAction.Progress.NoEffect;
+            gizmo = new RotateGizmo();
+        }
+
+        #endregion Constructors
+
+        #region ReversibleAction Overrides
+
         /// <summary>
         /// Returns a new instance of <see cref="RotateAction"/>.
         /// </summary>
@@ -41,168 +74,88 @@ namespace SEE.Controls.Actions
             return ActionStateTypes.Rotate;
         }
 
-        /// <summary>
-        /// The gameObject that is currently selected and should be rotated.
-        /// Will be null if no object has been selected yet.
-        /// </summary>
-        private GameObject objectToRotate;
+        #endregion ReversibleAction Overrides
+
+        #region Memento
 
         /// <summary>
-        /// The <see cref="NodeOperator"/> of <see cref="objectToRotate"/>.
+        /// A memento of the rotation of <see cref="gameNodeSelected"/> before or after,
+        /// respectively, it was rotated.
         /// </summary>
-        private NodeOperator nodeOperator;
-
-        /// <summary>
-        /// The rotation of <see cref="objectToRotate"/> before this action actually rotated it,
-        /// i.e., its orginial rotation. This value is needed for <see cref="Undo"/>.
-        /// </summary>
-        private Quaternion originalRotation;
-
-        /// <summary>
-        /// The rotation of <see cref="objectToRotate"/> after this action rotated it,
-        /// i.e., its new rotation. This value is needed for <see cref="Redo"/>.
-        /// </summary>
-        private Quaternion newRotation;
-
-        /// <summary>
-        /// Reverts the position and rotation of <paramref name="gameObject"/> to
-        /// <see cref="Position"/> and <see cref="Rotation"/>.
-        /// </summary>
-        /// <param name="gameObject">object whose position and rotation are to be restored</param>
-        public void Rotate(Quaternion Rotation)
+        private class RotateMemento : Memento<Quaternion>
         {
-            nodeOperator.RotateTo(Rotation, 0);
-            // TODO: We do not need lists. Only one object can be rotated.
-            new RotateNodeNetAction(new List<GameObject>() { objectToRotate }).Execute();
-        }
-
-        /// <summary>
-        /// Undoes this <see cref="RotateAction"/>.
-        /// </summary>
-        public override void Undo()
-        {
-            base.Undo();
-            Rotate(originalRotation);
-        }
-
-        /// <summary>
-        /// Redoes this <see cref="RotateAction"/>.
-        /// </summary>
-        public override void Redo()
-        {
-            base.Redo();
-            Rotate(newRotation);
-        }
-
-        /// <summary>
-        /// Disables the gizmo.
-        /// </summary>
-        public override void Stop()
-        {
-            base.Stop();
-            DisableGizmo();
-        }
-
-        /// <summary>
-        /// Gizmo used for rotating the objects.
-        /// </summary>
-        private ObjectTransformGizmo objectRotationGizmo = RTGizmosEngine.Get.CreateObjectRotationGizmo();
-
-        /// <summary>
-        /// The gizmo currently hovered, null unless a gizmo is hovered (updated externally).
-        /// </summary>
-        private Gizmo hoveredGizmo;
-
-        /// <summary
-        /// See <see cref="ReversibleAction.Update"/>.
-        ///
-        /// Note: The action is finalized only if the user selects anything except the
-        /// <see cref="objectToRotate"/> or any of the rotation gizmos.
-        /// </summary>
-        /// <returns>true if completed</returns>
-        public override bool Update()
-        {
-            hoveredGizmo = RTGizmosEngine.Get.HoveredGizmo;
-            if (hoveredGizmo != null && hoveredGizmo.IsHovered)
+            /// <summary>
+            /// Constructor taking a snapshot of the rotation of <paramref name="gameObject"/>.
+            /// </summary>
+            /// <param name="gameObject">object whose rotation is to be captured</param>
+            public RotateMemento(GameObject gameObject) : base(gameObject)
             {
-                // Rotation in progress
-                return false;
+                InitialState = gameObject.transform.rotation;
             }
 
-            if (SEEInput.Select())
+            /// <summary>
+            /// Should never be used.
+            /// </summary>
+            private RotateMemento() : base(null)
             {
-                if (Raycasting.RaycastGraphElement(out RaycastHit raycastHit, out GraphElementRef _) != HitGraphElement.Node)
-                {
-                    // Object outside the graph was selected, should be ignored.
-                    SaveRotationChanges();
-                    DisableGizmo();
-                    return true;
-                }
-                else if (objectToRotate != raycastHit.collider.gameObject)
-                {
-                    // Selected a different object - save changes and change object assigned to gizmo.
-                    objectToRotate = raycastHit.collider.gameObject;
-                    SaveRotationChanges();
-                    nodeOperator = objectToRotate.AddOrGetComponent<NodeOperator>();
-                    originalRotation = objectToRotate.transform.rotation;
-                    objectRotationGizmo.SetTargetObject(objectToRotate);
-                    objectRotationGizmo.SetEnabled(true);
-                    AudioManagerImpl.EnqueueSoundEffect(IAudioManager.SoundEffect.PICKUP_SOUND, objectToRotate);
-                }
+                throw new System.NotImplementedException();
             }
-            else if (SEEInput.Drag() || SEEInput.ToggleMenu() || SEEInput.Cancel())
+
+            protected override void Transform(Quaternion rotation)
             {
-                SaveRotationChanges();
-                DisableGizmo();
-                return true;
+                // FIXME: If a duration > 0 is used, the node operator does not rotate the object.
+                nodeOperator.RotateTo(rotation, 0);
             }
-            return false;
-        }
 
-        /// <summary>
-        /// Creates a memento after the rotation is done and propagates over network.
-        /// </summary>
-        private void SaveRotationChanges()
-        {
-            newRotation = objectToRotate.transform.rotation;
-        }
-
-        /// <summary>
-        /// Empty game object used for hiding gizmos since they cant be deleted after instantiation.
-        /// </summary>
-        private GameObject gizmoHidingObject;
-
-        /// <summary>
-        /// Disables the rotation gizmo.
-        /// </summary>
-        private void DisableGizmo()
-        {
-            if (gizmoHidingObject == null)
+            /// <summary>
+            /// Broadcasts the <paramref name="rotation"/> to call clients.
+            /// </summary>
+            /// <param name="rotation">rotation to be broadcast</param>
+            protected override void BroadcastState(Quaternion rotation)
             {
-                GameObject searchObject = GameObject.Find("Gizmo Hiding Object");
-                if (searchObject == null)
-                {
-                    gizmoHidingObject = new GameObject("Gizmo Hiding Object");
-                }
-                else
-                {
-                    gizmoHidingObject = searchObject;
-                }
+                new RotateNetAction(nodeOperator.name, rotation).Execute();
             }
-            objectRotationGizmo.SetTargetObject(gizmoHidingObject);
-            objectRotationGizmo.SetEnabled(false);
+        }
+
+        #endregion Memento
+
+        #region NodeManipulationAction Overrides
+
+        protected override void FinalizeAction()
+        {
+            base.FinalizeAction();
+            memento.Finalize(gameNodeSelected.transform.rotation);
         }
 
         /// <summary>
-        /// Returns all IDs of gameObjects manipulated by this action.
+        /// Yields true if the object to be manipulated has had a change.
+        /// Precondition: the object to be manipulated is not null.
         /// </summary>
-        /// <returns>all IDs of gameObjects manipulated by this action</returns>
-        public override HashSet<string> GetChangedObjects()
+        /// <returns>true if the object to be manipulated has had a change</returns>
+        protected override bool HasChanges()
         {
-            return objectToRotate == null ? new HashSet<string>() : new HashSet<string>()
-            {
-                objectToRotate.name
-            };
+            return gameNodeSelected.transform.rotation != memento.InitialState;
         }
+
+        protected override Memento<Quaternion> CreateMemento(GameObject gameNode)
+        {
+            return new RotateMemento(gameNode);
+        }
+
+        #endregion NodeManipulationAction Overrides
+
+        #region Gizmo
+
+        /// <summary>
+        /// Manages the gizmo to manipulate the selected game node.
+        /// </summary>
+        private class RotateGizmo : Gizmo
+        {
+            public RotateGizmo()
+            {
+                objectTransformationGizmo = RTGizmosEngine.Get.CreateObjectRotationGizmo();
+            }
+        }
+        #endregion
     }
 }
