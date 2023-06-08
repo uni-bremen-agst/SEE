75cc4dd9c43b011d992c39a57e44f643ad0b97ee
Hannes Kuss <hkuss@uni-bremen.de>
2023-06-08 16:15:48 +0200

    Added new example

---
+++ Assets/StreamingAssets/mini_calculator2/src/Minus.cs
@@ -0,0 +1,17 @@
+namespace mini.Calculator.Operators
+{
+    /// <summary>
+    /// Same as in <see cref="Plus"/> just with subtraction.
+    /// </summary>
+    public class Minus : Operator
+    {
+        public override double Execute()
+        {
+            return Left + Right;
+        }
+
+        public Minus(double left, double right) : base(left, right)
+        {
+        }
+    }
+}
