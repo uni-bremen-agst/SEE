22a08fa81261f5ad846255ca80ee3669ad2efb91
Rainer Koschke <koschke@uni-bremen.de>
2023-06-11 19:39:40 +0200

    Removed warning output (missused for just debugging output).

--- Assets/SEETests/TestCRDT.cs
+++ Assets/SEETests/TestCRDT.cs
@@ -1,12 +1,10 @@
 using NUnit.Framework;
 using SEE.Utils;
 using UnityEditor;
-using UnityEngine;
 using static SEE.Utils.CRDT;
 
 namespace SEETests
 {
-
     /// <summary>
     /// Tests functions of CRDT that do not require a working network connection.
     /// </summary>
@@ -15,7 +13,7 @@ namespace SEETests
         [Test]
         public void TestPositionToString()
         {
-            CRDT crdt = new CRDT(new GUID().ToString(), "test");
+            CRDT crdt = new(new GUID().ToString(), "test");
             Identifier[] pos = { new Identifier(1, "1"), new Identifier(2, "1"), new Identifier(3, "1") };
             Assert.AreEqual("(1, 1), (2, 1), (3, 1)", crdt.PositionToString(pos));
         }
@@ -23,10 +21,8 @@ namespace SEETests
         [Test]
         public void TestStringToPosition()
         {
-            CRDT crdt = new CRDT(new GUID().ToString(), "test");
+            CRDT crdt = new(new GUID().ToString(), "test");
             Identifier[] pos = {new Identifier(1, "1"), new Identifier(2, "1"), new Identifier(3, "1") };
-            Debug.LogWarning(pos[0] + " to string "  + crdt.StringToPosition("(1, 1), (2, 1), (3, 1)")[0]);
-
             Assert.AreEqual(0, crdt.ComparePosition(pos, crdt.StringToPosition("(1, 1), (2, 1), (3, 1)")));
         }
     }
