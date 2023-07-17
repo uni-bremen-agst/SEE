b8959dd400ede223e32127fe994eeadcc534e31c
Li-ID <linhen@uni-bremen.de>
2023-07-17 18:03:05 +0200

    Moved non prefab files from Assets/Resources/Prefabs/FaceCam to Assets/Plugins/FaceCam.

---
+++ Assets/Plugins/FaceCam/FaceCam.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:749af4ac841f52e930374fa7ecd4eb5f287fe610406e08bd2e9216c36fc60233
+size 35634
---
+++ Assets/Plugins/FaceCam/OpenCVForUnityUtils.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bf43367d525cf530e942db8562113f8c492c03b14f67be2f451f6406a44fb439
+size 28831
