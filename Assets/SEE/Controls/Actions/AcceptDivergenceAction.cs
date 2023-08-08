4373f92bb5bfd8334d6b539337d340b206b60004
m4xxed <m4xxed@pm.me>
2023-08-09 00:23:55 +0200

    complete overhaul of the docstrings and inline documentation

--- Assets/SEE/Controls/Actions/AcceptDivergenceAction.cs
+++ Assets/SEE/Controls/Actions/AcceptDivergenceAction.cs
@@ -12,9 +12,9 @@ using SEE.Audio;
 namespace SEE.Controls.Actions
 {
     /// <summary>
-    /// Action to solve a Divergence between Implementation and
-    /// Architecture by adding the Edge to the Architecture that
-    /// solves the Divergence.
+    /// Action to solve a divergence (see <see cref="ReflexionAnalysis.State"/>) between
+    /// implementation and architecture by adding the exact edge to the architecture that solves
+    /// this divergence.
     /// </summary>
     internal class AcceptDivergenceAction : AbstractPlayerAction
     {
@@ -27,8 +27,10 @@ namespace SEE.Controls.Actions
             return new AcceptDivergenceAction();
         }
 
+
         /// <summary>
-        /// Returns a new instance of <see cref="AcceptDivergenceAction"/>.
+        /// Returns a new instance of <see cref="AcceptDivergenceAction"/> as a <see
+        /// cref="ReversibleAction"/>.
         /// </summary>
         /// <returns>new instance</returns>
         public override ReversibleAction NewInstance()
@@ -36,20 +38,16 @@ namespace SEE.Controls.Actions
             return CreateReversibleAction();
         }
 
+
         /// <summary>
-        /// The edge that was hit by the user to be accepted
-        /// into the ArchitectureGraph. Set in <see cref="Update"/>.
+        /// The graph that the edge which was hit by the user to be accepted into the graph is
+        /// in. Set in <see cref="Update"/>.
         /// </summary>
         private ReflexionGraph graph;
 
-        /// <summary>
-        /// Contains all Edges that were explicitly added to
-        /// solve Divergences.
-        ///</summary>
-        private ISet<GameObject> syncedGameObjects;
 
         /// <summary>
-        /// The information we need to (re-)create an edge.
+        /// The information required to (re-)create the edge that solves the divergence.
         /// </summary>
         private struct Memento
         {
@@ -62,8 +60,10 @@ namespace SEE.Controls.Actions
             /// </summary>
             public Node to;
             /// <summary>
-            /// The type of the edge.
+            /// Construct a new memento.
             /// </summary>
+            /// <param name="source">the source node of the edge in the architecture graph</param>
+            /// <param name="target">the target node of the edge in the architecture grpah</param>
             public Memento(Node source, Node target)
             {
                 this.from = source;
@@ -71,29 +71,32 @@ namespace SEE.Controls.Actions
             }
         }
 
+
         /// <summary>
-        /// The information needed to re-create the synced edge after
-        /// undo.
+        /// The information required to (re-)create the edge that solves the divergence.
         /// </summary>
         private Memento memento;
 
+
         /// <summary>
-        /// The edge created by this action. Can be null if no edge
-        /// has been created yet or whether an Undo was called. The
-        /// created edge is stored only to delete it again if Undo is
-        /// called. All information to create the edge is kept in
-        /// <see cref="memento"/>.
+        /// The edge created by this action. Can be null if no edge has been created yet or whether
+        /// an Undo was called. The created edge is stored only to delete it again if Undo is
+        /// called. All information to create the edge is kept in <see cref="memento"/>.
         /// </summary>
         private Edge createdEdgeDataModel;
 
+
         /// <summary>
-        /// TODO
+        /// The edge created by this action's GameObject representation. Can be null if no edge has
+        /// been created yet or whether an Undo was called. The created edge is stored only to
+        /// delete it again if Undo is called. All information to create the edge is kept in <see
+        /// cref="memento"/>.
         /// </summary>
         private GameObject createdEdgeGameObject;
 
+
         /// <summary>
-        /// Registers itself at <see cref="InteractableObject"/> to
-        /// listen for hovering events.
+        /// Registers itself at <see cref="InteractableObject"/> to listen for hovering events.
         /// </summary>
         public override void Start()
         {
@@ -101,9 +104,9 @@ namespace SEE.Controls.Actions
             InteractableObject.LocalAnyHoverOut += LocalAnyHoverOut;
         }
 
+
         /// <summary>
-        /// Unregisters itself from <see
-        /// cref="InteractableObject"/>. Does no longer listen for
+        /// Unregisters itself from <see cref="InteractableObject"/>. Does no longer listen for
         /// hovering events.
         /// </summary>
         public override void Stop()
@@ -112,10 +115,12 @@ namespace SEE.Controls.Actions
             InteractableObject.LocalAnyHoverOut -= LocalAnyHoverOut;
         }
 
+
         /// <summary>
         /// The default type of an added edge.
         /// </summary>
         private const string DefaultEdgeType = "Source_Dependency";
+
 
         /// <summary>
         /// See <see cref="ReversibleAction.Update"/>.
@@ -123,6 +128,7 @@ namespace SEE.Controls.Actions
         /// <returns>true if completed</returns>
         public override bool Update()
         {
+            // indicates whether the divergence has been solved ("true" means "solved")
             bool result = false;
 
             // FIXME: Needs adaptation for VR where no mouse is available.
@@ -131,50 +137,50 @@ namespace SEE.Controls.Actions
                     out RaycastHit raycastHit,
                     out GraphElementRef _) != HitGraphElement.None)
             {
-                // Find the edge representing the divergence that should be solved.
+                // find the edge representing the divergence that should be solved.
                 GameObject selectedDivergenceEdge = raycastHit.collider.gameObject;
 
-                // Check whether the object selected is an edge.
+                // check whether the object selected is an edge.
                 if (selectedDivergenceEdge.TryGetEdge(out Edge selectedEdge))
                 {
-                    // Check if the selected edge represents a divergence
+                    // check if the selected edge represents a divergence
                     if (ReflexionGraph.IsDivergent(selectedEdge))
                     {
 
                         // acquire the containing ReflexionGraph
                         graph = (ReflexionGraph)selectedEdge.ItsGraph;
 
-                        // Find that node in the ArchitectureGraph,
+                        // find that node in the architecture graph,
                         // which the divergence's source node is
-                        // explicitly or implicitly mapped to.
+                        // explicitly or implicitly mapped to
                         Node source = graph.MapsTo(selectedEdge.Source);
 
-                        // Find that node in the ArchitectureGraph,
+                        // find that node in the ArchitectureGraph,
                         // which the divergence's target is explicitly
-                        // or implicitly mapped to.
+                        // or implicitly mapped to
                         Node target = graph.MapsTo(selectedEdge.Target);
 
-                        // We have both source and target of the edge
-                        // and use a memento struct to remember which
-                        // edge we have added.
+                        // we have both source and target of the edge and use a memento struct
+                        // to remember which edge we have added
                         memento = new Memento(source, target);
 
-                        // The Edge in the DataModel and it's
-                        // GameObject are created separately
+                        // the Edge in the DataModel and it's GameObject are created separately
                         var createdEdgeParts = CreateEdge(memento);
                         createdEdgeDataModel = createdEdgeParts.edgeDataModel;
                         createdEdgeGameObject = createdEdgeParts.edgeGameObject;
 
-                        // Check whether edge creation was successfull
+                        // check whether edge creation was successfull
                         result = (createdEdgeDataModel != null && createdEdgeGameObject != null);
 
-                        // Add audio cue to the appearance of the new architecture edge
+                        // add audio cue to the appearance of the new architecture edge
                         AudioManagerImpl.EnqueueSoundEffect(IAudioManager.SoundEffect.NEW_EDGE_SOUND);
 
-                        // Update the curent state
+                        // update the current state depending on whether the divergence has been solved
+                        // (required in order to register as an undo-able action)
                         currentState = result ? ReversibleAction.Progress.Completed : ReversibleAction.Progress.NoEffect;
 
-                        return true; // the selected object is synced and this action is done
+                        // the selected object is synced and this action is done
+                        return true;
                     }
                 }
                 else
@@ -192,19 +198,15 @@ namespace SEE.Controls.Actions
         {
             base.Undo();
 
-            // remove the synced edge (info is saved in memento)
+            // remove the synced edge (its info is saved in memento)
             var graph = (ReflexionGraph)createdEdgeDataModel.ItsGraph;
 
             if (graph != null)
             {
-                // Remove the edge's GameObject locally and on the network
+                // remove the edge's GameObject and graph representation locally and on the network
                 GameEdgeAdder.Remove(createdEdgeGameObject);
                 new DeleteNetAction(createdEdgeGameObject.name).Execute();
                 Destroyer.Destroy(createdEdgeGameObject);
-
-                // Remove the edge from the data model locally and on the network
-                // graph.RemoveFromArchitecture(createdEdgeDataModel);
-                // new DeleteNetAction(createdEdgeGameObject.name).Execute();
             }
             else
             {
@@ -222,29 +224,31 @@ namespace SEE.Controls.Actions
         public override void Redo()
         {
             base.Redo();
+            // recreate the edge
             var createdEdgeParts = CreateEdge(memento);
+            // save the references for later use
             createdEdgeDataModel = createdEdgeParts.edgeDataModel;
             createdEdgeGameObject = createdEdgeParts.edgeGameObject;
         }
 
         /// <summary>
-        /// Creates a new edge using the given <paramref name="memento"/>.
-        /// In case of any error, null will be returned.
+        /// Creates a new edge using the given <paramref name="memento"/>.  In case of any error,
+        /// null will be returned.
         /// </summary>
         /// <param name="memento">information needed to create the edge</param>
-        /// <returns>a new edge or null</returns>
+        /// <returns>the new edge's GameObject and a reference to itself, or both null</returns>
         private (Edge edgeDataModel, GameObject edgeGameObject) CreateEdge(Memento memento)
         {
-            // Add the new Edge to the ArhcitectureGraph
+            // add the new edge to the architecture graph
             var edgeDataModel = graph.ActuallyAddToArchitecture(
                 memento.from,
                 memento.to,
                 "Source_Dependency");
 
-            // Add the new Edge to the Game
+            // add the new edge to the (game)
             GameObject edgeGameObject = GameEdgeAdder.Draw(edgeDataModel);
 
-            // Sync the Solution of the Divergence via Network
+            // sync the solution of the divergence via network
             new AcceptDivergenceNetAction(memento.from.SourceName, memento.to.SourceName).Execute();
 
             return (edgeDataModel, edgeGameObject);
@@ -260,9 +264,9 @@ namespace SEE.Controls.Actions
         }
 
         /// <summary>
-        /// Returns all IDs of gameObjects manipulated by this action.
+        /// Returns all IDs of GameObjects manipulated by this action.
         /// </summary>
-        /// <returns>all IDs of gameObjects manipulated by this action</returns>
+        /// <returns>all IDs of GameObjects manipulated by this action</returns>
         public override HashSet<string> GetChangedObjects()
         {
             if (createdEdgeDataModel == null | createdEdgeGameObject == null)
