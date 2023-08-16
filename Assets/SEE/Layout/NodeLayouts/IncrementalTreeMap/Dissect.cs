63ec0e2e4e0ca70aaf1f26df343138ab4f24d534
jschramm <jschramm@uni-bremen.de>
2023-08-16 15:29:35 +0200

    563 doc and format Dissect.cs

--- Assets/SEE/Layout/NodeLayouts/IncrementalTreeMap/Dissect.cs
+++ Assets/SEE/Layout/NodeLayouts/IncrementalTreeMap/Dissect.cs
@@ -2,111 +2,138 @@ using System.Linq;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+
 namespace SEE.Layout.NodeLayouts.IncrementalTreeMap
 {
-    static class Dissect{
+    /// <summary>
+    /// Provides an algorithm to create a new layout.
+    /// </summary>
+    static class Dissect
+    {
+        /// <summary>
+        /// Calculates new Layout for <paramref name="nodes"/>
+        /// Assigns rectangles and segments to each node in <paramref name="nodes"/>.
+        /// </summary>
+        /// <param name="nodes">nodes to be laid out</param>
+        /// <param name="rectangle">rectangle in that the nodes should be placed</param>
         public static void Apply(IEnumerable<Node> nodes, Rectangle rectangle)
         {
             Node[] nodesArray = nodes.ToArray();
-            Array.Sort(nodesArray,(x,y) => (x.Size.CompareTo(y.Size)));
+            Array.Sort(nodesArray, (x, y) => (x.Size.CompareTo(y.Size)));
 
-            if(Math.Abs(nodesArray.Sum(x => x.Size) - rectangle.Area()) >= rectangle.Area() * Math.Pow(10, -3)
+            if (Math.Abs(nodesArray.Sum(x => x.Size) - rectangle.Area()) >= rectangle.Area() * Math.Pow(10, -3)
                 && nodesArray.Length > 1)
             {
                 Debug.LogWarning("Dissect: nodes doesnt fit in rectangle");
             }
 
             Apply(rectangle,
-                    nodesArray,
-                    leftBound : new Segment(true, true),
-                    rightBound: new Segment(true, true), 
-                    upperBound: new Segment(true, false),
-                    lowerBound: new Segment(true, false));
+                nodesArray,
+                leftBound: new Segment(true, true),
+                rightBound: new Segment(true, true),
+                upperBound: new Segment(true, false),
+                lowerBound: new Segment(true, false));
         }
 
-        private static void Apply( Rectangle rectangle, 
-                                Node[] nodes,
-                                Segment leftBound,
-                                Segment rightBound,
-                                Segment upperBound, 
-                                Segment lowerBound)
+        /// <summary>
+        /// Calculates the layout by slices the <paramref name="rectangle"/> recursively.
+        /// </summary>
+        /// <param name="rectangle">the rectangle</param>
+        /// <param name="nodes">nodes to be placed in rectangle</param>
+        /// <param name="leftBound">the <see cref="Direction.Left"/> segment of the rectangle</param>
+        /// <param name="rightBound">the <see cref="Direction.Right"/> segment of the rectangle</param>
+        /// <param name="upperBound">the <see cref="Direction.Upper"/> segment of the rectangle</param>
+        /// <param name="lowerBound">the <see cref="Direction.Lower"/> segment of the rectangle</param>
+        private static void Apply(Rectangle rectangle,
+            Node[] nodes,
+            Segment leftBound,
+            Segment rightBound,
+            Segment upperBound,
+            Segment lowerBound)
         {
-            if(nodes.Length == 1)
+            if (nodes.Length == 1)
             {
                 Node node = nodes[0];
                 node.Rectangle = rectangle;
                 node.RegisterSegment(leftBound, Direction.Left);
-                node.RegisterSegment(rightBound,Direction.Right);
-                node.RegisterSegment(lowerBound,Direction.Lower);
-                node.RegisterSegment(upperBound,Direction.Upper);
+                node.RegisterSegment(rightBound, Direction.Right);
+                node.RegisterSegment(lowerBound, Direction.Lower);
+                node.RegisterSegment(upperBound, Direction.Upper);
             }
             else
             {
                 int splitIndex = GetSplitIndex(nodes);
                 Node[] nodes1 = nodes[..splitIndex];
                 Node[] nodes2 = nodes[splitIndex..];
-                
+
                 float ratio = nodes1.Sum(x => x.Size) / nodes.Sum(x => x.Size);
 
                 Rectangle rectangle1 = rectangle.Clone();
                 Rectangle rectangle2 = rectangle.Clone();
-                if(rectangle.Width >= rectangle.Depth)
+                if (rectangle.Width >= rectangle.Depth)
                 {
                     rectangle1.Width *= ratio;
                     rectangle2.Width *= (1 - ratio);
                     rectangle2.X = rectangle1.X + rectangle1.Width;
-                    Segment newSegment = new Segment(false,true);
+                    Segment newSegment = new Segment(false, true);
 
                     Dissect.Apply(rectangle1, nodes1,
-                                    leftBound : leftBound,
-                                    rightBound: newSegment,
-                                    upperBound: upperBound,
-                                    lowerBound: lowerBound);
+                        leftBound: leftBound,
+                        rightBound: newSegment,
+                        upperBound: upperBound,
+                        lowerBound: lowerBound);
 
                     Dissect.Apply(rectangle2, nodes2,
-                                    leftBound :  newSegment,
-                                    rightBound: rightBound,
-                                    upperBound: upperBound,
-                                    lowerBound: lowerBound);
+                        leftBound: newSegment,
+                        rightBound: rightBound,
+                        upperBound: upperBound,
+                        lowerBound: lowerBound);
                 }
                 else
                 {
                     rectangle1.Depth *= ratio;
                     rectangle2.Depth *= (1 - ratio);
                     rectangle2.Z = rectangle1.Z + rectangle1.Depth;
-                    Segment newSegment = new Segment(false,false);
+                    Segment newSegment = new Segment(false, false);
 
                     Dissect.Apply(rectangle1, nodes1,
-                                    leftBound : leftBound,
-                                    rightBound: rightBound,
-                                    upperBound: newSegment,
-                                    lowerBound: lowerBound);
+                        leftBound: leftBound,
+                        rightBound: rightBound,
+                        upperBound: newSegment,
+                        lowerBound: lowerBound);
 
                     Dissect.Apply(rectangle2, nodes2,
-                                    leftBound :  leftBound,
-                                    rightBound: rightBound,
-                                    upperBound: upperBound,
-                                    lowerBound: newSegment);
+                        leftBound: leftBound,
+                        rightBound: rightBound,
+                        upperBound: upperBound,
+                        lowerBound: newSegment);
                 }
             }
         }
 
+        /// <summary>
+        /// Calculates the index, that separates the <paramref name="nodes"/> array into two
+        /// partitions
+        /// </summary>
+        /// <param name="nodes"> sorted list</param>
+        /// <returns>index</returns>
         private static int GetSplitIndex(Node[] nodes)
         {
-            if(nodes.Sum( x => x.Size)  <=  nodes.Last().Size * 3)
+            if (nodes.Sum(x => x.Size) <= nodes.Last().Size * 3)
             {
-                return nodes.Length -1;
+                return nodes.Length - 1;
             }
             else
             {
-                for (int i = 1; i <= nodes.Length+1; i++)
+                for (int i = 1; i <= nodes.Length + 1; i++)
                 {
-                    if( nodes[..i].Sum(x => x.Size) * 3 >= nodes.Sum(x => x.Size))
+                    if (nodes[..i].Sum(x => x.Size) * 3 >= nodes.Sum(x => x.Size))
                     {
                         return i;
                     }
                 }
             }
+
             throw new ArgumentException("We should never arrive here.\n");
         }
     }
