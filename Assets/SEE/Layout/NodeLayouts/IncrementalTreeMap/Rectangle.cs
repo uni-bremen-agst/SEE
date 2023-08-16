13eb37fa820622d1fa61397f1754d8d01d47e6bb
jschramm <jschramm@uni-bremen.de>
2023-08-16 14:11:53 +0200

    563 - doc and formatting Rectangle.cs

--- Assets/SEE/Layout/NodeLayouts/IncrementalTreeMap/Rectangle.cs
+++ Assets/SEE/Layout/NodeLayouts/IncrementalTreeMap/Rectangle.cs
@@ -1,10 +1,17 @@
 namespace SEE.Layout.NodeLayouts.IncrementalTreeMap
 {
     /// <summary>
-    /// 
+    /// A class that represents a rectangle as part of a layout
     /// </summary>
     public class Rectangle
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="x"><see cref="X"/></param>
+        /// <param name="z"><see cref="Z"/></param>
+        /// <param name="width"><see cref="Width"/></param>
+        /// <param name="depth"><see cref="Depth"/></param>
         public Rectangle(double x, double z, double width, double depth)
         {
             X = x;
@@ -13,23 +20,51 @@ namespace SEE.Layout.NodeLayouts.IncrementalTreeMap
             Depth = depth;
         }
 
-        public double X;      // x co-ordinate at corner 
-        public double Z;      // z co-ordinate at corner
-        public double Width;  // width
-        public double Depth;  // depth
+        /// <summary>
+        /// The position of the left side
+        /// </summary>
+        public double X;
 
+        /// <summary>
+        /// the position of the lower side 
+        /// </summary>
+        public double Z;
+
+        /// <summary>
+        /// The lenght of x-axis
+        /// </summary>
+        public double Width;
+
+        /// <summary>
+        /// The lenght of z-axis
+        /// </summary>
+        public double Depth; // depth
+
+        /// <summary>
+        /// The ratio of the longer side to the smaller one.
+        /// </summary>
+        /// <returns>val >= 1</returns>
         public double AspectRatio()
         {
             return Width >= Depth ? Width / Depth : Depth / Width;
         }
 
+        /// <summary>
+        /// the area of the rectangle 
+        /// </summary>
+        /// <returns><see cref="Width"/>*<see cref="Depth"/></returns>
         public double Area()
         {
-            return Width*Depth;
+            return Width * Depth;
         }
+
+        /// <summary>
+        /// Creates a new identical Rectangle
+        /// </summary>
+        /// <returns>the clone</returns>
         public Rectangle Clone()
         {
-            return new Rectangle(x:X,z:Z,width:Width,depth:Depth);
+            return new Rectangle(x: X, z: Z, width: Width, depth: Depth);
         }
     }
 }
