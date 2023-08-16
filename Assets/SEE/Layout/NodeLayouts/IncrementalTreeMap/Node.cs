5d12494456a26d9e89da897909e1f9923ef792fc
jschramm <jschramm@uni-bremen.de>
2023-08-16 14:29:54 +0200

    563 doc and formatting in Nodes.cs

--- Assets/SEE/Layout/NodeLayouts/IncrementalTreeMap/Node.cs
+++ Assets/SEE/Layout/NodeLayouts/IncrementalTreeMap/Node.cs
@@ -1,82 +1,135 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System;
+using static SEE.Layout.NodeLayouts.IncrementalTreeMap.Direction;
 
 namespace SEE.Layout.NodeLayouts.IncrementalTreeMap
 {
+    
+    /// <summary>
+    /// A core element in the Layout, each entity that should be laid out
+    /// has a corresponding node.
+    /// </summary>
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class Node
     {
-        public Node(string ID)
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="id">the id</param>
+        public Node(string id)
         {
-            this.ID = ID;
+            ID = id;
         }
 
+        /// <summary>
+        /// The <see cref="Rectangle"/> represent the position and size of the node in the layout.
+        /// </summary>
         public Rectangle Rectangle { get; set; }
 
+        /// <summary>
+        /// The size that the node SHOULD occupy, may differ from the actual size/area of its rectangle. 
+        /// </summary>
         public float Size { get; set; }
 
+        /// <summary>
+        /// the ID
+        /// </summary>
         public string ID { get; }
 
-        private Segment leftBoundingSegment;
-        private Segment rightBoundingSegment;
-        private Segment upperBoundingSegment;
-        private Segment lowerBoundingSegment;
+        /// <summary>
+        /// the adjacent segment on the <see cref="Direction.Left"/> side of the node
+        /// </summary>
+        private Segment _leftBoundingSegment;
+        
+        /// <summary>
+        ///the adjacent segment on the <see cref="Direction.Right"/> side of the node
+        /// </summary>
+        private Segment _rightBoundingSegment;
+        
+        /// <summary>
+        ///the adjacent segment on the <see cref="Direction.Upper"/> side of the node
+        /// </summary>
+        private Segment _upperBoundingSegment;
+        
+        /// <summary>
+        /// the adjacent segment on the <see cref="Direction.Lower"/> side of the node
+        /// </summary>
+        private Segment _lowerBoundingSegment;
 
+        /// <summary>
+        /// Register the node to a new segment, deregister from the old one on this side.
+        /// The node knows about new segment and the segment knows about new node.
+        /// </summary>
+        /// <param name="segment">the new adjacent segment</param>
+        /// <param name="dir">the side of the new adjacent segment</param>
         public void RegisterSegment(Segment segment, Direction dir)
         {
-            this.DeregisterSegment(dir);    
-            if( dir == Direction.Left)
+            DeregisterSegment(dir);    
+            switch (dir)
             {
-                this.leftBoundingSegment = segment;
-                segment.Side2Nodes.Add(this);
-            }
-            if( dir == Direction.Right)
-            {
-                this.rightBoundingSegment = segment;
-                segment.Side1Nodes.Add(this);
-            }
-            if( dir == Direction.Lower)
-            {
-                this.lowerBoundingSegment= segment;
-                segment.Side2Nodes.Add(this);
-            }
-            if( dir == Direction.Upper)
-            {
-                this.upperBoundingSegment = segment;
-                segment.Side1Nodes.Add(this);
+                case Left:
+                    _leftBoundingSegment = segment;
+                    segment.Side2Nodes.Add(this);
+                    break;
+                case Right:
+                    _rightBoundingSegment = segment;
+                    segment.Side1Nodes.Add(this);
+                    break;
+                case Lower:
+                    _lowerBoundingSegment= segment;
+                    segment.Side2Nodes.Add(this);
+                    break;
+                case Upper:
+                    _upperBoundingSegment = segment;
+                    segment.Side1Nodes.Add(this);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(dir), dir, "We should never arrive here");
             }
         }
 
+        /// <summary>
+        /// Remove node from a current adjacent segment
+        /// </summary>
+        /// <param name="dir">The side of the adjacent segment</param>
         public void DeregisterSegment(Direction dir)
         {
-            if( dir == Direction.Left)
+            switch (dir)
             {
-                this.leftBoundingSegment?.Side2Nodes.Remove(this);
-            } 
-            if( dir == Direction.Right)
-            {
-                this.rightBoundingSegment?.Side1Nodes.Remove(this);
-            }
-            if( dir == Direction.Lower)
-            {
-                this.lowerBoundingSegment?.Side2Nodes.Remove(this);
-            }
-            if( dir == Direction.Upper)
-            {
-                this.upperBoundingSegment?.Side1Nodes.Remove(this);
+                case Left:
+                    _leftBoundingSegment?.Side2Nodes.Remove(this);
+                    break;
+                case Right:
+                    _rightBoundingSegment?.Side1Nodes.Remove(this);
+                    break;
+                case Lower:
+                    _lowerBoundingSegment?.Side2Nodes.Remove(this);
+                    break;
+                case Upper:
+                    _upperBoundingSegment?.Side1Nodes.Remove(this);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(dir), dir, "We should never arrive here");
             }
         }
 
+        /// <summary>
+        /// Get all adjacent segments
+        /// </summary>
+        /// <returns>dictionary direction -> segment in this direction</returns>
         public IDictionary<Direction,Segment> SegmentsDictionary()
         {
             return new Dictionary<Direction,Segment>{
-                 {Direction.Left,  this.leftBoundingSegment},
-                 {Direction.Right, this.rightBoundingSegment},
-                 {Direction.Lower, this.lowerBoundingSegment},
-                 {Direction.Upper, this.upperBoundingSegment}};
+                 {Left,  _leftBoundingSegment},
+                 {Right, _rightBoundingSegment},
+                 {Lower, _lowerBoundingSegment},
+                 {Upper, _upperBoundingSegment}};
         }
         
+        /// <summary>
+        /// Method for easy overview in debugger
+        /// </summary>
         private string DebuggerDisplay =>
             string.Format(
                 "{0,15} x=[{1,-6}, {2,-6}] y=[{3,-6}, {4,-6}]",
