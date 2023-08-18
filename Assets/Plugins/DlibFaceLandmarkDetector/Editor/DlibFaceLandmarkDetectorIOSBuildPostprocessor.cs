b6aad0f807847fcc4f4b639bd01ca56fc3fc8f3a
Rainer Koschke <koschke@uni-bremen.de>
2023-08-18 11:39:15 +0200

    Updated Dlib FaceLandmark Detector to version 1.3.4.

--- Assets/Plugins/DlibFaceLandmarkDetector/Editor/DlibFaceLandmarkDetectorIOSBuildPostprocessor.cs
+++ Assets/Plugins/DlibFaceLandmarkDetector/Editor/DlibFaceLandmarkDetectorIOSBuildPostprocessor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f06debb6df07c3d5db2f392bee224ccf9659e62bc27e3cdf7c5b01ecbdcb3a1a
-size 2643
+oid sha256:c697673a522080dd1318211d778b99276974e49d68d0b3dada5ae51f939a4a91
+size 4317
--- Assets/Plugins/DlibFaceLandmarkDetector/Scripts/Utils.cs
+++ Assets/Plugins/DlibFaceLandmarkDetector/Scripts/Utils.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae53c5d881b5cc2893c770d39e8b3f47764392075f5611b4a662429ce9117488
+oid sha256:55cfee72f65c390b216e128f193e28e794ed5e4bac35e35e5b9a62ed9505a41d
 size 32845
