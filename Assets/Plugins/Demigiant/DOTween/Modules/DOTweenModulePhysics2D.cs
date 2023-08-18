4b4c57d125024f469cdbd45059844fd351abbbe8
Rainer Koschke <koschke@uni-bremen.de>
2023-08-18 12:37:04 +0200

    Updated DOTween Pro to version 1.0.375.

--- Assets/Plugins/Demigiant/DOTween/Modules/DOTweenModulePhysics2D.cs
+++ Assets/Plugins/Demigiant/DOTween/Modules/DOTweenModulePhysics2D.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6718774604c247549c30e1a52b93b96b249c36e78324d55109c8e4d9cc420f55
-size 12215
+oid sha256:d82bd5ceae8233f9808a2588e9fe0a956e4ba6c7931ddb85c7fe1e1dd4deb105
+size 12125
--- Assets/Plugins/Demigiant/DOTween/Modules/DOTweenModuleUI.cs
+++ Assets/Plugins/Demigiant/DOTween/Modules/DOTweenModuleUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee574facba751d2317860767a35d46fa7d5045eca39947eeb93157069e140b5f
-size 43296
+oid sha256:e6d6e9b2ae6010add7a3809419441abe762e41bdb0081006352b74e5a35ade91
+size 43245
