53d47f880ebd4f38e1e013983205ed0f8a39fa95
Rainer Koschke <koschke@uni-bremen.de>
2023-08-19 08:50:49 +0200

    Automated updates by Sirenx after upgrade.

--- Assets/Plugins/Sirenix/Odin Inspector/Modules/Unity.Mathematics/MathematicsDrawers.cs
+++ Assets/Plugins/Sirenix/Odin Inspector/Modules/Unity.Mathematics/MathematicsDrawers.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7daf164a852354f3ffeabb677fd2f9a7847d3bef82310e4324054353212c37fd
-size 43506
+oid sha256:e681ae183bcd54ffa75ef736287f7a9c7b37864ff152dd874e6f7b855be437ed
+size 44388
