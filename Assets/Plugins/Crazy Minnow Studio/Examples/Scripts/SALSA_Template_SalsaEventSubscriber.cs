d22ad99a55b00b7035e13b812584343b271a791a
Rainer Koschke <koschke@uni-bremen.de>
2023-08-21 15:26:34 +0200

    Updated SALSA LipSync Suite to version 2.5.5.

--- Assets/Plugins/Crazy Minnow Studio/Examples/Scripts/SALSA_Template_SalsaEventSubscriber.cs
+++ Assets/Plugins/Crazy Minnow Studio/Examples/Scripts/SALSA_Template_SalsaEventSubscriber.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a540bafb96f11266f834448a2f749a6a6d6b854713d678ca0cac0ac07c8a1394
-size 1005
+oid sha256:bc949cf26c14073df3288c77b3947e527902878caa3f385ae4703b113ddb6ed8
+size 1151
