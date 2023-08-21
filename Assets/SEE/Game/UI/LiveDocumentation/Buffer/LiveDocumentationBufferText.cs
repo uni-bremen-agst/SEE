7065f2f7c441d68c87023c9cbf292db35cdd51a9
Hannes Kuss <hkuss@uni-bremen.de>
2023-08-21 11:27:12 +0200

    Added missing documentation to LiveDocumentationBufferText

--- Assets/SEE/Game/UI/LiveDocumentation/Buffer/LiveDocumentationBufferText.cs
+++ Assets/SEE/Game/UI/LiveDocumentation/Buffer/LiveDocumentationBufferText.cs
@@ -1,21 +1,30 @@
 namespace SEE.Game.UI.LiveDocumentation.Buffer
 {
     /// <summary>
-    /// 
+    /// Represents some text which is part of the documentation displayed in the LiveDocumentation window.
     /// </summary>
     public class LiveDocumentationBufferText : ILiveDocumentationBufferItem
     {
-        public string Text { get; set; }
+        /// <summary>
+        /// The actual text which is should be displayed.
+        ///
+        /// This field is read only since altering after initialization isn't needed right now.
+        /// </summary>
+        public string Text { get; }
 
+        /// <summary>
+        /// Returns the text which should be displayed
+        /// </summary>
+        /// <returns>Returns just <see cref="Text"/> </returns>
         public string GetPrintableText()
         {
             return Text;
         }
 
-        public LiveDocumentationBufferText()
-        {
-        }
-
+        /// <summary>
+        /// Constructs a new instance of a <see cref="LiveDocumentationBufferText"/>
+        /// </summary>
+        /// <param name="text">The text which should be displayed in the LiveDocumentation window</param>
         public LiveDocumentationBufferText(string text)
         {
             Text = text;
