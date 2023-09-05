20ac52e731b10408c5245174d33433f34df687df
unknown <krausem@uni-bremen.de>
2023-09-05 17:56:48 +0200

    last used color, left right click

---
+++ Assets/SEE/Game/UI/Drawable/ColorPresets.cs
@@ -0,0 +1,79 @@
+﻿using HSVPicker;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Assets.SEE.Game.UI.Drawable
+{
+    /// <summary>
+    /// File from the hsvpicker packages, add remove color
+    /// </summary>
+    public class ColorPresets : MonoBehaviour
+    {
+        public ColorPicker picker;
+        public GameObject[] presets;
+        public Image createPresetImage;
+
+        private ColorPresetList _colors;
+
+        void Awake()
+        {
+            picker.onValueChanged.AddListener(ColorChanged);
+        }
+
+        void Start()
+        {
+            _colors = ColorPresetManager.Get(picker.Setup.PresetColorsId);
+
+            if (_colors.Colors.Count < picker.Setup.DefaultPresetColors.Length)
+            {
+                _colors.UpdateList(picker.Setup.DefaultPresetColors);
+            }
+
+            _colors.OnColorsUpdated += OnColorsUpdate;
+            OnColorsUpdate(_colors.Colors);
+        }
+
+        private void OnColorsUpdate(List<Color> colors)
+        {
+            for (int cnt = 0; cnt < presets.Length; cnt++)
+            {
+                if (colors.Count <= cnt)
+                {
+                    presets[cnt].SetActive(false);
+                    continue;
+                }
+
+
+                presets[cnt].SetActive(true);
+                presets[cnt].GetComponent<Image>().color = colors[cnt];
+
+            }
+
+            createPresetImage.gameObject.SetActive(colors.Count < presets.Length);
+
+        }
+
+        public void CreatePresetButton()
+        {
+            _colors.AddColor(picker.CurrentColor);
+        }
+
+        public void PresetSelect(Image sender)
+        {
+            picker.CurrentColor = sender.color;
+        }
+
+        private void ColorChanged(Color color)
+        {
+            createPresetImage.color = color;
+        }
+
+        public void RemoveColor(Image sender)
+        {
+            _colors.RemoveColor(sender.color);
+        }
+    }
+}
---
+++ Assets/SEE/Game/UI/Drawable/GUIClickController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+/// <summary>
+/// found at: https://forum.unity.com/threads/can-the-ui-buttons-detect-a-right-mouse-click.279027/#post-3344931
+/// @braur
+/// </summary>
+public class GUIClickController : MonoBehaviour, IPointerClickHandler
+{
+    public UnityEvent onLeft;
+    public UnityEvent onRight;
+    public UnityEvent onMiddle;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            onLeft.Invoke();
+        }
+        else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            onRight.Invoke();
+        }
+        else if (eventData.button == PointerEventData.InputButton.Middle)
+        {
+            onMiddle.Invoke();
+        }
+    }
+}
