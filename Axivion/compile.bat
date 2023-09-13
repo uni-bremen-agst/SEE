35d2f600cf058fe2b4f2f1dd854ce778308b3087
Rainer Koschke <koschke@uni-bremen.de>
2023-09-13 11:04:17 +0200

    csharp2rfg is used instead of cafesharp. The latter runs out of stack and is deprecated.

--- Axivion/compile.bat
+++ Axivion/compile.bat
@@ -9,21 +9,26 @@ REM path to resulting RFG (relative to SEE root directory).
 SET "RFG=Data\GXL\SEE\SEE.rfg"
 
 REM The path to MSBuild.
-SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" 
+SET MSBUILD="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" 
+
+REM The Visual Studio .csproj files need to be created before we can start the build.
+"C:\Program Files\Unity\Hub\Editor\2022.3.9f1\Editor\Unity.exe" ^
+ -batchmode -nographics -logFile - ^
+ -executeMethod CITools.SolutionGenerator.Sync -projectPath . -quit
 
 REM Compile C# code into IR
-REM  -solution:SEE.sln ^
-build_csproj ^
- -msbuild:%MSBUILD% ^
- -p:langversion=latest ^
- -p:AdditionalOptions="/B%cd%" ^
- SEE.csproj
-
+REM build_csproj -msbuild:%MSBUILD% -p:langversion=latest -p:AdditionalOptions="/B%cd%" SEE.csproj
 REM Extract RFG from IR
-ir2rfg Temp\bin\Debug\SEE.dll.ir %RFG%
+REM ir2rfg Temp\bin\Debug\SEE.dll.ir %RFG%
 
+csharp2rfg --library --no_duplicate_edges ^
+ --configuration DEBUG ^
+ --msbuild_path "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" ^
+ -v --framework_path "C:\Program Files\dotnet\shared\Microsoft.AspNetCore.App\6.0.22" ^
+ SEE.csproj %RFG%
+ 
 REM Reduce the graph to all components in SEE and only its immediate neighbors.
-rfgscript Axivion\reduce.py --graph %RFG% --view "Code Facts" --namespace SEE %RFG%
+rfgscript Axivion\reduce.py --graph %RFG% --view "Code Facts" --namespace SEE %RFG%                                     
  
 REM RFG can be visualized as follows:
-REM gravis Data\GXL\SEE\SEE.rfg
+REM gravis %RFG%
