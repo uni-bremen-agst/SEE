87e79ac6aa04e6407b6300a760f135a7b1d2678f
Rainer Koschke <koschke@uni-bremen.de>
2023-10-02 09:41:24 +0200

    Updated Hightlight Plus to version 11.1.

--- Assets/Plugins/HighlightPlus/Editor/HighlightEffectEditor.cs
+++ Assets/Plugins/HighlightPlus/Editor/HighlightEffectEditor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2fe02976a46c63c3342139039cedc618e16152c69f7eab4280d867b700f3e18
-size 48939
+oid sha256:c61f3b4238a0de644bd68f93c0361aa7339f93bb5bd745efcc7c6b1d002c7c39
+size 49200
--- Assets/Plugins/HighlightPlus/README.txt
+++ Assets/Plugins/HighlightPlus/README.txt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df88d4c97479d5fdc01e2573220dda12d76fed27bd5304d51dadc2d5ae8d6d0b
-size 20207
+oid sha256:385d4062a073436b5eb33ee9e65ee5edd454a80f0970df50755bbd50e64528a9
+size 20338
