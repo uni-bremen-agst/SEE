7f1143ed35e13a516e58f076671cd03f643e248b
Rainer Koschke <koschke@uni-bremen.de>
2023-10-05 08:38:34 +0200

    #644 New class offering the local player.

---
+++ Assets/SEE/Game/LocalPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SEE.Game
+{
+    /// <summary>
+    /// Provides access to the game object representing the active local player,
+    /// that is, the player executing on this local instance of Unity.
+    /// </summary>
+    public static class LocalPlayer
+    {
+        /// <summary>
+        /// The game object representing the active local player, that is, the player
+        /// executing on this local instance of Unity.
+        /// </summary>
+        public static GameObject Instance;
+    }
+}
