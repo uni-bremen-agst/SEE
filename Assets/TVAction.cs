ed1b2fa027e5443ed2ed03d73b6ccc560ce3165b
Lysander <73167687+lilSander@users.noreply.github.com>
2023-11-02 18:31:02 +0100

    Fixed TVAction and the URL bar.

--- Assets/TVAction.cs
+++ Assets/TVAction.cs
@@ -5,13 +5,16 @@ public class DialogueCanvas : MonoBehaviour
     /// <summary>
     /// A Toggle to switch the Browser on and off with the key F3.
     /// </summary>
+    public GameObject Browser;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.F3))
         {
             //disables/enables the Browser
-            for (int i=0;i<=5;i++)
+            Browser.SetActive(!Browser.activeSelf);
+            //disables/enables the Buttons
+            for (int i=0;i<=4;i++)
             {
                 transform.GetChild(i).gameObject.SetActive(!transform.GetChild(i).gameObject.activeSelf);
             }
