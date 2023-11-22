8de9de50c53d3c72883c24157f1bef486202caf4
Falko Galperin <falko1@uni-bremen.de>
2023-11-22 15:51:29 +0100

    Implement static Icons class

---
+++ Assets/SEE/Utils/Icons.cs
@@ -0,0 +1,33 @@
+namespace SEE.Utils
+{
+    /// <summary>
+    /// Contains unicode characters for icons in the FontAwesome 6 free font.
+    ///
+    /// If you need to find out what a given icon looks like, search for the Unicode sequence
+    /// (e.g. "F1B2") on https://fontawesome.com/icons. The first result should be the icon you're looking for.
+    ///
+    /// See https://github.com/uni-bremen-agst/SEE/wiki/Icons for more information.
+    /// </summary>
+    public static class Icons
+    {
+        public const char Node = '\uF1B2';
+        public const char Edge = '\uF542';
+        public const char OutgoingEdge = '\uF2F5';
+        public const char IncomingEdge = '\uF2F6';
+        public const char LiftedIncomingEdge = '\uF090';
+        public const char LiftedOutgoingEdge = '\uF08B';
+        public const char EmptyCheckbox = '\uF0C8';
+        public const char CheckedCheckbox = '\uF14A';
+        public const char MinusCheckbox = '\uF146';
+        public const char DoubleCheckmark = '\uF560';
+        public const char Checkmark = '\uF00C';
+        public const char Trash = '\uF1F8';
+        public const char Info = '\uF05A';
+        public const char LightBulb = '\uF0EB';
+        public const char Code = '\uF121';
+        public const char TreeView = '\uF802';
+        public const char Compare = '\uE13A';
+        public const char Hide = '\uF070';
+        public const char Show = '\uF06E';
+    }
+}
