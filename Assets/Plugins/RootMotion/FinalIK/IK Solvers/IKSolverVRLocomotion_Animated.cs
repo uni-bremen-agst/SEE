064dc7073a5da9892fc5172c9c02be1594e69831
Rainer Koschke <koschke@uni-bremen.de>
2023-12-14 17:57:30 +0100

    Updated Final IK to version 2.3.

--- Assets/Plugins/RootMotion/FinalIK/IK Solvers/IKSolverVRLocomotion_Animated.cs
+++ Assets/Plugins/RootMotion/FinalIK/IK Solvers/IKSolverVRLocomotion_Animated.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62f56f132f7d254124faa32530af92af64bf14a696bf6511f4308f6d019c8b8b
-size 16339
+oid sha256:ecaa8a8b5bc8e4071b4d1fda2d1bcfa94344cabb4023c9c78a4d25db76c7df67
+size 16110
