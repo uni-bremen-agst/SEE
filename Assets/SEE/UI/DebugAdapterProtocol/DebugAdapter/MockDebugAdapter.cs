dc760cf77cdd0e3a8a340b28171e8d358737d95b
Ferdinand <ferdinand.rohlfing@googlemail.com>
2024-01-25 21:52:49 +0100

    #645 Added mock and netcore debug adapters

--- Assets/SEE/UI/DebugAdapterProtocol/DebugAdapter/MockDebugAdapter.cs
+++ Assets/SEE/UI/DebugAdapterProtocol/DebugAdapter/MockDebugAdapter.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.Shared.VSCodeDebugProtocol.Messages;
 using SEE.UI.PropertyDialog;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ namespace SEE.UI.DebugAdapterProtocol.DebugAdapter
 {
     public class MockDebugAdapter : DebugAdapter
     {
+
         public override string Name => "Mock";
 
         public override string AdapterWorkingDirectory { get; set; } = Path.Combine(AdapterDirectory, "vscode-mock-debug", "out");
@@ -17,9 +19,16 @@ namespace SEE.UI.DebugAdapterProtocol.DebugAdapter
         private string launchProgram = Path.Combine(AdapterDirectory, "vscode-mock-debug", "sampleWorkspace", "readme.md");
         private bool launchStopOnEntry = true;
         private bool launchTrace = true;
-        private string launchCompileError = "default"; // default, show, hide
+        private string launchCompileError = launchCompileErrorOptions[0];
 
         private StringProperty launchProgramProperty;
+        private SelectionProperty launchStopOnEntryProperty;
+        private SelectionProperty launchTraceProperty;
+        private SelectionProperty launchCompileErrorProperty;
+
+        private static readonly string[] launchStopOnEntryOptions = new string[] { "Stop on entry - Enabled", "Stop on entry - Disabled" };
+        private static readonly string[] launchTraceOptions = new string[] { "Logging - Enabled", "Logging - Disabled" };
+        private static readonly string[] launchCompileErrorOptions = new string[] { "Fake compile error - Show", "Fake compile error - Hide" };
 
         public override void SetupLaunchConfig(GameObject go, PropertyGroup group)
         {
@@ -28,18 +37,49 @@ namespace SEE.UI.DebugAdapterProtocol.DebugAdapter
             launchProgramProperty.Value = launchProgram;
             launchProgramProperty.Description = "Absolute path to a text file.";
             group.AddProperty(launchProgramProperty);
+
+            launchStopOnEntryProperty = go.AddComponent<SelectionProperty>();
+            launchStopOnEntryProperty.Name = "Stop on Entry";
+            launchStopOnEntryProperty.Description = "Automatically stop on entry.";
+            launchStopOnEntryProperty.AddOptions(launchStopOnEntryOptions);
+            launchStopOnEntryProperty.Value = launchStopOnEntry ? launchStopOnEntryOptions[0] : launchStopOnEntryOptions[1];
+            group.AddProperty(launchStopOnEntryProperty);
+
+            launchTraceProperty = go.AddComponent<SelectionProperty>();
+            launchTraceProperty.Name = "Trace";
+            launchTraceProperty.Description = "Enable logging of the Debug Adapter Protocol.";
+            launchTraceProperty.AddOptions(launchTraceOptions);
+            launchTraceProperty.Value = launchTrace ? launchTraceOptions[0] : launchTraceOptions[1];
+            group.AddProperty(launchTraceProperty);
+
+            launchCompileErrorProperty = go.AddComponent<SelectionProperty>();
+            launchCompileErrorProperty.Name = "Compile Error";
+            launchCompileErrorProperty.Description = "Simulates a compile error in 'launch' request.";
+            launchCompileErrorProperty.AddOptions(launchCompileErrorOptions);
+            launchCompileErrorProperty.Value = launchCompileError;
+            group.AddProperty(launchCompileErrorProperty);
+
         }
 
         public override void SaveLaunchConfig()
         {
             launchProgram = launchProgramProperty.Value;
+            launchStopOnEntry = launchStopOnEntryProperty.Value == launchStopOnEntryOptions[0];
+            launchTrace = launchTraceProperty.Value == launchTraceOptions[0];
+            launchCompileError = launchCompileErrorProperty.Value;
         }
 
         public override LaunchRequest GetLaunchRequest()
         {
             return new()
             {
-
+                ConfigurationProperties = new()
+                {
+                    {"program", launchProgram},
+                    {"stopOnEntry", launchStopOnEntry },
+                    {"trace", launchTrace },
+                    {"compileError", launchCompileError == launchCompileErrorOptions[0] ? "show" : "hide"},
+                }
             };
         }
     }
--- Assets/SEE/UI/DebugAdapterProtocol/DebugAdapter/NetCoreDebugAdapter.cs
+++ Assets/SEE/UI/DebugAdapterProtocol/DebugAdapter/NetCoreDebugAdapter.cs
@@ -4,6 +4,8 @@ using SEE.UI.PropertyDialog;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 
 namespace SEE.UI.DebugAdapterProtocol.DebugAdapter
@@ -16,11 +18,12 @@ namespace SEE.UI.DebugAdapterProtocol.DebugAdapter
         public override string AdapterFileName { get; set; } = "netcoredbg.exe";
         public override string AdapterArguments { get; set; } = "--interpreter=vscode";
 
+        // https://github.com/Samsung/netcoredbg/blob/27606c317017beb81bc1b81846cdc460a7a6aed3/test-suite/NetcoreDbgTest/VSCode/VSCodeProtocolRequest.cs#L44
         private string launchName = ".NET Core Launch";
         private string launchType = "coreclr";
         private string launchPreLaunchTask = "build";
         private string launchProgram = "program.dll";
-        private List<string> launchArgs = new();
+        private List<string> launchArgs = new() { "Hello", "World"};
         private string launchCwd = "";
         private Dictionary<string, string> launchEnv = new();
         private string launchConsole = "internalConsole";
@@ -31,40 +34,51 @@ namespace SEE.UI.DebugAdapterProtocol.DebugAdapter
         private string launchSessionId = Guid.NewGuid().ToString();
 
         private StringProperty launchProgramProperty;
+        private StringProperty launchArgsProperty;
         private StringProperty launchCwdProperty;
         private SelectionProperty launchStopAtEntryProperty;
+
+        private static readonly string[] launchStopAtEntryOptions = new string[] { "Stop at entry\nEnabled", "Stop at Entry\nDisabled" };
 
         public override void SetupLaunchConfig(GameObject go, PropertyGroup group)
         {
             launchProgramProperty = go.AddComponent<StringProperty>();
             launchProgramProperty.Name = "Program";
+            launchProgramProperty.Description = "The absolute path of a dll file.";
             launchProgramProperty.Value = launchProgram;
             group.AddProperty(launchProgramProperty);
 
+            launchArgsProperty = go.AddComponent<StringProperty>();
+            launchArgsProperty.Name = "Args";
+            launchArgsProperty.Description = "The program arguments.";
+            launchArgsProperty.Value = espaceList(launchArgs);
+            group.AddProperty(launchArgsProperty);
+
             launchCwdProperty = go.AddComponent<StringProperty>();
             launchCwdProperty.Name = "Cwd";
+            launchCwdProperty.Description = "The working directory.";
             launchCwdProperty.Value = launchCwd;
             group.AddProperty(launchCwdProperty);
 
             launchStopAtEntryProperty = go.AddComponent<SelectionProperty>();
             launchStopAtEntryProperty.Name = "Stop at Entry";
-            launchStopAtEntryProperty.AddOptions(new[]{ "Stop at entry", "Don't stop at entry"});
-            launchStopAtEntryProperty.Value = launchStopAtEntry ? "Stop at entry" : "Don't stop at entry";
+            launchStopAtEntryProperty.Description = "Automatically stops after launch.";
+            launchStopAtEntryProperty.AddOptions(launchStopAtEntryOptions);
+            launchStopAtEntryProperty.Value = launchStopAtEntry ? launchStopAtEntryOptions[0] : launchStopAtEntryOptions[1];
             group.AddProperty(launchStopAtEntryProperty);
-
         }
 
         public override void SaveLaunchConfig()
         {
             launchProgram = launchProgramProperty.Value;
+            launchArgs = parseList(launchArgsProperty.Value);
             launchCwd = launchCwdProperty.Value;
-            launchStopAtEntry = launchStopAtEntryProperty.Value == "true";
+            launchStopAtEntry = launchStopAtEntryProperty.Value == launchStopAtEntryOptions[0];
         }
 
         
         public override LaunchRequest GetLaunchRequest()
         {
-            // https://github.com/Samsung/netcoredbg/blob/27606c317017beb81bc1b81846cdc460a7a6aed3/test-suite/NetcoreDbgTest/VSCode/VSCodeProtocolRequest.cs#L44
             return new()
             {
                 ConfigurationProperties = new Dictionary<string, JToken>
@@ -84,6 +98,51 @@ namespace SEE.UI.DebugAdapterProtocol.DebugAdapter
                     { "__sessionId", launchSessionId },
                 }
             };
+        }
+
+        private static string espaceList(List<string> args)
+        {
+            if (args.Count == 0) return "";
+            return "\"" + String.Join("\", \"", 
+                args.Select(arg => arg.Replace("\\", "\\\\").Replace("\"", "\\\""))) + "\"";
+        }
+
+        private static List<string> parseList(string text)
+        {
+            if (text.Length == 0) return new();
+
+            List<string> arguments = new();
+            StringBuilder currentArgument = new();
+            bool inString = false;
+            for (int i=0; i<text.Length; i++)
+            {
+                char c = text[i];
+                // quotations marks mark the start and end of each argument
+                if (c == '"')
+                {
+                    if (!inString)
+                    {
+                        currentArgument = new();
+                    } else
+                    {
+                        arguments.Add(currentArgument.ToString());
+                    }
+                    inString = !inString;
+                } else if (inString)
+                {
+                    // backslash escapes the next character (allows quotation marks)
+                    if (c == '\\')
+                    {
+                        i += 1;
+                        c = text[i];
+                    }
+                    currentArgument.Append(c); 
+                } else
+                {
+                    // characters outside of quotations marks are ignored
+                }
+            }
+            return arguments;
         }
     }
 }
