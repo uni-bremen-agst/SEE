2022b1d93bbd774a1dc67e69a80e0d66424661d7
Rainer Koschke <koschke@uni-bremen.de>
2024-01-26 09:47:20 +0100

    #672 A source-location based node index and its tests. The index allows to search for graph nodes ased on a path and source line.
    Still work in progress.

---
+++ Assets/SEE/DataModel/DG/SourceRangeIndex.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SEE.DataModel.DG
+{
+    /// <summary>
+    /// A source-location based node index that allows to search for graph nodes
+    /// based on a path and source line.
+    /// </summary>
+    internal class SourceRangeIndex
+    {
+        /// <summary>
+        /// Creates the index for <paramref name="graph"/>.
+        /// </summary>
+        /// <param name="graph">graph whose nodes are to be indexed</param>
+        public SourceRangeIndex(Graph graph)
+        {
+            foreach (Node root in graph.GetRoots())
+            {
+                Traverse(root);
+            }
+            Dump();
+        }
+
+        public bool IsConsistent()
+        {
+            bool result = true;
+
+            Stack<Range> stack = new();
+
+            foreach (File file in files.Values)
+            {
+                // Ranges at the same level must not overlap.
+                for (int i = 0; i < file.Children.Count - 1; i++)
+                {
+                    Range pred = file.Children[i];
+                    Range succ = file.Children[i + 1];
+                    if (pred.End >= succ.Start)
+                    {
+                        Debug.LogWarning($"The ranges of {pred.Node.ID} (ends at line {pred.End}) and {succ.Node.ID} (starts at line {succ.Start}) overlap.\n");
+                        result = false;
+                    }
+                }
+
+                // There are two kinds of hierarchies: (1) the node hierarchy in the graph,
+                // which is determined syntactically, and (2) the range hierarchy here,
+                // which is determined by the nesting of source code ranges.
+                // Because nodes in the graph that do not have source-location information,
+                // the two hierarchies do not necessarily need to be isomorphic. The second hierarchy
+                // needs to be only homomorphic to the first one. That is, if N is a node in
+                // the range hierarchy and P its parent in the range hierarchy, then
+                // P must be an ancestor of N in the node hierarchy.
+                foreach (Range child in file.Children.Values)
+                {
+                    result &= IsHomomorphic(child);
+                }
+            }
+
+            return result;
+
+            bool IsHomomorphic(Range range)
+            {
+                bool result = true;
+                if (stack.Count > 0)
+                {
+                    Range parentRange = stack.Peek();
+                    bool isdescendant = range.Node.IsDescendantOf(parentRange.Node);
+                    if (!isdescendant)
+                    {
+                        Debug.LogWarning($"Range {range} is subsumed by {parentRange}, but {range.Node.ID} is "
+                            + "not a descendant of {parentRange.Node.ID} in the node hierarchy.\n");
+                    }
+                    result &= isdescendant;
+                }
+                stack.Push(range);
+                foreach (Range child in range.Children.Values)
+                {
+                    result &= IsHomomorphic(child);
+                }
+                stack.Pop();
+                return result;
+            }
+        }
+
+        private class File
+        {
+            public readonly SortedList<int, Range> Children = new();
+
+            public void Add(Node node)
+            {
+                int? sourceLine = node.SourceLine;
+                if (sourceLine.HasValue)
+                {
+                    Range descendant = Find(sourceLine.Value);
+                    if (descendant == null)
+                    {
+                        Children.Add(sourceLine.Value, new Range(sourceLine, node.EndLine(), node));
+                    }
+                    else
+                    {
+                        descendant.Add(node);
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning($"{node.ID} does not have a source line. Will be ignored.\n");
+                }
+            }
+
+            public Range Find(int line)
+            {
+                // FIXME: Use binary search instead.
+                foreach (Range range in Children.Values)
+                {
+                    if (range.Start <=  line && line <= range.End)
+                    {
+                        Range child = range.Find(line);
+                        return child ?? range;
+                    }
+                }
+                return null;
+            }
+        }
+
+        private class Range : File
+        {
+            public int? Start;
+            public int? End;
+            public Node Node;
+            public Range(int? start, int? end, Node node)
+            {
+                Start = start;
+                End = end;
+                Node = node;
+            }
+
+            public override string ToString()
+            {
+                return $"{Node.ID}@[{Start}, {End}]";
+            }
+        }
+
+        private readonly Dictionary<string, File> files = new();
+
+        private void Dump()
+        {
+            foreach (var entry in files)
+            {
+                Debug.Log($"*** {entry.Key} ***\n");
+                DumpFile(entry.Value);
+            }
+
+            void DumpFile(File file)
+            {
+                int i = 1;
+                foreach (Range range in file.Children.Values)
+                {
+                    DumpRange(range, i.ToString());
+                    i++;
+                }
+            }
+
+            void DumpRange(Range range, string prefix)
+            {
+                Debug.Log($"{prefix} {range}\n");
+                int i = 1;
+                foreach (Range child in range.Children.Values)
+                {
+                    DumpRange(child, prefix + "." + i.ToString());
+                    i++;
+                }
+            }
+        }
+
+        private void Traverse(Node node)
+        {
+            AddToIndex(node);
+
+            foreach (Node child in node.Children())
+            {
+                Traverse(child);
+            }
+        }
+
+        private void AddToIndex(Node node)
+        {
+            // Only nodes with a filename can be added to the index because
+            // the index is organized by filenames.
+            if (!string.IsNullOrEmpty(node.Filename))
+            {
+                // Note: path cannot be empty because node.Filename is not empty.
+                string path = node.Path();
+                // If we do not already have a File for path, we will add one to the index.
+                if (!files.TryGetValue(path, out File file))
+                {
+                    files.Add(path, file = new File());
+                }
+                file.Add(node);
+            }
+            else
+            {
+                Debug.LogWarning($"{node.ID} does not have a filename. Will be ignored.\n");
+            }
+        }
+    }
+}
---
+++ Assets/SEETests/TestSourceRangeIndex.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+
+namespace SEE.DataModel.DG
+{
+    /// <summary>
+    /// Tests for <see cref="SourceRangeIndex"/>.
+    /// </summary>
+    internal class TestSourceRangeIndex : TestGraphBase
+    {
+        [Test]
+        public void TestConsistentGraph()
+        {
+            SourceRangeIndex index = new(g);
+            Assert.IsTrue(index.IsConsistent());
+        }
+
+        [Test]
+        public void TestNonHomormorphicGraph()
+        {
+            // This node is logically in c1m1, but spatially nested in c1m1M3
+            Node c1m1 = g.GetNode("c1.m1");
+            Child(g, c1m1, "c1.m1.M4", type: "Method", directory: "mydir/", filename: "myfile.java", line: 56, length: 1);
+
+            SourceRangeIndex index = new(g);
+            Assert.IsFalse(index.IsConsistent());
+        }
+
+        [Test]
+        public void TestOverlappingGraph()
+        {
+            // This node overlaps with c1m1M3
+            //HIER WEITER
+            Node c1m1 = g.GetNode("c1.m1");
+            Child(g, c1m1, "c1.m1.M4", type: "Method", directory: "mydir/", filename: "myfile.java", line: 56, length: 1);
+
+            SourceRangeIndex index = new(g);
+            Assert.IsFalse(index.IsConsistent());
+        }
+
+        private Graph g;
+        private Node c1;
+        private Node c1m1;
+        private Node c1m1M1;
+        private Node c1m1M2;
+        private Node c1m1M3;
+        private Node c1m1M3M1;
+        private Node c1m2;
+        private Node c1m3;
+        private Node c1m4;
+        private Node c1m5;
+        private Node c1m6;
+
+        [SetUp]
+        public void SetUp()
+        {
+            g = NewGraph();
+            c1 = NewNode(g, "c1", type: "Class", directory: "mydir/", filename: "myfile.java", line: 50, length: 20);
+
+            c1m1 = Child(g, c1, "c1.m1", type: "Method", directory: "mydir/", filename: "myfile.java", line: 52, length: 5);
+            c1m1M1 = Child(g, c1m1, "c1.m1.M1", type: "Method", directory: "mydir/", filename: "myfile.java", line: 52, length: 1);
+            c1m1M2 = Child(g, c1m1, "c1.m1.M2", type: "Method", directory: "mydir/", filename: "myfile.java", line: 53, length: 1);
+            c1m1M3 = Child(g, c1m1, "c1.m1.M3", type: "Method", directory: "mydir/", filename: "myfile.java", line: 56, length: 1);
+
+            // This node is logically and spatially nested in c1m1M3
+            c1m1M3M1 = Child(g, c1m1M3, "c1.m1.M3.M1", type: "Method", directory: "mydir/", filename: "myfile.java", line: 56, length: 1);
+
+            c1m2 = Child(g, c1, "c1.m2", type: "Method", directory: "mydir/", filename: "myfile.java", line: 60, length: 5);
+            // No filename => will be ignored.
+            c1m3 = Child(g, c1, "c1.m3", type: "Method");
+            // No filename => will be ignored.
+            c1m4 = Child(g, c1, "c1.m4", type: "Method", directory: "mydir/", line: 60, length: 5);
+
+            // In a new file, because the directory is missing.
+            c1m5 = Child(g, c1, "c1.m5", type: "Method", filename: "myfile.java", line: 66, length: null);
+            // No source line => will be ignored.
+            c1m6 = Child(g, c1, "c1.m6", type: "Method", filename: "myfile.java", line: null, length: 5);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            c1 = null;
+            c1m1 = null;
+            c1m1M1 = null;
+            c1m1M2 = null;
+            c1m1M3 = null;
+            c1m1M3M1 = null;
+            c1m2 = null;
+            c1m3 = null;
+            c1m4 = null;
+            c1m5 = null;
+            c1m6 = null;
+            g = null;
+        }
+    }
+}
