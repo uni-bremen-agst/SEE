c974e548ee2179a8848d4b4ccce49346305d10c6
koschke@uni-bremen.de <koschke@uni-bremen.de>
2024-01-29 17:52:40 +0100

    #672 Extended the example a bit to learn more about JaCoCo.

--- Assets/StreamingAssets/JLGExample/Unterordner/UnterOrdnerTest.java
+++ Assets/StreamingAssets/JLGExample/Unterordner/UnterOrdnerTest.java
@@ -8,4 +8,23 @@ public class UnterOrdnerTest {
     public void printTest(){
         System.out.println("UnterOrdnerTest!");
     }
+	
+	private class InnereKlasse {
+	  public int foobar(int j)
+	  {
+		  int i = 5;
+		  return i * j;
+	  }
+	}
+}
+
+class ZweiteKlasse {
+	
+	  public ZweiteKlasse() {}
+	  
+	  public int foo(int j)
+	  {
+		  int i = 10;
+		  return i + j;
+	  }
 }
