15180bc536524d2be603b88742f81a7778f41195
Rainer Koschke <koschke@uni-bremen.de>
2024-01-30 15:07:28 +0100

    #672 Impoved script. Checks whether environment variables are already set. Setting environment variables only locally. Echo the various steps. Avoid error messages when deleting files that do not exist.

--- Assets/StreamingAssets/JLGExample/analyze.bat
+++ Assets/StreamingAssets/JLGExample/analyze.bat
@@ -1,10 +1,25 @@
 @REM Must be executed in an Axivion command shell
 
-@set "JAVA_HOME=C:\Program Files\OpenJDK\jdk-17.0.2"
-@set "PATH=%JAVA_HOME%\bin;%PATH%"
+@REM This is to be able to set the environment variables only for this run of the script.
+@setlocal
+
+if "%JAVA_HOME%"=="" (
+  @set "JAVA_HOME=C:\Program Files\OpenJDK\jdk-17.0.2"
+  @set "PATH=%JAVA_HOME%\bin;%PATH%"
+)
+
+if "%JACOCO%"=="" (
+  @set "JACOCO=C:\Program Files\jacoco\lib"
+)
+
+if "%AXIVION%"=="" (
+  set "AXIVION=C:\Program Files (x86)\Bauhaus"
+)
+
 @set PROJECT=CodeFacts
 @set JAVAFILES=files.txt
-@set "JACOCO=C:\Program Files\jacoco\lib"
+
+@echo Compiling Java project...
 
 @REM Determine the Java source files to be analyzed.
 @dir /b/s *.java > %JAVAFILES%
@@ -12,14 +27,20 @@
 @REM Compile Java files.
 @javac -g -cp "%TOOLSJAR%" @%JAVAFILES%
 
+@echo Running dynamic analysis...
+
 @REM Run dynamic analysis.
 @java -cp . ExecutedLoCLogger -output %PROJECT%.jlg Main both mystring vowels mystring consonants mystring count 100 
 
+@echo Running static analysis...
+
 @REM Create RFG.
-@java2rfg -rfg %PROJECT%.rfg -nocode -8 -cp . @%JAVAFILES%
+@"%AXIVION%\bin\java2rfg" -rfg %PROJECT%.rfg -nocode -8 -cp . @%JAVAFILES%
 
 @REM Export RFG to GXL.
-@rfgexport -f GXL --view "Code Facts" %PROJECT%.rfg %PROJECT%.gxl
+@"%AXIVION%\bin\rfgexport" -f GXL --view "Code Facts" %PROJECT%.rfg %PROJECT%.gxl
+
+@echo Gathering test coverage with JaCoCo...
 
 if exist "%JACOCO%\jacococli.jar" (
 @   REM Run coverge analysis. Results are contained in jacoco.exec.
@@ -29,8 +50,11 @@ if exist "%JACOCO%\jacococli.jar" (
 @   echo "'%JACOCO%' does not exist."
 )
 
+@echo Cleaning up...
 @REM Clean up generated files not needed.
-@del *.class Unterordner\*.class
-@del *.rfg
-@del %JAVAFILES%
-@del jacoco.exec
+@del *.class Unterordner\*.class 2>nul
+@del *.rfg 2>nul
+@del %JAVAFILES% 2>nul
+@del jacoco.exec 2>nul
+
+:end
