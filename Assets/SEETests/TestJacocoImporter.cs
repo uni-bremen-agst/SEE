80dc8193444c4aff3b381effa774cb091d8ca811
Rainer Koschke <koschke@uni-bremen.de>
2024-01-30 15:13:21 +0100

    #672 Adjusted test cases to JLGExample. The missed and covered metrics are integer attributes now.

--- Assets/SEETests/TestJacocoImporter.cs
+++ Assets/SEETests/TestJacocoImporter.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 
 namespace SEE.DataModel.DG
 {
-    // FIXME:
-    // TODO (#672) Change path of test-data
-
     /// <summary>
     /// Unit-Tests for JaCoCoImporter
     /// </summary>
@@ -31,151 +28,133 @@ namespace SEE.DataModel.DG
             return graphReader.GetGraph();
         }
 
+        /// <summary>
+        /// The graph that was loaded by <see cref="SetUp"/> before each test case is executed.
+        /// </summary>
         private Graph graph;
 
         [SetUp]
         public void SetUp()
         {
-            string gxlPath = Application.dataPath + "/../Assets/StreamingAssets/JLGExample/CodeFacts.gxl.xz";
-            string xmlPath = Application.dataPath + "/../Assets/StreamingAssets/JLGExample/jacoco.xml";
+            string gxlPath = Application.streamingAssetsPath + "/JLGExample/CodeFacts.gxl.xz";
+            string xmlPath = Application.streamingAssetsPath + "/JLGExample/jacoco.xml";
 
             graph = LoadGraph(gxlPath);
-            JaCoCoImporter.StartReadingTestXML(graph, xmlPath);
+            JaCoCoImporter.Load(graph, xmlPath);
         }
 
         [TearDown]
         public void TearDown()
         {
-            graph = null;   
+            graph = null;
         }
 
         /// <summary>
-        /// Test if metrics are set for the Project-Root. In JaCoCo-Test-Report its named "report".
+        /// Test if metrics are set for the project root. In JaCoCo-Test-Report it is named "report".
         /// </summary>
         [Test]
         public void AddMetricToRootNode()
         {
             Node nodeToTest = graph.GetRoots()[0];
+            Assert.IsNotNull(nodeToTest);
 
-            Assert.AreEqual(1395f, nodeToTest.GetFloat(AttributeNamesExtensions.InstructionMissed));
-            Assert.AreEqual(494f, nodeToTest.GetFloat(AttributeNamesExtensions.InstructionCovered));
+            Assert.AreEqual(1313, nodeToTest.GetInt(AttributeNamesExtensions.InstructionMissed));
+            Assert.AreEqual(441, nodeToTest.GetInt(AttributeNamesExtensions.InstructionCovered));
 
-            Assert.AreEqual(110f, nodeToTest.GetFloat(AttributeNamesExtensions.BranchMissed));
-            Assert.AreEqual(22f, nodeToTest.GetFloat(AttributeNamesExtensions.BranchCovered));
+            Assert.AreEqual(101, nodeToTest.GetInt(AttributeNamesExtensions.BranchMissed));
+            Assert.AreEqual(27, nodeToTest.GetInt(AttributeNamesExtensions.BranchCovered));
 
-            Assert.AreEqual(351f, nodeToTest.GetFloat(AttributeNamesExtensions.LineMissed));
-            Assert.AreEqual(100f, nodeToTest.GetFloat(AttributeNamesExtensions.LineCovered));
+            Assert.AreEqual(330, nodeToTest.GetInt(AttributeNamesExtensions.LineMissed));
+            Assert.AreEqual(83, nodeToTest.GetInt(AttributeNamesExtensions.LineCovered));
 
-            Assert.AreEqual(102f, nodeToTest.GetFloat(AttributeNamesExtensions.ComplexityMissed));
-            Assert.AreEqual(37f, nodeToTest.GetFloat(AttributeNamesExtensions.ComplexityCovered));
+            Assert.AreEqual(107, nodeToTest.GetInt(AttributeNamesExtensions.ComplexityMissed));
+            Assert.AreEqual(20, nodeToTest.GetInt(AttributeNamesExtensions.ComplexityCovered));
 
-            Assert.AreEqual(47f, nodeToTest.GetFloat(AttributeNamesExtensions.MethodMissed));
-            Assert.AreEqual(26f, nodeToTest.GetFloat(AttributeNamesExtensions.MethodCovered));
+            Assert.AreEqual(55, nodeToTest.GetInt(AttributeNamesExtensions.MethodMissed));
+            Assert.AreEqual(8, nodeToTest.GetInt(AttributeNamesExtensions.MethodCovered));
 
-            Assert.AreEqual(5f, nodeToTest.GetFloat(AttributeNamesExtensions.ClassMissed));
-            Assert.AreEqual(7f, nodeToTest.GetFloat(AttributeNamesExtensions.ClassCovered));
+            Assert.AreEqual(6, nodeToTest.GetInt(AttributeNamesExtensions.ClassMissed));
+            Assert.AreEqual(4, nodeToTest.GetInt(AttributeNamesExtensions.ClassCovered));
         }
 
         /// <summary>
-        /// Test if metrics are set for class-node. In JaCoCo-Test-Report its named "class".
+        /// Test if metrics are set for a class node. In JaCoCo-Test-Report it is named "class".
         /// </summary>
         [Test]
         public void AddMetricToClassNode()
         {
-            Node nodeToTest = graph.GetNode("BankAccount.Account");
+            Node nodeToTest = graph.GetNode("counter.CountConsonants");
+            Assert.IsNotNull(nodeToTest);
 
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.InstructionMissed));
-            Assert.AreEqual(44f, nodeToTest.GetFloat(AttributeNamesExtensions.InstructionCovered));
+            Assert.AreEqual(7f, nodeToTest.GetInt(AttributeNamesExtensions.InstructionMissed));
+            Assert.AreEqual(130f, nodeToTest.GetInt(AttributeNamesExtensions.InstructionCovered));
 
-            Assert.AreEqual(0.0f, nodeToTest.GetFloat(AttributeNamesExtensions.BranchMissed));
-            Assert.AreEqual(2f, nodeToTest.GetFloat(AttributeNamesExtensions.BranchCovered));
+            Assert.AreEqual(0.0f, nodeToTest.GetInt(AttributeNamesExtensions.BranchMissed));
+            Assert.AreEqual(6f, nodeToTest.GetInt(AttributeNamesExtensions.BranchCovered));
 
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.LineMissed));
-            Assert.AreEqual(14f, nodeToTest.GetFloat(AttributeNamesExtensions.LineCovered));
+            Assert.AreEqual(3f, nodeToTest.GetInt(AttributeNamesExtensions.LineMissed));
+            Assert.AreEqual(11f, nodeToTest.GetInt(AttributeNamesExtensions.LineCovered));
 
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.ComplexityMissed));
-            Assert.AreEqual(7f, nodeToTest.GetFloat(AttributeNamesExtensions.ComplexityCovered));
+            Assert.AreEqual(2f, nodeToTest.GetInt(AttributeNamesExtensions.ComplexityMissed));
+            Assert.AreEqual(5f, nodeToTest.GetInt(AttributeNamesExtensions.ComplexityCovered));
 
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.MethodMissed));
-            Assert.AreEqual(6f, nodeToTest.GetFloat(AttributeNamesExtensions.MethodCovered));
+            Assert.AreEqual(2f, nodeToTest.GetInt(AttributeNamesExtensions.MethodMissed));
+            Assert.AreEqual(2f, nodeToTest.GetInt(AttributeNamesExtensions.MethodCovered));
 
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.ClassMissed));
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.ClassCovered));
+            Assert.AreEqual(0f, nodeToTest.GetInt(AttributeNamesExtensions.ClassMissed));
+            Assert.AreEqual(1f, nodeToTest.GetInt(AttributeNamesExtensions.ClassCovered));
         }
 
         /// <summary>
-        /// Test if metrics are set for class-node, when there is a second class with the same name.
-        /// </summary>
-        [Test]
-        public void AddMetricToClassNodeWhichNameIsUsedTwice()
-        {
-            Node nodeToTest = graph.GetNode("BankAccount.Main");
-
-            Assert.AreEqual(3f, nodeToTest.GetFloat(AttributeNamesExtensions.InstructionMissed));
-            Assert.AreEqual(77f, nodeToTest.GetFloat(AttributeNamesExtensions.InstructionCovered));
-
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.BranchMissed));
-            Assert.AreEqual(2f, nodeToTest.GetFloat(AttributeNamesExtensions.BranchCovered));
-
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.LineMissed));
-            Assert.AreEqual(17f, nodeToTest.GetFloat(AttributeNamesExtensions.LineCovered));
-
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.ComplexityMissed));
-            Assert.AreEqual(2f, nodeToTest.GetFloat(AttributeNamesExtensions.ComplexityCovered));
-
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.MethodMissed));
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.MethodCovered));
-
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.ClassMissed));
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.ClassCovered));
-        }
-
-        /// <summary>
-        /// Test if metrics are set for package-node. In JaCoCo-Test-Report its named "package".
+        /// Test if metrics are set for a package node. In JaCoCo-Test-Report it is named "package".
         /// </summary>
         [Test]
         public void AddMetricToPackageNode()
         {
-            Node nodeToTest = graph.GetNode("BankAccount");
+            Node nodeToTest = graph.GetNode("counter");
+            Assert.IsNotNull(nodeToTest);
 
-            Assert.AreEqual(3f, nodeToTest.GetFloat(AttributeNamesExtensions.InstructionMissed));
-            Assert.AreEqual(153f, nodeToTest.GetFloat(AttributeNamesExtensions.InstructionCovered));
+            Assert.AreEqual(31, nodeToTest.GetInt(AttributeNamesExtensions.InstructionMissed));
+            Assert.AreEqual(313, nodeToTest.GetInt(AttributeNamesExtensions.InstructionCovered));
 
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.BranchMissed));
-            Assert.AreEqual(4f, nodeToTest.GetFloat(AttributeNamesExtensions.BranchCovered));
+            Assert.AreEqual(1, nodeToTest.GetInt(AttributeNamesExtensions.BranchMissed));
+            Assert.AreEqual(17, nodeToTest.GetInt(AttributeNamesExtensions.BranchCovered));
 
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.LineMissed));
-            Assert.AreEqual(43f, nodeToTest.GetFloat(AttributeNamesExtensions.LineCovered));
+            Assert.AreEqual(13, nodeToTest.GetInt(AttributeNamesExtensions.LineMissed));
+            Assert.AreEqual(45, nodeToTest.GetInt(AttributeNamesExtensions.LineCovered));
 
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.ComplexityMissed));
-            Assert.AreEqual(15f, nodeToTest.GetFloat(AttributeNamesExtensions.ComplexityCovered));
+            Assert.AreEqual(9, nodeToTest.GetInt(AttributeNamesExtensions.ComplexityMissed));
+            Assert.AreEqual(14, nodeToTest.GetInt(AttributeNamesExtensions.ComplexityCovered));
 
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.MethodMissed));
-            Assert.AreEqual(13f, nodeToTest.GetFloat(AttributeNamesExtensions.MethodCovered));
+            Assert.AreEqual(8, nodeToTest.GetInt(AttributeNamesExtensions.MethodMissed));
+            Assert.AreEqual(6, nodeToTest.GetInt(AttributeNamesExtensions.MethodCovered));
 
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.ClassMissed));
-            Assert.AreEqual(4f, nodeToTest.GetFloat(AttributeNamesExtensions.ClassCovered));
+            Assert.AreEqual(0, nodeToTest.GetInt(AttributeNamesExtensions.ClassMissed));
+            Assert.AreEqual(3, nodeToTest.GetInt(AttributeNamesExtensions.ClassCovered));
         }
 
         /// <summary>
-        /// Test if metrics are set for method-node. In JaCoCo-Test-Report its named "method".
+        /// Test if metrics are set for a method node. In JaCoCo-Test-Report it is named "method".
         /// </summary>
         [Test]
         public void AddMetricToMethodNode()
         {
-            Node nodeToTest = graph.GetNode("BankAccount.Bank.getName()");
+            Node nodeToTest = graph.GetNode("counter.CountConsonants.countConsonants(java.lang.String;)");
 
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.InstructionMissed));
-            Assert.AreEqual(3f, nodeToTest.GetFloat(AttributeNamesExtensions.InstructionCovered));
+            Assert.AreEqual(0, nodeToTest.GetInt(AttributeNamesExtensions.InstructionMissed));
+            Assert.AreEqual(39, nodeToTest.GetInt(AttributeNamesExtensions.InstructionCovered));
 
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.LineMissed));
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.LineCovered));
+            Assert.AreEqual(0, nodeToTest.GetInt(AttributeNamesExtensions.BranchMissed));
+            Assert.AreEqual(6, nodeToTest.GetInt(AttributeNamesExtensions.BranchCovered));
 
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.ComplexityMissed));
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.ComplexityCovered));
+            Assert.AreEqual(0, nodeToTest.GetInt(AttributeNamesExtensions.LineMissed));
+            Assert.AreEqual(8, nodeToTest.GetInt(AttributeNamesExtensions.LineCovered));
 
-            Assert.AreEqual(0f, nodeToTest.GetFloat(AttributeNamesExtensions.MethodMissed));
-            Assert.AreEqual(1f, nodeToTest.GetFloat(AttributeNamesExtensions.MethodCovered));
+            Assert.AreEqual(0, nodeToTest.GetInt(AttributeNamesExtensions.ComplexityMissed));
+            Assert.AreEqual(4, nodeToTest.GetInt(AttributeNamesExtensions.ComplexityCovered));
+
+            Assert.AreEqual(0, nodeToTest.GetInt(AttributeNamesExtensions.MethodMissed));
+            Assert.AreEqual(1, nodeToTest.GetInt(AttributeNamesExtensions.MethodCovered));
         }
     }
 }
