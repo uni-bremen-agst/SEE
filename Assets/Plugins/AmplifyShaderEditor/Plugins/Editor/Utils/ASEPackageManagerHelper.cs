769c8c30e4a36462272ab480cd55348a064db077
Rainer Koschke <koschke@uni-bremen.de>
2024-01-31 08:55:06 +0100

    Updated Amplify Shader Editor to version 1.9.3.

--- Assets/Plugins/AmplifyShaderEditor/Plugins/Editor/Utils/ASEPackageManagerHelper.cs
+++ Assets/Plugins/AmplifyShaderEditor/Plugins/Editor/Utils/ASEPackageManagerHelper.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4305e7636e48ff5e59e82b4784871675a8a919c0027e807f8a4f72090771ca1e
+oid sha256:8e9e494dac47ca0772abe4e27ec56146f18b95d4293b4ef11287e9b33aea299b
 size 15972
