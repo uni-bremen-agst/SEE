439e450256f241160d93ea8c7ccab36fae655c53
Rainer Koschke <koschke@uni-bremen.de>
2024-02-06 15:00:18 +0100

    Post-update updates by DOTween Pro.

--- Assets/Plugins/Demigiant/DOTweenPro/DOTweenTextMeshPro.cs
+++ Assets/Plugins/Demigiant/DOTweenPro/DOTweenTextMeshPro.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:356a41f65918e81618b1903832e1ae96b4a03bc082264ca0e43710169d8604af
-size 61855
+oid sha256:6b04ed4bfb966e6d8d8e1b9ee368b2bb21c86e9eff8cc47429e2119d06f239c1
+size 61851
--- Assets/Plugins/Demigiant/DOTweenPro/Editor/DOTweenAnimationInspector.cs
+++ Assets/Plugins/Demigiant/DOTweenPro/Editor/DOTweenAnimationInspector.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90f46da3a3c33a2b0c54da1e7c829eb424a296743f7c9f9a7a677ce32a48a24d
-size 40528
+oid sha256:cdf523c4d84c10d3ae80ad53ea76d312d3aa17126e9185748264225624f73780
+size 40521
