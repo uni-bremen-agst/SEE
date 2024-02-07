888ded45ae3b2dbe52afaa1306cfda93bc69371a
Rainer Koschke <koschke@uni-bremen.de>
2024-02-07 16:41:26 +0100

    A copy of DummyHereForTestVCS.cs

---
+++ Assets/SEETests/CopyOfDummyHereForTestVCS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEETests
+{
+    /// <summary>
+    /// This class is here only to serve for tests of TestVCS.
+    /// </summary>
+    internal class DummyHereForTestVCS
+    {
+    }
+}
