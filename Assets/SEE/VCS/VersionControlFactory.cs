9450c81f6a060ef8a27541e634552fee637edb02
Rainer Koschke <koschke@uni-bremen.de>
2024-02-08 13:04:20 +0100

    #685 It's better to represent the VCS as an enum.

--- Assets/SEE/VCS/VersionControlFactory.cs
+++ Assets/SEE/VCS/VersionControlFactory.cs
@@ -12,11 +12,11 @@ namespace SEE.VCS
         /// </summary>
         /// <param name="system">the version control system</param>
         /// <returns>the functionality for the given version control system</returns>
-        internal static IVersionControl GetVersionControl(string system, string repositoryPath)
+        internal static IVersionControl GetVersionControl(VCSKind system, string repositoryPath)
         {
-            return system.ToLower() switch
+            return system switch
             {
-                "git" => new GitVersionControl(repositoryPath),
+                VCSKind.Git => new GitVersionControl(repositoryPath),
                 // Add cases for other version control systems
                 _ => throw new ArgumentException("Unsupported version control system", nameof(system)),
             };
