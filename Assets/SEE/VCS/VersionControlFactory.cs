ef53e504ed083d148915f4be8d5087381c996346
Rainer Koschke <koschke@uni-bremen.de>
2024-02-11 14:38:01 +0100

    #685 Documented second parameter. Renamed first one.

--- Assets/SEE/VCS/VersionControlFactory.cs
+++ Assets/SEE/VCS/VersionControlFactory.cs
@@ -10,15 +10,16 @@ namespace SEE.VCS
         /// <summary>
         /// Returns the functionality of the given version control system.
         /// </summary>
-        /// <param name="system">the version control system</param>
+        /// <param name="vcsKind">the version control system</param>
+        /// <param name="repositoryPath">the path to the root of the repository</param>
         /// <returns>the functionality for the given version control system</returns>
-        internal static IVersionControl GetVersionControl(VCSKind system, string repositoryPath)
+        internal static IVersionControl GetVersionControl(VCSKind vcsKind, string repositoryPath)
         {
-            return system switch
+            return vcsKind switch
             {
                 VCSKind.Git => new GitVersionControl(repositoryPath),
                 // Add cases for other version control systems
-                _ => throw new ArgumentException("Unsupported version control system", nameof(system)),
+                _ => throw new ArgumentException("Unsupported version control system", nameof(vcsKind)),
             };
         }
     }
