50f78dd3422f147951a171cc3a3f492c842f828c
unknown <krausem@uni-bremen.de>
2024-03-04 14:11:25 +0100

    Fix WindowsDragger Problem

--- Assets/Plugins/Modern UI Pack/Scripts/Window/WindowDragger.cs
+++ Assets/Plugins/Modern UI Pack/Scripts/Window/WindowDragger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ff2c6e9a1913ae09df6474ce33acf897a2c7b6477b80e5d19eb163cb7a4af9c
-size 3383
+oid sha256:0294ec1f6638db4cff047179b607583d9bb07b9c8f869cd7be593cb2799839c7
+size 3607
