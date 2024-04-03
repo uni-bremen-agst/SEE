a881a0bd6a6fee334cc74412e6663051e833a999
Rainer Koschke <koschke@uni-bremen.de>
2024-04-03 18:00:05 +0200

    #718 Imported Assets/Plugins/Beautify/Builtin/BeautifyBuiltinPipeline.unitypackage. The import puts its contents into Assets. I moved it to Assets/Plugins.

---
+++ Assets/Plugins/Beautify/Resources/Shaders/Beautify.cginc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:37f497d21539cebded84c6f1f86cf956c58687e391169e13678d31ecd9593373
+size 21039
---
+++ Assets/Plugins/Beautify/Scripts/Beautify.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:be37ba7518bc6051eb0ab4f7d044c380e5957cc4d3f38d02370bfd526a83624c
+size 181372
