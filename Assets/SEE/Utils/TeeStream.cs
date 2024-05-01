aa3a87f770708fd6dc774a2d421d821916948b67
Rainer Koschke <koschke@uni-bremen.de>
2024-05-01 20:31:37 +0200

    Names should never start with an underscore.

--- Assets/SEE/Utils/TeeStream.cs
+++ Assets/SEE/Utils/TeeStream.cs
@@ -15,12 +15,12 @@ namespace SEE.Utils
         /// <summary>
         /// The primary stream, which is the stream to read from.
         /// </summary>
-        private readonly Stream _primaryStream;
+        private readonly Stream primaryStream;
 
         /// <summary>
-        /// The secondary stream to write the <see cref="_primaryStream"/> to.
+        /// The secondary stream to write the <see cref="primaryStream"/> to.
         /// </summary>
-        private readonly Stream _secondaryStream;
+        private readonly Stream secondaryStream;
 
         /// <summary>
         /// Creates a new instance of the <see cref="TeeStream"/> class.
@@ -32,49 +32,49 @@ namespace SEE.Utils
         /// </exception>
         public TeeStream(Stream primaryStream, Stream secondaryStream)
         {
-            _primaryStream = primaryStream ?? throw new ArgumentNullException(nameof(primaryStream));
-            _secondaryStream = secondaryStream ?? throw new ArgumentNullException(nameof(secondaryStream));
+            this.primaryStream = primaryStream ?? throw new ArgumentNullException(nameof(primaryStream));
+            this.secondaryStream = secondaryStream ?? throw new ArgumentNullException(nameof(secondaryStream));
         }
 
-        public override bool CanRead => _primaryStream.CanRead;
-        public override bool CanSeek => _primaryStream.CanSeek;
-        public override bool CanWrite => _primaryStream.CanWrite;
-        public override long Length => _primaryStream.Length;
+        public override bool CanRead => primaryStream.CanRead;
+        public override bool CanSeek => primaryStream.CanSeek;
+        public override bool CanWrite => primaryStream.CanWrite;
+        public override long Length => primaryStream.Length;
 
         public override long Position
         {
-            get => _primaryStream.Position;
-            set => _primaryStream.Position = value;
+            get => primaryStream.Position;
+            set => primaryStream.Position = value;
         }
 
         public override void Flush()
         {
-            _primaryStream.Flush();
-            _secondaryStream.Flush();
+            primaryStream.Flush();
+            secondaryStream.Flush();
         }
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            int bytesRead = _primaryStream.Read(buffer, offset, count);
-            _secondaryStream.Write(buffer, offset, bytesRead);
-            _secondaryStream.Flush(); // Make sure everything is immediately logged
+            int bytesRead = primaryStream.Read(buffer, offset, count);
+            secondaryStream.Write(buffer, offset, bytesRead);
+            secondaryStream.Flush(); // Make sure everything is immediately logged
             return bytesRead;
         }
 
         public override long Seek(long offset, SeekOrigin origin)
         {
-            return _primaryStream.Seek(offset, origin);
+            return primaryStream.Seek(offset, origin);
         }
 
         public override void SetLength(long value)
         {
-            _primaryStream.SetLength(value);
+            primaryStream.SetLength(value);
         }
 
         public override void Write(byte[] buffer, int offset, int count)
         {
-            _primaryStream.Write(buffer, offset, count);
-            _secondaryStream.Write(buffer, offset, count);
+            primaryStream.Write(buffer, offset, count);
+            secondaryStream.Write(buffer, offset, count);
         }
     }
 
