28e8205330905772444824b80389941313ae678a
Rainer Koschke <koschke@uni-bremen.de>
2024-05-10 08:16:45 +0200

    #700 Simplified code. Improved documentation and parameter naming.

--- Assets/SEE/UI/Window/MetricWindow.cs
+++ Assets/SEE/UI/Window/MetricWindow.cs
@@ -13,6 +13,8 @@ namespace SEE.UI.Window
 {
     /// <summary>
     /// Represents a movable, scrollable window containing metrics of a <see cref="GraphElement"/>.
+    /// It consists of a search field and a list of metrics, where each metric is represented by a row
+    /// holding the attribute name and its value.
     /// </summary>
     public class MetricWindow : BaseWindow
     {
@@ -38,51 +40,61 @@ namespace SEE.UI.Window
         }
 
         /// <summary>
-        /// Searches for a specified string within <paramref name="elements"/>.
-        /// Activates or deactivates them based on the search results.
-        /// GameObjects are considered "active" when the search string has been found within their names.
+        /// Activates all <paramref name="metricRows"/> if they match the <paramref name="searchQuery"/>.
+        /// All others are deactivated. In other words, the <paramref name="searchQuery"/> is applied as
+        /// a filter.
         /// </summary>
-        /// <param name="searchQuery"> string to search for </param>
-        /// <param name="searchableObjects"> Dictionary of GameObjects to search in </param>
-        private void InputSearchField(string searchQuery, Dictionary<string, GameObject> searchableObjects)
+        /// <param name="searchQuery"> attribute name to search for </param>
+        /// <param name="metricRows">mapping of attribute names onto gameObjects representing
+        /// the corresponding metric row</param>
+        private static void ActivateMatches(string searchQuery, Dictionary<string, GameObject> metricRows)
         {
-            // Remove Whitespaces
+            // Remove whitespace.
             searchQuery = searchQuery.Trim();
+
             if (string.IsNullOrEmpty(searchQuery))
             {
-                foreach (var ele in searchableObjects)
-                {
-                    ele.Value.SetActive(true);
-                }
+                // There is no search query, so activate all metric rows.
+                SetActive(metricRows, true);
             }
             else
             {
-                // Deactivate every GameObject
-                IEnumerable<string> searchList = Search(searchQuery, searchableObjects.Values.ToArray());
-                foreach(var ele in searchableObjects)
+                // First, deactivate all metric rows and then activate only those that match the
+                // search results.
+                SetActive(metricRows, false);
+                foreach (string attributeName in Search(searchQuery, metricRows.Values.ToArray()))
                 {
-                    ele.Value.SetActive(false);
+                    if (metricRows.TryGetValue(attributeName, out GameObject activeObject))
+                    {
+                        activeObject.SetActive(true);
+                    }
                 }
-                // Activate GameObjects that match the search results from the fuzzy search.
-                foreach (string ele in searchList)
+            }
+
+            return;
+
+            static void SetActive(Dictionary<string, GameObject> searchableObjects, bool activate)
+            {
+                foreach (GameObject go in searchableObjects.Values)
                 {
-                    searchableObjects.TryGetValue(ele, out GameObject activeObject);
-                    activeObject.SetActive(true);
+                    go.SetActive(activate);
                 }
             }
         }
 
         /// <summary>
-        /// Returns the attributes of all <paramref name="gameObjects"/> whose name matches the
+        /// Returns the attribute names of all <paramref name="metricRows"/> whose attribute name matches the
         /// <paramref name="query"/> using a fuzzy search.
         /// </summary>
         /// <param name="query"> the search query (part of an attribute name)</param>
-        /// <param name="gameObjects"> the game objects to search through</param>
-        /// <param name="cutoff"> representing the cutoff score for relevance </param>
-        /// <returns> the attributes whose names match the <paramref name="query"/> </returns>
-        public IEnumerable<string> Search(string query, GameObject[] gameObjects, int limit = 15, int cutoff = 40)
+        /// <param name="metricRows"> the game objects representing metric rows to search through</param>
+        /// <param name="limit"> the maximum number of results to return </param>
+        /// <param name="cutoff"> the cutoff score for relevance (deviation from the <paramref name="query"/>
+        /// of the fuzzy search)</param>
+        /// <returns> the attribute names matching the <paramref name="query"/> </returns>
+        private static IEnumerable<string> Search(string query, GameObject[] metricRows, int limit = 15, int cutoff = 40)
         {
-            List<string> attributesList = gameObjects.Select(AttributeName).ToList();
+            List<string> attributesList = metricRows.Select(AttributeName).ToList();
             IEnumerable<(int score, string attribute)> searchResults
                 = Process.ExtractTop(query, attributesList, limit: limit, cutoff: cutoff).Select(x => (x.Score, x.Value));
             searchResults = searchResults.OrderByDescending(x => x.score);
@@ -112,7 +124,7 @@ namespace SEE.UI.Window
         /// <remarks>Assumes that the attribute name is stored in the first child of the metric row.</remarks>
         private static TextMeshProUGUI Attribute(GameObject metricRow)
         {
-            return metricRow.transform.GetChild(0).MustGetComponent<TextMeshProUGUI>();
+            return metricRow.transform.GetChild(0).gameObject.MustGetComponent<TextMeshProUGUI>();
         }
 
         /// <summary>
@@ -125,44 +137,38 @@ namespace SEE.UI.Window
         /// <remarks>Assumes that the attribute name is stored in the second child of the metric row.</remarks>
         private static TextMeshProUGUI Value(GameObject metricRow)
         {
-            return metricRow.transform.GetChild(1).MustGetComponent<TextMeshProUGUI>();
+            return metricRow.transform.GetChild(1).gameObject.MustGetComponent<TextMeshProUGUI>();
         }
 
         /// <summary>
-        /// The GameObject representing the metric window. It must be stored in this field;
-        /// otherwise, it will be garbage collected.
-        /// </summary>
-        private GameObject metricWindowObject;
-
-        /// <summary>
-        /// Creates the Metric Window
+        /// Creates the metric window.
         /// </summary>
         public void CreateUIInstance()
         {
             // Instantiate MetricWindow
-            metricWindowObject = PrefabInstantiator.InstantiatePrefab(WindowPrefab, Window.transform.Find("Content"), false);
-            metricWindowObject.name = "Scrollable";
+            GameObject metricWindow = PrefabInstantiator.InstantiatePrefab(WindowPrefab, Window.transform.Find("Content"), false);
+            metricWindow.name = "Metric Window";
 
-            Transform scrollViewContent = metricWindowObject.transform.Find("Content/Items").transform;
-            TMP_InputField inputField = metricWindowObject.transform.Find("Search/SearchField").gameObject.MustGetComponent<TMP_InputField>();
+            Transform scrollViewContent = metricWindow.transform.Find("Content/Items").transform;
+            TMP_InputField searchField = metricWindow.transform.Find("Search/SearchField").gameObject.MustGetComponent<TMP_InputField>();
 
-            inputField.onSelect.AddListener(_ => SEEInput.KeyboardShortcutsEnabled = false);
-            inputField.onDeselect.AddListener(_ => SEEInput.KeyboardShortcutsEnabled = true);
+            searchField.onSelect.AddListener(_ => SEEInput.KeyboardShortcutsEnabled = false);
+            searchField.onDeselect.AddListener(_ => SEEInput.KeyboardShortcutsEnabled = true);
 
             // Int Attributes
-            DisplayAttributes(GraphElement.IntAttributes, metricWindowObject);
+            DisplayAttributes(GraphElement.IntAttributes, metricWindow);
 
             // Float Attributes
-            DisplayAttributes(GraphElement.FloatAttributes, metricWindowObject);
+            DisplayAttributes(GraphElement.FloatAttributes, metricWindow);
 
-            // Save GameObjects in dictionary for SearchField
+            // Create mapping of attribute names onto gameObjects representing the corresponding metric row.
             Dictionary<string, GameObject> activeElements = new();
             foreach (Transform child in scrollViewContent)
             {
                 activeElements.Add(AttributeName(child.gameObject), child.gameObject);
             }
 
-            inputField.onValueChanged.AddListener(str => InputSearchField(str, activeElements));
+            searchField.onValueChanged.AddListener(searchQuery => ActivateMatches(searchQuery, activeElements));
         }
 
         /// <summary>
