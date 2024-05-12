7bd95994296beec394f4abae16a81780a931ae65
Hannes Kuss <hkuss@uni-bremen.de>
2024-05-12 12:03:48 +0200

    Optimized BuildGraphFromPath with new method GetOrAddNode

--- Assets/SEE/Utils/GraphUtils.cs
+++ Assets/SEE/Utils/GraphUtils.cs
@@ -1,10 +1,17 @@
+using System;
 using System.IO;
+using System.Linq;
 using SEE.DataModel.DG;
 
 namespace SEE.Utils
 {
     public class GraphUtils
     {
+        private const string FileType = "file";
+
+        private const string DirectoryType = "directory";
+
+
         /// <summary>
         /// Creates and returns a new node to <paramref name="graph"/>.
         /// </summary>
@@ -30,6 +37,68 @@ namespace SEE.Utils
         }
 
         /// <summary>
+        /// Recursive algorithm to add a file with the path <paramref name="fullRelativePath"/> to the graph <paramref name="g"/>
+        ///
+        /// This method will also add all directories in between.
+        ///
+        /// Files will have the node type <see cref="FileType"/>
+        /// Diecotries will have the node type <see cref="DirectoryType"/> 
+        /// </summary>
+        /// <param name="fullRelativePath">The full relative path of the file</param>
+        /// <param name="rootNode">The root node of the repository</param>
+        /// <param name="g">The graph to add the nodes to</param>
+        /// <returns>The found file node</returns>
+        public static Node GetOrAddNode(string fullRelativePath, Node rootNode, Graph g) =>
+            GetOrAddNode(fullRelativePath, fullRelativePath, rootNode, g);
+
+
+        /// <summary>
+        /// The same as <see cref="GetOrAddNode"/> but with the actual logic.
+        /// </summary>
+        /// <param name="fullRelativePath">The full relative path of the file</param>
+        /// <param name="path">The root node of the repository</param>
+        /// <param name="parent">The parent of the current node</param>
+        /// <param name="g">The graph to add the nodes to</param>
+        /// <returns></returns>
+        private static Node GetOrAddNode(string fullRelativePath, string path, Node parent, Graph g)
+        {
+            string[] pathSegments = path.Split(Path.AltDirectorySeparatorChar);
+            // If we are in the directory of the file
+            if (pathSegments.Length == 1)
+            {
+                // If the file node exists
+                if (parent.Children().Any(x => x.ID == fullRelativePath))
+                {
+                    return parent.Children().First(x => x.ID == fullRelativePath);
+                }
+
+                // Create a new file node and return it
+                Node addedFileNode = NewNode(g, fullRelativePath,
+                    FileType, path);
+                parent.AddChild(addedFileNode);
+                return addedFileNode;
+            }
+
+            string directoryName = parent.ID + Path.AltDirectorySeparatorChar + pathSegments.First();
+
+            // If the current Node parent already has the next directory with the name directoryName
+            if (parent.Children().Any(x => x.ID == directoryName))
+            {
+                Node dirNode = parent.Children().First(x =>
+                    x.ID == parent.ID + Path.AltDirectorySeparatorChar + pathSegments.First());
+                return GetOrAddNode(fullRelativePath, String.Join(Path.AltDirectorySeparatorChar, pathSegments.Skip(1)),
+                    dirNode, g);
+            }
+
+            // Create a new directory node
+            Node addedDirectoryNode = NewNode(g, directoryName,
+                DirectoryType, directoryName);
+            parent.AddChild(addedDirectoryNode);
+            return GetOrAddNode(fullRelativePath, String.Join(Path.AltDirectorySeparatorChar, pathSegments.Skip(1)),
+                addedDirectoryNode, g);
+        }
+
+        /// <summary>
         /// Creates a new node for each element of a filepath, that does not
         /// already exists in the graph.
         /// </summary>
@@ -38,6 +107,7 @@ namespace SEE.Utils
         /// <param name="parentPath">The path of the current parent, which will eventually be part of the ID</param>
         /// <param name="graph">The graph to which the new node belongs to</param>
         /// <param name="mainNode">The root node of the main directory</param>
+        [Obsolete("Replaced by GetOrAddNode")]
         public static Node BuildGraphFromPath(string path, Node parent, string parentPath, Graph graph, Node mainNode)
         {
             string[] pathSegments = path.Split(Path.AltDirectorySeparatorChar);
