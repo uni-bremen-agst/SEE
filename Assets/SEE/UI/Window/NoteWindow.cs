1f17c2017cdb6a5bed5bc69e8d56f00e06a22876
Tim Dinh <timdinh1@uni-bremen.de>
2024-06-06 13:40:04 +0200

    #717 scrollview added + path to save and load is now choosable

--- Assets/SEE/UI/Window/NoteWindow.cs
+++ Assets/SEE/UI/Window/NoteWindow.cs
@@ -21,11 +21,7 @@ namespace SEE.UI.Window
         /// </summary>
         private static string WindowPrefab => UIPrefabFolder + "NoteWindow";
 
-        private const string path = "C:\\Users\\Fujitsu\\Desktop\\SEETxt\\";
-
         public TMP_InputField searchField;
-
-        public GraphElement Graph;
 
         // Start is called before the first frame update
         protected override void StartDesktop()
@@ -40,43 +36,39 @@ namespace SEE.UI.Window
             GameObject noteWindow = PrefabInstantiator.InstantiatePrefab(WindowPrefab, Window.transform.Find("Content"), false);
             noteWindow.name = "Note Window";
 
-            searchField = noteWindow.transform.Find("InputField").gameObject.MustGetComponent<TMP_InputField>();
+            searchField = noteWindow.transform.Find("ScrollView/Viewport/Content/InputField").gameObject.MustGetComponent<TMP_InputField>();
 
             searchField.onSelect.AddListener(_ => SEEInput.KeyboardShortcutsEnabled = false);
             searchField.onDeselect.AddListener(_ => SEEInput.KeyboardShortcutsEnabled = true);
 
-            ButtonManagerBasic saveButton = noteWindow.transform.Find("SaveButton").gameObject.MustGetComponent<ButtonManagerBasic>();
+            ButtonManagerBasic saveButton = noteWindow.transform.Find("ScrollView/Viewport/Content/SaveButton").gameObject.MustGetComponent<ButtonManagerBasic>();
             saveButton.clickEvent.AddListener(WriteToFile);
 
-            ButtonManagerBasic loadButton = noteWindow.transform.Find("LoadButton").gameObject.MustGetComponent<ButtonManagerBasic>();
+            ButtonManagerBasic loadButton = noteWindow.transform.Find("ScrollView/Viewport/Content/LoadButton").gameObject.MustGetComponent<ButtonManagerBasic>();
             loadButton.clickEvent.AddListener(LoadFromFile);
         }
 
-        [MenuItem("Examples/Save Texture to file")]
         public void WriteToFile()
         {
-            string fileName = path + Title + ".txt";
-            TMP_Text stringText = searchField.transform.Find("TextArea/Text").gameObject.MustGetComponent<TMP_Text>();
-            File.WriteAllText(fileName, stringText.text);
+            string path = EditorUtility.SaveFilePanel(
+            "Save Note",
+            "",
+            "Note",
+            "");
+            if (path.Length != 0)
+            {
+                string pngData = searchField.text;
+                if (pngData != null)
+                    File.WriteAllText(path, pngData);
+            }
         }
 
         public void LoadFromFile()
         {
-            /*Texture2D texture = Selection.activeObject as Texture2D;
-            if(texture == null)
-            {
-                EditorUtility.DisplayDialog(
-                    "Select Texture",
-                    "You must select a texture first!",
-                    "OK");
-                return;
-            }*/
-            string path = EditorUtility.OpenFilePanel("Overwrite with txt", "", "txt");
+            string path = EditorUtility.OpenFilePanel("Overwrite with txt", "", "");
             if (path.Length != 0)
             {
-                //var fileContent = File.ReadAllBytes(path);
                 string fileContent = File.ReadAllText(path);
-                Debug.Log(fileContent);
                 searchField.text = fileContent;
             }
 
