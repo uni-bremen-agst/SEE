3742dc36237e3357e66c25a252af4cf10486b570
koschke@uni-bremen.de <koschke@uni-bremen.de>
2024-06-12 19:11:45 +0200

    #723 AddMetrics moved to the top of the file. Removed unused methods.

--- Assets/SEE/GraphProviders/VCSMetrics.cs
+++ Assets/SEE/GraphProviders/VCSMetrics.cs
@@ -12,152 +12,6 @@ namespace SEE.GraphProviders
     public static class VCSMetrics
     {
         /// <summary>
-        /// Calculates the number of lines of code added and deleted for each file changed
-        /// between two commits and adds them as metrics to <paramref name="graph"/>.
-        /// <param name="graph">an existing graph where to add the metrics</param>
-        /// <param name="vcsPath">the path to the VCS containing the two revisions to be compared</param>
-        /// <param name="oldCommit">the older commit that constitutes the baseline of the comparison</param>
-        /// <param name="newCommit">the newer commit against which the <paramref name="oldCommit"/> is
-        /// to be compared</param>
-        public static void AddLineofCodeChurnMetric(Graph graph, string vcsPath, Commit oldCommit, Commit newCommit)
-        {
-            using Repository repo = new(vcsPath);
-            AddLinesOfCodeChurnMetric(graph, repo, oldCommit, newCommit);
-        }
-
-        private static void AddLinesOfCodeChurnMetric(Graph graph, Repository repo, Commit oldCommit, Commit newCommit)
-        {
-            Patch changes = repo.Diff.Compare<Patch>(oldCommit.Tree, newCommit.Tree);
-
-            foreach (PatchEntryChanges change in changes)
-            {
-                foreach (Node node in graph.Nodes())
-                {
-                    if (node.ID.Replace('\\', '/') == change.Path)
-                    {
-                        node.SetInt(DataModel.DG.VCS.LinesAdded, change.LinesAdded);
-                        node.SetInt(DataModel.DG.VCS.LinesDeleted, change.LinesDeleted);
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Calculates the number of unique developers who contributed to each file for each file changed
-        /// between two commits and adds it as a metric to <paramref name="graph"/>.
-        /// </summary>
-        /// <param name="graph">an existing graph where to add the metrics</param>
-        /// <param name="vcsPath">the path to the VCS containing the two revisions to be compared</param>
-        /// <param name="oldCommit">the older commit that constitutes the baseline of the comparison</param>
-        /// <param name="newCommit">the newer commit against which the <paramref name="oldCommit"/> is
-        /// to be compared</param>
-        public static void AddNumberofDevelopersMetric(Graph graph, string vcsPath, Commit oldCommit, Commit newCommit)
-        {
-            using Repository repo = new(vcsPath);
-            AddNumberOfDevelopersMetric(graph, repo, oldCommit, newCommit);
-        }
-
-        private static void AddNumberOfDevelopersMetric(Graph graph, Repository repo, Commit oldCommit, Commit newCommit)
-        {
-            ICommitLog commits = repo.Commits.QueryBy(new CommitFilter { SortBy = CommitSortStrategies.Topological });
-
-            Dictionary<string, HashSet<string>> uniqueContributorsPerFile = new();
-
-            foreach (Commit commit in commits)
-            {
-                if (commit.Author.When >= oldCommit.Author.When && commit.Author.When <= newCommit.Author.When)
-                {
-                    foreach (Commit parent in commit.Parents)
-                    {
-                        Patch changes = repo.Diff.Compare<Patch>(parent.Tree, commit.Tree);
-
-                        foreach (PatchEntryChanges change in changes)
-                        {
-                            string filePath = change.Path;
-                            string id = commit.Author.Email;
-
-                            if (!uniqueContributorsPerFile.ContainsKey(filePath))
-                            {
-                                uniqueContributorsPerFile[filePath] = new HashSet<string>();
-                            }
-                            uniqueContributorsPerFile[filePath].Add(id);
-                        }
-                    }
-                }
-            }
-
-            foreach (KeyValuePair<string, HashSet<string>> entry in uniqueContributorsPerFile)
-            {
-                foreach (Node node in graph.Nodes())
-                {
-                    if (node.ID.Replace('\\', '/') == entry.Key)
-                    {
-                        node.SetInt(DataModel.DG.VCS.NumberOfDevelopers, entry.Value.Count);
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Calculates the number of times each file was changed for each file changed between
-        /// two commits and adds it as a metric to <paramref name="graph"/>.
-        /// </summary>
-        /// <param name="graph">an existing graph where to add the metrics</param>
-        /// <param name="vcsPath">the path to the VCS containing the two revisions to be compared</param>
-        /// <param name="oldCommit">the older commit that constitutes the baseline of the comparison</param>
-        /// <param name="newCommit">the newer commit against which the <paramref name="oldCommit"/> is
-        /// to be compared</param>
-        public static void AddCommitFrequencyMetric(Graph graph, string vcsPath, Commit oldCommit, Commit newCommit)
-        {
-            using Repository repo = new(vcsPath);
-            AddCommitFrequencyMetric(graph, repo, oldCommit, newCommit);
-        }
-
-        private static void AddCommitFrequencyMetric(Graph graph, Repository repo, Commit oldCommit, Commit newCommit)
-        {
-            ICommitLog commitsBetween = repo.Commits.QueryBy(new CommitFilter
-            {
-                IncludeReachableFrom = newCommit,
-                ExcludeReachableFrom = oldCommit
-            });
-
-            Dictionary<string, int> fileCommitCounts = new();
-
-            foreach (Commit commit in commitsBetween)
-            {
-                foreach (Commit parent in commit.Parents)
-                {
-                    TreeChanges changes = repo.Diff.Compare<TreeChanges>(parent.Tree, commit.Tree);
-
-                    foreach (TreeEntryChanges change in changes)
-                    {
-                        string filePath = change.Path;
-
-                        if (fileCommitCounts.ContainsKey(filePath))
-                        {
-                            fileCommitCounts[filePath]++;
-                        }
-                        else
-                        {
-                            fileCommitCounts.Add(filePath, 1);
-                        }
-                    }
-                }
-            }
-
-            foreach (KeyValuePair<string, int> entry in fileCommitCounts.OrderByDescending(x => x.Value))
-            {
-                foreach (Node node in graph.Nodes())
-                {
-                    if (node.ID.Replace('\\', '/') == entry.Key)
-                    {
-                        node.SetInt(DataModel.DG.VCS.CommitFrequency, entry.Value);
-                    }
-                }
-            }
-        }
-
-        /// <summary>
         /// Adds VCS metrics to all nodes in <paramref name="graph"/> based on the
         /// VCS information derived from <paramref name="repository"/>. The metrics are gathered
         /// in between the <paramref name="oldRevision"/> and <paramref name="newRevision"/>.
@@ -194,7 +48,134 @@ namespace SEE.GraphProviders
 
             AddLinesOfCodeChurnMetric(graph, repository, oldCommit, newCommit);
             AddNumberOfDevelopersMetric(graph, repository, oldCommit, newCommit);
-            AddCommitFrequencyMetric(graph, repository, newCommit, oldCommit);
+            AddCommitFrequencyMetric(graph, repository, oldCommit, newCommit);
+        }
+
+        /// <summary>
+        /// Calculates the number of lines of code added and deleted for each file changed
+        /// between two commits and adds them as metrics to <paramref name="graph"/>.
+        /// <param name="graph">an existing graph where to add the metrics</param>
+        /// <param name="repository">the VCS containing the two revisions to be compared</param>
+        /// <param name="oldCommit">the older commit that constitutes the baseline of the comparison</param>
+        /// <param name="newCommit">the newer commit against which the <paramref name="oldCommit"/> is
+        private static void AddLinesOfCodeChurnMetric(Graph graph, Repository repository, Commit oldCommit, Commit newCommit)
+        {
+            Patch changes = repository.Diff.Compare<Patch>(oldCommit.Tree, newCommit.Tree);
+
+            foreach (PatchEntryChanges change in changes)
+            {
+                foreach (Node node in graph.Nodes())
+                {
+                    if (node.ID.Replace('\\', '/') == change.Path)
+                    {
+                        node.SetInt(DataModel.DG.VCS.LinesAdded, change.LinesAdded);
+                        node.SetInt(DataModel.DG.VCS.LinesDeleted, change.LinesDeleted);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Calculates the number of unique developers who contributed to each file for each file changed
+        /// between two commits and adds it as a metric to <paramref name="graph"/>.
+        /// </summary>
+        /// <param name="graph">an existing graph where to add the metrics</param>
+        /// <param name="repository">the VCS containing the two revisions to be compared</param>
+        /// <param name="oldCommit">the older commit that constitutes the baseline of the comparison</param>
+        /// <param name="newCommit">the newer commit against which the <paramref name="oldCommit"/> is
+        /// to be compared</param>
+        private static void AddNumberOfDevelopersMetric(Graph graph, Repository repository, Commit oldCommit, Commit newCommit)
+        {
+            ICommitLog commits = repository.Commits.QueryBy(new CommitFilter { SortBy = CommitSortStrategies.Topological });
+
+            Dictionary<string, HashSet<string>> uniqueContributorsPerFile = new();
+
+            foreach (Commit commit in commits)
+            {
+                if (commit.Author.When >= oldCommit.Author.When && commit.Author.When <= newCommit.Author.When)
+                {
+                    foreach (Commit parent in commit.Parents)
+                    {
+                        Patch changes = repository.Diff.Compare<Patch>(parent.Tree, commit.Tree);
+
+                        foreach (PatchEntryChanges change in changes)
+                        {
+                            string filePath = change.Path;
+                            string id = commit.Author.Email;
+
+                            if (!uniqueContributorsPerFile.ContainsKey(filePath))
+                            {
+                                uniqueContributorsPerFile[filePath] = new HashSet<string>();
+                            }
+                            uniqueContributorsPerFile[filePath].Add(id);
+                        }
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<string, HashSet<string>> entry in uniqueContributorsPerFile)
+            {
+                foreach (Node node in graph.Nodes())
+                {
+                    if (node.ID.Replace('\\', '/') == entry.Key)
+                    {
+                        node.SetInt(DataModel.DG.VCS.NumberOfDevelopers, entry.Value.Count);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Calculates the number of times each file was changed for each file changed between
+        /// two commits and adds it as a metric to <paramref name="graph"/>.
+        /// </summary>
+        /// <param name="graph">an existing graph where to add the metrics</param>
+        /// <param name="repository">the VCS containing the two revisions to be compared</param>
+        /// <param name="oldCommit">the older commit that constitutes the baseline of the comparison</param>
+        /// <param name="newCommit">the newer commit against which the <paramref name="oldCommit"/> is
+        /// to be compared</param>
+        private static void AddCommitFrequencyMetric(Graph graph, Repository repository, Commit oldCommit, Commit newCommit)
+        {
+            ICommitLog commitsBetween = repository.Commits.QueryBy(new CommitFilter
+            {
+                IncludeReachableFrom = newCommit,
+                ExcludeReachableFrom = oldCommit
+            });
+
+            Dictionary<string, int> fileCommitCounts = new();
+
+            foreach (Commit commit in commitsBetween)
+            {
+                foreach (Commit parent in commit.Parents)
+                {
+                    TreeChanges changes = repository.Diff.Compare<TreeChanges>(parent.Tree, commit.Tree);
+
+                    foreach (TreeEntryChanges change in changes)
+                    {
+                        string filePath = change.Path;
+
+                        if (fileCommitCounts.ContainsKey(filePath))
+                        {
+                            fileCommitCounts[filePath]++;
+                        }
+                        else
+                        {
+                            fileCommitCounts.Add(filePath, 1);
+                        }
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<string, int> entry in fileCommitCounts.OrderByDescending(x => x.Value))
+            {
+                foreach (Node node in graph.Nodes())
+                {
+                    if (node.ID.Replace('\\', '/') == entry.Key)
+                    {
+                        node.SetInt(DataModel.DG.VCS.CommitFrequency, entry.Value);
+                    }
+                }
+            }
         }
     }
 }
