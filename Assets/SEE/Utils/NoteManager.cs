84c21e1ad1ff06ff10fcb95c50bd3ff7265496c1
Tim Dinh <timdinh1@uni-bremen.de>
2024-06-13 16:11:15 +0200

    #717 Notes are now saved only in one instance

---
+++ Assets/SEE/Utils/NoteManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SEE.Utils
+{
+    public class NoteManager : MonoBehaviour
+    {
+        private static NoteManager _instance;
+
+        public static NoteManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject("NoteManager");
+                    _instance = go.AddComponent<NoteManager>();
+                    DontDestroyOnLoad(go);
+                }
+                return _instance;
+            }
+        }
+
+        private Dictionary<string, string> notesDictionary = new Dictionary<string, string>();
+
+        public void SaveNote(string title, string content)
+        {
+            if (!string.IsNullOrEmpty(title))
+            {
+                notesDictionary[title] = content;
+            }
+        }
+
+        public string LoadNoteContent(string title)
+        {
+            if (notesDictionary.ContainsKey(title))
+            {
+                return notesDictionary[title];
+            }
+            return "";
+        }
+    }
+}
