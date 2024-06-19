044da4a188bfa8f758aad52ea156615377b4f200
Rainer Koschke <koschke@uni-bremen.de>
2024-06-19 18:14:29 +0200

    Reordered declarations more semantically. Renamed async method. Fixed typo in comment.

--- Assets/SEE/GraphProviders/Evolution/GXLEvolutionGraphProvider.cs
+++ Assets/SEE/GraphProviders/Evolution/GXLEvolutionGraphProvider.cs
@@ -18,18 +18,6 @@ namespace SEE.GraphProviders
     /// </summary>
     public class GXLEvolutionGraphProvider : MultiGraphProvider
     {
-        private const string evolutionFoldoutGroup = "Evolution settings";
-
-        /// <summary>
-        /// Label of attribute <see cref="MaxRevisionsToLoad"/> in the configuration file.
-        /// </summary>
-        private const string maxRevisionsToLoadLabel = "MaxRevisionsToLoad";
-
-        /// <summary>
-        /// Label of attribute <see cref="GXLDirectory"/> in the configuration file.
-        /// </summary>
-        private const string gxlDirectoryLabel = "GXLDirectory";
-
         /// <summary>
         /// The directory where the GXL file are stored
         /// </summary>
@@ -41,7 +29,7 @@ namespace SEE.GraphProviders
         /// </summary>
         [SerializeField, ShowInInspector, Tooltip("Maximum number of revisions to load."),
          FoldoutGroup(evolutionFoldoutGroup), RuntimeTab(evolutionFoldoutGroup)]
-        public int MaxRevisionsToLoad = 500; // serialized by Unity
+        public int MaxRevisionsToLoad = 500;
 
         /// <summary>
         /// Provides an evolution graph series for gxl files
@@ -54,14 +42,14 @@ namespace SEE.GraphProviders
         public override UniTask<List<Graph>> ProvideAsync(List<Graph> graphs, AbstractSEECity city,
             Action<float> changePercentage = null,
             CancellationToken token = default) =>
-            LoadGraph(city);
+            LoadGraphAsync(city);
 
         /// <summary>
         /// Loads the actual graph series from the gxl files in <see cref="GXLDirectory"/>
         /// </summary>
         /// <param name="city">The city where the evolution should be displayed</param>
         /// <returns>The graph series generated from the gxl files</returns>
-        private async UniTask<List<Graph>> LoadGraph(AbstractSEECity city)
+        private async UniTask<List<Graph>> LoadGraphAsync(AbstractSEECity city)
         {
             GraphsReader reader = new();
             await reader.LoadAsync(GXLDirectory.Path, city.HierarchicalEdges, basePath: city.SourceCodeDirectory.Path,
@@ -77,6 +65,22 @@ namespace SEE.GraphProviders
             => MultiGraphProviderKind.GXLEvolution;
 
         /// <summary>
+        /// Name of the foldout group for the evolution settings.
+        /// </summary>
+        private const string evolutionFoldoutGroup = "Evolution settings";
+
+        #region Config I/O
+        /// <summary>
+        /// Label of attribute <see cref="MaxRevisionsToLoad"/> in the configuration file.
+        /// </summary>
+        private const string maxRevisionsToLoadLabel = "MaxRevisionsToLoad";
+
+        /// <summary>
+        /// Label of attribute <see cref="GXLDirectory"/> in the configuration file.
+        /// </summary>
+        private const string gxlDirectoryLabel = "GXLDirectory";
+
+        /// <summary>
         /// Saves the attributes of this provider
         /// </summary>
         /// <param name="writer">The writer to where the attributes should be saved</param>
@@ -87,7 +91,7 @@ namespace SEE.GraphProviders
         }
 
         /// <summary>
-        /// Restors the attributes of this provider
+        /// Restores the attributes of this provider
         /// </summary>
         /// <param name="attributes">The attributes to restore</param>
         protected override void RestoreAttributes(Dictionary<string, object> attributes)
@@ -95,5 +99,7 @@ namespace SEE.GraphProviders
             GXLDirectory.Restore(attributes, gxlDirectoryLabel);
             ConfigIO.Restore(attributes, maxRevisionsToLoadLabel, ref MaxRevisionsToLoad);
         }
+
+        #endregion
     }
 }
