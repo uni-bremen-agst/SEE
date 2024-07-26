83736034cd7d32d669ada2b02f1bb26b506a22d4
Rainer Koschke <koschke@uni-bremen.de>
2024-07-26 17:58:06 +0200

    Moved class ColorPresetList to a file of its own.

--- Assets/SEE/UI/Drawable/ColorPresetsManager.cs
+++ Assets/SEE/UI/Drawable/ColorPresetsManager.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace SEE.UI.Drawable
 {
@@ -34,86 +33,6 @@ namespace SEE.UI.Drawable
                 presets.Add(listId, preset);
             }
             return preset;
-        }
-    }
-
-    /// <summary>
-    /// This class holds the colors for the preset boxes.
-    /// </summary>
-    public class ColorPresetList
-    {
-        /// <summary>
-        /// The list name.
-        /// </summary>
-        public string ListId { get; private set; }
-
-        /// <summary>
-        /// The colors of the preset boxes.
-        /// </summary>
-        public List<Color> Colors { get; private set; }
-
-        /// <summary>
-        /// The action that will be called when the list were updated.
-        /// </summary>
-        public event UnityAction<List<Color>> OnColorsUpdated;
-
-        /// <summary>
-        /// The constructor of ColorPresetList.
-        /// </summary>
-        /// <param name="listId">is the id/name of the list.</param>
-        /// <param name="colors">are the colors that the list holds.</param>
-        public ColorPresetList(string listId, List<Color> colors = null)
-        {
-            if (colors == null)
-            {
-                colors = new List<Color>();
-            }
-
-            Colors = colors;
-            ListId = listId;
-        }
-
-        /// <summary>
-        /// Adds a new color to the list and updates the UI.
-        /// </summary>
-        /// <param name="color">is the color that should be added.</param>
-        public void AddColor(Color color)
-        {
-            Colors.Add(color);
-            OnColorsUpdated?.Invoke(Colors);
-        }
-
-        /// <summary>
-        /// Removes the given color from the list.
-        /// </summary>
-        /// <param name="color">is the color that should be removed.</param>
-        public void RemoveColor(Color color)
-        {
-            Colors.Remove(color);
-            OnColorsUpdated?.Invoke(Colors);
-        }
-
-        /// <summary>
-        /// This method change the color of an already existing color preset.
-        /// </summary>
-        /// <param name="index">is the index of the color preset that should be changed.</param>
-        /// <param name="newColor">is the new color for the color preset.</param>
-        public void ChangeColor(int index, Color newColor)
-        {
-            Colors[index] = newColor;
-            OnColorsUpdated?.Invoke(Colors);
-        }
-
-        /// <summary>
-        /// Updates the list.
-        /// </summary>
-        /// <param name="colors">are the new colors for the list.</param>
-        public void UpdateList(IEnumerable<Color> colors)
-        {
-            Colors.Clear();
-            Colors.AddRange(colors);
-
-            OnColorsUpdated?.Invoke(Colors);
         }
     }
 }
