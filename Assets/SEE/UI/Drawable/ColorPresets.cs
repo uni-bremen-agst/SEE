f88412462b61f7e1a41426119cd0f75e4c112b7e
Rainer Koschke <koschke@uni-bremen.de>
2024-07-26 17:51:09 +0200

    The name of a public fields must start with a capital letter. Added private for Awake() and Start(). Fields should not start with a _.

--- Assets/SEE/UI/Drawable/ColorPresets.cs
+++ Assets/SEE/UI/Drawable/ColorPresets.cs
@@ -2,16 +2,17 @@
 using SEE.Game.Drawable;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace SEE.UI.Drawable
 {
     /// <summary>
-    /// This class holds the presets and a image button to create new presets.
-    /// 
+    /// This class holds the presets and an image button to create new presets.
+    ///
     /// The file comes from the HSVPicker package.
     /// https://github.com/judah4/HSV-Color-Picker-Unity/blob/master/Packages/com.judahperez.hsvcolorpicker/UI/ColorPresets.cs
-    /// It has been extended to include the functionality of removing and modifying colors, 
+    /// It has been extended to include the functionality of removing and modifying colors,
     /// as well as loading and saving colors from the previous session.
     /// </summary>
     public class ColorPresets : MonoBehaviour
@@ -19,54 +20,57 @@ namespace SEE.UI.Drawable
         /// <summary>
         /// The picker will be needed for update the selected color to the HSVPicker.
         /// </summary>
-        public ColorPicker picker;
+        [FormerlySerializedAs("picker")]
+        public ColorPicker Picker;
 
         /// <summary>
         /// This array holds the GameObjects for the up to eleven presets.
         /// </summary>
-        public GameObject[] presets;
+        [FormerlySerializedAs("presets")]
+        public GameObject[] Presets;
 
         /// <summary>
         /// Is used as a button, to create new color presets.
         /// </summary>
-        public Image createPresetImage;
+        [FormerlySerializedAs("createPresetImage")]
+        public Image CreatePresetImage;
 
         /// <summary>
         /// Is the used ColorPresetList.
         /// </summary>
-        private ColorPresetList _colors;
+        private ColorPresetList colors;
 
         /// <summary>
         /// Adds a new onValueChangs Listener to the picker.
         /// </summary>
-        void Awake()
+        private void Awake()
         {
-            picker.onValueChanged.AddListener(ColorChanged);
+            Picker.onValueChanged.AddListener(ColorChanged);
         }
 
         /// <summary>
-        /// Loads the colors from the file, if the file exists. 
+        /// Loads the colors from the file, if the file exists.
         /// Otherwise it loads the default colors.
         /// It adds the OnColorsUpdate method to the ColorPresetList action onColorsUpdated.
         /// Then this method will be called with the given colors.
         /// </summary>
-        void Start()
+        private void Start()
         {
             if (ColorPresetsConfigManager.IsFileExists())
             {
-                _colors = ColorPresetManager.Get(ColorPresetsConfigManager.LoadColors().Colors);
+                colors = ColorPresetManager.Get(ColorPresetsConfigManager.LoadColors().Colors);
             }
             else
             {
-                _colors = ColorPresetManager.Get(picker.Setup.DefaultPresetColors, "default");
+                colors = ColorPresetManager.Get(Picker.Setup.DefaultPresetColors, "default");
             }
 
-            _colors.OnColorsUpdated += OnColorsUpdate;
-            OnColorsUpdate(_colors.Colors);
+            colors.OnColorsUpdated += OnColorsUpdate;
+            OnColorsUpdate(colors.Colors);
         }
 
         /// <summary>
-        /// Assigns each color element from the <paramref name="colors"/> to a preset box. 
+        /// Assigns each color element from the <paramref name="colors"/> to a preset box.
         /// The respective preset box is made active and is assigned the respective color.
         /// Once the eleventh preset box has been assigned, the Create button will be hidden.
         /// In addition, each time this method is called, the list of color presets is saved to a file.
@@ -74,21 +78,21 @@ namespace SEE.UI.Drawable
         /// <param name="colors">are the passed list of colors.</param>
         private void OnColorsUpdate(List<Color> colors)
         {
-            for (int cnt = 0; cnt < presets.Length; cnt++)
+            for (int cnt = 0; cnt < Presets.Length; cnt++)
             {
                 /// Disables the unassigned presets.
                 if (colors.Count <= cnt)
                 {
-                    presets[cnt].SetActive(false);
+                    Presets[cnt].SetActive(false);
                     continue;
                 }
                 /// Enables the assigned preset and assigns colors to them.
-                presets[cnt].SetActive(true);
-                presets[cnt].GetComponent<Image>().color = colors[cnt];
+                Presets[cnt].SetActive(true);
+                Presets[cnt].GetComponent<Image>().color = colors[cnt];
 
             }
             /// Enables the button for adding a color, or disables it if all eleven preset slots are filled.
-            createPresetImage.gameObject.SetActive(colors.Count < presets.Length);
+            CreatePresetImage.gameObject.SetActive(colors.Count < Presets.Length);
 
             /// Saves the presets.
             ColorPresetsConfigManager.SaveColors(colors.ToArray());
@@ -100,7 +104,7 @@ namespace SEE.UI.Drawable
         /// </summary>
         public void CreatePresetButton()
         {
-            _colors.AddColor(picker.CurrentColor);
+            colors.AddColor(Picker.CurrentColor);
         }
 
         /// <summary>
@@ -109,7 +113,7 @@ namespace SEE.UI.Drawable
         /// </summary>
         public void PresetSelect(Image sender)
         {
-            picker.CurrentColor = sender.color;
+            Picker.CurrentColor = sender.color;
         }
 
         /// <summary>
@@ -118,7 +122,7 @@ namespace SEE.UI.Drawable
         /// </summary>
         private void ColorChanged(Color color)
         {
-            createPresetImage.color = color;
+            CreatePresetImage.color = color;
         }
 
         /// <summary>
@@ -126,7 +130,7 @@ namespace SEE.UI.Drawable
         /// </summary>
         public void RemoveColor(Image sender)
         {
-            _colors.RemoveColor(sender.color);
+            colors.RemoveColor(sender.color);
         }
 
         /// <summary>
@@ -134,7 +138,7 @@ namespace SEE.UI.Drawable
         /// </summary>
         public void ChangeColor(int index)
         {
-            _colors.ChangeColor(index, picker.CurrentColor);
+            colors.ChangeColor(index, Picker.CurrentColor);
         }
     }
 }
