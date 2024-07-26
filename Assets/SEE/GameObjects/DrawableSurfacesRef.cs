9655b9d4b8441de8a589fc12ba53aee835bf3ed7
Rainer Koschke <koschke@uni-bremen.de>
2024-07-26 17:38:25 +0200

    Can use default property.

--- Assets/SEE/GameObjects/DrawableSurfacesRef.cs
+++ Assets/SEE/GameObjects/DrawableSurfacesRef.cs
@@ -1,31 +1,25 @@
-﻿using Assets.SEE.UI.Window.DrawableManagerWindow;
-using SEE.Game;
+﻿using SEE.Game;
 using UnityEngine;
 
 namespace SEE.GO
 {
     /// <summary>
-    /// Class that provides a list of all drawable surfaces in the scene. 
+    /// Class that provides a list of all drawable surfaces in the scene.
     /// This is needed for the <see cref="DrawableManagerWindow"> to detect and record newly added or removed surfaces.
     /// </summary>
     public class DrawableSurfacesRef : MonoBehaviour
     {
         /// <summary>
-        /// The list of all surfaces in the scene.
-        /// </summary>
-        private readonly DrawableSurfaces surfacesInScene;
-
-        /// <summary>
         /// Getter property for the list.
         /// </summary>
-        public DrawableSurfaces SurfacesInScene { get { return surfacesInScene; } }
+        public DrawableSurfaces SurfacesInScene { get; private set; }
 
         /// <summary>
         /// Constructor.
         /// </summary>
         public DrawableSurfacesRef()
         {
-            surfacesInScene = new DrawableSurfaces();
+            SurfacesInScene = new DrawableSurfaces();
         }
     }
 }
