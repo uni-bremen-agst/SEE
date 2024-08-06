613c3330f154630e044c5570bee119647cb7a5e7
Rainer Koschke <koschke@uni-bremen.de>
2024-08-06 08:08:49 +0200

    Updated Odin Inspector and Serializer and Odin Validator to version 3.3.1.5.

--- Assets/Plugins/Sirenix/Odin Validator/EnsureCorrectOdinVersion.cs
+++ Assets/Plugins/Sirenix/Odin Validator/EnsureCorrectOdinVersion.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd0644d28893b3e659715ea49bc9411d471520d559148a90c9f076f2079aab6c
+oid sha256:82ff20ef8dacf09e3a5a6f4d4a23c588449ad09a88caedfca000bc91ec02dfe7
 size 7465
