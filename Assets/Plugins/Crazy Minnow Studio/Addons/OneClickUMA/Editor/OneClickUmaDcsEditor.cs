3cf22abb273788dd4e8c30199eb6d48e4ef1f002
Rainer Koschke <koschke@uni-bremen.de>
2024-08-28 08:40:30 +0200

    #766 Removed the addon OneClickUMA by Crazy Minnow Studio (SALSA LipSync).

--- Assets/Plugins/Crazy Minnow Studio/Addons/OneClickUMA/Editor/OneClickUmaDcsEditor.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:07753199f6cde5dece17a1b9c15033acbebe191bf956378e869b08b5eda5fae0
-size 3355
--- Assets/Plugins/Crazy Minnow Studio/Addons/OneClickUMA/OneClickUmaDcsEyes.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e25454d574322a43bfb5cd5eb6a815d8354a7890459b24bf1a4fe2b594740082
-size 4121
