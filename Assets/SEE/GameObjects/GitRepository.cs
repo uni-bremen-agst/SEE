c2f2999dab3e482cabf630f739e21325a955b81e
Rainer Koschke <koschke@uni-bremen.de>
2024-09-15 18:23:26 +0200

    #726 Split long lines. Constant can be private. Non-public constants must start with a lower letter.

--- Assets/SEE/GameObjects/GitRepository.cs
+++ Assets/SEE/GameObjects/GitRepository.cs
@@ -18,20 +18,24 @@ namespace SEE.GraphProviders
         /// <summary>
         /// Used for the tab name in runtime config menu.
         /// </summary>
-        protected const string GraphProviderFoldoutGroup = "Data";
+        private const string graphProviderFoldoutGroup = "Data";
 
         /// <summary>
         /// The path to the git repository.
         /// </summary>
-        [ShowInInspector, Tooltip("Path to the git repository."), HideReferenceObjectPicker, RuntimeTab(GraphProviderFoldoutGroup)]
+        [ShowInInspector, Tooltip("Path to the git repository."), HideReferenceObjectPicker,
+            RuntimeTab(graphProviderFoldoutGroup)]
         public DataPath RepositoryPath = new();
 
         /// <summary>
-        /// The List of filetypes that get included/excluded.
+        /// The list of file globbings for file inclusion/exclusion.
+        /// The key is the globbing pattern and the value is the inclusion status.
+        /// If the latter is true, the pattern is included, otherwise it is excluded.
         /// </summary>
         [OdinSerialize]
         [ShowInInspector, ListDrawerSettings(ShowItemCount = true),
-         Tooltip("Paths and their inclusion/exclusion status."), RuntimeTab(GraphProviderFoldoutGroup),
+         Tooltip("Path globbings and whether they are inclusive (true) or exclusive (false)."),
+            RuntimeTab(graphProviderFoldoutGroup),
          HideReferenceObjectPicker]
         public Dictionary<string, bool> PathGlobbing = new()
         {
