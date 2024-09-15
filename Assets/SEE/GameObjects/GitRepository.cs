f483d072ae1dac4dd9eff05e38d6757407ac106c
Rainer Koschke <koschke@uni-bremen.de>
2024-09-15 18:52:49 +0200

    #726 Added config I/O.

--- Assets/SEE/GameObjects/GitRepository.cs
+++ Assets/SEE/GameObjects/GitRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using SEE.Game.City;
 using SEE.UI.RuntimeConfigMenu;
+using SEE.Utils.Config;
 using SEE.Utils.Paths;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
@@ -42,5 +43,46 @@ namespace SEE.GraphProviders
             { "", false }
         };
 
+        #region Config I/O
+
+        /// <summary>
+        /// Label for serializing the <see cref="RepositoryPath"/> field.
+        /// </summary>
+        private const string repositoryPathLabel = "RepositoryPath";
+
+        /// <summary>
+        /// Label for serializing the <see cref="PathGlobbing"/> field.
+        /// </summary>
+        private const string pathGlobbingLabel = "PathGlobbing";
+
+        /// <summary>
+        /// Saves the attributes to the configuration file under the given <paramref name="label"/>
+        /// using <paramref name="writer"/>.
+        /// </summary>
+        /// <param name="writer">used to write the attributes</param>
+        /// <param name="label">the label under which the attributes are written</param>
+        public void Save(ConfigWriter writer, string label)
+        {
+            writer.BeginGroup(label);
+            writer.Save(PathGlobbing as Dictionary<string, bool>, pathGlobbingLabel);
+            RepositoryPath.Save(writer, repositoryPathLabel);
+            writer.EndGroup();
+        }
+
+        /// <summary>
+        /// Restores the marker values from the given <paramref name="attributes"/> looked up
+        /// under the given <paramref name="label"/>
+        /// </summary>
+        public void Restore(Dictionary<string, object> attributes, string label)
+        {
+            if (attributes.TryGetValue(label, out object dictionary))
+            {
+                Dictionary<string, object> values = dictionary as Dictionary<string, object>;
+                ConfigIO.Restore(values, pathGlobbingLabel, ref PathGlobbing);
+                RepositoryPath.Restore(values, repositoryPathLabel);
+            }
+        }
+
     }
+    #endregion
 }
