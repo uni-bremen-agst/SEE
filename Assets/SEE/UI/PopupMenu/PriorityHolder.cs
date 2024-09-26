5282f588c4f5f47750ea1df22dae035cce746904
Rainer Koschke <koschke@uni-bremen.de>
2024-09-26 11:17:41 +0200

    Added period at the end of a comment. Added default initialization.

--- Assets/SEE/UI/PopupMenu/PriorityHolder.cs
+++ Assets/SEE/UI/PopupMenu/PriorityHolder.cs
@@ -3,13 +3,13 @@
 namespace SEE.UI.PopupMenu
 {
     /// <summary>
-    /// Component which holds the priority of a <see cref="PopupMenuEntry"/>
+    /// Component which holds the priority of a <see cref="PopupMenuEntry"/>.
     /// </summary>
     public class PriorityHolder : MonoBehaviour
     {
         /// <summary>
         /// The priority of a <see cref="PopupMenuEntry"/>.
         /// </summary>
-        public int Priority { get; set; }
+        public int Priority { get; set; } = 0;
     }
 }
