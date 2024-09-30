9ac2dd61f805a472facb20ae79468010b8c3ecdc
Rainer Koschke <koschke@uni-bremen.de>
2024-09-30 13:28:17 +0200

    Bug fix provided by Kronnect for Highlight Plus 21.0. This patch fixes the problem with drawing when HighlightPlus.QualityLevel.Highest is used for the outer glow quality.
    The fix was provided to us by the developer through a personal channel.

--- Assets/Plugins/HighlightPlus/Scripts/HighlightEffect.cs
+++ Assets/Plugins/HighlightPlus/Scripts/HighlightEffect.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:617db04c1dcba956c9a1921d7cc2a82405850482ff934965ae39bf5a0a454450
-size 159991
+oid sha256:2aec37da9d37d02e0943ef17d275ac262551bd8d2227fa193a54d7e1e6fc9e6d
+size 160090
