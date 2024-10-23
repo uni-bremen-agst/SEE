583bc0989bc9c7e4cf94e3c30c74094fe5b3eb1c
Michel Krause <krausem@uni-bremen.de>
2024-10-23 15:28:20 +0200

    Adding a component for selecting and adding a city, along with a corresponding dialog. The use of strings for type selection will be improved.

---
+++ Assets/SEE/GameObjects/CitySelectionManager.cs
@@ -0,0 +1,80 @@
+﻿using SEE.Controls;
+using SEE.UI.Notification;
+using SEE.UI.PropertyDialog;
+using SEE.Utils;
+using UnityEngine;
+
+namespace SEE.GameObjects
+{
+    /// <summary>
+    /// Component for selecting a city to be added to the table.
+    /// </summary>
+    public class CitySelectionManager : MonoBehaviour
+    {
+        /// <summary>
+        /// The progress states for selecting and adding a city to a table.
+        /// </summary>
+        private enum ProgressState
+        {
+            Start,
+            ChoseCity,
+        }
+
+        /// <summary>
+        /// The current progress state.
+        /// </summary>
+        private ProgressState progressState = ProgressState.Start;
+
+        /// <summary>
+        /// The dialog for selecting a city type.
+        /// </summary>
+        private readonly CitySelectionProperty citySelectionProperty = new();
+
+        /// <summary>
+        /// Provides the selection and addition of a city.
+        /// </summary>
+        void Update()
+        {
+            switch (progressState)
+            {
+                case ProgressState.Start:
+                    if (SEEInput.Select()
+                        && Raycasting.RaycastAnything(out RaycastHit raycastHit)
+                        && raycastHit.collider.gameObject == gameObject)
+                    {
+                        citySelectionProperty.Open();
+                        progressState = ProgressState.ChoseCity;
+                    }
+                    break;
+                case ProgressState.ChoseCity:
+                    if (citySelectionProperty.TryGetCity(out string cityType))
+                    {
+                        switch(cityType)
+                        {
+                            case "ReflexionCity":
+                            case "CodeCity":
+                            case "DiffCity":
+                            case "EvolutionCity":
+                            case "BranchCity":
+                            case "DynamicCity":
+                                ShowNotification.Info("City selected", $"{cityType} was selected");
+                                progressState = ProgressState.Start;
+                                enabled = false;
+                                break;
+                            default:
+                                ShowNotification.Warn("Error", "Error in city selection; The city cannot be added.");
+                                break;
+                        }
+                    }
+                    if (citySelectionProperty.WasCanceled())
+                    {
+                        progressState = ProgressState.Start;
+                    }
+                    break;
+                default:
+                    ShowNotification.Warn("Error", "Error in city selection");
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/SEE/UI/PropertyDialog/CitySelectionProperty.cs
@@ -0,0 +1,85 @@
+﻿using SEE.Controls;
+using SEE.UI.PropertyDialog.HolisticMetrics;
+using SEE.Utils;
+using UnityEngine;
+
+namespace SEE.UI.PropertyDialog
+{
+    /// <summary>
+    /// This class manages the dialog for adding a city.
+    /// </summary>
+    internal class CitySelectionProperty : HolisticMetricsDialog
+    {
+        /// <summary>
+        /// The city which the player selected.
+        /// </summary>
+        private static string city;
+
+        /// <summary>
+        /// This input field lets the player pick a city type.
+        /// </summary>
+        private SelectionProperty selectedCity;
+
+        /// <summary>
+        /// This method instantiates the dialog and then displays it to the player.
+        /// </summary>
+        public void Open()
+        {
+            Dialog = new GameObject("Add city dialog");
+            PropertyGroup group = Dialog.AddComponent<PropertyGroup>();
+            group.Name = "Add city dialog";
+
+            string[] cities = { "ReflexionCity", "CodeCity", "DiffCity", "EvolutionCity", "BranchCity", "DynamicCity" };
+
+            selectedCity = Dialog.AddComponent<SelectionProperty>();
+            selectedCity.Name = "Select a city type.";
+            selectedCity.Description = "Select a city type to be added.";
+            selectedCity.AddOptions(cities);
+            selectedCity.Value = cities[0];
+            group.AddProperty(selectedCity);
+
+            /// Adds the property dialog to the dialog.
+            PropertyDialog = Dialog.AddComponent<PropertyDialog>();
+            PropertyDialog.Title = "Select a city type";
+            PropertyDialog.Description = "Select a city type; then hit the OK button.";
+            PropertyDialog.Icon = Resources.Load<Sprite>("Materials/ModernUIPack/Picker");
+            PropertyDialog.AddGroup(group);
+
+            PropertyDialog.OnConfirm.AddListener(OnConfirm);
+            PropertyDialog.OnCancel.AddListener(Cancel);
+
+            SEEInput.KeyboardShortcutsEnabled = false;
+            PropertyDialog.DialogShouldBeShown = true;
+        }
+
+        /// <summary>
+        /// This method gets called when the player confirms the dialog. It will save the selected city type in a
+        /// variable and set <see cref="HolisticMetricsDialog.GotInput"/> to true.
+        /// </summary>
+        private void OnConfirm()
+        {
+            SEEInput.KeyboardShortcutsEnabled = true;
+            city = selectedCity.Value;
+            GotInput = true;
+            Destroyer.Destroy(Dialog);
+        }
+
+        /// <summary>
+        /// Fetches the city type given by the player.
+        /// </summary>
+        /// <param name="cityType">If given and not yet fetched, this will be the city type the player selected.
+        /// </param>
+        /// <returns>The value of <see cref="HolisticMetricsDialog.GotInput"/></returns>
+        internal bool TryGetCity(out string cityType)
+        {
+            if (GotInput)
+            {
+                cityType = city;
+                GotInput = false;
+                return true;
+            }
+            cityType = null;
+            return false;
+        }
+    }
+}
