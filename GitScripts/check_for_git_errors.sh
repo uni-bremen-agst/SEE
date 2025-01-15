a5483f120356a15c4d5d037a85188fa954eb0adf
Falko Galperin <falko1@uni-bremen.de>
2025-01-15 20:15:10 +0100

    Speed up local GitScripts runs (pre-commit hooks)

--- GitScripts/check_for_git_errors.sh
+++ GitScripts/check_for_git_errors.sh
@@ -4,12 +4,10 @@
 # For more information on what constitutes a Git error, review the
 # documentation of `git-fsck`.
 
-if [ -n "$CI" ]; then
-    set -x
-else
-    # This check takes too long to execute.
-    # We do not want to run it locally on every commit.
-    exit 0
+if [ -z "$CI" ]; then
+  # This check takes too long to execute.
+  # We do not want to run it locally on every commit.
+  exit 0
 fi
 
 # Dangling errors do not matter.
@@ -17,13 +15,13 @@ GIT_FSCK="git fsck --strict --no-dangling --no-progress"
 
 output=$($GIT_FSCK)
 if [ $? != 0 ]; then
-    echo ""
-    echo -n "::error title=Git error detected::This branch contains at least one Git error, as detected by \`git fsck\`.%0A"
-    echo -n "The fix depends on the specific error type, which is reproduced below."
-    echo -n "You may need to rebase your branch to get rid of the error.%0A"
-    echo -n "Check locally by running \`git fsck --strict --no-dangling\`.%0A"
-    echo -n "If you are not able to identify or fix the error, please contact @koschke or @falko1 on Mattermost.%0A"
-    echo -n "The detected Git errors will be shown below:%0A"
-    echo "$output"
-    exit 1
+  echo ""
+  echo -n "::error title=Git error detected::This branch contains at least one Git error, as detected by \`git fsck\`.%0A"
+  echo -n "The fix depends on the specific error type, which is reproduced below."
+  echo -n "You may need to rebase your branch to get rid of the error.%0A"
+  echo -n "Check locally by running \`git fsck --strict --no-dangling\`.%0A"
+  echo -n "If you are not able to identify or fix the error, please contact @koschke or @falko1 on Mattermost.%0A"
+  echo -n "The detected Git errors will be shown below:%0A"
+  echo "$output"
+  exit 1
 fi
--- GitScripts/check_for_old_commits.sh
+++ GitScripts/check_for_old_commits.sh
@@ -4,23 +4,24 @@
 # This commit will only exist on old histories. If it exists, it must not be pushed,
 # otherwise all of the old history we took care to remove will be present again.
 
-if [ -n "$CI" ]; then
-    set -ex
-else
-    set -e
+set -e
+
+if [ -z "$CI" ]; then
+  # The rebase has been some years away, no need to run this locally.
+  exit 0
 fi
 
-if git --no-replace-objects show -s d5eb0645d999623b72025d0bf19227a787af1fb1 2>/dev/null = 0; then
-    if [ -n "$CI" ]; then
-        echo ""
-        echo -n "::error title=Old history pushed::!!!!! DO NOT MERGE THIS BRANCH INTO MASTER !!!!!%0A"
-        echo -n "You have pushed the old repository history onto the remote repository.%0A"
-        echo -n "Please rebase this branch to make sure only the new history (post 2023-01-01) is present.%0A"
-        echo -n "If you are unsure what to do, please use the ~Entwicklung channel in Mattermost.%0A"
-    else
-        echo "Looks like you have a copy of the repository with a bad commit that should no longer be present."
-        echo "Please save your work to a temporary location and delete this repository."
-        echo "If you create a fresh clone, that should fix this problem."
-    fi
-    exit 1
+if git --no-replace-objects show -s d5eb0645d999623b72025d0bf19227a787af1fb1 = 0 2>/dev/null; then
+  if [ -n "$CI" ]; then
+    echo ""
+    echo -n "::error title=Old history pushed::!!!!! DO NOT MERGE THIS BRANCH INTO MASTER !!!!!%0A"
+    echo -n "You have pushed the old repository history onto the remote repository.%0A"
+    echo -n "Please rebase this branch to make sure only the new history (post 2023-01-01) is present.%0A"
+    echo -n "If you are unsure what to do, please use the ~Entwicklung channel in Mattermost.%0A"
+  else
+    echo "Looks like you have a copy of the repository with a bad commit that should no longer be present."
+    echo "Please save your work to a temporary location and delete this repository."
+    echo "If you create a fresh clone, that should fix this problem."
+  fi
+  exit 1
 fi
