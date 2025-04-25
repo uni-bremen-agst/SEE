fad1ceebe2f7f0b7e55bb8f06c6e995694f4a904
Michel Krause <krausem@uni-bremen.de>
2025-04-25 15:12:12 +0200

    First version of move in modify table action.

--- Assets/SEE/Controls/Actions/Table/ModifyTableAction.cs
+++ Assets/SEE/Controls/Actions/Table/ModifyTableAction.cs
@@ -1,7 +1,15 @@
-﻿using SEE.UI.Menu;
+﻿using SEE.Game;
+using SEE.Game.Drawable;
+using SEE.Game.Table;
+using SEE.GO;
+using SEE.GameObjects;
+using SEE.UI.Menu;
+using SEE.UI.Notification;
+using SEE.Utils;
 using SEE.Utils.History;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreLinq;
 
 namespace SEE.Controls.Actions.Table
 {
@@ -11,6 +19,31 @@ namespace SEE.Controls.Actions.Table
     /// </summary>
     public class ModifyTableAction : AbstractPlayerAction
     {
+        /// <summary>
+        /// The progress states of this action.
+        /// </summary>
+        private enum ProgressState
+        {
+            None,
+            Selection,
+            ChoseOption,
+            Move,
+            Rotate,
+            Scale,
+            Delete,
+            Finish
+        }
+
+        /// <summary>
+        /// The current progress state.
+        /// </summary>
+        private ProgressState currentProgressState = ProgressState.Selection;
+
+        /// <summary>
+        /// The executed operation.
+        /// </summary>
+        private ProgressState executedOperation = ProgressState.None;
+
         /// <summary>
         /// The menu for this action.
         /// </summary>
@@ -22,21 +55,145 @@ namespace SEE.Controls.Actions.Table
         private GameObject modifiedTable;
 
         /// <summary>
-        /// Enables the table menu.
+        /// This dictionary contains the previous state of each city's <see cref="CitySelectionManager"/>.
+        /// </summary>
+        private readonly Dictionary<GameObject, bool> previousCSMState = new();
+
+        /// <summary>
+        /// Saves all the information to revert or repeat this action.
+        /// </summary>
+        private Memento memento;
+
+        /// <summary>
+        /// This struct can store all the information needed to revert or repeat a <see cref="ModifyTableAction"/>
+        /// </summary>
+        private struct Memento
+        {
+            /// <summary>
+            /// The name of the modified table.
+            /// </summary>
+            public string Name;
+
+            /// <summary>
+            /// The old transform data of the table.
+            /// </summary>
+            public TransformData Old;
+
+            /// <summary>
+            /// The new transform data of the table.
+            /// </summary>
+            public TransformData New;
+
+            /// <summary>
+            /// The operation that was executed.
+            /// </summary>
+            public ProgressState Operation;
+
+            /// <summary>
+            /// Indicates whether the new transform data has been set.
+            /// </summary>
+            private bool newDataWasSet;
+
+            /// <summary>
+            /// The relevant transform data (position, euler angles and scale).
+            /// </summary>
+            public struct TransformData
+            {
+                /// <summary>
+                /// The position.
+                /// </summary>
+                public Vector3 Position;
+
+                /// <summary>
+                /// The euler angles.
+                /// </summary>
+                public Vector3 EulerAngles;
+
+                /// <summary>
+                /// The scale.
+                /// </summary>
+                public Vector3 Scale;
+
+                /// <summary>
+                /// The constructor.
+                /// </summary>
+                /// <param name="table">The modified table.</param>
+                public TransformData(GameObject table)
+                {
+                    Transform transform = table.transform;
+                    Position = transform.position;
+                    EulerAngles = transform.eulerAngles;
+                    Scale = transform.localScale;
+                }
+            }
+
+            /// <summary>
+            /// The constructor.
+            /// </summary>
+            /// <param name="table">The modified table.</param>
+            /// <param name="operation">The operation that was executed.</param>
+            public Memento(GameObject table)
+            {
+                Name = table.name;
+                Old = new(table);
+                New = new();
+                Operation = ProgressState.None;
+                newDataWasSet = false;
+            }
+
+            /// <summary>
+            /// Sets the new transform data of the given <paramref name="table"/>
+            /// after the specified <paramref name="operation"/> is executed.
+            /// </summary>
+            /// <param name="table">The modified table.</param>
+            /// <param name="operation">The operation that was executed.</param>
+            public void SetNewData(GameObject table, ProgressState operation)
+            {
+                if (!newDataWasSet)
+                {
+                    newDataWasSet = true;
+                    New = new(table);
+                    Operation = operation;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disables and saves the current state of each city's <see cref="CitySelectionManager"/> component.
         /// </summary>
         public override void Awake()
         {
             base.Awake();
-            menu = new();
+            if (LocalPlayer.TryGetCitiesHolder(out CitiesHolder citiesHolder))
+            {
+                citiesHolder.Cities.Values.ForEach(city =>
+                {
+                    previousCSMState.Add(city, city.GetComponent<CitySelectionManager>().enabled);
+                    city.GetComponent<CitySelectionManager>().enabled = false;
+                });
+            }
         }
 
         /// <summary>
-        /// Destroys the table menu.
+        /// Stops the action and restores the previous state of each city's <see cref="CitySelectionManager"/>.
+        /// Also destroys the menu and the <see cref="CollisionDetectionManager"/> component, if present.
         /// </summary>
         public override void Stop()
         {
             base.Stop();
-            menu.Destroy();
+            if (menu != null)
+            {
+                menu.Destroy();
+            }
+            if (modifiedTable != null)
+            {
+                Destroyer.Destroy(modifiedTable.GetComponent<CollisionDetectionManager>());
+            }
+            if (LocalPlayer.TryGetCitiesHolder(out CitiesHolder citiesHolder))
+            {
+                previousCSMState.Keys.ForEach(city =>
+                    city.GetComponent<CitySelectionManager>().enabled = previousCSMState[city]);
+            }
         }
 
         /// <summary>
@@ -45,6 +202,59 @@ namespace SEE.Controls.Actions.Table
         /// <returns></returns>
         public override bool Update()
         {
+            if (!Raycasting.IsMouseOverGUI())
+            {
+                switch (currentProgressState)
+                {
+                    case ProgressState.Selection:
+                        if (SEEInput.LeftMouseDown() && Raycasting.RaycastAnything(out RaycastHit raycastHit)
+                            && (raycastHit.collider.gameObject.CompareTag(Tags.CodeCity)
+                                || GameFinder.HasParentWithTag(raycastHit.collider.gameObject, Tags.CodeCity))
+                            && modifiedTable == null)
+                        {
+                            GameObject raycastObj = raycastHit.collider.gameObject;
+                            GameObject city = raycastObj.CompareTag(Tags.CodeCity) ?
+                                   raycastObj : raycastObj.ContainingCity().gameObject;
+                            modifiedTable = city.transform.parent.gameObject;
+                            modifiedTable.AddComponent<CollisionDetectionManager>();
+                        }
+                        if (modifiedTable != null && SEEInput.MouseUp(MouseButton.Left))
+                        {
+                            currentProgressState = ProgressState.ChoseOption;
+                            menu = new();
+                        }
+                        break;
+                    case ProgressState.ChoseOption:
+                        memento = new(modifiedTable);
+                        currentProgressState = ProgressState.Move;
+                        executedOperation = ProgressState.Move;
+                        break;
+                    case ProgressState.Move:
+                        if (Raycasting.RaycastAnything(out RaycastHit raycast))
+                        {
+                            GameTableManager.Move(modifiedTable, raycast.point);
+                            if (SEEInput.LeftMouseDown())
+                            {
+                                if (modifiedTable.GetComponent<CollisionDetectionManager>().IsInCollision())
+                                {
+                                    ShowNotification.Warn("Table can't be placed",
+                                        "The table can't be placed because it is colliding with another object.");
+                                    return false;
+                                }
+                                currentProgressState = ProgressState.Finish;
+                            }
+                        }
+                        break;
+                    case ProgressState.Rotate:
+                    case ProgressState.Scale:
+                    case ProgressState.Delete:
+                        break;
+                    case ProgressState.Finish:
+                        memento.SetNewData(modifiedTable, executedOperation);
+                        CurrentState = IReversibleAction.Progress.Completed;
+                        return true;
+                }
+            }
             return false;
         }
 
@@ -54,7 +264,11 @@ namespace SEE.Controls.Actions.Table
         public override void Undo()
         {
             base.Undo();
-
+            if (LocalPlayer.TryGetCitiesHolder(out CitiesHolder citiesHolder))
+            {
+                modifiedTable = citiesHolder.FindTable(memento.Name);
+                GameTableManager.Move(modifiedTable, memento.Old.Position);
+            }
         }
 
         /// <summary>
@@ -63,7 +277,11 @@ namespace SEE.Controls.Actions.Table
         public override void Redo()
         {
             base.Redo();
-
+            if (LocalPlayer.TryGetCitiesHolder(out CitiesHolder citiesHolder))
+            {
+                modifiedTable = citiesHolder.FindTable(memento.Name);
+                GameTableManager.Move(modifiedTable, memento.New.Position);
+            }
         }
 
         /// <summary>
