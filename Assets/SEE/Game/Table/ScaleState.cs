4fa6cf0ad2d1a6f48118e3b76f374a0b36ef55b3
Michel Krause <krausem@uni-bremen.de>
2025-05-05 15:35:32 +0200

    Add a scale determiner.

---
+++ Assets/SEE/Game/Table/ScaleState.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace SEE.Game.Table
+{
+    /// <summary>
+    /// The scale states.
+    /// </summary>
+    public enum ScaleState
+    {
+        NotScaled,
+        Increased,
+        Decreased
+    }
+
+    /// <summary>
+    /// Determines the scale state based on a given scale value.
+    /// </summary>
+    public static class ScaleDeterminer
+    {
+        /// <summary>
+        /// Determines the <see cref="ScaleState"/> based on the given <paramref name="scaleValue"/>.
+        /// </summary>
+        /// <param name="scaleValue">The value to evaluate.</param>
+        /// <returns>The corresponding <see cref="ScaleState"/> based on the value of <paramref name="scaleValue"/>.</returns>
+        public static ScaleState DetermineScale(float scaleValue)
+        {
+            if (scaleValue < 1f && !Mathf.Approximately(scaleValue, 1f))
+            {
+                return ScaleState.Increased;
+            }
+            else if (scaleValue > 1f && !Mathf.Approximately(scaleValue, 1f))
+            {
+                return ScaleState.Decreased;
+            }
+            else
+            {
+                return ScaleState.NotScaled;
+            }
+        }
+    }
+}
