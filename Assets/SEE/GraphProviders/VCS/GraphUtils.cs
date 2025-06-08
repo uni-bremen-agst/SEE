d61fecc885ff4c6eb573db3b251b9a01ec273c0c
Rainer Koschke <koschke@uni-bremen.de>
2025-06-08 14:09:34 +0200

    #849 Renamed GraphUtils.GetOrAddNode to GetOrAddFileNode. Better conveys its purpose. Improved documentation.

--- Assets/SEE/GraphProviders/VCS/GraphUtils.cs
+++ Assets/SEE/GraphProviders/VCS/GraphUtils.cs
@@ -48,26 +48,34 @@ namespace SEE.GraphProviders.VCS
         ///
         /// This method will also add all directories in between.
         ///
-        /// Files will have the node type <see cref="fileType"/> and also a Filename and Directory,
-        /// so that the files can be opened in the CodeEditor.
-        /// Directories will have the node type <see cref="directoryType"/>.
+        /// Files will have the node type <see cref="DataModel.DG.VCS.FileType"/>.
+        /// Their <see cref="GraphElement.Filename"/>  and <see cref="GraphElement.Directory"/>
+        /// will be set to the full relative path of the file so that the files can be
+        /// opened in the CodeEditor.
+        ///
+        /// Directories will have the node type <see cref="DataModel.DG.VCS.DirectoryType"/>.
         /// </summary>
-        /// <param name="fullRelativePath">The full relative path of the file this will become the ID of the newly created node.</param>
+        /// <param name="fullRelativePath">The full relative path of the file.
+        /// This will become the ID of the newly created node.</param>
         /// <param name="rootNode">The root node of the repository.</param>
         /// <param name="graph">The graph to add the nodes to.</param>
-        /// <returns>The found file node</returns>
-        public static Node GetOrAddNode(string fullRelativePath, Node rootNode, Graph graph, string idSuffix = "") =>
-            GetOrAddNode(fullRelativePath, fullRelativePath, rootNode, graph, idSuffix: idSuffix);
+        /// <returns>The found or newly create file node</returns>
+        public static Node GetOrAddFileNode(string fullRelativePath, Node rootNode, Graph graph, string idSuffix = "") =>
+            GetOrAddFileNode(fullRelativePath, fullRelativePath, rootNode, graph, idSuffix: idSuffix);
 
         /// <summary>
-        /// The same as <see cref="GetOrAddNode"/> but with the actual logic.
+        /// The same as <see cref="GetOrAddFileNode"/> but with the actual logic.
         /// </summary>
         /// <param name="fullRelativePath">The full relative path of the file.</param>
         /// <param name="path">The root node of the repository.</param>
         /// <param name="parent">The parent of the current node.</param>
         /// <param name="graph">The graph to add the nodes to.</param>
         /// <returns>The newly created or found node.</returns>
-        private static Node GetOrAddNode(string fullRelativePath, string path, Node parent, Graph graph,
+        private static Node GetOrAddFileNode
+            (string fullRelativePath,
+            string path,
+            Node parent,
+            Graph graph,
             string idSuffix = "")
         {
             string[] pathSegments = path.Split(Path.AltDirectorySeparatorChar);
@@ -101,7 +109,7 @@ namespace SEE.GraphProviders.VCS
             {
                 Node dirNode = parent.Children().First(x =>
                     x.ID == parent.ID + Path.AltDirectorySeparatorChar + pathSegments.First() + idSuffix);
-                return GetOrAddNode(fullRelativePath, String.Join(Path.AltDirectorySeparatorChar, pathSegments.Skip(1)),
+                return GetOrAddFileNode(fullRelativePath, String.Join(Path.AltDirectorySeparatorChar, pathSegments.Skip(1)),
                     dirNode, graph, idSuffix: idSuffix);
             }
 
@@ -110,7 +118,7 @@ namespace SEE.GraphProviders.VCS
                 DataModel.DG.VCS.DirectoryType, directoryName);
             addedDirectoryNode.Directory = directoryName;
             parent.AddChild(addedDirectoryNode);
-            return GetOrAddNode(fullRelativePath, String.Join(Path.AltDirectorySeparatorChar, pathSegments.Skip(1)),
+            return GetOrAddFileNode(fullRelativePath, String.Join(Path.AltDirectorySeparatorChar, pathSegments.Skip(1)),
                 addedDirectoryNode, graph, idSuffix: idSuffix);
         }
     }
