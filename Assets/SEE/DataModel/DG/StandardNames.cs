fe4126f78eab49e47ac4273945ded9f8d5c8975c
Rainer Koschke <koschke@uni-bremen.de>
2025-06-09 18:17:52 +0200

    #849 Added new metric names.

--- Assets/SEE/DataModel/DG/StandardNames.cs
+++ Assets/SEE/DataModel/DG/StandardNames.cs
@@ -69,9 +69,24 @@
         public const string Prefix = "Metric.";
 
         /// <summary>
+        /// Prefix for line metrics.
+        /// </summary>
+        public const string Lines = Prefix + "Lines.";
+
+        /// <summary>
         /// Name of lines of code (LOC) metric.
         /// </summary>
-        public const string LOC = Prefix + "LOC";
+        public const string LOC = Lines + "LOC";
+
+        /// <summary>
+        /// Number of comments in the source code.
+        /// </summary>
+        public const string Comments = Lines + "Comments";
+
+        /// <summary>
+        /// Number of tokens in the source code.
+        /// </summary>
+        public const string NumberOfTokens = Prefix + "Number_of_Tokens";
 
         /// <summary>
         /// Name of McCabe complexity metric.
