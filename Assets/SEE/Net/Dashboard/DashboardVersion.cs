95e392c4fd66df3a25c99bf64c4062725e0b0979
Rainer Koschke <koschke@uni-bremen.de>
2025-06-10 16:09:07 +0200

    #849 Updated Dashboard version.

--- Assets/SEE/Net/Dashboard/DashboardVersion.cs
+++ Assets/SEE/Net/Dashboard/DashboardVersion.cs
@@ -35,7 +35,7 @@ namespace SEE.Net.Dashboard
         /// Latest supported version of the Axivion Dashboard.
         /// Should be updated when new (supported and tested) versions come out.
         /// </summary>
-        public static readonly DashboardVersion SupportedVersion = new(7, 8, 0, 15004);
+        public static readonly DashboardVersion SupportedVersion = new(7, 9, 4, 16514);
 
         /// <summary>
         /// Represents the difference of another version in comparison to this one.
