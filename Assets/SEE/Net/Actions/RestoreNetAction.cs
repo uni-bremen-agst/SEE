7cfdc65a8a03f7c58d8e1435f12ee4b70ab3a73a
Rainer Koschke <koschke@uni-bremen.de>
2025-06-23 15:56:26 +0200

    Introduced common superclass for RestoreNetAction and ReviveNetAction. It removes the redundancy.

--- Assets/SEE/Net/Actions/RestoreNetAction.cs
+++ Assets/SEE/Net/Actions/RestoreNetAction.cs
@@ -6,47 +6,47 @@ using System.Collections.Generic;
 namespace SEE.Net.Actions
 {
     /// <summary>
-    /// This class propagates an undo of <see cref="DeleteAction"/> to all clients
-    /// in the network, that is, it restores game objects representing a node
-    /// or edge previously deleted.
+    /// This class re-creates nodes and/or edges previously deleted at all clients
+    /// in the network.
+    ///
+    /// Precondition: The objects to be re-created were in fact previously deleted.
     /// </summary>
-    public class RestoreNetAction : AbstractNetAction
+    /// <remarks>If the objects to be re-created were actually only marked as deleted,
+    ///  use <see cref="ReviveNetAction"/> instead.</remarks>
+    public class RestoreNetAction : RegenerateNetAction
     {
+        // Note: All attributes are made public so that they will be serialized
+        // for the network transfer.
+
         /// <summary>
         /// A serialization of the map of the graph elements
         /// and their corresponding layouts, if applicable.
+        /// The type is assumed to be a list of <see cref="RestoreGraphElement"/>.
         /// </summary>
         public string NodesOrEdges;
-
-        /// <summary>
-        /// A serialization of the map of the node types to be restored.
-        /// </summary>
-        public string NodeTypeList;
 
         /// <summary>
         /// The constructor.
         /// It serializes the maps into strings.
         /// </summary>
-        /// <param name="nodesOrEdges">The deleted graph elements and their corresponding layouts, if applicable.</param>
+        /// <param name="nodesOrEdges">The deleted graph elements and their corresponding
+        /// layouts, if applicable.</param>
         /// <param name="nodeTypes">The deleted node types.</param>
         public RestoreNetAction
             (List<RestoreGraphElement> nodesOrEdges,
              Dictionary<string, VisualNodeAttributes> nodeTypes)
+            : base(nodeTypes)
         {
             NodesOrEdges = RestoreGraphElementListSerializer.Serialize(nodesOrEdges);
-            NodeTypeList = nodeTypes != null && nodeTypes.Count > 0 ?
-                NodeTypesSerializer.Serialize(nodeTypes) : "";
         }
 
         /// <summary>
-        /// Restores the nodes or edges on each client.
+        /// Re-creates the nodes and/or edges at each client.
         /// </summary>
         public override void ExecuteOnClient()
         {
-            Dictionary<string, VisualNodeAttributes> nodeTypes = !string.IsNullOrEmpty(NodeTypeList)?
-                NodeTypesSerializer.Unserialize(NodeTypeList) : new();
-            List<RestoreGraphElement> nodesOrEdges = RestoreGraphElementListSerializer.Unserialize(NodesOrEdges);
-            GameElementDeleter.Restore(nodesOrEdges, nodeTypes);
+            GameElementDeleter.Restore(RestoreGraphElementListSerializer.Unserialize(NodesOrEdges),
+                                       ToMap());
         }
     }
 }
