2cdd0c803d1c8703a25fbde6b145c30d81aedd69
Rainer Koschke <koschke@uni-bremen.de>
2025-07-07 14:54:03 +0200

    It is more general than the use in GitGraphGenerator.

--- Assets/SEE/GraphProviders/VCS/GitGraphGenerator.cs
+++ Assets/SEE/GraphProviders/VCS/GitGraphGenerator.cs
@@ -102,41 +102,6 @@ namespace SEE.GraphProviders.VCS
 
         #endregion Truck Factor
 
-        #region Author Aliasing
-        /// <summary>
-        /// If <paramref name="consultAliasMap"/> is false, the original <paramref name="author"/>
-        /// will be returned.
-        /// Otherwise: Returns the alias of the specified <paramref name="author"/> if it exists in the alias mapping;
-        /// or else returns the original <paramref name="author"/>.
-        ///
-        /// For two <see cref="FileAuthor"/>s to match, they must have same name and email address,
-        /// where the string comparison for both facets is case-insensitive.
-        /// </summary>
-        /// <param name="author">The author whose alias is to be retrieved.</param>
-        /// <param name="consultAliasMap">If <paramref name="authorAliasMap"/> should be consulted at all.</param>
-        /// <param name="authorAliasMap">Where to to look up an alias. Can be null if <paramref name="consultAliasMap"/>
-        /// is false.</param>
-        /// <returns>A <see cref="FileAuthor"/> instance representing the alias of the author if found,
-        /// or the original author if no alias exists or if <paramref name="consultAliasMap"/> is false.</returns>
-        private static FileAuthor GetAuthorAliasIfExists(FileAuthor author, bool consultAliasMap, AuthorMapping authorAliasMap)
-        {
-            // FIXME: Move this code to GitRepository.
-            // If the author is not in the alias map or combining of author aliases is disabled, use the original author
-            return ResolveAuthorAliasIfEnabled(author, consultAliasMap, authorAliasMap) ?? author;
-
-            static FileAuthor ResolveAuthorAliasIfEnabled(FileAuthor author, bool combineSimilarAuthors, AuthorMapping authorAliasMap)
-            {
-                if (!combineSimilarAuthors)
-                {
-                    return null;
-                }
-                return authorAliasMap
-                    .FirstOrDefault(alias => alias.Value.Any(x => String.Equals(x.Email, author.Email, StringComparison.OrdinalIgnoreCase)
-                                                               && String.Equals(x.Name,  author.Name,  StringComparison.OrdinalIgnoreCase))).Key;
-            }
-        }
-        #endregion Author Aliasing
-
         /// <summary>
         /// Adds nodes of type <see cref="DataModel.DG.VCS.FileType"/> and <see cref="DataModel.DG.VCS.DirectoryType"/>
         /// to the <paramref name="graph"/> for the relevant files in the given <paramref name="repository"/>
@@ -331,8 +296,9 @@ namespace SEE.GraphProviders.VCS
 
             HashSet<string> files = new(fileToMetrics.Keys);
 
-            FileAuthor committer = GetAuthorAliasIfExists(new FileAuthor(commit.Author.Name, commit.Author.Email),
-                                                          consultAliasMap, authorAliasMap);
+            FileAuthor committer
+                = GitRepository.GetAuthorAliasIfExists(new FileAuthor(commit.Author.Name, commit.Author.Email),
+                                                       consultAliasMap, authorAliasMap);
 
             foreach (PatchEntryChanges changedFile in patch)
             {
