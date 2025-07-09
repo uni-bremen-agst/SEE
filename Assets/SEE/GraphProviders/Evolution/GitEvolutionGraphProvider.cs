fa423d2aca429622d6263fd7221bf7d9a8867300
Rainer Koschke <koschke@uni-bremen.de>
2025-07-09 11:32:56 +0200

    Applied suggestion by hlkuss during code review.
    Co-authored-by: Hannes Lennart Kuss <102760046+hlkuss@users.noreply.github.com>

--- Assets/SEE/GraphProviders/Evolution/GitEvolutionGraphProvider.cs
+++ Assets/SEE/GraphProviders/Evolution/GitEvolutionGraphProvider.cs
@@ -148,7 +148,7 @@ namespace SEE.GraphProviders.Evolution
             IEnumerable<KeyValuePair<Commit, Patch>> commits
                 = commitChanges.Where(x => x.Value.Any(patch => files.Contains(patch.Path)));
 
-            int íteration = 0;
+            int iteration = 0;
             int commitLength = commits.Count();
 
             foreach (KeyValuePair<Commit, Patch> currentCommit in commits)
@@ -157,7 +157,7 @@ namespace SEE.GraphProviders.Evolution
                 {
                     throw new OperationCanceledException(token);
                 }
-                changePercentage?.Invoke(Mathf.Clamp((float)íteration / commitLength, 0.2f, 0.98f));
+                changePercentage?.Invoke(Mathf.Clamp((float)Ã­teration / commitLength, 0.2f, 0.98f));
 
                 // All commits between the first commit in commitList and the current commit
                 List<Commit> commitsInBetween =
@@ -165,7 +165,7 @@ namespace SEE.GraphProviders.Evolution
 
                 graph.Add(GetGraphOfCommit(repositoryName, currentCommit.Key, commitsInBetween,
                                            commitChanges, files));
-                íteration++;
+                Ã­teration++;
             }
 
             return graph;
