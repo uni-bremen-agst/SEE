a10c1147d03708e88f142f0dd21ebd5f0eda6f32
zama9 <116647782+zama9@users.noreply.github.com>
2025-07-13 20:47:34 +0200

    commite wieder neu

---
+++ Assets/SEE/Controls/Actions/ShowEdgesOfSelectedNode.cs
@@ -0,0 +1,35 @@
+using SEE.Controls;
+using SEE.Controls.Actions;
+using SEE.DataModel.DG;
+using SEE.Game.Operator;
+using SEE.GO;
+using SEE.Tools.ReflexionAnalysis;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ShowEdgesOfSelectedNode : MonoBehaviour
+{
+  NodeRef nodeRef;
+
+
+  public string ShowEdgesHere(InteractableObject obj)
+  {
+    if (obj == null) return "-1";
+
+    obj.TryGetComponent<NodeRef>(out nodeRef);
+    if (nodeRef == null) return "-2";
+
+    Node? node = nodeRef.Value;
+
+    ISet<Edge> edges = node?.Edges;
+
+    string types = "";
+    foreach (Edge edge in edges ?? new HashSet<Edge>())
+    {
+
+      types += edge.State() + " : ";
+    }
+    return types;
+  }
+}
