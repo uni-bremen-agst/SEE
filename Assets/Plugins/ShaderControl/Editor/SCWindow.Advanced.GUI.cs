e369e41af7c5e95d3375a60441c5b9d5b64498a9
Rainer Koschke <koschke@uni-bremen.de>
2025-07-18 10:14:43 +0200

    Updated Shader Control to version 7.1.1.

--- Assets/Plugins/ShaderControl/Editor/SCWindow.Advanced.GUI.cs
+++ Assets/Plugins/ShaderControl/Editor/SCWindow.Advanced.GUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0d430390727e744d2570010fafd5daee32435387c3f755b9376296b02232b49
-size 10240
+oid sha256:a1b87e8e83932cfcfd12fe381bbb9b97df251514e39a4c05ece7399dbef79886
+size 10231
