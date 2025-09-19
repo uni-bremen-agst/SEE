31dc6677ed6c2161cda13c89cb5d75b102a57f04
Rainer Koschke <koschke@uni-bremen.de>
2025-09-19 17:59:00 +0200

    #869 Improved documentation. Better identifiers. Add Churn attribute (not yet used).

--- Assets/SEE/GameObjects/AuthorEdge.cs
+++ Assets/SEE/GameObjects/AuthorEdge.cs
@@ -3,18 +3,24 @@ using Sirenix.OdinInspector;
 namespace SEE.GameObjects
 {
     /// <summary>
-    /// Attribute of an author edge connecting an author sphere to a node.
+    /// Attribute of an author edge connecting an <see cref="AuthorSphere"/>
+    /// to <see cref="AuthorRef"/>.
     /// </summary>
     public class AuthorEdge : SerializedMonoBehaviour
     {
         /// <summary>
         /// Reference to the target node this edge connects to.
         /// </summary>
-        public AuthorRef targetNode;
+        public AuthorRef TargetNode;
 
         /// <summary>
-        /// Reference to the <see cref="AuthorSphere"/> that this edge originates from.
+        /// Reference to the <see cref="GameObjects.AuthorSphere"/> that this edge originates from.
         /// </summary>
-        public AuthorSphere authorSphere;
+        public AuthorSphere AuthorSphere;
+
+        /// <summary>
+        /// Churn of the author for the specific file this edge connects to.
+        /// </summary>
+        public int Churn;
     }
 }
