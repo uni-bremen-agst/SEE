4e4c6f70edb9db6b7caa88ee0dfecec8a3a266aa
Rainer Koschke <koschke@uni-bremen.de>
2025-09-29 16:27:29 +0200

    #875 Separated graph renderering from polling.

--- Assets/SEE/Game/City/GitPoller.cs
+++ Assets/SEE/Game/City/GitPoller.cs
@@ -1,21 +1,15 @@
 using Cysharp.Threading.Tasks;
-using SEE.Utils;
-using SEE.DataModel.DG;
-using SEE.GO;
 using SEE.GraphProviders;
-using SEE.UI.Notification;
+using SEE.Utils;
 using SEE.VCS;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Timers;
 using UnityEngine;
 
 namespace SEE.Game.City
 {
     /// <summary>
-    /// <see cref="GitPoller"/> is used to regularly fetch for new changes in the
-    /// git repositories specified im <see cref="BranchCity"/>.
+    /// <see cref="GitPoller"/> is used to regularly fetch for new changes in a given
+    /// <see cref="Repository"/>.
     ///
     /// When a new commit was detected on any branch, a refresh of the CodeCity is initiated.
     /// Newly added or changed nodes will be marked after the refresh.
@@ -26,32 +20,35 @@ namespace SEE.Game.City
     public class GitPoller : PollerBase
     {
         /// <summary>
-        /// The code city where the <see cref="GitBranchesGraphProvider"/> graph provider
-        /// was executed and which should be updated when a new commit is detected.
-        /// </summary>
-        public BranchCity BranchCity;
-
-        /// <summary>
-        /// The time in seconds for how long the node markers should be shown for newly
-        /// added or modified nodes.
-        /// </summary>
-        public int MarkerTime = 10;
-
-        /// <summary>
-        /// MarkerFactory for generating node markers.
-        /// </summary>
-        private MarkerFactory markerFactory;
-
-        /// <summary>
         /// Specifies that the poller should currently not run.
         /// This is set to true when git fetch is in progress.
         /// </summary>
         private bool doNotPoll = false;
 
         /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="pollingInterval">time in seconds for the polling period</param>
+        /// <param name="gitRepository">repository to be polled</param>
+        public GitPoller(int pollingInterval, GitRepository gitRepository)
+        {
+            PollingInterval = pollingInterval;
+            Repository = gitRepository;
+        }
+
+        /// <summary>
+        /// Releases the reference to the <see cref="Repository"/>.
+        /// </summary>
+        ~GitPoller()
+        {
+            Stop();
+            Repository = null;
+        }
+
+        /// <summary>
         /// The <see cref="GitRepository"/> to poll.
         /// </summary>
-        public GitRepository Repository { set; private get; }
+        private GitRepository Repository { set; get; }
 
         /// <summary>
         /// Starts the actual poller.
@@ -60,16 +57,37 @@ namespace SEE.Game.City
         {
             base.Start();
 
+            Debug.Log($"Starting GitPoller on {Repository.RepositoryPath.Path}...\n");
             if (Repository == null)
             {
                 Debug.Log("No watched repositories.\n");
                 return;
             }
 
-            markerFactory = new MarkerFactory(BranchCity.MarkerAttributes);
-
             timer.Elapsed += OnTimedEvent;
         }
+
+        /// <summary>
+        /// Stops the poller.
+        /// </summary>
+        public override void Stop()
+        {
+            timer.Elapsed -= OnTimedEvent;
+            base.Stop();
+            Debug.Log($"Stopping GitPoller on {Repository.RepositoryPath.Path}...\n");
+        }
+
+        /// <summary>
+        /// Delegate to be called when a change was detected in the repository.
+        /// </summary>
+        public delegate void ChangeDetected();
+
+        /// <summary>
+        /// Occurs when a change is detected in the monitored <see cref="Repository"/>.
+        /// </summary>
+        /// <remarks>Subscribe to this event to handle changes as they occur. The event is triggered
+        /// whenever the poller detects a relevant change, and the associated event handler will be invoked.</remarks>
+        public event ChangeDetected OnChangeDetected;
 
         /// <summary>
         /// Executed on every timer event. Runs the <see cref="PollReposAsync"/> method.
@@ -78,19 +96,6 @@ namespace SEE.Game.City
         {
             PollReposAsync().Forget();
         }
-
-        /// <summary>
-        /// Shows a message to the user that a new commit was detected.
-        /// </summary>
-        private void ShowNewCommitsMessage()
-        {
-            ShowNotification.Info("New commits detected", "Refreshing code city.");
-        }
-
-        /// <summary>
-        /// Allows the comparison of two instances of <see cref="Node"/> from different graphs.
-        /// </summary>
-        private static readonly NodeEqualityComparer nodeEqualityComparer = new();
 
         /// <summary>
         /// Is called in every <see cref="PollingInterval"/> seconds.
@@ -111,69 +116,12 @@ namespace SEE.Game.City
 
                 if (needsUpdate)
                 {
-                    ShowNewCommitsMessage();
-
-                    // Backup old graph
-                    Graph oldGraph = BranchCity.LoadedGraph.Clone() as Graph;
-                    await BranchCity.LoadDataAsync();
-                    try
-                    {
-                        BranchCity.ReDrawGraph();
-                    }
-                    catch (Exception e)
-                    {
-                        Debug.LogError(e);
-                    }
-
-                    BranchCity.LoadedGraph.Diff(oldGraph,
-                        g => g.Nodes(),
-                        (g, id) => g.GetNode(id),
-                        GraphExtensions.AttributeDiff(BranchCity.LoadedGraph, oldGraph),
-                        nodeEqualityComparer,
-                        out ISet<Node> addedNodes,
-                        out ISet<Node> removedNodes,
-                        out ISet<Node> changedNodes,
-                        out _);
-
-                    foreach (Node changedNode in changedNodes)
-                    {
-                        markerFactory.MarkChanged(GraphElementIDMap.Find(changedNode.ID, true));
-                    }
-
-                    foreach (Node addedNode in addedNodes)
-                    {
-                        markerFactory.MarkBorn(GraphElementIDMap.Find(addedNode.ID, true));
-                    }
-
-                    // Removed nodes are not marked, because they are not in the graph anymore.
-                    // In the futre, we may want animate their removal.
-                    ShowRemovedNodes(removedNodes);
-                    RemoveMarkerAsync().Forget();
+                    Debug.Log($"Change detected in repository {Repository.RepositoryPath.Path}...\n");
+                    OnChangeDetected?.Invoke();
                 }
 
                 doNotPoll = false;
             }
-        }
-
-        /// <summary>
-        /// Notifies the user about removed files in the repository.
-        /// </summary>
-        /// <param name="removedNodes">The list of removed nodes.</param>
-        private void ShowRemovedNodes(ISet<Node> removedNodes)
-        {
-            foreach (Node removedNode in removedNodes)
-            {
-                ShowNotification.Info("File removed", $"File {removedNode.ID} was removed from the repository {Repository.RepositoryPath.Path}.");
-            }
-        }
-
-        /// <summary>
-        /// Removes all markers after <see cref="MarkerTime"/> seconds.
-        /// </summary>
-        private async UniTaskVoid RemoveMarkerAsync()
-        {
-            await Task.Delay(MarkerTime);
-            markerFactory.Clear();
         }
     }
 }
---
+++ Assets/SEE/Game/CityRendering/TransitionRenderer.cs
@@ -0,0 +1,155 @@
+using Cysharp.Threading.Tasks;
+using SEE.DataModel.DG;
+using SEE.Game.City;
+using SEE.GO;
+using SEE.GraphProviders;
+using SEE.UI.Notification;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SEE.Game.CityRendering
+{
+    /// <summary>
+    /// Renders transitions in a <see cref="BranchCity"/> when new commits are detected.
+    /// </summary>
+    public class TransitionRenderer
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="branchCity">the city to be re-drawn</param>
+        /// <param name="poller">the poller notifying when there are changes</param>
+        /// <param name="markerTime">the time in seconds the new markers should be drawn for;
+        /// after that they will be removed again</param>
+        public TransitionRenderer(BranchCity branchCity, GitPoller poller, int markerTime)
+        {
+            this.branchCity = branchCity;
+            this.markerTime = markerTime;
+            this.poller = poller;
+            markerFactory = new MarkerFactory(this.branchCity.MarkerAttributes);
+            poller.OnChangeDetected += Render;
+        }
+
+        /// <summary>
+        /// Finalizer to unsubscribe from the poller event.
+        /// </summary>
+        ~TransitionRenderer()
+        {
+            poller.OnChangeDetected -= Render;
+        }
+
+        /// <summary>
+        /// The poller notifying when there are changes.
+        /// </summary>
+        private readonly GitPoller poller;
+
+        /// <summary>
+        /// Renders the transition when new commits were detected.
+        /// This method is used as a delegate for <see cref="GitPoller.OnChangeDetected"/>.
+        /// </summary>
+        private void Render()
+        {
+            RenderAsync().Forget();
+        }
+
+        /// <summary>
+        /// Renders the transition when new commits were detected.
+        /// This method implements the actual rendering.
+        /// </summary>
+        /// <returns>task</returns>
+        private async UniTask RenderAsync()
+        {
+            ShowNewCommitsMessage();
+
+            // Backup old graph
+            Graph oldGraph = branchCity.LoadedGraph.Clone() as Graph;
+            await branchCity.LoadDataAsync();
+            try
+            {
+                branchCity.ReDrawGraph();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
+
+            branchCity.LoadedGraph.Diff(oldGraph,
+                g => g.Nodes(),
+                (g, id) => g.GetNode(id),
+                GraphExtensions.AttributeDiff(branchCity.LoadedGraph, oldGraph),
+                nodeEqualityComparer,
+                out ISet<Node> addedNodes,
+                out ISet<Node> removedNodes,
+                out ISet<Node> changedNodes,
+                out _);
+
+            foreach (Node changedNode in changedNodes)
+            {
+                markerFactory.MarkChanged(GraphElementIDMap.Find(changedNode.ID, true));
+            }
+
+            foreach (Node addedNode in addedNodes)
+            {
+                markerFactory.MarkBorn(GraphElementIDMap.Find(addedNode.ID, true));
+            }
+
+            // Removed nodes are not marked, because they are not in the graph anymore.
+            // In the futre, we may want animate their removal.
+            ShowRemovedNodes(removedNodes);
+            RemoveMarkerAsync().Forget();
+        }
+
+        /// <summary>
+        /// Allows the comparison of two instances of <see cref="Node"/> from different graphs.
+        /// </summary>
+        private static readonly NodeEqualityComparer nodeEqualityComparer = new();
+
+        /// <summary>
+        /// Notifies the user about removed files in the repository.
+        /// </summary>
+        /// <param name="removedNodes">The list of removed nodes.</param>
+        private void ShowRemovedNodes(ISet<Node> removedNodes)
+        {
+            foreach (Node removedNode in removedNodes)
+            {
+                ShowNotification.Info("File removed", $"File {removedNode.ID} was removed from the repository.");
+            }
+        }
+
+        /// <summary>
+        /// Removes all markers after <see cref="markerTime"/> seconds.
+        /// </summary>
+        private async UniTaskVoid RemoveMarkerAsync()
+        {
+            await Task.Delay(markerTime);
+            markerFactory.Clear();
+        }
+
+        /// <summary>
+        /// The code city where the <see cref="GitBranchesGraphProvider"/> graph provider
+        /// was executed and which should be updated when a new commit is detected.
+        /// </summary>
+        private readonly BranchCity branchCity;
+
+        /// <summary>
+        /// The time in seconds for how long the node markers should be shown for newly
+        /// added or modified nodes.
+        /// </summary>
+        private readonly int markerTime = 10;
+
+        /// <summary>
+        /// MarkerFactory for generating node markers.
+        /// </summary>
+        private readonly MarkerFactory markerFactory;
+
+        /// <summary>
+        /// Shows a message to the user that a new commit was detected.
+        /// </summary>
+        private void ShowNewCommitsMessage()
+        {
+            ShowNotification.Info("New commits detected", "Refreshing code city.");
+        }
+    }
+}
