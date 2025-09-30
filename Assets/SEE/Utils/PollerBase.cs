529fa9dd786e015f674c4ca7bb454b95e062b37f
Rainer Koschke <koschke@uni-bremen.de>
2025-09-30 15:51:22 +0200

    #875 Turned PollingInterval into a property to make sure changes are reflected.

--- Assets/SEE/Utils/PollerBase.cs
+++ Assets/SEE/Utils/PollerBase.cs
@@ -8,9 +8,34 @@ namespace SEE.Utils
     public abstract class PollerBase
     {
         /// <summary>
+        /// Backing attribute for <see cref="PollingInterval"/>.
+        /// </summary>
+        private int pollingInterval = 5;
+
+        /// <summary>
+        /// The number of milliseconds in a second.
+        /// </summary>
+        private const int millisecondsPerSecond = 1000;
+
+        /// <summary>
         /// The time interval in seconds between two polls.
         /// </summary>
-        public int PollingInterval = 5;
+        public int PollingInterval
+        {
+            get => pollingInterval;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new System.ArgumentOutOfRangeException("Polling interval must be positive.");
+                }
+                pollingInterval = value;
+                if (timer != null)
+                {
+                    timer.Interval = pollingInterval * millisecondsPerSecond;
+                }
+            }
+        }
 
         /// <summary>
         /// The timer which triggers the polls.
@@ -23,7 +48,7 @@ namespace SEE.Utils
         public virtual void Start()
         {
             // Create a timer with a PollingInterval-second interval (1000 milliseconds)
-            timer = new(PollingInterval * 1000)
+            timer = new(PollingInterval * millisecondsPerSecond)
             {
                 // This ensures the timer repeats. It's the default, but it's good practice to be explicit.
                 AutoReset = true,
