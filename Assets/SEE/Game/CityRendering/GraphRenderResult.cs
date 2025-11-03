4e62b47c25a66b3a90d31567a73d06b23e52a9da
Hannes Lennart Kuß <102760046+hlkuss@users.noreply.github.com>
2025-11-03 10:55:18 +0100

    fix: ensure nodes and edges lists are initialized in GraphRenderResult.cs

--- Assets/SEE/Game/CityRendering/GraphRenderResult.cs
+++ Assets/SEE/Game/CityRendering/GraphRenderResult.cs
@@ -11,11 +11,11 @@ namespace SEE.Game.CityRendering
         /// <summary>
         /// The nodes that were rendered with their layout.
         /// </summary>
-        public ICollection<ILayoutNode> Nodes { get; set; }
+        public ICollection<ILayoutNode> Nodes { get; set; } = new List<ILayoutNode>();
 
         /// <summary>
         /// The edges that were rendered.
         /// </summary>
-        public ICollection<ILayoutEdge<ILayoutNode>> Edges { get; set; }
+        public ICollection<ILayoutEdge<ILayoutNode>> Edges { get; set; } = new List<ILayoutEdge<ILayoutNode>>();
     }
 }
