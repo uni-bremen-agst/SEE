743fd1a3baaa564c74ea4320021b35fab6babfac
Rainer Koschke <koschke@uni-bremen.de>
2025-11-08 13:59:29 +0100

    Updated Dissonance Voice Chat to version 9.0.8.

--- Assets/Plugins/Dissonance/VoiceReceiptTrigger.cs
+++ Assets/Plugins/Dissonance/VoiceReceiptTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b977ca90c9eab24d3550436ca1e9b52049b42bcd6bb006ccfb69701f11582e92
-size 4172
+oid sha256:83d7844d1977bd078f28cd2710d8d7641424d7bb6d17a8c46014ed3c36820d7e
+size 4169
