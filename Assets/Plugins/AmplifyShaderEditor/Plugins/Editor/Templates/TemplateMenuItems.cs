e1cbd3a0e62a8ac744de4bda5175678dab39b512
Rainer Koschke <koschke@uni-bremen.de>
2025-11-08 13:50:56 +0100

    Updated AmplifyShaderEditor.

--- Assets/Plugins/AmplifyShaderEditor/Plugins/Editor/Templates/TemplateMenuItems.cs
+++ Assets/Plugins/AmplifyShaderEditor/Plugins/Editor/Templates/TemplateMenuItems.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50ba7c0c4bb793c35d543c8e2ecf471bbe64ba7e91307f5a1af488d491a8d708
-size 3238
+oid sha256:675a8346e2ae83a9492cd01476bd1ddc436656d36b704b68e3714374fad2aec1
+size 3431
