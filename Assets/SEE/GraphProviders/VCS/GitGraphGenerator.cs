63daa8957407d88adcff14d806897c096cad3853
Rainer Koschke <koschke@uni-bremen.de>
2025-11-11 17:38:28 +0100

    Better self-explanatory names.
    Performance markers will be removed once we identfied the bottleneck.

--- Assets/SEE/GraphProviders/VCS/GitGraphGenerator.cs
+++ Assets/SEE/GraphProviders/VCS/GitGraphGenerator.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using UnityEngine;
+using Unity.Profiling;
 
 namespace SEE.GraphProviders.VCS
 {
@@ -18,6 +19,8 @@ namespace SEE.GraphProviders.VCS
     /// </summary>
     public static class GitGraphGenerator
     {
+        private static readonly ProfilerMarker marker = new ProfilerMarker("GitGraphGenerator");
+
         /// <summary>
         /// A mapping of filenames (relative paths in a repository) onto their <see cref="GitFileMetrics"/>.
         /// </summary>
@@ -164,20 +167,20 @@ namespace SEE.GraphProviders.VCS
             // Includes all commits between the baseline commit and the commitID
             // including the commitID itself but excluding the baseline commit.
 
-            repository.ForEachCommitBetween(baselineCommitID, commitID, Apply);
+            repository.ForEachCommitBetween(baselineCommitID, commitID, UpdateMetricsForCommit);
 
             Finalize(graph, simplifyGraph, repository, repositoryName, fileToMetrics);
 
             changePercentage?.Invoke(1f);
             return graph;
 
-            void Apply(Repository repository, Commit commit)
+            void UpdateMetricsForCommit(Repository repository, Commit commit)
             {
                 if (token.IsCancellationRequested)
                 {
                     throw new OperationCanceledException(token);
                 }
-                UpdateMetricsForCommit(fileToMetrics, repository, commit, consultAliasMap, authorAliasMap);
+                GitGraphGenerator.UpdateMetricsForCommit(fileToMetrics, repository, commit, consultAliasMap, authorAliasMap);
             }
         }
 
@@ -247,11 +250,20 @@ namespace SEE.GraphProviders.VCS
              Action<float> changePercentage,
              CancellationToken token)
         {
+            Debug.Log("Starting AddNodesAfterDate...\n");
+            marker.Begin();
+
             /// Note: The following code is very similar to
             /// <see cref="AddNodesForCommit(Graph, bool, GitRepository, string, string, bool, AuthorMapping, Action{float}, CancellationToken)"/>".
             /// The difference is that we consider all relevant files passing the repository
             /// filter and all commits after <paramref name="startDate"/>.
             HashSet<string> files = repository.AllFiles(token);
+            if (files.Count == 0)
+            {
+                Debug.LogWarning("No files were matched.\n");
+                changePercentage?.Invoke(1.0f);
+                return;
+            }
             changePercentage?.Invoke(0.3f);
 
             FileToMetrics fileToMetrics = Prepare(graph, files);
@@ -260,19 +272,23 @@ namespace SEE.GraphProviders.VCS
                 throw new OperationCanceledException(token);
             }
 
-            repository.ForEachCommitAfter(startDate, Apply);
+            repository.ForEachCommitAfter(startDate, UpdateMetricsForCommit);
             changePercentage?.Invoke(0.6f);
 
             Finalize(graph, simplifyGraph, repository, repositoryName, fileToMetrics);
             changePercentage?.Invoke(1f);
 
-            void Apply(Repository repo, Commit commit)
+            marker.End();
+            Debug.Log("Ending AddNodesAfterDate...\n");
+
+            void UpdateMetricsForCommit(Repository repo, Commit commit)
             {
                 if (token.IsCancellationRequested)
                 {
                     throw new OperationCanceledException(token);
                 }
-                UpdateMetricsForCommit(fileToMetrics, repo, commit, consultAliasMap, authorAliasMap);
+                GitGraphGenerator.UpdateMetricsForCommit
+                    (fileToMetrics, repo, commit, consultAliasMap, authorAliasMap);
             }
         }
 
@@ -412,6 +428,7 @@ namespace SEE.GraphProviders.VCS
             }
             if (commit.Parents.Any())
             {
+                // There may in fact be multiple parents.
                 foreach (Commit parent in commit.Parents)
                 {
                     UpdateMetricsForPatch(fileToMetrics, commit, GitRepository.Diff(repository, parent, commit), consultAliasMap, authorAliasMap);
@@ -419,6 +436,7 @@ namespace SEE.GraphProviders.VCS
             }
             else
             {
+                // It is the very first commit.
                 UpdateMetricsForPatch(fileToMetrics, commit, GitRepository.Diff(repository, null, commit), consultAliasMap, authorAliasMap);
             }
         }
