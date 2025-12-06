13caf23db9e5e7611825d460fd3c436b7adc4e50
Rainer Koschke <koschke@uni-bremen.de>
2025-12-06 18:27:56 +0100

    #887 The dynamic marking is now handled by GraphElementOperator.

--- Assets/SEE/GameObjects/AuthorRef.cs
+++ Assets/SEE/GameObjects/AuthorRef.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using HighlightPlus;
+using SEE.Game.Operator;
 using SEE.GO;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
-using UnityEngine;
 
 namespace SEE.GameObjects
 {
     /// <summary>
-    /// Holds the connections of a file all its authors.
-    /// This component will be attached to all file game nodes.
+    /// Holds the connections of a file to all its authors.
+    /// This component will be attached to all file game nodes in a <see cref="BranchCity"/>.
     /// </summary>
     public class AuthorRef : SerializedMonoBehaviour, IEnumerable<AuthorEdge>
     {
@@ -31,28 +30,6 @@ namespace SEE.GameObjects
         /// </summary>
         private void Awake()
         {
-            effect = gameObject.GetComponent<HighlightEffect>();
-            if (effect == null)
-            {
-                effect = gameObject.AddComponent<HighlightEffect>();
-                /// By default, an outline is set, which we do not want.
-                /// We reset it to 0 only if we created the <see cref="HighlightEffect"/>;
-                /// otherwise we assume whoever set it chose a meaningful value, which
-                /// we do not want to disable.
-                effect.outline = 0;
-            }
-            // FIXME: The icon's mesh must respect the portal.
-            effect.iconFXLightColor = Color.yellow;
-            effect.iconFXDarkColor = Color.yellow;
-            // The iconFXOffset is relative to the object, that is, not world space.
-            effect.iconFXOffset = Vector3.zero;
-            effect.iconFXRotationSpeed = 0f;
-            effect.iconFXAnimationOption = IconAnimationOption.VerticalBounce;
-            effect.iconFXAnimationAmount = 0.1f;
-            effect.iconFXAnimationSpeed = 1f;
-            effect.iconFXScale = 0.2f;
-            effect.iconFXStayDuration = float.PositiveInfinity;
-
             // We may already have edges from the serialization and might
             // need to update these.
             UpdateEdges();
@@ -69,31 +46,18 @@ namespace SEE.GameObjects
         }
 
         /// <summary>
-        /// Backing field for the <see cref="HighlightEffect"/>
-        /// attached to this game object.
-        /// </summary>
-        private HighlightEffect effect;
-
-        /// <summary>
         /// If there are more than one edge, the highlight effect for a potential
         /// edit conflict is turned on; otherwise it is turned off.
         /// </summary>
-        private void UpdateConflictVisibility()
+        public void UpdateConflictVisibility(bool enableConflictMarkers)
         {
-            if (edges.Count > 1)
+            if (enableConflictMarkers && edges.Count > 1)
             {
-                // It is not suffient to only activate iconFX. This field means
-                // only that the icon should be used when the object is highlighted;
-                // it doesn't mean that it is actually highlighted. That is why
-                // we also need to set highlighted to true here.
-                effect.highlighted = true;
-                effect.iconFX = true;
+                gameObject.AddOrGetComponent<NodeOperator>().EnableDynamicMark();
             }
             else
             {
-                // We do not set highlighted to false here because there might be
-                // other reasons why the object is highlighted.
-                effect.iconFX = false;
+                gameObject.AddOrGetComponent<NodeOperator>().DisableDynamicMark();
             }
         }
 
