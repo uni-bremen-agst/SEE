7a3d06b0b26ba4f07796143675cbe9d848995bd0
Rainer Koschke <koschke@uni-bremen.de>
2025-12-10 15:43:15 +0100

    #870 Handling ParsingConfig being null. Report progress. Added documentation. Simplified call to ParsingConfig.Save().

--- Assets/SEE/GraphProviders/ReportGraphProvider.cs
+++ Assets/SEE/GraphProviders/ReportGraphProvider.cs
@@ -34,7 +34,7 @@ namespace SEE.GraphProviders
         /// <param name="city">
         /// City that provides context for the graph. Must not be null.
         /// </param>
-        /// <param name="_changePercentage">
+        /// <param name="changePercentage">
         /// Change percentage callback. This parameter is currently ignored.
         /// </param>
         /// <param name="token">
@@ -53,7 +53,7 @@ namespace SEE.GraphProviders
         public override async UniTask<Graph> ProvideAsync
             (Graph graph,
              AbstractSEECity city,
-             Action<float> _changePercentage = null,
+             Action<float> changePercentage = null,
              CancellationToken token = default)
         {
             token.ThrowIfCancellationRequested();
@@ -61,16 +61,27 @@ namespace SEE.GraphProviders
             CheckArguments(city);
             if (graph == null)
             {
+                changePercentage?.Invoke(1.0f);
                 throw new NotImplementedException($"{nameof(ReportGraphProvider)} requires an existing graph to apply metrics to. Creating a new graph from a report is not currently supported.");
+            }
+
+            if (ParsingConfig == null)
+            {
+                changePercentage?.Invoke(1.0f);
+                throw new ArgumentNullException(nameof(ParsingConfig));
             }
 
             await UniTask.SwitchToThreadPool();
 
             IReportParser parser = ParsingConfig.CreateParser();
             MetricSchema metricSchema = await parser.ParseAsync(Path, token);
+            changePercentage?.Invoke(0.5f);
+
             MetricApplier.ApplyMetrics(graph, metricSchema, ParsingConfig);
 
             await UniTask.SwitchToMainThread();
+            changePercentage?.Invoke(1.0f);
+
             return graph;
         }
 
@@ -87,15 +98,18 @@ namespace SEE.GraphProviders
 
         #region Config I/O
 
+        /// <summary>
+        /// Label of <see cref="ParsingConfig"/> in the configuration file.
+        /// </summary>
+        ///
         private const string parsingConfigLabel = "ParsingConfig";
+
         protected override void SaveAttributes(ConfigWriter writer)
         {
             base.SaveAttributes(writer);
-            if (ParsingConfig != null)
-            {
-                ParsingConfig.Save(writer, parsingConfigLabel);
-            }
+            ParsingConfig?.Save(writer, parsingConfigLabel);
         }
+
         protected override void RestoreAttributes(Dictionary<string, object> attributes)
         {
             base.RestoreAttributes(attributes);
@@ -103,6 +117,5 @@ namespace SEE.GraphProviders
         }
 
         #endregion
-
     }
 }
