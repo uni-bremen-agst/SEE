0effdc3ab3b26923720b77a56b9a0dd91a82438f
Rainer Koschke <koschke@uni-bremen.de>
2026-01-01 13:00:42 +0100

    Path needed to be adjusted. We placed MediaPipeUnity in Plugins folder instead of Packages folder.

--- Assets/Plugins/MediaPipeUnity/Runtime/Scripts/Unity/ResourceManager/LocalResourceManager.cs
+++ Assets/Plugins/MediaPipeUnity/Runtime/Scripts/Unity/ResourceManager/LocalResourceManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21ab7257b0bea4df7b564ebb772a49544c6b8a8ead92f022f04f64bfe5e83db4
-size 2230
+oid sha256:58143ef9618bb1321efb8625db8597d56a5b00d8dda2e5ead5753d79a2dea047
+size 2222
