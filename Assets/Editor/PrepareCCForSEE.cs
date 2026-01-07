78ddda64a1f4b72fde115d1208f8d7b4c5318094
Rainer Koschke <koschke@uni-bremen.de>
2026-01-07 19:34:58 +0100

    Added documentation.

--- Assets/Editor/PrepareCCForSEE.cs
+++ Assets/Editor/PrepareCCForSEE.cs
@@ -36,27 +36,51 @@ namespace SEEEditor
             }
         }
 
-        private static void PrepareLookAtIK(GameObject go)
+        /// <summary>
+        /// Configures the <see cref="LookAtIK"/> component of the avatar <paramref name="avatar"/>.
+        /// </summary>
+        /// <param name="avatar">The root game object representing the avatar.</param>
+        private static void PrepareLookAtIK(GameObject avatar)
         {
-            if (go.TryGetComponentOrLog(out LookAtIK lookAtIK))
+            if (avatar.TryGetComponentOrLog(out LookAtIK lookAtIK))
             {
                 // Configure component Look At IK by adding child game object
                 // CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_NeckTwist01/CC_Base_NeckTwist02/CC_Base_Head
                 // to attribute Head.
-                Transform head = MustFind(go, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_NeckTwist01/CC_Base_NeckTwist02/CC_Base_Head");
+                Transform head = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_NeckTwist01/CC_Base_NeckTwist02/CC_Base_Head");
                 lookAtIK.solver.head.transform = head;
             }
         }
 
+        /// <summary>
+        /// Name of the game object representing the aim target for <see cref="AimIK"/>.
+        /// </summary>
         private const string aimTargetName = "AimTarget";
 
-        private static void PrepareAimIK(GameObject go)
+        /// <summary>
+        /// Configures the <see cref="AimIK"/> component of the avatar <paramref name="avatar"/>.
+        /// </summary>
+        /// <param name="avatar">The root game object representing the avatar.</param>
+        private static void PrepareAimIK(GameObject avatar)
         {
-            Transform aimTarget = PrepareAimTarget(go);
+            Transform aimTarget = PrepareAimTarget(avatar);
 
-            if (go.TryGetComponentOrLog(out AimIK aimIK))
+            if (avatar.TryGetComponentOrLog(out AimIK aimIK))
             {
                 aimIK.solver.target = aimTarget;
+
+                // Set the bones. AddBone adds at the end and would leave the first five bones undefined.
+                aimIK.solver.bones[0].transform = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01");
+                aimIK.solver.bones[1].transform = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02");
+                aimIK.solver.bones[2].transform = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_R_Clavicle");
+                aimIK.solver.bones[3].transform = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_R_Clavicle/CC_Base_R_Upperarm");
+                aimIK.solver.bones[4].transform = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_R_Clavicle/CC_Base_R_Upperarm/CC_Base_R_Forearm");
+                // Set the weight of each bone.
+                aimIK.solver.bones[0].weight = 0.3f;
+                aimIK.solver.bones[1].weight = 0.5f;
+                aimIK.solver.bones[2].weight = 0.8f;
+                aimIK.solver.bones[3].weight = 0.846f;
+                aimIK.solver.bones[4].weight = 1.0f;
             }
 
             // Prepares and returns AimTarget.
@@ -84,12 +108,20 @@ namespace SEEEditor
             }
         }
 
-        private static Transform MustFind(GameObject go, string path)
+        /// <summary>
+        /// Returns the descendant of <paramref name="avatar"/> under the
+        /// given <paramref name="path"/>.
+        /// </summary>
+        /// <param name="avatar">The root game object representing the avatar.</param>
+        /// <param name="path">The hierarchical name of the descendant.</param>
+        /// <returns>The resulting descendant.</returns>
+        /// <exception cref="Exception">Thrown if there is no such descendant.</exception>
+        private static Transform MustFind(GameObject avatar, string path)
         {
-            Transform result = go.transform.Find(path);
+            Transform result = avatar.transform.Find(path);
             if (result == null)
             {
-                throw new Exception($"Avatar {go.name} does not have a descendant named {path}");
+                throw new Exception($"Avatar {avatar.name} does not have a descendant named {path}");
             }
             return result;
         }
