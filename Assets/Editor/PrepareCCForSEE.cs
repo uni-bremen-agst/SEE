dc60005380817de43afec8be863e7de0a3bf66e9
Rainer Koschke <koschke@uni-bremen.de>
2026-01-08 10:25:37 +0100

    SALSA and FullBodyBipedIK will be configured. Added documentation.

--- Assets/Editor/PrepareCCForSEE.cs
+++ Assets/Editor/PrepareCCForSEE.cs
@@ -13,6 +13,42 @@ namespace SEEEditor
     /// Adds a menu item "SEE/Prepare CC5 Avatar" to the Unity Editor to
     /// prepare an avatar imported from Reallusion Character Creator (CC)
     /// to be used in SEE. It adds and wires the necessary components.
+    ///
+    /// The following components are already attached to the BaseAvatar and
+    /// will be inherited from it (the ones that need configuration are marked
+    /// with a *; these cannot be configured in the BaseAvatar because - generally -
+    /// they depend upon the bones and bones are added through the CC5 avatar
+    /// sceleton; those not needing additional configuration are marked by -):
+    ///
+    /// - <see cref="UnityEngine.CapsuleCollider"/> (currently disabled)
+    /// * <see cref="CrazyMinnow.SALSA.DissonanceLink.SalsaDissonanceLink"/>
+    /// - <see cref="Dissonance.Demo.TriggerVisualizer"/> (currently disabled and not used)
+    /// - <see cref="Dissonance.VoiceBroadcastTrigger"/>
+    /// - <see cref="Dissonance.Integrations.Unity_NFGO.NfgoPlayer"/>
+    /// * <see cref="RootMotion.FinalIK.AimIK"/>
+    /// * <see cref="RootMotion.FinalIK.FullBodyBipedIK"/>
+    /// * <see cref="RootMotion.FinalIK.LookAtIK"/>
+    /// - <see cref="SEE.Game.Avatars.ActionUnits"/>
+    /// - <see cref="SEE.Game.Avatars.AvatarAdapter"/>
+    /// - <see cref="SEE.Game.Avatars.AvatarAimingSystem"/>
+    /// - <see cref="SEE.Game.Avatars.AvatarHandAnimationsSync"/>
+    /// - <see cref="SEE.Game.Avatars.AvatarMovementAnimator"/>
+    /// - <see cref="SEE.Game.Avatars.BodyAnimator"/>
+    /// - <see cref="SEE.Net.ClientNetworkTransform"/>
+    /// - <see cref="Unity.Netcode.NetworkObject"/>
+    /// * <see cref="UnityEngine.Animator"/>
+    /// - <see cref="UnityEngine.CharacterController"/>
+    ///
+    /// The following components should be added to the avatar prefab because they
+    /// are not inherited from the BaseAvatar (again because they depend upon bones):
+    ///
+    /// <see cref="FACSnimator"/> (located in plugin FACSvatar)
+    ///
+    /// There is a Unity Editor menu entry "GameObject>Grazy Minnow Studio>SALSA LipSync>One-Clicks>Reallusion>CC4."
+    /// that allows a user to configure the SALSA components for the selected avatar.
+    /// This entry will call <see cref="CrazyMinnow.SALSA.OneClicks.OneClickCCEditor.OneClickSetup_CC4GameReady"/>.
+    /// To call this method, we need an assembly reference to
+    /// Assets/Plugins/Crazy Minnow Studio/SALSA LipSync/Editor/SALSALipSyncOneClicksEditor.asmdef
     /// </summary>
     public static class PrepareCCForSEE
     {
@@ -26,9 +62,11 @@ namespace SEEEditor
         {
             if (Selection.activeGameObject != null)
             {
-                GameObject go = Selection.activeGameObject;
-                PrepareLookAtIK(go);
-                PrepareAimIK(go);
+                GameObject avatar = Selection.activeGameObject;
+                CrazyMinnow.SALSA.OneClicks.OneClickCCEditor.OneClickSetup_CC4GameReady();
+                PrepareLookAtIK(avatar);
+                PrepareAimIK(avatar);
+                PrepareFullBodyBipedIK(avatar);
             }
             else
             {
@@ -37,7 +75,7 @@ namespace SEEEditor
         }
 
         /// <summary>
-        /// Configures the <see cref="LookAtIK"/> component of the avatar <paramref name="avatar"/>.
+        /// Configures the <see cref="LookAtIK"/> component of the <paramref name="avatar"/>.
         /// </summary>
         /// <param name="avatar">The root game object representing the avatar.</param>
         private static void PrepareLookAtIK(GameObject avatar)
@@ -58,7 +96,7 @@ namespace SEEEditor
         private const string aimTargetName = "AimTarget";
 
         /// <summary>
-        /// Configures the <see cref="AimIK"/> component of the avatar <paramref name="avatar"/>.
+        /// Configures the <see cref="AimIK"/> component of the <paramref name="avatar"/>.
         /// </summary>
         /// <param name="avatar">The root game object representing the avatar.</param>
         private static void PrepareAimIK(GameObject avatar)
@@ -105,6 +143,41 @@ namespace SEEEditor
                 collider.enabled = false;
                 aimTarget.gameObject.AddOrGetComponent<ClientNetworkTransform>();
                 return aimTarget;
+            }
+        }
+
+        /// <summary>
+        /// Configures the <see cref="FullBodyBipedIK"/> component of the <paramref name="avatar"/>.
+        /// </summary>
+        /// <param name="avatar">The root game object representing the avatar.</param>
+        private static void PrepareFullBodyBipedIK(GameObject avatar)
+        {
+            if (avatar.TryGetComponentOrLog(out FullBodyBipedIK ik))
+            {
+                ik.references.pelvis = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip");
+
+                ik.references.leftThigh = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Pelvis/CC_Base_L_Thigh");
+                ik.references.leftCalf = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Pelvis/CC_Base_L_Thigh/CC_Base_L_Calf");
+                ik.references.leftFoot = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Pelvis/CC_Base_L_Thigh/CC_Base_L_Calf/CC_Base_L_Foot");
+
+                ik.references.rightThigh = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Pelvis/CC_Base_R_Thigh");
+                ik.references.rightCalf = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Pelvis/CC_Base_R_Thigh/CC_Base_R_Calf");
+                ik.references.rightFoot = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Pelvis/CC_Base_R_Thigh/CC_Base_R_Calf/CC_Base_R_Foot");
+
+                ik.references.leftUpperArm = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_L_Clavicle/CC_Base_L_Upperarm");
+                ik.references.leftForearm = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_L_Clavicle/CC_Base_L_Upperarm/CC_Base_L_Forearm");
+                ik.references.leftHand = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_L_Clavicle/CC_Base_L_Upperarm/CC_Base_L_Forearm/CC_Base_L_Hand");
+
+                ik.references.rightUpperArm = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_R_Clavicle/CC_Base_R_Upperarm");
+                ik.references.rightForearm = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_R_Clavicle/CC_Base_R_Upperarm/CC_Base_R_Forearm");
+                ik.references.rightHand = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_R_Clavicle/CC_Base_R_Upperarm/CC_Base_R_Forearm/CC_Base_R_Hand");
+
+                ik.references.head = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01/CC_Base_Spine02/CC_Base_NeckTwist01/CC_Base_NeckTwist02/CC_Base_Head");
+
+                ik.references.spine[0] = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist");
+                ik.references.spine[1] = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01");
+
+                ik.references.root = MustFind(avatar, "CC_Base_BoneRoot/CC_Base_Hip/CC_Base_Waist/CC_Base_Spine01");
             }
         }
 
