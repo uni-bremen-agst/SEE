af5fcc08e54ad952a73002c3acbe59a18f221386
Hannes Lennart Kuß <102760046+hlkuss@users.noreply.github.com>
2026-01-23 10:48:33 +0100

    feat(#923): send livekit url and token to client via rpc when connecting

--- Assets/SEE/Net/ActionNetwork.cs
+++ Assets/SEE/Net/ActionNetwork.cs
@@ -211,7 +211,7 @@ namespace SEE.Net
             }
 
             ulong senderId = rpcParams.Receive.SenderClientId;
-            SyncFilesClientRpc(Network.ServerId, UserSettings.Instance.Network.BackendServerAPI, RpcTarget.Single(senderId, RpcTargetUse.Temp));
+            SyncFilesClientRpc(Network.ServerId, UserSettings.Instance.Network.BackendServerAPI, UserSettings.Instance.Video.LiveKitUrl, UserSettings.Instance.Video.RoomName, RpcTarget.Single(senderId, RpcTargetUse.Temp));
         }
 
         /// <summary>
@@ -447,7 +447,7 @@ namespace SEE.Net
         /// <param name="backendDomain">The domain of the backend .</param>
         /// <param name="rpcParams">The additional RPC parameters.</param>
         [Rpc(SendTo.SpecifiedInParams)]
-        private void SyncFilesClientRpc(string backendServerId, string backendDomain, RpcParams rpcParams = default)
+        private void SyncFilesClientRpc(string backendServerId, string backendDomain, string livekitUrl, string livekitRoom, RpcParams rpcParams = default)
         {
             if (IsHost || IsServer)
             {
@@ -462,6 +462,8 @@ namespace SEE.Net
 
             Network.ServerId = backendServerId;
             UserSettings.Instance.Network.BackendServerAPI = backendDomain;
+            UserSettings.Instance.Video.LiveKitUrl = livekitUrl;
+            UserSettings.Instance.Video.RoomName = livekitRoom;
 
             BackendSyncUtil.InitializeClientAsync().Forget();
         }
