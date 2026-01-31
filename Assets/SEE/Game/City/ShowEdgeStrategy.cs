5af345e5b63e6f83a8e7c4082bd9261de50f78b5
Rainer Koschke <koschke@uni-bremen.de>
2026-01-31 17:26:51 +0100

    New enum for when to show graph edges.

---
+++ Assets/SEE/Game/City/ShowEdgeStrategy.cs
@@ -0,0 +1,23 @@
+﻿namespace SEE.Game.City
+{
+    /// <summary>
+    /// This strategy determines when to show graph edges.
+    /// </summary>
+    public enum ShowEdgeStrategy : byte
+    {
+        /// <summary>
+        /// Do not show any edge.
+        /// </summary>
+        Never,
+
+        /// <summary>
+        /// Show edges all the time.
+        /// </summary>
+        Always,
+
+        /// <summary>
+        /// Show edges only when the user hovers over the author sphere or the node.
+        /// </summary>
+        OnHoverOnly,
+    }
+}
