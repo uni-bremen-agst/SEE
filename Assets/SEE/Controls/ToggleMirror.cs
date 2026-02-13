143d5ef522a94d4a76c61e9b242c4b2bf7692270
Rainer Koschke <koschke@uni-bremen.de>
2026-02-13 16:15:41 +0100

    #909 Improved documentation.

--- Assets/SEE/Controls/ToggleMirror.cs
+++ Assets/SEE/Controls/ToggleMirror.cs
@@ -6,6 +6,13 @@
     /// <remarks>This method is assumed to be attached to the mirror in the scene.</remarks>
     public class ToggleMirror : ToggleChildren
     {
+        /// <summary>
+        /// When the user requests to toggle the mirror, the children of the game object
+        /// this component is attached to, will be enabled/disabled. The child object
+        /// of the game object is the mirror object holding the cam mimicing a mirror.
+        /// In other words, this camera is enabled or disabled, respectively.
+        /// </summary>
+        /// <returns>True if the user requested to toggle the mirror.</returns>
         protected override bool ToggleCondition()
         {
             return SEEInput.ToggleMirror();
