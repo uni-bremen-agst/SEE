d8acc62797c1f3119198257fc6c9905949b1be3b
Rainer Koschke <koschke@uni-bremen.de>
2026-03-05 11:06:54 +0100

    #909 All normal lines are now children under the same game object. This way we can more quickly delete them.

--- Assets/Editor/ShowNormalVectors.cs
+++ Assets/Editor/ShowNormalVectors.cs
@@ -1,5 +1,6 @@
 ﻿#if UNITY_EDITOR
 
+using SEE.GO;
 using UnityEditor;
 using UnityEngine;
 
@@ -36,13 +37,14 @@ namespace SEEEditor
                 Vector3[] vertices = mesh.vertices;
                 Vector3[] normals = mesh.normals;
 
+                GameObject parent = new("Normal Vectors of " + gameObject.FullName());
                 for (int i = 0; i < vertices.Length; i++)
                 {
                     // Convert local vertex positions and normals to world space
                     Vector3 worldPos = gameObject.transform.TransformPoint(vertices[i]);
                     Vector3 worldNormal = gameObject.transform.TransformDirection(normals[i]);
 
-                    Draw(worldPos, worldNormal * normalsLength);
+                    Draw(parent, worldPos, worldNormal * normalsLength);
                 }
             }
         }
@@ -52,9 +54,10 @@ namespace SEEEditor
         /// <paramref name="from"/> + <paramref name="direction"/> with given
         /// <see cref="color"/> and <see cref="normalsLength"/>.
         /// </summary>
+        /// <param name="parent">The parent under which the lines will be added as children.</param>
         /// <param name="from">Origin of the line.</param>
         /// <param name="direction">Direction of the line.</param>
-        private static void Draw(Vector3 from, Vector3 direction)
+        private static void Draw(GameObject parent, Vector3 from, Vector3 direction)
         {
             GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             gameObject.name = "NormalVector";
@@ -73,6 +76,8 @@ namespace SEEEditor
             line.positionCount = 2;
             line.SetPosition(0, from);
             line.SetPosition(1, from + direction); // Calculates the destination point
+
+            gameObject.transform.SetParent(parent.transform);
         }
     }
 }
