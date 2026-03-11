a0c3e4fc45aaeba08dd147bbc88a87edfba3aa02
Rainer Koschke <koschke@uni-bremen.de>
2026-03-11 19:05:29 +0100

    There were formerly in AbstractSEECity as buttons, but they do not change anything for city. They change the global state.

---
+++ Assets/Editor/ClearMap.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+
+using SEE.Game;
+using UnityEditor;
+
+namespace SEEEditor
+{
+    /// <summary>
+    /// Clear <see cref="GraphElementIDMap"/>. Will be empty afterwards.
+    /// </summary>
+    /// <remarks>Used for cleaning in case something failed and graph elements
+    /// were not removed properly from the map.</remarks>
+    public static class ClearMap
+    {
+        [MenuItem("SEE/Clear Graph Element Map")]
+        public static void ClearGraphElementMap()
+        {
+            GraphElementIDMap.Clear();
+        }
+    }
+}
+
+#endif
---
+++ Assets/Editor/DumpMap.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+
+using SEE.Game;
+using UnityEditor;
+
+namespace SEEEditor
+{
+    /// <summary>
+    /// Dumps the content of <see cref="GraphElementIDMap"/>.
+    /// </summary>
+    /// <remarks>Used for debugging.</remarks>
+    public static class DumpMap
+    {
+        [MenuItem("SEE/Dump Graph Element Map")]
+        public static void DumpGraphElementMap()
+        {
+            GraphElementIDMap.Dump();
+        }
+    }
+}
+
+#endif
