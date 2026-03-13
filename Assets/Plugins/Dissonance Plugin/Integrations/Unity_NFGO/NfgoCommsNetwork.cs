40158d78ba15dff2bcdf293612d8dcfc93a8db4b
Rainer Koschke <koschke@uni-bremen.de>
2026-03-13 18:02:37 +0100

    Updated Dissonance For Netcode for GameObjects to version 9.0.7.

--- Assets/Plugins/Dissonance Plugin/Integrations/Unity_NFGO/NfgoCommsNetwork.cs
+++ Assets/Plugins/Dissonance Plugin/Integrations/Unity_NFGO/NfgoCommsNetwork.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c10a8c05c797213bf684eb5591b9242e8bb0df4d8f5b6bee6f1ed5e7cd026410
-size 8409
+oid sha256:a5658bb7701b499dc19f0c58682d4a361fe15462caa944dcec315522b0795d04
+size 8528
