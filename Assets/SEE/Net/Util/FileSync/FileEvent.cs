4948d281cd5344efc45b3e706c6346ff02564414
Hannes Lennart Kuß <102760046+hlkuss@users.noreply.github.com>
2026-03-20 13:22:39 +0100

    refactor(#919): generalized file event types

---
+++ Assets/SEE/Net/Util/FileSync/FileEvent.cs
@@ -0,0 +1,22 @@
+using Newtonsoft.Json;
+
+namespace SEE.Net.Util.FileSync
+{
+
+    public class FileEvent
+    {
+        /// <summary>
+        /// The relative path to the project dir.
+        /// </summary>
+        [JsonProperty(PropertyName = "fileName")]
+        public string FileName;
+
+        /// <summary>
+        /// The project type.
+        /// This will be one of the first subdirs in the Multiplayer directory.
+        /// </summary>
+        [JsonProperty(PropertyName = "projectType")]
+        public string ProjectType;
+
+    }
+}
--- Assets/SEE/Net/Util/FileSync/FileUpdateEvent.cs
+++ Assets/SEE/Net/Util/FileSync/FileUpdateEvent.cs
@@ -7,21 +7,8 @@ namespace SEE.Net.Util.FileSync
     /// Event message when a file was updated.
     /// </summary>
     [Serializable]
-    public class FileUpdateEvent
+    public class FileUpdateEvent : FileEvent
     {
-        /// <summary>
-        /// The relative path to the project dir.
-        /// </summary>
-        [JsonProperty(PropertyName = "fileName")]
-        public string FileName;
-
-        /// <summary>
-        /// The project type.
-        /// This will be one of the first subdirs in the Multiplayer directory.
-        /// </summary>
-        [JsonProperty(PropertyName = "projectType")]
-        public string ProjectType;
-
         /// <summary>
         /// The new content of the file.
         /// </summary>
