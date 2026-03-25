bfdf786e1d876234dd4c89891d0a65d98836b7aa
Rainer Koschke <koschke@uni-bremen.de>
2026-03-25 08:21:29 +0100

    #945 Removed mapping example.

--- Axivion/architecture/rules/create_mapping.py
+++ Axivion/architecture/rules/create_mapping.py
@@ -21,21 +21,4 @@ def create_mapping(node: Node):
 for namespace in INPUT_RFG.nodes(code_facts, only_namespaces):
     create_mapping(namespace)
 
-
-#ARCH.SEE.depends_on(ARCH.)
-
-# Sources for Engine_Ctrl.
-# MAPPING.add_mapping("src/engine_ctrl", ARCH.Engine_Ctrl)
-
-# Sources for Sensors.
-# MAPPING.add_mapping("src/sensors", ARCH.Sensor)
-
-# Sources for IO.
-# MAPPING.add_mapping("src/io/file_system", ARCH.IO.File_System)
-# MAPPING.add_mapping("src/io/display", ARCH.IO.Display)
-
-# Sources for HW.
-# Everything in the component is private.
-# MAPPING.add_mapping("src/hw", ARCH.HW, is_private=True)
-# Except public headers, like hw_public.h
-# MAPPING.add_mapping("src/hw/error_code.h", ARCH.HW, is_private=False)
+MAPPING.add_mapping(arch.SEE.UI.Extensions, arch.SEE.UI, is_private=True)
