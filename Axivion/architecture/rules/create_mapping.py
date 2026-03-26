b3105b29f8a9742e28ab7794e616105468cb9876
Rainer Koschke <koschke@uni-bremen.de>
2026-03-26 12:57:20 +0100

    #945 Implemented map_onto correctly.

--- Axivion/architecture/rules/create_mapping.py
+++ Axivion/architecture/rules/create_mapping.py
@@ -3,25 +3,36 @@
 """
 
 from typing import TYPE_CHECKING
+
 # This is True in PyCharm, but False when you run the script
 if TYPE_CHECKING:
     from setup_initial import *
+    from bauhaus.rfg.hierarchies import *
 
 
-MAPPING.add_mapping("SEE/UI/Extensions", ARCH.SEE)
+def map_onto(full_name: str, target: Component):
+    """
+        Maps the node with the given full_name onto the target component.
+    """
+    names = full_name.split(".")
+    node = get_node_by_path(code_facts, names)
+    if node is None:
+        print(f"No such node name {full_name}")
+        return
+    MAPPING.add_concrete_mapping(node, target)
+
 
 def create_mapping(node: Node):
     """
        Creates a mapping of given implementation node onto its corresponding Component.
     """
     full_name = fullname(code_facts, node)
-    print(f"mapping {full_name}")
-    MAPPING.add_concrete_mapping(full_name, component_map[full_name])
-
+    map_onto(full_name, component_map[full_name])
 
 # Map each namespace in code_facts onto the corresponding architecture component
 # with the same name.
-#for namespace in INPUT_RFG.nodes(code_facts, only_namespaces):
-#    create_mapping(namespace)
+for namespace in INPUT_RFG.nodes(code_facts, only_namespaces):
+    create_mapping(namespace)
 
-# MAPPING.add_mapping("SEE.UI.Extensions", ARCH.SEE.UI.Extensions)
+#map_onto("SEE.UI.Extensions", ARCH.SEE.UI.Extensions)
+
