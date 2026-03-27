125c3d80d6f48c17c25bd538da6ddfb5fa107a8e
Rainer Koschke <koschke@uni-bremen.de>
2026-03-27 17:05:47 +0100

    #945 Improved documentation.

--- Assets/SEE/DataModel/DG/IO/IO.cs
+++ Assets/SEE/DataModel/DG/IO/IO.cs
@@ -1,6 +1,6 @@
 ﻿/// <summary>
-/// SEE.DataModel.DG.IO provides input/output for persistent storage
-/// and retrieval of data structures in SEE.DataModel.DG.
+/// <see cref="SEE.DataModel.DG.IO"/> provides input/output for persistent storage
+/// and retrieval of graph data structures in <see cref="SEE.DataModel.DG"/>.
 /// </summary>
 namespace SEE.DataModel.DG.IO
 {
