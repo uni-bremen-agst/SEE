aa216c45c4b03d2442e9db3543cce199dca5ef1b
Michel Krause <krausem@uni-bremen.de>
2026-04-24 11:33:35 +0200

    Add load on a specific current page.

--- Assets/SEE/UI/Drawable/DrawableFileBrowser.cs
+++ Assets/SEE/UI/Drawable/DrawableFileBrowser.cs
@@ -41,7 +41,9 @@ namespace SEE.UI.Drawable
         /// using a <see cref="FileBrowser"/>.
         /// </summary>
         /// <param name="loadState">The chosen load state (regular/specific).</param>
-        public void LoadDrawableConfiguration(LoadState loadState)
+        /// <param name="pageMode">The mode defining how loaded page indices should be handled.</param>
+        public void LoadDrawableConfiguration(LoadState loadState,
+                                              LoadPageMode pageMode = LoadPageMode.KeepStoredPages)
         {
             string title = "";
             switch (loadState)
@@ -50,7 +52,9 @@ namespace SEE.UI.Drawable
                 case LoadState.Specific:
                     DrawableConfigManager.EnsureDrawableDirectoryExists(DrawableConfigManager.ConfigurationPath);
                     initPath = DrawableConfigManager.ConfigurationPath;
-                    title = "Load on specific Drawable";
+                    title = pageMode == LoadPageMode.CurrentSelectedPage
+                        ? "Load on current page"
+                        : "Load on specific Drawable";
                     break;
                 /// Block for loading on the regular drawable.
                 case LoadState.Regular:
