30e5e1e2a7dffdbdf7dffce2e99683583aba187e
Rainer Koschke <koschke@uni-bremen.de>
2026-04-28 12:28:27 +0200

    #945 Documented namespaces.

---
+++ Assets/SEE/Components/GameEdges/GameEdges.cs
@@ -0,0 +1,6 @@
+﻿/// <summary>
+/// Namespace for components to be attached to game edges.
+/// </summary>
+namespace SEE.Components.GameEdges
+{
+}
---
+++ Assets/SEE/Components/GraphElements/GraphElements.cs
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// Namespace for components to be attached to game nodes or edges.
+/// </summary>
+
+namespace SEE.Components.GraphElements
+{
+}
