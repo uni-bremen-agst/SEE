bb12199ee7bf13f12de80aef5e741adab971f427
Rainer Koschke <koschke@uni-bremen.de>
2026-04-28 11:52:47 +0200

    #945 Removed. No longer needed.

--- Assets/SEE/Controls/Actions/Actions.cs
+++
@@ -1,8 +0,0 @@
-﻿/// <summary>
-/// Contains actions. An action is an operation triggered by the user
-/// to manipulate a game object. An action is added to the <see cref="GlobalActionHistory"/>
-/// and propagated to all clients. It implements <see cref="IReversibleAction"/>.
-/// </summary>
-namespace SEE.Controls.Actions
-{
-}
