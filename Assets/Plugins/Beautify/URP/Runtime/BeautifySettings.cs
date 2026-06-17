6603a24454a3e752f984f1b3b66856d05a6f1972
Rainer Koschke <koschke@uni-bremen.de>
2026-06-17 14:02:47 +0200

    Updated Beautify 3 to version 30.0.2.

--- Assets/Plugins/Beautify/URP/Runtime/BeautifySettings.cs
+++ Assets/Plugins/Beautify/URP/Runtime/BeautifySettings.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e33889ff5597c47e26484b41f8930c0317e94082af34b94abc8a195eacfaeee2
-size 8016
+oid sha256:d19ac6433e88901b9b3d2aebb2fbb6e75d53eb1306d2d22123099c1d1baa051a
+size 7901
