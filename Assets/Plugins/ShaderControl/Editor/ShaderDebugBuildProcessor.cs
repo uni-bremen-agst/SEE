0e6fee857416589ee8423021920b8780d77db97d
Rainer Koschke <koschke@uni-bremen.de>
2026-06-17 14:28:30 +0200

    Updated Shader Control to version 8.2.3.

--- Assets/Plugins/ShaderControl/Editor/ShaderDebugBuildProcessor.cs
+++ Assets/Plugins/ShaderControl/Editor/ShaderDebugBuildProcessor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3420046bf0fd0167c8014ccdf8a1398e8bb565cc186bb189462b21de10fdfaf9
-size 9645
+oid sha256:0de6df85daa4e7ae21ff6ddb10f01ffecc704df4288867cbe93808fa9a1b473a
+size 9775
