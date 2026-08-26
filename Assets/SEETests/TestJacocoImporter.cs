954254570dfb6cf1e5cc7aa21bb613432ac731d0
Rainer Koschke <koschke@uni-bremen.de>
2026-08-26 17:51:02 +0200

    #981 Removed unused import.

--- Assets/SEETests/TestJacocoImporter.cs
+++ Assets/SEETests/TestJacocoImporter.cs
@@ -4,7 +4,6 @@ using SEE.DataModel.DG.IO;
 using SEE.Utils.Paths;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace SEE.DataModel.DG
