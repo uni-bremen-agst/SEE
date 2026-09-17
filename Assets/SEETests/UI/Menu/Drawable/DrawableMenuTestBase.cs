4ccbf3ae67d8dbf7ecaab259e812ff3ba032aab8
Michel Krause <krausem@uni-bremen.de>
2026-09-17 14:05:16 +0200

    Add mind map parent selection tests.

---
+++ Assets/SEETests/UI/Menu/Drawable/DrawableMenuTestBase.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace SEE.UI.Menu.Drawable
+{
+    /// <summary>
+    /// Provides the common UI environment required by EditMode tests of
+    /// Drawable menus.
+    /// </summary>
+    public abstract class DrawableMenuTestBase
+    {
+        /// <summary>
+        /// The UI canvas required by Drawable menus when instantiating their
+        /// prefabs.
+        /// </summary>
+        protected GameObject UICanvasObject { get; private set; }
+
+        /// <summary>
+        /// Creates the UI environment required by Drawable menu tests.
+        /// </summary>
+        [SetUp]
+        public void SetUpDrawableMenuTest()
+        {
+            UICanvasObject = new GameObject(
+                "UI Canvas",
+                typeof(RectTransform),
+                typeof(Canvas));
+        }
+
+        /// <summary>
+        /// Destroys the UI environment created for the current test.
+        /// </summary>
+        [TearDown]
+        public void TearDownDrawableMenuTest()
+        {
+            if (UICanvasObject != null)
+            {
+                Object.DestroyImmediate(UICanvasObject);
+            }
+        }
+    }
+}
---
+++ Assets/SEETests/UI/Menu/Drawable/MindMap/TestMindMapParentSelectionMenu.cs
@@ -0,0 +1,163 @@
+﻿using Michsky.UI.ModernUIPack;
+using NUnit.Framework;
+using SEE.Game;
+using SEE.Game.Drawable;
+using SEE.Game.Drawable.ValueHolders;
+using TMPro;
+using UnityEngine;
+
+namespace SEE.UI.Menu.Drawable
+{
+    /// <summary>
+    /// Tests the parent selection behavior of
+    /// <see cref="MindMapParentSelectionMenu"/>.
+    /// </summary>
+    [TestFixture]
+    public class TestMindMapParentSelectionMenu : DrawableMenuTestBase
+    {
+        /// <summary>
+        /// The object containing the Mind Map nodes used by the tests.
+        /// </summary>
+        private GameObject attachedObjects;
+
+        /// <summary>
+        /// The node for which a parent should be selected.
+        /// </summary>
+        private GameObject addedNode;
+
+        /// <summary>
+        /// The first available parent node.
+        /// </summary>
+        private GameObject firstParent;
+
+        /// <summary>
+        /// The second available parent node.
+        /// </summary>
+        private GameObject secondParent;
+
+        /// <summary>
+        /// Creates the UI canvas and Mind Map hierarchy required by each test.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            attachedObjects =
+                new GameObject("AttachedObjects");
+
+            addedNode = CreateNode(
+                "AddedNode",
+                GameMindMap.NodeKind.Leaf);
+
+            firstParent = CreateNode(
+                "FirstParent",
+                GameMindMap.NodeKind.Theme);
+
+            secondParent = CreateNode(
+                "SecondParent",
+                GameMindMap.NodeKind.Subtheme);
+        }
+
+        /// <summary>
+        /// Destroys the parent selection menu and all objects created for the
+        /// current test.
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            MindMapParentSelectionMenu.Instance.Destroy();
+
+            if (attachedObjects != null)
+            {
+                Object.DestroyImmediate(attachedObjects);
+            }
+        }
+
+        /// <summary>
+        /// Verifies that reopening the parent selection menu initializes the
+        /// chosen parent with the currently displayed default item instead of
+        /// retaining a selection from a previous menu instance.
+        /// </summary>
+        [Test]
+        public void TestReopeningMenuUsesDisplayedDefaultParent()
+        {
+            MindMapParentSelectionMenu.Enable(
+                attachedObjects,
+                addedNode);
+
+            HorizontalSelector selector =
+                FindParentSelector();
+
+            /// Select the second parent in the first menu instance.
+            selector.selectorEvent.Invoke(1);
+
+            Assert.That(
+                MindMapParentSelectionMenu.GetChosenParent(),
+                Is.SameAs(secondParent));
+
+            MindMapParentSelectionMenu.Instance.Destroy();
+
+            /// Reopen the menu without interacting with its selector.
+            MindMapParentSelectionMenu.Enable(
+                attachedObjects,
+                addedNode);
+
+            /// The selected parent must correspond to the displayed default
+            /// item and must not retain the previous selection.
+            Assert.That(
+                MindMapParentSelectionMenu.GetChosenParent(),
+                Is.SameAs(firstParent));
+        }
+
+        /// <summary>
+        /// Finds the parent selector of the currently instantiated parent
+        /// selection menu.
+        /// </summary>
+        /// <returns>The parent selector.</returns>
+        private static HorizontalSelector FindParentSelector()
+        {
+            foreach (HorizontalSelector candidate in
+                     Object.FindObjectsByType<HorizontalSelector>(
+                         FindObjectsInactive.Include,
+                         FindObjectsSortMode.None))
+            {
+                if (candidate.gameObject.name == "ParentSelection")
+                {
+                    return candidate;
+                }
+            }
+
+            Assert.Fail(
+                "Could not find the parent selector of the Mind Map parent selection menu.");
+
+            return null;
+        }
+
+        /// <summary>
+        /// Creates a Mind Map node with the given name and node kind.
+        /// </summary>
+        /// <param name="name">The name of the node.</param>
+        /// <param name="nodeKind">The kind of the node.</param>
+        /// <returns>The created Mind Map node.</returns>
+        private GameObject CreateNode(
+            string name,
+            GameMindMap.NodeKind nodeKind)
+        {
+            GameObject node =
+                new GameObject(name);
+
+            node.tag = Tags.MindMapNode;
+            node.transform.SetParent(
+                attachedObjects.transform);
+
+            MMNodeValueHolder valueHolder =
+                node.AddComponent<MMNodeValueHolder>();
+            valueHolder.NodeKind = nodeKind;
+
+            TextMeshPro text =
+                node.AddComponent<TextMeshPro>();
+            text.text = name;
+
+            return node;
+        }
+    }
+}
