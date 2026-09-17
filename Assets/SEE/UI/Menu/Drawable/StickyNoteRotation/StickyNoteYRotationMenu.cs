f822f798cad5b021a4000861ddf2135194c926fb
Michel Krause <krausem@uni-bremen.de>
2026-09-17 11:04:23 +0200

    Extract sticky note y rotation menu.
    Checks #961: Review and split `StickyNoteRotationMenu` into focused X/Y rotation components if the current duplicated flows remain distinct

---
+++ Assets/SEE/UI/Menu/Drawable/StickyNoteRotation/StickyNoteYRotationMenu.cs
@@ -0,0 +1,320 @@
+﻿using Michsky.UI.ModernUIPack;
+using SEE.Game.Drawable;
+using SEE.Net.Actions.Drawable;
+using SEE.UI.Drawable;
+using SEE.UI.Menu.Drawable;
+using SEE.Utils;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SEE.UI.Menu.Drawable.StickyNoteRotation
+{
+    /// <summary>
+    /// Provides the menu for configuring the y rotation of a sticky note.
+    /// </summary>
+    internal sealed class StickyNoteYRotationMenu
+    {
+        /// <summary>
+        /// The prefab of the sticky note y rotation menu.
+        /// </summary>
+        private const string yRotationMenuPrefab =
+            "Prefabs/UI/Drawable/StickyNoteYRotation";
+
+        /// <summary>
+        /// The instantiated sticky note y rotation menu.
+        /// </summary>
+        private GameObject yRotationMenu;
+
+        /// <summary>
+        /// Enables the y rotation menu for the given sticky note.
+        /// </summary>
+        /// <param name="stickyNoteHolder">
+        /// The sticky note holder that should be rotated.
+        /// </param>
+        /// <param name="spawnMode">
+        /// Whether the menu was opened while spawning a sticky note.
+        /// </param>
+        /// <param name="returnCall">
+        /// An optional action returning to the parent menu.
+        /// </param>
+        /// <param name="backCall">
+        /// The action invoked when returning to the x rotation menu.
+        /// </param>
+        /// <param name="finishCall">
+        /// The action invoked when the rotation configuration is finished.
+        /// </param>
+        internal void Enable(
+            GameObject stickyNoteHolder,
+            bool spawnMode,
+            UnityAction returnCall,
+            UnityAction backCall,
+            UnityAction finishCall)
+        {
+            yRotationMenu =
+                Menu.InstantiatePrefab(yRotationMenuPrefab);
+
+            RotationSliderController slider =
+                yRotationMenu.GetComponentInChildren<RotationSliderController>();
+
+            SetUpSlider(
+                slider,
+                stickyNoteHolder,
+                spawnMode);
+
+            GameObject surface =
+                GameFinder.GetDrawableSurface(stickyNoteHolder);
+
+            string surfaceParentName =
+                GameFinder.GetDrawableSurfaceParentName(surface);
+
+            SetUpReturnButton(returnCall);
+
+            SetUpDegreeButton(
+                "0",
+                0,
+                stickyNoteHolder,
+                slider,
+                spawnMode,
+                surface,
+                surfaceParentName);
+
+            SetUpDegreeButton(
+                "90",
+                90,
+                stickyNoteHolder,
+                slider,
+                spawnMode,
+                surface,
+                surfaceParentName);
+
+            SetUpDegreeButton(
+                "180",
+                180,
+                stickyNoteHolder,
+                slider,
+                spawnMode,
+                surface,
+                surfaceParentName);
+
+            SetUpDegreeButton(
+                "270",
+                270,
+                stickyNoteHolder,
+                slider,
+                spawnMode,
+                surface,
+                surfaceParentName);
+
+            SetUpBackButton(backCall);
+            SetUpFinishButton(finishCall);
+        }
+
+        /// <summary>
+        /// Hides the y rotation menu if it has already been instantiated.
+        /// </summary>
+        internal void Hide()
+        {
+            if (yRotationMenu != null)
+            {
+                yRotationMenu.SetActive(false);
+            }
+        }
+
+        /// <summary>
+        /// Destroys the y rotation menu if it has been instantiated.
+        /// </summary>
+        internal void Destroy()
+        {
+            if (yRotationMenu != null)
+            {
+                Destroyer.Destroy(yRotationMenu);
+            }
+        }
+
+        /// <summary>
+        /// Returns whether the y rotation menu currently exists and is visible.
+        /// </summary>
+        /// <returns>
+        /// True if the y rotation menu is active; otherwise, false.
+        /// </returns>
+        internal bool IsActive()
+        {
+            return yRotationMenu != null
+                   && yRotationMenu.activeInHierarchy;
+        }
+
+        /// <summary>
+        /// Assigns a value to the rotation slider of the y rotation menu.
+        /// </summary>
+        /// <param name="degree">
+        /// The rotation value in degrees.
+        /// </param>
+        internal void AssignValueToSlider(float degree)
+        {
+            if (yRotationMenu != null)
+            {
+                yRotationMenu
+                    .GetComponentInChildren<RotationSliderController>()
+                    .AssignValue(degree);
+            }
+        }
+
+        /// <summary>
+        /// Sets up one of the predefined y rotation buttons.
+        /// </summary>
+        /// <param name="buttonName">
+        /// The name of the button in the menu hierarchy.
+        /// </param>
+        /// <param name="degree">
+        /// The y rotation assigned by the button.
+        /// </param>
+        /// <param name="stickyNoteHolder">
+        /// The sticky note holder that should be rotated.
+        /// </param>
+        /// <param name="slider">
+        /// The y rotation slider.
+        /// </param>
+        /// <param name="spawnMode">
+        /// Whether the sticky note is currently being spawned.
+        /// </param>
+        /// <param name="surface">
+        /// The drawable surface containing the sticky note.
+        /// </param>
+        /// <param name="surfaceParentName">
+        /// The name of the parent of the drawable surface.
+        /// </param>
+        private void SetUpDegreeButton(
+            string buttonName,
+            float degree,
+            GameObject stickyNoteHolder,
+            RotationSliderController slider,
+            bool spawnMode,
+            GameObject surface,
+            string surfaceParentName)
+        {
+            GameFinder.FindAttachedOrLocalDescendant(
+                    yRotationMenu,
+                    buttonName)
+                .GetComponent<ButtonManagerBasic>()
+                .clickEvent.AddListener(() =>
+                {
+                    slider.AssignValue(degree);
+
+                    GameStickyNoteManager.SetRotateY(
+                        stickyNoteHolder,
+                        degree);
+
+                    if (!spawnMode)
+                    {
+                        new StickyNoteRoateYNetAction(
+                            surface.name,
+                            surfaceParentName,
+                            degree,
+                            stickyNoteHolder.transform.position)
+                            .Execute();
+                    }
+                });
+        }
+
+        /// <summary>
+        /// Sets up the y rotation slider.
+        /// </summary>
+        /// <param name="slider">
+        /// The rotation slider that should be configured.
+        /// </param>
+        /// <param name="stickyNote">
+        /// The sticky note that should be rotated.
+        /// </param>
+        /// <param name="spawnMode">
+        /// Whether the sticky note is currently being spawned.
+        /// </param>
+        private void SetUpSlider(
+            RotationSliderController slider,
+            GameObject stickyNote,
+            bool spawnMode)
+        {
+            GameObject surface =
+                GameFinder.GetDrawableSurface(stickyNote);
+
+            string surfaceParentName =
+                GameFinder.GetDrawableSurfaceParentName(surface);
+
+            Transform transform = stickyNote.transform;
+
+            slider.AssignValue(transform.localEulerAngles.y);
+
+            slider.OnValueChanged.AddListener(degree =>
+            {
+                GameStickyNoteManager.SetRotateY(
+                    stickyNote,
+                    degree);
+
+                if (!spawnMode)
+                {
+                    new StickyNoteRoateYNetAction(
+                        surface.name,
+                        surfaceParentName,
+                        degree,
+                        stickyNote.transform.position)
+                        .Execute();
+                }
+            });
+        }
+
+        /// <summary>
+        /// Sets up the button returning to the x rotation menu.
+        /// </summary>
+        /// <param name="backCall">
+        /// The action invoked when the back button is pressed.
+        /// </param>
+        private void SetUpBackButton(UnityAction backCall)
+        {
+            GameFinder.FindAttachedOrLocalDescendant(
+                    yRotationMenu,
+                    "Back")
+                .GetComponent<ButtonManagerBasic>()
+                .clickEvent.AddListener(backCall);
+        }
+
+        /// <summary>
+        /// Sets up the button finishing the rotation configuration.
+        /// </summary>
+        /// <param name="finishCall">
+        /// The action invoked when the finish button is pressed.
+        /// </param>
+        private void SetUpFinishButton(UnityAction finishCall)
+        {
+            GameFinder.FindAttachedOrLocalDescendant(
+                    yRotationMenu,
+                    "Finish")
+                .GetComponent<ButtonManagerBasic>()
+                .clickEvent.AddListener(finishCall);
+        }
+
+        /// <summary>
+        /// Sets up the return button if a return action is available.
+        /// Otherwise, the button is hidden.
+        /// </summary>
+        /// <param name="returnCall">
+        /// The optional action returning to the parent menu.
+        /// </param>
+        private void SetUpReturnButton(UnityAction returnCall)
+        {
+            GameObject returnButton =
+                GameFinder.FindAttachedOrLocalDescendant(
+                    yRotationMenu,
+                    "ReturnBtn");
+
+            if (returnCall != null)
+            {
+                returnButton
+                    .GetComponent<ButtonManagerBasic>()
+                    .clickEvent.AddListener(returnCall);
+            }
+            else
+            {
+                returnButton.SetActive(false);
+            }
+        }
+    }
+}
--- Assets/SEE/UI/Menu/Drawable/StickyNoteRotationMenu.cs
+++ Assets/SEE/UI/Menu/Drawable/StickyNoteRotationMenu.cs
@@ -1,9 +1,4 @@
-﻿using Michsky.UI.ModernUIPack;
-using SEE.Game.Drawable;
-using SEE.Net.Actions.Drawable;
-using SEE.UI.Drawable;
-using SEE.UI.Menu.Drawable.StickyNoteRotation;
-using SEE.Utils;
+﻿using SEE.UI.Menu.Drawable.StickyNoteRotation;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -20,14 +15,9 @@ namespace SEE.UI.Menu.Drawable
         private static readonly StickyNoteXRotationMenu xRotationMenu = new();
 
         /// <summary>
-        /// The prefab of the sticky note for the y rotation menu.
+        /// Manages the sticky note y rotation menu.
         /// </summary>
-        private const string yRotationMenuPrefab = "Prefabs/UI/Drawable/StickyNoteYRotation";
-
-        /// <summary>
-        /// The instance for the sticky note for the y rotation menu.
-        /// </summary>
-        private static GameObject yRotationMenu;
+        private static readonly StickyNoteYRotationMenu yRotationMenu = new();
 
         /// <summary>
         /// Whether this class has a finished rotation that hasn't been fetched yet.
@@ -39,12 +29,8 @@ namespace SEE.UI.Menu.Drawable
         /// </summary>
         public static void Destroy()
         {
-            if (yRotationMenu != null)
-            {
-                Destroyer.Destroy(yRotationMenu);
-            }
-
             xRotationMenu.Destroy();
+            yRotationMenu.Destroy();
         }
 
         /// <summary>
@@ -74,196 +60,22 @@ namespace SEE.UI.Menu.Drawable
                 {
                     xRotationMenu.Hide();
 
-                    EnableYRotation(
+                    yRotationMenu.Enable(
                         stickyNoteHolder,
                         hitObject != null,
-                        returnCall);
+                        returnCall,
+                        () =>
+                        {
+                            yRotationMenu.Hide();
+                            xRotationMenu.Show();
+                        },
+                        () =>
+                        {
+                            xRotationMenu.Destroy();
+                            yRotationMenu.Destroy();
+                            isFinished = true;
+                        });
                 });
-        }
-
-        /// <summary>
-        /// Enables the menu for y-rotation.
-        /// </summary>
-        /// <param name="stickyNoteHolder">The sticky note that should be rotated.</param>
-        private static void EnableYRotation(GameObject stickyNoteHolder, bool spawnMode = true, UnityAction returnCall = null)
-        {
-            yRotationMenu = Menu.InstantiatePrefab(yRotationMenuPrefab);
-            RotationSliderController slider = yRotationMenu.GetComponentInChildren<RotationSliderController>();
-            SliderListener(slider, stickyNoteHolder, spawnMode);
-            GameObject surface = GameFinder.GetDrawableSurface(stickyNoteHolder);
-            string surfaceParentName = GameFinder.GetDrawableSurfaceParentName(surface);
-
-            /// Initialize or disables the return button.
-            YReturnButton(returnCall);
-
-            /// Initialize the different degrees button
-            Zero(stickyNoteHolder, slider, spawnMode, surface, surfaceParentName);
-            Ninety(stickyNoteHolder, slider, spawnMode, surface, surfaceParentName);
-            OneHundredEighty(stickyNoteHolder, slider, spawnMode, surface, surfaceParentName);
-            TwoHundredSeventy(stickyNoteHolder, slider, spawnMode, surface, surfaceParentName);
-
-            /// Initialize the back button, it returns to the xRotation menu.
-            GameFinder.FindAttachedOrLocalDescendant(
-                    yRotationMenu,
-                    "Back")
-                .GetComponent<ButtonManagerBasic>()
-                .clickEvent.AddListener(() =>
-                {
-                    yRotationMenu.SetActive(false);
-                    xRotationMenu.Show();
-                });
-
-            /// Initalize the finish button. It closes the rotation menus.
-            /// And set <see cref="isFinished"/> to true.
-            GameFinder.FindAttachedOrLocalDescendant(yRotationMenu, "Finish").GetComponent<ButtonManagerBasic>()
-                .clickEvent.AddListener(() =>
-            {
-                xRotationMenu.Destroy();
-                Destroyer.Destroy(yRotationMenu);
-                isFinished = true;
-            });
-        }
-
-        /// <summary>
-        /// Initializes the zero button for the y-rotation.
-        /// It sets the y Euler angle to 0°.
-        /// </summary>
-        /// <param name="stickyNoteHolder">The sticky note holder to be rotated.</param>
-        /// <param name="slider">The rotation slider.</param>
-        /// <param name="spawnMode">Whether the menu was called from a spawn action.</param>
-        /// <param name="surface">The drawable surface of the sticky note holder.</param>
-        /// <param name="surfaceParentName">The sticky note id.</param>
-        private static void Zero(GameObject stickyNoteHolder, RotationSliderController slider,
-            bool spawnMode, GameObject surface, string surfaceParentName)
-        {
-            GameFinder.FindAttachedOrLocalDescendant(yRotationMenu, "0").GetComponent<ButtonManagerBasic>()
-                .clickEvent.AddListener(() =>
-            {
-                slider.AssignValue(0);
-                GameStickyNoteManager.SetRotateY(stickyNoteHolder, 0);
-                if (!spawnMode)
-                {
-                    new StickyNoteRoateYNetAction(surface.name, surfaceParentName, 0,
-                               stickyNoteHolder.transform.position).Execute();
-                }
-            });
-        }
-
-        /// <summary>
-        /// Initializes the ninety button for the y rotation.
-        /// It sets the y Euler angle to 90°.
-        /// </summary>
-        /// <param name="stickyNoteHolder">The sticky note holder to be rotated.</param>
-        /// <param name="slider">The rotation slider.</param>
-        /// <param name="spawnMode">Whether the menu was called from a spawn action.</param>
-        /// <param name="surface">The drawable surface of the sticky note holder.</param>
-        /// <param name="surfaceParentName">The sticky note id.</param>
-        private static void Ninety(GameObject stickyNoteHolder, RotationSliderController slider,
-            bool spawnMode, GameObject surface, string surfaceParentName)
-        {
-            GameFinder.FindAttachedOrLocalDescendant(yRotationMenu, "90").GetComponent<ButtonManagerBasic>()
-                .clickEvent.AddListener(() =>
-                {
-                    slider.AssignValue(90);
-                    GameStickyNoteManager.SetRotateY(stickyNoteHolder, 90);
-                    if (!spawnMode)
-                    {
-                        new StickyNoteRoateYNetAction(surface.name, surfaceParentName, 90,
-                                       stickyNoteHolder.transform.position).Execute();
-                    }
-                });
-        }
-
-        /// <summary>
-        /// Initializes the one hundred eighty button for the y-rotation.
-        /// It sets the y Euler angle to 180°.
-        /// </summary>
-        /// <param name="stickyNoteHolder">The sticky note holder to be rotated.</param>
-        /// <param name="slider">The rotation slider.</param>
-        /// <param name="spawnMode">Whether the menu was called from a spawn action.</param>
-        /// <param name="surface">The drawable surface of the sticky note holder.</param>
-        /// <param name="surfaceParentName">The sticky note id.</param>
-        private static void OneHundredEighty(GameObject stickyNoteHolder, RotationSliderController slider,
-            bool spawnMode, GameObject surface, string surfaceParentName)
-        {
-            GameFinder.FindAttachedOrLocalDescendant(yRotationMenu, "180").GetComponent<ButtonManagerBasic>()
-                .clickEvent.AddListener(() =>
-                {
-                    slider.AssignValue(180);
-                    GameStickyNoteManager.SetRotateY(stickyNoteHolder, 180);
-                    if (!spawnMode)
-                    {
-                        new StickyNoteRoateYNetAction(surface.name, surfaceParentName, 180,
-                            stickyNoteHolder.transform.position).Execute();
-                    }
-                });
-        }
-
-        /// <summary>
-        /// Initializes the two hundred seventy button for the y rotation.
-        /// It sets the y euler angle to 270°.
-        /// </summary>
-        /// <param name="stickyNoteHolder">The sticky note holder to be rotated.</param>
-        /// <param name="slider">The rotation slider.</param>
-        /// <param name="spawnMode">Whether the menu was called from a spawn action.</param>
-        /// <param name="surface">The drawable surface of the sticky note holder.</param>
-        /// <param name="surfaceParentName">The sticky note id.</param>
-        private static void TwoHundredSeventy(GameObject stickyNoteHolder, RotationSliderController slider,
-            bool spawnMode, GameObject surface, string surfaceParentName)
-        {
-            GameFinder.FindAttachedOrLocalDescendant(yRotationMenu, "270").GetComponent<ButtonManagerBasic>()
-                .clickEvent.AddListener(() =>
-                {
-                    slider.AssignValue(270);
-                    GameStickyNoteManager.SetRotateY(stickyNoteHolder, 270);
-                    if (!spawnMode)
-                    {
-                        new StickyNoteRoateYNetAction(surface.name, surfaceParentName, 270,
-                            stickyNoteHolder.transform.position).Execute();
-                    }
-                });
-        }
-
-        /// <summary>
-        /// Sets up the return button for the yRotation menu if the <paramref name="returnCall"/> is not null.
-        /// Otherwise the button will disabled.
-        /// </summary>
-        /// <param name="returnCall">The return call action to return to the parent menu.</param>
-        private static void YReturnButton(UnityAction returnCall)
-        {
-            if (returnCall != null)
-            {
-                GameFinder.FindAttachedOrLocalDescendant(yRotationMenu, "ReturnBtn").GetComponent<ButtonManagerBasic>()
-                    .clickEvent.AddListener(returnCall);
-            }
-            else
-            {
-                GameFinder.FindAttachedOrLocalDescendant(yRotationMenu, "ReturnBtn").SetActive(false);
-            }
-        }
-
-        /// <summary>
-        /// Adds the handler for the y-Rotate Slider Controller.
-        /// </summary>
-        /// <param name="slider">The slider controller where the AddListener should be add.</param>
-        /// <param name="stickyNote">The sticky note to rotate.</param>
-        /// <param name="spawnMode">True, if the menu was called from the sticky note spawn action.</param>
-        private static void SliderListener(RotationSliderController slider, GameObject stickyNote, bool spawnMode)
-        {
-            GameObject surface = GameFinder.GetDrawableSurface(stickyNote);
-            string surfaceParentName = GameFinder.GetDrawableSurfaceParentName(surface);
-
-            Transform transform = stickyNote.transform;
-
-            slider.AssignValue(transform.localEulerAngles.y);
-            slider.OnValueChanged.AddListener(degree =>
-            {
-                GameStickyNoteManager.SetRotateY(stickyNote, degree);
-                if (!spawnMode)
-                {
-                    new StickyNoteRoateYNetAction(surface.name, surfaceParentName, degree, stickyNote.transform.position).Execute();
-                }
-            });
         }
 
         /// <summary>
@@ -286,24 +98,25 @@ namespace SEE.UI.Menu.Drawable
         }
 
         /// <summary>
-        /// Returns true if the y rotation menu is enabled.
+        /// Returns whether the y rotation menu is enabled.
         /// </summary>
-        /// <returns>True if the menu is not null and visible.</returns>
+        /// <returns>
+        /// True if the y rotation menu is active; otherwise, false.
+        /// </returns>
         public static bool IsYActive()
         {
-            return yRotationMenu != null && yRotationMenu.activeInHierarchy;
+            return yRotationMenu.IsActive();
         }
 
         /// <summary>
-        /// Assigns a new degree to the <see cref="RotationSliderController"/> of the Y-Rotation Menu.
+        /// Assigns a new degree to the y rotation slider.
         /// </summary>
-        /// <param name="degree">The new degree.</param>
+        /// <param name="degree">
+        /// The new rotation value in degrees.
+        /// </param>
         public static void AssignValueToYSlider(float degree)
         {
-            if (yRotationMenu != null)
-            {
-                yRotationMenu.GetComponentInChildren<RotationSliderController>().AssignValue(degree);
-            }
+            yRotationMenu.AssignValueToSlider(degree);
         }
     }
 }
